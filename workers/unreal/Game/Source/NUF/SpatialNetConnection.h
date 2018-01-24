f8bfb07772ab55431d58c904ed17946edf611a93
giray <giray@improbable.io>
2018-01-24 11:09:53 +0000

    Go back to deriving from UIpConnection.

--- workers/unreal/Game/Source/NUF/SpatialNetConnection.h
+++ workers/unreal/Game/Source/NUF/SpatialNetConnection.h
@@ -19,8 +19,14 @@ public:
 	virtual void InitBase(UNetDriver* InDriver, class FSocket* InSocket, const FURL& InURL, EConnectionState InState, int32 InMaxPacket = 0, int32 InPacketOverhead = 0) override;
 	virtual void InitRemoteConnection(UNetDriver* InDriver, class FSocket* InSocket, const FURL& InURL, const class FInternetAddr& InRemoteAddr, EConnectionState InState, int32 InMaxPacket = 0, int32 InPacketOverhead = 0) override;
 	virtual void InitLocalConnection(UNetDriver* InDriver, class FSocket* InSocket, const FURL& InURL, EConnectionState InState, int32 InMaxPacket = 0, int32 InPacketOverhead = 0) override;
-	virtual bool ClientHasInitializedLevelFor(const UObject* TestObject) const override;
 	virtual void LowLevelSend(void* Data, int32 CountBytes, int32 CountBits) override;
+	virtual bool ClientHasInitializedLevelFor(const UObject* TestObject) const override;
+	
+	// These functions don't make a lot of sense in a SpatialOS implementation.
+	virtual FString LowLevelGetRemoteAddress(bool bAppendPort = false) override { return TEXT(""); }
+	virtual FString LowLevelDescribe() override { return TEXT(""); }
+	virtual FString RemoteAddressToString() override { return TEXT(""); }
+	///////
 
 	UPROPERTY()
 	bool bReliableSpatialConnection;
