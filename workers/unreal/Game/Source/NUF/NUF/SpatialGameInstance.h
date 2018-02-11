d307d6a90978d6ff72dba0d093cc40e8c7c589b2
David Avedissian <david@improbable.io>
2018-02-11 21:37:26 +0000

    Moved files into NUF folder.

---
+++ workers/unreal/Game/Source/NUF/NUF/SpatialGameInstance.h
@@ -0,0 +1,23 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SpatialGameInstance.generated.h"
+
+/**
+*
+*/
+UCLASS()
+class NUF_API USpatialGameInstance : public UGameInstance
+{
+GENERATED_BODY()
+
+	bool StartGameInstance_NUFClient(FString& Error);
+
+#if WITH_EDITOR
+	virtual FGameInstancePIEResult StartPlayInEditorGameInstance(ULocalPlayer* LocalPlayer, const FGameInstancePIEParameters& Params) override;
+#endif
+	virtual void StartGameInstance() override;
+};
