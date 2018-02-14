3fba05690ddd5fef0a03d903089b0af60890c9ba
giray <giray@improbable.io>
2018-02-13 19:18:42 -0800

    "Fix" standalone server

--- launch_server.sh
+++ launch_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ${UNREAL_HOME}/Engine/Binaries/Win64/UE4Editor.exe \
-	${PWD}/"workers\unreal\Game\NUF.uproject" -server -log -workerType UnrealWorker -stdout -nowrite -unattended -nologtimes -nopause -noin -messaging -resX=400 -resY=300 -windowed
+	${PWD}/"workers\unreal\Game\NUF.uproject" ThirdPersonExampleMap -server -log -workerType UnrealWorker -stdout -nowrite -unattended -nologtimes -nopause -noin -messaging -resX=400 -resY=300 -windowed
