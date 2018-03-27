d4e2a83c284d4610582b94c8c896e446cc574e01
giray <giray@improbable.io>
2018-03-27 15:04:31 +0100

    Fix build

--- workers/unreal/Game/Source/NUF/NUFCharacter.h
+++ workers/unreal/Game/Source/NUF/NUFCharacter.h
@@ -26,8 +26,6 @@ public:
 
 	virtual void BeginPlay() override;
 
-	virtual void Tick(float DeltaTime) override;
-
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseTurnRate;
