9cc92541c5ba1eadeb17a4c8c32454d6405b920d
INTERNAL\danielhall <danielhall@improbable.io>
2018-04-16 16:33:47 +0100

    Fixed the codegen update dll export macros

---
+++ SpatialGDK/Scripts/patch_sdk_code_gen.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e -u -x -o pipefail
+
+cd "$(dirname "$0")"
+
+pushd "../Generated/UClasses"
+
+find -name '*.h' | xargs sed -i 's/SPATIALOS_API/SPATIALGDK_API/'
+
+popd 
