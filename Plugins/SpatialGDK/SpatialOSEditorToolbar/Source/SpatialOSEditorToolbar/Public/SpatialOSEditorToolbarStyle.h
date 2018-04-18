41cd404c9a98f9651f663e1db6ae263ddceb39b1
INTERNAL\danielhall <danielhall@improbable.io>
2018-04-18 17:12:32 +0100

    Added the legacy SpatialOSEditorToolbar

---
+++ Plugins/SpatialGDK/SpatialOSEditorToolbar/Source/SpatialOSEditorToolbar/Public/SpatialOSEditorToolbarStyle.h
@@ -0,0 +1,28 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Styling/SlateStyle.h"
+
+class FSlateStyleSet;
+
+class FSpatialOSEditorToolbarStyle
+{
+public:
+  static void Initialize();
+
+  static void Shutdown();
+
+  static void ReloadTextures();
+
+  static const ISlateStyle& Get();
+
+  static FName GetStyleSetName();
+
+private:
+  static TSharedRef<FSlateStyleSet> Create();
+
+private:
+  static TSharedPtr<FSlateStyleSet> StyleInstance;
+};
