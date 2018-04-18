b40aad514b24d2389a2a3d3841e4f0dec6afd515
INTERNAL\danielhall <danielhall@improbable.io>
2018-04-18 16:52:24 +0100

    Removed stale script and updated the build scripts

--- Scripts/patch_sdk_code_gen.sh
+++ Scripts/patch_sdk_code_gen.sh
@@ -4,8 +4,10 @@ set -e -u -x -o pipefail
 
 cd "$(dirname "$0")"
 
-pushd "../Generated/UClasses"
+pushd "../Source/SpatialGDK/Generated/UClasses"
 
 find -name '*.h' | xargs sed -i 's/SPATIALOS_API/SPATIALGDK_API/'
+find -name '*.h' -o -name '*.cpp' | xargs sed -i 's/SpatialOSViewTypes.h/SpatialGDKViewTypes.h/'
+find -name '*.h' -o -name '*.cpp' | xargs sed -i 's/SpatialOSWorkerTypes.h/SpatialGDKWorkerTypes.h/'
 
 popd
