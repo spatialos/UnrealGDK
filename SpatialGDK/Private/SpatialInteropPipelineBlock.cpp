7c6724ca8ccbf0402e8c52528ad95f9b4a263c82
joshuahuburn <joshuahuburn@improbable.io>
2018-04-26 12:19:01 +0100

    Removed comments

--- SpatialGDK/Private/SpatialInteropPipelineBlock.cpp
+++ SpatialGDK/Private/SpatialInteropPipelineBlock.cpp
@@ -300,8 +300,6 @@ AActor* USpatialInteropPipelineBlock::GetOrCreateActor(TSharedPtr<worker::Connec
 	// 2) A "pure" Spatial create entity request, which means we need to spawn an actor that was manually registered to correspond to it.
 	// 3) A SpawnActor() call that was initiated from a different worker, which means we need to find and spawn the corresponding "native" actor that corresponds to it.
 	//	  This can happen on either the client (for all actors) or server (for actors which were spawned by a different server worker, or are migrated).
-
-	// This is the worker which made the actor.=, so it just associates the entityID with the actor.
 	if (EntityActor)
 	{
 		// Option 1
@@ -320,7 +318,6 @@ AActor* USpatialInteropPipelineBlock::GetOrCreateActor(TSharedPtr<worker::Connec
 	else
 	{
 		UClass* ActorClass = nullptr;
-		// GetRegisteredEntityClass is the part looking for a blueprint.
 		if ((ActorClass = GetRegisteredEntityClass(MetadataComponent)) != nullptr)
 		{
 			// Option 2
@@ -328,8 +325,7 @@ AActor* USpatialInteropPipelineBlock::GetOrCreateActor(TSharedPtr<worker::Connec
 			EntityActor = SpawnNewEntity(PositionComponent, ActorClass);
 			EntityRegistry->AddToRegistry(EntityId, EntityActor);
 		}
-		// This finds an UnrealClass, doesn't have to be a blueprint.
-		else if ((ActorClass = GetNativeEntityClass(MetadataComponent)) != nullptr) // Conditional hack to detect that we are creating a player spawner.
+		else if ((ActorClass = GetNativeEntityClass(MetadataComponent)) != nullptr)
 		{
 			// Option 3
 			UNetConnection* Connection = nullptr;
