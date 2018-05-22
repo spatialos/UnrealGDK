0da26a8509db76da4b03cbab2e2675b016a9abc0
Michael Samiec <michaelsamiec@improbable.io>
2018-05-22 11:27:19 +0100

    UNR-237: Gracefully exit when encountered unknown RPC (#108)
    * Replace unknown RPC check with error message
    
    * Added fallback check back in

--- Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/TypeBindingGenerator.cpp
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/TypeBindingGenerator.cpp
@@ -1115,7 +1115,12 @@ void GenerateFunction_SendRPCCommand(FCodeWriter& SourceWriter, UClass* Class)
 	SourceWriter.Print(R"""(
 		TSharedPtr<worker::Connection> Connection = Interop->GetSpatialOS()->GetConnection().Pin();
 		auto SenderFuncIterator = RPCToSenderMap.Find(Function->GetFName());
-		checkf(*SenderFuncIterator, TEXT("Sender for %s has not been registered with RPCToSenderMap."), *Function->GetFName().ToString());
+		if (SenderFuncIterator == nullptr)
+		{
+			UE_LOG(LogSpatialOSInterop, Error, TEXT("Sender for %s has not been registered with RPCToSenderMap."), *Function->GetFName().ToString());
+			return;
+		}
+		checkf(*SenderFuncIterator, TEXT("Sender for %s has been registered as null."), *Function->GetFName().ToString());
 		(this->*(*SenderFuncIterator))(Connection.Get(), Parameters, TargetObject);)""");
 	SourceWriter.End();
 }
