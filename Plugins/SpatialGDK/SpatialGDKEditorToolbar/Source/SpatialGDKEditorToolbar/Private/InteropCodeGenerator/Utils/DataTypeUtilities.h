bbe63980c82a459d64238a27778bbae596c2af60
INTERNAL\michaelsamiec <michaelsamiec@improbable.io>
2018-05-29 18:45:15 +0100

    Added #include

--- Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/Utils/DataTypeUtilities.h
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/Utils/DataTypeUtilities.h
@@ -2,5 +2,7 @@
 
 #pragma once
 
+#include "CoreMinimal.h"
+
 // Return the string representation of the underlying data type of an enum property
 FString GetEnumDataType(const UEnumProperty* EnumProperty);
