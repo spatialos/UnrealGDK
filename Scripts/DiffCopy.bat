a7a182d34820130e2d46e2b50379196545f90690
Valentyn Nykoliuk <valentyn@improbable.io>
2018-06-07 11:27:01 +0100

    Fix incorrect paths in scripts

--- Scripts/DiffCopy.bat
+++ Scripts/DiffCopy.bat
@@ -1,11 +1,13 @@
 @echo off
 
-cd "%~dp0..\"
+pushd "%~dp0..\"
 
 if not exist "Intermediate\Improbable" mkdir "Intermediate\Improbable"
 
 csc "Scripts/DiffCopy.cs" /nologo /out:"Intermediate\Improbable\DiffCopy.exe" || exit /b 1
 
 Intermediate\Improbable\DiffCopy.exe %*
+
+popd
 
 exit /b %ERRORLEVEL%
