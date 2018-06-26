a8ccbbae42d182ff8e993ac0644eabf6ef468888
INTERNAL\danielhall <danielhall@improbable.io>
2018-06-26 11:48:29 +0100

    Updated comments

--- Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/Utils/DataTypeUtilities.h
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/Utils/DataTypeUtilities.h
@@ -12,12 +12,12 @@ FString GetEnumDataType(const UEnumProperty* EnumProperty);
 // Given a class or function name, generates the name used for naming schema components and types. Currently, this just removes underscores.
 FString UnrealNameToSchemaTypeName(const FString& UnrealName);
 
-// Given a replicated property group and Unreal type, generates the name of the corresponding component.
+// Given a replicated property group and Unreal type, generates the name of the corresponding schema component.
 // For example: UnrealCharacterMultiClientRepData
 FString SchemaReplicatedDataName(EReplicatedPropertyGroup Group, UStruct* Type, bool bPrependNamespace = false);
 
 // Given an unreal type, generates the name of the component which stores worker to worker replication data.
-// For example: UnrealCharacterWorkerRepData
+// For example: UnrealCharacterMigratableData
 FString SchemaMigratableDataName(UStruct* Type, bool bPrependNamespace = false);
 
 // Given an RPC type and Unreal type, generates the name of the corresponding RPC container component.
@@ -25,11 +25,11 @@ FString SchemaMigratableDataName(UStruct* Type, bool bPrependNamespace = false);
 FString SchemaRPCComponentName(ERPCType RpcType, UStruct* Type, bool bPrependNamespace = false);
 
 // Given a UFunction, generates the command request data type.
-// For example: UnrealServerMoveRequest.
+// For example: ServerMove() -> UnrealServerMoveRequest.
 FString SchemaRPCRequestType(UFunction* Function, bool bPrependNamespace = false);
 
 // Given a UFunction, generates the command request data type.
-// For example: UnrealServerMoveResponse.
+// For example: ServerMove() -> UnrealServerMoveResponse.
 FString SchemaRPCResponseType(UFunction* Function);
 
 // Given a UFunction, generates the schema command name. Currently just returns the function name in lowercase.
