785c873008a8e36cf17359eef097016553b44831
INTERNAL\michaelsamiec <michaelsamiec@improbable.io>
2018-07-04 16:06:38 +0100

    Updated interop codegen to work with new handover tag

--- Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/TypeStructure.cpp
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/TypeStructure.cpp
@@ -453,12 +453,12 @@ TSharedPtr<FUnrealType> CreateUnrealTypeInfo(UStruct* Type, uint32 ParentChecksu
 
 	// Find the handover properties.
 	uint16 MigratableDataHandle = 1;
-	VisitAllProperties(TypeNode, [&MigratableDataHandle](TSharedPtr<FUnrealProperty> PropertyInfo)
+	VisitAllProperties(TypeNode, [&MigratableDataHandle](TSharedPtr<FUnrealProperty> Property)
 	{
-		if (PropertyInfo->Property->PropertyFlags & CPF_Handover)
+		if (Property->Property->PropertyFlags & CPF_Handover)
 		{
-			PropertyInfo->MigratableData = MakeShared<FUnrealMigratableData>();
-			PropertyInfo->MigratableData->Handle = MigratableDataHandle++;
+			Property->MigratableData = MakeShared<FUnrealMigratableData>();
+			Property->MigratableData->Handle = MigratableDataHandle++;
 		}
 		return true;
 	}, true);
