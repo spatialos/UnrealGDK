7439233f1fb8043f518744582ee990eb2fd8857d
Michal Pandy <michalpandy@improbable.io>
2018-07-20 14:38:58 +0100

    rename hover over messagess for codegen and snapshot generation

--- Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/SpatialGDKEditorToolbarCommands.cpp
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/SpatialGDKEditorToolbarCommands.cpp
@@ -6,8 +6,8 @@
 
 void FSpatialGDKEditorToolbarCommands::RegisterCommands()
 {
-	UI_COMMAND(CreateSpatialGDKSnapshot, "SpatialGDK Snapshot", "Creates SpatialGDK snapshot.", EUserInterfaceActionType::Button, FInputGesture());
-	UI_COMMAND(GenerateInteropCode, "Interop Codegen", "Generates SpatialGDK interop code.", EUserInterfaceActionType::Button, FInputGesture());
+	UI_COMMAND(CreateSpatialGDKSnapshot, "SpatialGDK Snapshot", "Creates snapshot.", EUserInterfaceActionType::Button, FInputGesture());
+	UI_COMMAND(GenerateInteropCode, "Interop Codegen", "Generates interop code.", EUserInterfaceActionType::Button, FInputGesture());
 	UI_COMMAND(StartSpatialOSStackAction, "Launch", "Starts SpatialOS locally.", EUserInterfaceActionType::Button, FInputGesture());
 	UI_COMMAND(StopSpatialOSStackAction, "Stop", "Stops SpatialOS.", EUserInterfaceActionType::Button, FInputGesture());
 	UI_COMMAND(LaunchInspectorWebPageAction, "Inspector", "Launches default web browser to SpatialOS Inspector.", EUserInterfaceActionType::Button, FInputGesture());
