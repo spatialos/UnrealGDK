96f9fab6a98803b7ccd5641630696a9799ed50e8
Daniel Hall <danielhall@improbable.io>
2018-07-24 18:13:44 +0100

    UNR-437: Rewrite the linter in C# and remove dependency on go. (#204)
    * Current state of the linter
    
    * Updated solution
    
    * Replaced the csc calls with calls to the solution
    
    * Updated the tracking of the build project
    
    * Updated project dependencies
    
    * Added solution and fixed build output
    
    * Removed stale scripts
    
    * Removed app.config files
    
    * Removed unused files
    
    * Updated the linter source
    
    * Removed assembly info
    
    * fixed compilation
    
    * Got both linters tests to work
    
    * review feedback
    
    * Updated files with lint
    
    * Removed the go dependencies
    
    * Revert "Updated files with lint"
    
    This reverts commit 5d5d3973677a19f3a640158e222f0214ac8d511e.
    
    * Updated linter to have a less aggressive check
    
    * Batch scripts for linting
    
    * Moved the copying into the build step
    
    * Removed references to csc
    
    * Review feedback
    
    * Fixed broken copy step
    
    * Moved codegen to use post build copy step
    
    * Updated formatting
    
    * Review feedback
    
    * Review feedback

---
+++ Source/Programs/Improbable.Unreal.Scripts/Linter/Linter.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Improbable
+{
+    public static class Linter
+    {
+        /// <summary>
+        /// The linter currently only checks for missing copyright headers
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            var help = args.Count(arg => arg == "/?" || arg.ToLowerInvariant() == "--help") > 0;
+            var validCommand = args.Count(arg => arg.ToLowerInvariant() == checkCommand || arg.ToLowerInvariant() == fixCommand) == 1;
+            int exitCode = 0;
+
+            if (help || args.Length < 2 || !validCommand)
+            {
+                PrintHelp();
+                Environment.Exit(exitCode);
+            }
+
+            try
+            {
+                var command = args.First().ToLower();
+                var paths = args.Skip(1).ToList();
+                switch (command)
+                {
+                    case checkCommand:
+                        {
+                            CheckLint(paths);
+                        }
+                        break;
+                    case fixCommand:
+                        {
+                            FixLint(paths);
+                        }
+                        break;
+                    default:
+                        {
+                            PrintHelp();
+                            exitCode = 1;
+                        }
+                        break;
+                }
+            }
+            catch (System.Exception e)
+            {
+                Console.Error.WriteLine(@"{0} Exception caught.", e);
+                exitCode = 1;
+            }
+
+            Environment.Exit(exitCode);
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Usage: [Command] <paths>");
+            Console.WriteLine("Available Commands:");
+            Console.WriteLine("\tcheck : Checks the paths for lint.");
+            Console.WriteLine("\tix: Fix lint in the specified paths.");
+        }
+
+        private static void CheckLint(List<string> paths)
+        {
+            var pathsWithLint = GetFailedValidationPaths(paths);
+
+            if (pathsWithLint.Count > 0)
+            {
+                Console.Error.WriteLine(@"{0} files failed linting", pathsWithLint.Count);
+            }
+        }
+
+        private static void FixLint(List<string> paths)
+        {
+            var pathsWithLint = GetFailedValidationPaths(paths);
+
+            foreach (var path in pathsWithLint)
+            {
+                var content = File.ReadAllText(path);
+                content = copyrightHeader + content;
+                File.WriteAllText(path, content, new UTF8Encoding(false));
+
+                Console.WriteLine(@"Fixed {0}", path);
+            }
+        }
+
+        private static List<string> GetFailedValidationPaths(List<string> paths)
+        {
+            var directories = paths.Select(path => new DirectoryInfo(path));
+
+            List<string> filesWithLint = new List<string>();
+
+            foreach (var directory in directories.Where(IsIncluded))
+            {
+                var files = directory.EnumerateFiles("*.*", SearchOption.AllDirectories)
+                                     .Where(s => s.FullName.EndsWith(".h") || s.FullName.EndsWith(".cpp") || s.FullName.EndsWith(".Build.cs"));
+
+                foreach (var file in files.Where(NeedsChanges))
+                {
+                    filesWithLint.Add(file.FullName);
+                }
+            }
+
+            return filesWithLint;
+        }
+
+        private static bool IsIncluded(DirectoryInfo Directory)
+        {
+            var result = Common.RunRedirectedWithExitCode("git", new[]
+            {
+                "check-ignore",
+                Directory.FullName
+            });
+
+            return result != 0;
+        }
+
+        private static bool NeedsChanges(FileInfo fileInfo)
+        {
+            var content = File.ReadAllText(fileInfo.FullName);
+            if (!content.StartsWith("// Copyright"))
+            {
+                Console.Error.WriteLine(@"{0} is missing the copyright header", fileInfo.FullName);
+                return true;
+            }
+
+            return false;
+        }
+
+        private const string copyrightHeader = "// Copyright (c) Improbable Worlds Ltd, All Rights Reserved\r\n\r\n";
+        private const string checkCommand = "check";
+        private const string fixCommand = "fix";
+    }
+}
