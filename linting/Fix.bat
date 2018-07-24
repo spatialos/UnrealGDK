8576c88548081ef7f3eb23e7835e938832fe6e9a
Valentyn Nykoliuk <valentyn@improbable.io>
2018-07-24 16:15:47 +0100

    Batch scripts for linting

---
+++ linting/Fix.bat
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal
+
+pushd "%~dp0..\"
+
+set LINTER_EXE="Binaries\ThirdParty\Improbable\Programs\Linter.exe"
+if not exist %LINTER_EXE% (
+    echo Error: Could not find %LINTER_EXE%. Please run Build.bat to generate it.
+    pause
+    exit /b 1
+)
+
+echo This will analyze the SpatialGDK module and plugins for any formatting issues and fix them.
+set /p CONTINUE=Would you like to continue?[Y/N]:
+if /I "%CONTINUE%" == "Y" (
+    %LINTER_EXE% fix "Source\SpatialGDK\Public" ^
+                     "Source\SpatialGDK\Private" ^
+                     "Source\SpatialGDK\Legacy" ^
+                     "Plugins"
+)
+
+popd
+
+pause
+exit /b %ERRORLEVEL%
