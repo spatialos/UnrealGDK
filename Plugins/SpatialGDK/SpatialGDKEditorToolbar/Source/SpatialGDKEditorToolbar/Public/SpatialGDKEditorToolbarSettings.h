d62e699a226826f9d19601cabd9182c5956b1783
Michal Pandy <michalpandy@improbable.io>
2018-07-25 13:36:21 +0100

    Rename spatial CLI

--- Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbarSettings.h
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbarSettings.h
@@ -14,15 +14,15 @@ public:
 	USpatialGDKEditorToolbarSettings(const FObjectInitializer& ObjectInitializer);
 
 	/** Root folder of your SpatialOS Unreal GDK project. */
-	UPROPERTY(EditAnywhere, config, Category = "'spatial CLI' Unreal GDK Configuration", meta = (ConfigRestartRequired = false))
+	UPROPERTY(EditAnywhere, config, Category = "'spatial' CLI Unreal GDK Configuration", meta = (ConfigRestartRequired = false))
 	FDirectoryPath ProjectRootFolder;
 
 	/** Launch configuration file used for `spatial local launch`. */
-	UPROPERTY(EditAnywhere, config, Category = "'spatial CLI' Unreal GDK Configuration", meta = (ConfigRestartRequired = false, DisplayName = "'spatial CLI' Launch Configuration"))
+	UPROPERTY(EditAnywhere, config, Category = "'spatial' CLI Unreal GDK Configuration", meta = (ConfigRestartRequired = false, DisplayName = "'spatial' CLI Launch Configuration"))
 	FString SpatialOSLaunchConfig;
 
-	/** Stop 'spatial CLI' when shutting down editor. */
-	UPROPERTY(EditAnywhere, config, Category = "'spatial CLI' Unreal GDK Configuration", meta = (ConfigRestartRequired = false, DisplayName = "Stop 'spatial CLI' on Exit"))
+	/** Stop 'spatial' CLI when shutting down editor. */
+	UPROPERTY(EditAnywhere, config, Category = "'spatial' CLI Unreal GDK Configuration", meta = (ConfigRestartRequired = false, DisplayName = "Stop 'spatial' CLI on Exit"))
 	bool bStopSpatialOnExit;
 
 	UFUNCTION()
