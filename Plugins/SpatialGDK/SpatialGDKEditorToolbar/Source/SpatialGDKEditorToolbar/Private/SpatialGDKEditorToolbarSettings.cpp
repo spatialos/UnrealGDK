6e28ffcf93f1a3fcfc05649756c9a4778d6a9e91
Michal Pandy <michalpandy@improbable.io>
2018-07-30 11:05:07 +0100

    Extend settings by snapshot configs

--- Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/SpatialGDKEditorToolbarSettings.cpp
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/SpatialGDKEditorToolbarSettings.cpp
@@ -4,6 +4,8 @@
 USpatialGDKEditorToolbarSettings::USpatialGDKEditorToolbarSettings(const FObjectInitializer& ObjectInitializer) : SpatialOSLaunchConfig(TEXT("default_launch.json")), bStopSpatialOnExit(false), Super(ObjectInitializer)
 {
 	ProjectRootFolder.Path = FPaths::ConvertRelativePathToFull(FPaths::GetPath(FPaths::GetProjectFilePath()) + FString(TEXT("/../spatial/")));
+	SpatialOSSnapshotPath.Path = FPaths::Combine(*ProjectRootFolder.Path, TEXT("snapshots/"));
+	SpatialOSSnapshotFile = FString(TEXT("default.snapshot"));
 }
 
 FString USpatialGDKEditorToolbarSettings::ToString()
@@ -12,8 +14,11 @@ FString USpatialGDKEditorToolbarSettings::ToString()
 	Args.Add(ProjectRootFolder.Path);
 	Args.Add(SpatialOSLaunchConfig);
 	Args.Add(bStopSpatialOnExit);
+	Args.Add(SpatialOSSnapshotPath.Path);
+	Args.Add(SpatialOSSnapshotFile);
 
 	return FString::Format(TEXT("ProjectRootFolder={0}, SpatialOSLaunchArgument={1}, "
-								"bStopSpatialOnExit={2}"),
+		"bStopSpatialOnExit={2}, SpatialOSSnapshotPath={3}, "
+		"SpatialOSSnapshotFile={4}"),
 						   Args);
 }
