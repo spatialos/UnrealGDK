43b5b08546074787ff567a32d276c47e3856ae32
mpmisko <mpmisko@gmail.com>
2018-08-02 14:01:11 +0100

    UNR-464 - common plugin utils (#260)
    * Create common FConfigFile utils for the plugin

---
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/SpatialGDKEditorUtils.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "SpatialGDKEditorUtils.h"
+
+DEFINE_LOG_CATEGORY(LogSpatialGDKEditorToolbarUtils);
+
+const FConfigFile* GetConfigFile(const FString& ConfigFilePath)
+{
+	const FConfigFile* ConfigFile = GConfig->Find(ConfigFilePath, false);
+	if (!ConfigFile)
+	{
+		UE_LOG(LogSpatialGDKEditorToolbarUtils, Error, TEXT("Could not open .ini file: \"%s\""), *ConfigFilePath);
+		return nullptr;
+	}
+	return ConfigFile;
+}
+
+const FConfigFile* LoadConfigFile(const FString& ConfigFilePath)
+{
+	GConfig->LoadFile(ConfigFilePath);
+	return GetConfigFile(ConfigFilePath);
+}
+
+const FConfigSection* GetConfigSection(const FString& ConfigFilePath, const FString& SectionName)
+{
+	if (const FConfigFile* ConfigFile = GetConfigFile(ConfigFilePath))
+	{
+		if (const FConfigSection* Section = ConfigFile->Find(SectionName))
+		{
+			return Section;
+		}
+
+		UE_LOG(LogSpatialGDKEditorToolbarUtils, Error, TEXT("Could not find section '%s' in '%s'."), *SectionName, *ConfigFilePath);
+	}
+
+	return nullptr;
+}
