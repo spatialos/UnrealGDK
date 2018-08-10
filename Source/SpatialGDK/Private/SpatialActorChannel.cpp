d72e13e76bc6f4638f236942af9806fd50d74dbe
Sahil Dhanju <sahildhanju@improbable.io>
2018-08-10 10:46:46 +0100

    Clarified stuff

--- Source/SpatialGDK/Private/SpatialActorChannel.cpp
+++ Source/SpatialGDK/Private/SpatialActorChannel.cpp
@@ -544,7 +544,7 @@ void USpatialActorChannel::OnReserveEntityIdResponse(const worker::ReserveEntity
 		//todo: From now on, this actor channel will be useless. We need better error handling, or a retry mechanism here.
 		return;
 	}
-	UE_LOG(LogSpatialGDKActorChannel, Warning, TEXT("Received entity id (%d) for: %s."), Op.EntityId.value_or(0), *Actor->GetName());
+	UE_LOG(LogSpatialGDKActorChannel, Verbose, TEXT("Received entity id (%d) for: %s."), Op.EntityId.value_or(0), *Actor->GetName());
 	ActorEntityId = *Op.EntityId;
 
 	SpatialNetDriver->GetEntityRegistry()->AddToRegistry(ActorEntityId, GetActor());
