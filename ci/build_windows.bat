c69b49d00ccdeb68c4c63edf3f1ca3edd5e8a983
Daniel Hall <danielhall@improbable.io>
2018-09-17 11:59:06 +0100

    Fixed ci build scripts

--- ci/build_windows.bat
+++ ci/build_windows.bat
@@ -2,7 +2,7 @@
 
 pushd "%~dp0../SpatialGDK"
 
-call Setup.bat
+call ../Setup.bat
 
 call "%UNREAL_HOME%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="%~dp0../SpatialGDK/SpatialGDK.uplugin" -TargetPlatforms=Win64 -Package="%~dp0../SpatialGDK/Intermediate/BuildPackage/Win64"
 
