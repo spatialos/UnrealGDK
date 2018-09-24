e75ed2a8c35a875674d60ef2a61ceb31518fc2d8
INTERNAL\danielhall <danielhall@improbable.io>
2018-09-24 11:50:03 +0100

    ci shell scripts

---
+++ ci/build_linux.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e -u -x -o pipefail
+
+cd "$(dirname "$0")/../"
+
+source ci/profiling.sh
+
+markStartOfBlock "$0"
+
+markStartOfBlock "Setup dependencies"
+
+    ./Setup.bat
+
+markEndOfBlock "Setup dependencies"
+
+markStartOfBlock "Build the TestSuite (Linux)"
+
+    pushd "SpatialGDK"
+
+    "${UNREAL_HOME}/Engine/Build/BatchFiles/RunUAT.bat" BuildPlugin -Plugin="$PWD/SpatialGDK.uplugin" -TargetPlatforms=Linux -Package="$PWD/Intermediate/BuildPackage/Linux"
+
+    popd
+
+markEndOfBlock "Build the TestSuite (Linux)"
+
+markEndOfBlock "$0"
