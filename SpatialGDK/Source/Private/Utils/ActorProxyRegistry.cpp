f9ada516b39099fc5002bbe620a534361e33571d
Daniel Hall <danielhall@improbable.io>
2018-10-15 14:54:55 +0100

    Added the first implementation of the actor proxy registry

---
+++ SpatialGDK/Source/Private/Utils/ActorProxyRegistry.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "ActorProxyRegistry.h"
+
+#include "EngineUtils.h"
+
+DEFINE_LOG_CATEGORY(LogActorProxyRegistry);
+
+void UActorProxyRegistry::AddToRegistry(const FUnrealObjectRef& ObjectRef, AActor* Actor)
+{
+	ObjectRefToActor.Add(ObjectRef, Actor);
+	ActorToObjectRef.Add(Actor, ObjectRef);
+}
+
+void UActorProxyRegistry::RemoveFromRegistry(const AActor* Actor)
+{
+	FUnrealObjectRef ObjectRef = GetObjectRefFromActor(Actor);
+	RemoveFromRegistryImpl(Actor, ObjectRef);
+}
+
+void UActorProxyRegistry::RemoveFromRegistry(const FUnrealObjectRef& ObjectRef)
+{
+	AActor* Actor = GetActorFromObjectRef(ObjectRef);
+	RemoveFromRegistryImpl(Actor, ObjectRef);
+}
+
+FUnrealObjectRef UActorProxyRegistry::GetObjectRefFromActor(const AActor* Actor) const
+{
+	if (ActorToObjectRef.Contains(Actor))
+	{
+		return *ActorToObjectRef.Find(Actor);
+	}
+
+	return SpatialConstants::NULL_OBJECT_REF;
+}
+
+AActor* UActorProxyRegistry::GetActorFromObjectRef(const FUnrealObjectRef& ObjectRef) const
+{
+	if (ObjectRefToActor.Contains(ObjectRef))
+	{
+		return *ObjectRefToActor.Find(ObjectRef);
+	}
+
+	return nullptr;
+}
+
+void UActorProxyRegistry::RemoveFromRegistryImpl(const AActor* Actor, const FUnrealObjectRef& ObjectRef)
+{
+	ActorToObjectRef.Remove(Actor);
+	ObjectRefToActor.Remove(ObjectRef);
+}
---
+++ SpatialGDK/Source/Public/Utils/ActorProxyRegistry.h
@@ -0,0 +1,65 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpatialConstants.h"
+
+#include "improbable/UnrealObjectRef.h"
+
+#include "ActorProxyRegistry.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogActorProxyRegistry, Log, All);
+
+UCLASS()
+class SPATIALGDK_API UActorProxyRegistry : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	/**
+	* Adds a mapping from an FUnrealObjectRef to a AActor to the registry
+	*
+	* @param ObjectRef the FUnrealObjectRef for the AActor that is added to the registry.
+	* @param Actor the AActor instance to be added to the registry.
+	**/
+	void AddToRegistry(const FUnrealObjectRef& ObjectRef, AActor* Actor);
+
+	/**
+	* Removes the entry for an AActor in the registry if present in the registry.
+	*
+	* @param Actor the AActor instance to be removed from the registry.
+	**/
+	void RemoveFromRegistry(const AActor* Actor);
+
+	/**
+	* Removes the entry for an FUnrealObjectRef in the registry if present in the registry.
+	*
+	* @param ObjectRef the FUnrealObjectRef instance to be removed from the registry.
+	**/
+	void RemoveFromRegistry(const FUnrealObjectRef& ObjectRef);
+
+	/**
+	* Get the FUnrealObjectRef associated with an Unreal AActor.
+	* Returns nullptr if no associated FUnrealObjectRef found.
+	*
+	* @param Actor the AActor for which the FUnrealObjectRef is requested.
+	*/
+	FUnrealObjectRef GetObjectRefFromActor(const AActor* Actor) const;
+
+	/**
+	* Get the AActor associated with an Worker_EntityId.
+	* Returns nullptr if no associated AActor found.
+	*
+	* @param ObjectRef the Worker_EntityId for which the AActor is requested.
+	*/
+	AActor* GetActorFromObjectRef(const FUnrealObjectRef& ObjectRef) const;
+
+private:
+
+	void RemoveFromRegistryImpl(const AActor* Actor, const FUnrealObjectRef& ObjectRef);
+
+	TMap<FUnrealObjectRef, AActor*> ObjectRefToActor;
+	TMap<AActor*, FUnrealObjectRef> ActorToObjectRef;
+};
