b14397e830ea7ae6722d88a2c2539805d525976d
Daniel Hall <danielhall@improbable.io>
2018-10-26 11:26:02 +0100

    First stab at sending the entity query

--- SpatialGDK/Source/Private/Utils/ActorProxyRegistry.cpp
+++ SpatialGDK/Source/Private/Utils/ActorProxyRegistry.cpp
@@ -4,13 +4,41 @@
 
 #include "EngineUtils.h"
 #include "SpatialConstants.h"
+#include "Schema/StandardLibrary.h"
+#include "SpatialTypebindingManager.h"
+#include "SpatialNetDriver.h"
 
 DEFINE_LOG_CATEGORY(LogActorProxyRegistry);
 
-void UActorProxyRegistry::AddToRegistry(const FUnrealObjectRef& ObjectRef, AActor* Actor)
+void UActorProxyRegistry::AddToRegistry(const FUnrealObjectRef& ActorProxyObjectRef, AActor* OwnerActor)
 {
-	ObjectRefToActor.Add(ObjectRef, Actor);
-	ActorToObjectRef.Add(Actor, ObjectRef);
+	ObjectRefToActor.Add(ActorProxyObjectRef, OwnerActor);
+	ActorToObjectRef.Add(OwnerActor, ActorProxyObjectRef);
+
+	// Send a component update with the new query
+	improbable::Interest InterestComponent;
+
+	improbable::ComponentInterest::Query NewQuery;
+	NewQuery.Constraint.EntityIdConstraint = ActorProxyObjectRef.Entity;
+	NewQuery.ResultComponentId.Add(SpatialConstants::UNREAL_METADATA_COMPONENT_ID);
+	NewQuery.Frequency = 10.0f;
+
+	improbable::ComponentInterest NewCompnentInterest;
+	NewCompnentInterest.Queries.Add(NewQuery);
+
+	USpatialNetDriver* NetDriver = Cast<USpatialNetDriver>(GetWorld()->GetNetDriver());
+	USpatialTypebindingManager* TypebindingManager = NetDriver->TypebindingManager;
+	FClassInfo* Info = TypebindingManager->FindClassInfoByClass(OwnerActor->GetClass());
+	check(Info);
+
+	// TODO: Determine more accurately which components we should have access to
+	InterestComponent.ComponentInterest.Add(Info->RPCComponents[RPC_Client], NewCompnentInterest);
+	InterestComponent.ComponentInterest.Add(Info->RPCComponents[RPC_Server], NewCompnentInterest);
+	InterestComponent.ComponentInterest.Add(Info->RPCComponents[RPC_CrossServer], NewCompnentInterest);
+	InterestComponent.ComponentInterest.Add(Info->RPCComponents[RPC_NetMulticast], NewCompnentInterest);
+
+	Worker_ComponentUpdate NewInterestQueryUpdate = InterestComponent.CreateInterestUpdate();
+	NetDriver->Connection->SendComponentUpdate(OwnerActor->UnrealObjectRef.Entity, &NewInterestQueryUpdate);
 }
 
 void UActorProxyRegistry::RemoveFromRegistry(const AActor* Actor)
