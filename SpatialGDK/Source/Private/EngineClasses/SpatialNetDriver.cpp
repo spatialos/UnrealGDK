7bae7913ab8fc9ae903d7f704022a58784c4bf70
Sahil Dhanju <sahildhanju@improbable.io>
2018-10-28 18:14:50 +0000

    Remove sending bunches

--- SpatialGDK/Source/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/Private/EngineClasses/SpatialNetDriver.cpp
@@ -491,7 +491,6 @@ int32 USpatialNetDriver::ServerReplicateActors_ProcessPrioritizedActors(UNetConn
 				FinalRelevantCount++;
 				UE_LOG(LogNetTraffic, Log, TEXT("Server replicate actor creating destroy channel for NetGUID <%s,%s> Priority: %d"), *PriorityActors[j]->DestructionInfo->NetGUID.ToString(), *PriorityActors[j]->DestructionInfo->PathName, PriorityActors[j]->Priority);
 
-				Channel->SetChannelActorForDestroy(PriorityActors[j]->DestructionInfo);						   // Send a close bunch on the new channel
 				InConnection->GetDestroyedStartupOrDormantActorGUIDs().Remove(PriorityActors[j]->DestructionInfo->NetGUID); // Remove from connections to-be-destroyed list (close bunch of reliable, so it will make it there)
 			}
 			continue;
