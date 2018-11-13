2d41b10e65e9d49d4c320efaa34fabc5091f3310
Valentyn Nykoliuk <valentyn@improbable.io>
2018-11-13 13:41:22 +0000

    Update the error in BuildWorker.bat to say the correct setup script name

--- SpatialGDK/Build/Scripts/BuildWorker.bat
+++ SpatialGDK/Build/Scripts/BuildWorker.bat
@@ -11,7 +11,7 @@ pushd "%~dp0..\..\..\..\..\"
 set BUILD_EXE_PATH="Plugins\UnrealGDK\SpatialGDK\Binaries\ThirdParty\Improbable\Programs\Build.exe"
 
 if not exist %BUILD_EXE_PATH% (
-	echo Error: Build executable not found! Please run BuildGDK.bat in your SpatialGDK directory to generate it.
+	echo Error: Build executable not found! Please run Setup.bat in your UnrealGDK root to generate it.
 	exit /b 1
 )
 
