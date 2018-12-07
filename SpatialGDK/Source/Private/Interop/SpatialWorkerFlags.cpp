cdbbe0d1ab90f0ffb791c4db46ff6efb0c4e3172
Sahil Dhanju <Vatyx@users.noreply.github.com>
2018-12-07 17:41:49 +0000

    Added function to get worker flags in blueprints (#546)

---
+++ SpatialGDK/Source/Private/Interop/SpatialWorkerFlags.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "Interop/SpatialWorkerFlags.h"
+#include <WorkerSDK/improbable/c_worker.h>
+
+TMap<FString, FString> USpatialWorkerFlags::WorkerFlags;
+
+bool USpatialWorkerFlags::GetWorkerFlag(const FString& Name, FString& OutValue)
+{
+	if (FString* ValuePtr = WorkerFlags.Find(Name))
+	{
+		OutValue = *ValuePtr;
+		return true;
+	}
+
+	return false;
+}
+
+void USpatialWorkerFlags::ApplyWorkerFlagUpdate(const Worker_FlagUpdateOp& Op)
+{
+	FString NewName = FString(UTF8_TO_TCHAR(Op.name));
+
+	if (Op.value != nullptr)
+	{
+		FString NewValue = FString(UTF8_TO_TCHAR(Op.value));
+		FString& ValueFlag = WorkerFlags.FindOrAdd(NewName);
+		ValueFlag = NewValue;
+	}
+	else
+	{
+		WorkerFlags.Remove(NewName);
+	}
+}
