1de36d36d21bd272f0d2111f9f419afaf56277da
Joshua Huburn <joshuahuburn@improbable.io>
2018-12-18 15:31:05 +0000

    Changed UnrealGDKVersion

--- UnrealGDKVersion.bat
+++ UnrealGDKVersion.bat
@@ -1,4 +1,4 @@
 rem Versioning file for the UnrealGDK. 
 rem Updating this file will cause a `git pull` to run Setup.bat for users.
 
-set /p UNREAL_GDK_VERSION=0.1.1.2
+set /p UNREAL_GDK_VERSION=0.1.0
