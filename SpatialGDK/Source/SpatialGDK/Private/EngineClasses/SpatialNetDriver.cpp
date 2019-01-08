9cfc209e94ca761eee19c3ed98f43126a2e87038
peterimprobable <petergriffin@improbable.io>
2019-01-08 15:54:21 +0000

    Bugfix/unr815 force net update on reserved entity (#579)
    * Force Net Update on an actor when an EntityId has been reserved to create its Entity
    
    * Checkf to ensure Replication driver is not defined/enabled

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
@@ -39,6 +39,9 @@ bool USpatialNetDriver::InitBase(bool bInitAsClient, FNetworkNotify* InNotify, c
 		return false;
 	}
 
+	// This is a temporary measure until we can look into replication graph support, required due to UNR-832
+	checkf(!GetReplicationDriver(), TEXT("Replication Driver not supported, please remove it from config"));
+
 	bConnectAsClient = bInitAsClient;
 	bAuthoritativeDestruction = true;
 
