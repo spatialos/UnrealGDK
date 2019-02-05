9dc1230e2f66de9df1b81a619f3c6b8889cc92d9
Ben Naccarato <bennaccarato@improbable.io>
2019-02-05 13:33:21 +0000

    Bugfix/null player state (#635)
    * Bug fix: lack of authority when resolving missing component
    
    This resulted in some null-refs in the PlayerState. The fix was to store a list of of updates that failed to send due to a lack of authority, and process them once authority is gained.
    
    * Add associated task to fix
    
    * Update SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialReceiver.cpp
    
    Co-Authored-By: BenNaccarato <bennaccarato@improbable.io>
    
    * Update SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialSender.cpp
    
    Co-Authored-By: BenNaccarato <bennaccarato@improbable.io>
    
    * Update SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialSender.cpp
    
    Co-Authored-By: BenNaccarato <bennaccarato@improbable.io>
    
    * PR feedback - neaten logs and comments
    
    * PR feedback - take by reference, instead of making a copy
    
    * Update SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialSender.cpp
    
    Co-Authored-By: BenNaccarato <bennaccarato@improbable.io>
    
    * PR feedback - method call, and neaten map check
    
    * Update SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialReceiver.cpp
    
    Co-Authored-By: BenNaccarato <bennaccarato@improbable.io>

--- SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialSender.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialSender.cpp
@@ -275,7 +275,13 @@ void USpatialSender::SendComponentUpdates(UObject* Object, FClassInfo& Info, USp
 	{
 		if (!NetDriver->StaticComponentView->HasAuthority(EntityId, Update.component_id))
 		{
-			UE_LOG(LogSpatialSender, Warning, TEXT("Trying to send component update but don't have authority! Update will not be sent. Component Id: %d, entity: %lld"), Update.component_id, EntityId);
+			UE_LOG(LogSpatialSender, Log, TEXT("Trying to send component update but don't have authority! Update will be queued and sent when authority gained. Component Id: %d, entity: %lld"), Update.component_id, EntityId);
+
+			// This is a temporary fix. A task to improve this has been created: UNR-955
+			// It may be the case that upon resolving a component, we do not have authority to send the update. In this case, we queue the update, to send upon receiving authority.
+			// Note: This will break in a multi-worker context, if we try to create an entity that we don't intend to have authority over. For this reason, this fix is only temporary.
+			TArray<Worker_ComponentUpdate>& UpdatesQueuedUntilAuthority = UpdatesQueuedUntilAuthorityMap.FindOrAdd(EntityId);
+			UpdatesQueuedUntilAuthority.Add(Update);
 			continue;
 		}
 
@@ -283,6 +289,19 @@ void USpatialSender::SendComponentUpdates(UObject* Object, FClassInfo& Info, USp
 	}
 }
 
+
+// Apply (and clean up) any updates queued, due to being sent previously when they didn't have authority.
+void USpatialSender::ProcessUpdatesQueuedUntilAuthority(Worker_EntityId EntityId)
+{
+	if (TArray<Worker_ComponentUpdate>* UpdatesQueuedUntilAuthority = UpdatesQueuedUntilAuthorityMap.Find(EntityId))
+	{
+		for (Worker_ComponentUpdate& Update : *UpdatesQueuedUntilAuthority)
+		{
+			Connection->SendComponentUpdate(EntityId, &Update);
+		}
+		UpdatesQueuedUntilAuthorityMap.Remove(EntityId);
+	}
+}
 
 void FillComponentInterests(FClassInfo& Info, bool bNetOwned, TArray<Worker_InterestOverride>& ComponentInterest)
 {
@@ -450,7 +469,7 @@ void USpatialSender::FlushRetryRPCs()
 {
 	// Retried RPCs are sorted by their index.
 	RetryRPCs.Sort([](const TSharedPtr<FPendingRPCParams>& A, const TSharedPtr<FPendingRPCParams>& B) { return A->RetryIndex < B->RetryIndex; });
-	for(auto& RetryRPC : RetryRPCs)
+	for (auto& RetryRPC : RetryRPCs)
 	{
 		SendRPC(RetryRPC);
 	}
