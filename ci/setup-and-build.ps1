8d058ca93527c21e86b37c67f40329c1ce648bf2
Claire Thomas <claire@improbable.io>
2019-02-08 17:34:35 +0000

    Tidying setup-and-build, added start-event and finish-event for nice logging

--- ci/setup-and-build.ps1
+++ ci/setup-and-build.ps1
@@ -17,19 +17,49 @@ function Write-Log() {
   }
 }
 
-Write-Output "Starting Unreal GDK build pipeline.."
+function Start-Event() {
+    param(
+        [string] $event_name,
+        [string] $event_parent
+    )
+
+    # Start this tracing span.
+    Start-Process -NoNewWindow "imp-ci" -ArgumentList @(`
+        "events", "new", `
+        "--name", "$($event_name)", `
+        "--child-of", "$($event_parent)"
+    ) | Out-Null
+
+    Write-Log "--- $($event_name)"
+}
+
+function Finish-Event() {
+    param(
+        [string] $event_name,
+        [string] $event_parent
+    )
+
+    # Emit the end marker for this tracing span.
+    Start-Process -NoNewWindow "imp-ci"  -ArgumentList @(`
+        "events", "new", `
+        "--name", "$($event_name)", `
+        "--child-of", "$($event_parent)"
+    ) | Out-Null
+}
 
 pushd "$($gdk_home)"
 
     # Fetch the version of Unreal Engine we need
     pushd "ci"
         $unreal_version = Get-Content -Path "unreal-engine.version" -Raw
-        Write-Log "Using Unreal Engine version $($unreal_version)"
+        Write-Log "Using Unreal Engine version: $($unreal_version)"
     popd
 
-    Write-Log "Create an UnrealEngine directory if it doesn't already exist"
+    ## Create an UnrealEngine directory if it doesn't already exist
     New-Item -Name "UnrealEngine" -ItemType Directory -Force
 
+
+    Start-Event "download-unreal-engine" "build-unreal-gdk-:windows:"
     pushd "UnrealEngine"
         Write-Log "Downloading the Unreal Engine artifacts from GCS"
         $gcs_unreal_location = "$($unreal_version).zip"
@@ -53,8 +83,9 @@ pushd "$($gdk_home)"
             Write-Log "Failed to unzip Unreal Engine. Error: $($zip_proc.ExitCode)" 
             Throw "Failed to unzip Unreal Engine."  
         }
-
     popd
+    Finish-Event "download-unreal-engine" "build-unreal-gdk-:windows:"
+
 
     $unreal_path = "$($gdk_home)\UnrealEngine\$($unreal_version)\"
     Write-Log "Setting UNREAL_HOME environment variable to $($unreal_path)"
@@ -77,8 +108,8 @@ pushd "$($gdk_home)"
     New-Item -Path "$($core_sdk_dir)\worker_sdk" -ItemType Directory -Force
     New-Item -Path "$($binaries_dir)" -ItemType Directory -Force
 
-    Write-Log "Downloading spatial packages.."
-    
+
+    Start-Event "download-spatial-packages" "build-unreal-gdk-:windows:"
     Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
         "package", `
         "retrieve", `
@@ -87,7 +118,6 @@ pushd "$($gdk_home)"
         "$($pinned_core_sdk_version)", `
         "$($core_sdk_dir)\tools\schema_compiler-x86_64-win32.zip" `
     )
-
     Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
         "package", `
         "retrieve", `
@@ -96,7 +126,6 @@ pushd "$($gdk_home)"
         "$($pinned_core_sdk_version)", `
         "$($core_sdk_dir)\schema\standard_library.zip" `
     )
-
     Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
         "package", `
         "retrieve", `
@@ -106,7 +135,6 @@ pushd "$($gdk_home)"
         "$($core_sdk_dir)\worker_sdk\c-dynamic-x86-msvc_md-win32.zip" `
     )
 
-
     Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
         "package", `
         "retrieve", `
@@ -115,7 +143,6 @@ pushd "$($gdk_home)"
         "$($pinned_core_sdk_version)", `
         "$($core_sdk_dir)\worker_sdk\c-dynamic-x86_64-msvc_md-win32.zip" `
     )
-
     Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
         "package", `
         "retrieve", `
@@ -124,13 +151,17 @@ pushd "$($gdk_home)"
         "$($pinned_core_sdk_version)", `
         "$($core_sdk_dir)\worker_sdk\c-dynamic-x86_64-gcc_libstdcpp-linux.zip" `
     )
+    Finish-Event "download-spatial-packages" "build-unreal-gdk-:windows:"
 
-    Write-Log "Extracting spatial packages.."
+
+    Start-Event "extract-spatial-packages" "build-unreal-gdk-:windows:"
     Expand-Archive -Path "$($core_sdk_dir)\tools\schema_compiler-x86_64-win32.zip" -DestinationPath "$($binaries_dir)\Programs\" -Force
     Expand-Archive -Path "$($core_sdk_dir)\schema\standard_library.zip" -DestinationPath "$($binaries_dir)\Programs\schema\" -Force
     Expand-Archive -Path "$($core_sdk_dir)\worker_sdk\c-dynamic-x86-msvc_md-win32.zip" -DestinationPath "$($binaries_dir)\Win32\" -Force
     Expand-Archive -Path "$($core_sdk_dir)\worker_sdk\c-dynamic-x86_64-msvc_md-win32.zip" -DestinationPath "$($binaries_dir)\Win64\" -Force
     Expand-Archive -Path "$($core_sdk_dir)\worker_sdk\c-dynamic-x86_64-gcc_libstdcpp-linux.zip" -DestinationPath "$($binaries_dir)\Linux\" -Force
+    Finish-Event "extract-spatial-packages" "build-unreal-gdk-:windows:"
+
 
     # Copy from binaries_dir
     Copy-Item "$($binaries_dir)\Win64\include" "$($worker_sdk_dir)" -Force -Recurse
@@ -139,7 +170,8 @@ pushd "$($gdk_home)"
     #call "%UNREAL_HOME%\Engine\Build\BatchFiles\GetMSBuildPath.bat"
     $msbuild_exe = "${env:ProgramFiles(x86)}\MSBuild\14.0\bin\MSBuild.exe"
 
-    Write-Log "Build utilities"
+
+    Start-Event "build-utilities" "build-unreal-gdk-:windows:"
     #%MSBUILD_EXE% /nologo /verbosity:minimal .\SpatialGDK\Build\Programs\Improbable.Unreal.Scripts\Improbable.Unreal.Scripts.sln /property:Configuration=Release
     Start-Process -Wait -PassThru -NoNewWindow -FilePath "$($msbuild_exe)" -ArgumentList @(`
         "/nologo", `
@@ -147,16 +179,18 @@ pushd "$($gdk_home)"
         "SpatialGDK\Build\Programs\Improbable.Unreal.Scripts\Improbable.Unreal.Scripts.sln", `
         "/property:Configuration=Release" `
     )
+    Finish-Event "build-utilities" "build-unreal-gdk-:windows:"
 
-  pushd "SpatialGDK"
-    Write-Log "Build Unreal GDK"
-    Start-Process -Wait -PassThru -NoNewWindow -FilePath "$($unreal_path)\Engine\Build\BatchFiles\RunUAT.bat" -ArgumentList @(`
-        "BuildPlugin", `
-        " -Plugin=`"$($gdk_home)/SpatialGDK/SpatialGDK.uplugin`"", `
-        "-TargetPlatforms=Win64", `
-        "-Package=`"$gdk_home/SpatialGDK/Intermediate/BuildPackage/Win64`"" `
-    )
 
-  popd
+    Start-Event "build-unreal-gdk" "build-unreal-gdk-:windows:"
+    pushd "SpatialGDK"
+        Start-Process -Wait -PassThru -NoNewWindow -FilePath "$($unreal_path)\Engine\Build\BatchFiles\RunUAT.bat" -ArgumentList @(`
+            "BuildPlugin", `
+            " -Plugin=`"$($gdk_home)/SpatialGDK/SpatialGDK.uplugin`"", `
+            "-TargetPlatforms=Win64", `
+            "-Package=`"$gdk_home/SpatialGDK/Intermediate/BuildPackage/Win64`"" `
+        )
+    popd
+    Finish-Event "build-unreal-gdk" "build-unreal-gdk-:windows:"
 
 popd
