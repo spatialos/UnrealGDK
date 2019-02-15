99420e29b342bc4f5fcb7418c62f20957111bf2f
Patrick Hansell <patrickhansell@improbable.io>
2019-02-15 18:01:14 +0000

    Fixing paths

--- ci/setup-and-build-gdk.ps1
+++ ci/setup-and-build-gdk.ps1
@@ -1,3 +1,8 @@
+# Download Unreal Engine 
 &$PSScriptRoot"\get-engine.ps1"
+
+# Run the required setup steps
 &$PSScriptRoot"\setup-gdk.ps1"
+
+# Build the GDK plugin
 &$PSScriptRoot"\build-gdk.ps1"
