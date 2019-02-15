1d34a25c4ece6aef1be527926d06e8643d600a14
Patrick Hansell <patrickhansell@improbable.io>
2019-02-15 16:32:53 +0000

    AND AGAIN

--- ci/setup-and-build-gdk.ps1
+++ ci/setup-and-build-gdk.ps1
@@ -1,5 +1,3 @@
-$PSScriptRoot+"\goodbuy.ps1"
-
-&$PSScriptRoot+"\get-engine.ps1"
-&$PSScriptRoot+"\setup-gdk.ps1"
-&$PSScriptRoot+"\build-gdk.ps1"
+&$PSScriptRoot"\get-engine.ps1"
+&$PSScriptRoot"\setup-gdk.ps1"
+&$PSScriptRoot"\build-gdk.ps1"
