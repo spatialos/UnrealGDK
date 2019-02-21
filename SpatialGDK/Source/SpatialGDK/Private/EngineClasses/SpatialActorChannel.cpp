75bb470e0e7b612f1c5d0d2b00db2a99ac5806f5
Sahil Dhanju <Vatyx@users.noreply.github.com>
2019-02-21 14:00:28 -0600

    Don't update shadow data if we created the entity locally (#687)
    * Fixed shadow data being incorrectly reinitialized
    
    * Delete qq
    
    * PR feedback
    
    * Fix compilation

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialActorChannel.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialActorChannel.cpp
@@ -73,6 +73,7 @@ USpatialActorChannel::USpatialActorChannel(const FObjectInitializer& ObjectIniti
 	, NetDriver(nullptr)
 	, LastSpatialPosition(FVector::ZeroVector)
 	, bCreatingNewEntity(false)
+	, bCreatedEntity(false)
 {
 }
 
@@ -159,6 +160,15 @@ bool USpatialActorChannel::IsDynamicArrayHandle(UObject* Object, uint16 Handle)
 void USpatialActorChannel::UpdateShadowData()
 {
 	check(Actor);
+
+	// If this channel was responsible for creating the channel, we do not want to initialize our shadow data
+	// to the latest state since there could have been state that has changed between creation of the entity
+	// and gaining of authority. Revisit this with UNR-1034
+	// TODO: UNR-1029 - log when the shadow data differs from the current state of the Actor.
+	if (bCreatedEntity)
+	{
+		return;
+	}
 
 	// Refresh shadow data when crossing over servers to prevent stale/out-of-date data.
 	ActorReplicator->RepLayout->InitShadowData(ActorReplicator->ChangelistMgr->GetRepChangelistState()->StaticBuffer, Actor->GetClass(), (uint8*)Actor);
@@ -714,6 +724,7 @@ void USpatialActorChannel::OnCreateEntityResponse(const Worker_CreateEntityRespo
 		return;
 	}
 
+	bCreatedEntity = true;
 	UE_LOG(LogSpatialActorChannel, Verbose, TEXT("Created entity (%lld) for: %s."), Op.entity_id, *Actor->GetName());
 }
 
