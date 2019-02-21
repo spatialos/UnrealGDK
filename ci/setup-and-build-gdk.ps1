acf7d64be5e424314d416f7f245e9be1e8eb83c1
Daniel Hall <danielhall@improbable.io>
2019-02-21 14:38:23 +0000

    Fixed missing build scripts

--- ci/setup-and-build-gdk.ps1
+++ ci/setup-and-build-gdk.ps1
@@ -1,3 +1,9 @@
+param(
+  [string] $gdk_home = (get-item "$($PSScriptRoot)").parent.FullName, ## The root of the UnrealGDK repo
+  [string] $gcs_publish_bucket = "io-internal-infra-unreal-artifacts-production/UnrealEngine",
+  [string] $msbuild_exe = "${env:ProgramFiles(x86)}\MSBuild\14.0\bin\MSBuild.exe"
+)
+
 # Download Unreal Engine 
 &$PSScriptRoot"\get-engine.ps1"
 
