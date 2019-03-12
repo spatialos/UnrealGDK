f76f5e66483b6e66308ab83a33b0b04cc15b108b
Sahil Dhanju <sahildhanju@improbable.io>
2019-03-12 13:16:03 +0000

    Disabled chunk interest in auto gened config

--- SpatialGDK/Source/SpatialGDKEditorToolbar/Private/SpatialGDKEditorToolbar.cpp
+++ SpatialGDK/Source/SpatialGDKEditorToolbar/Private/SpatialGDKEditorToolbar.cpp
@@ -452,6 +452,10 @@ bool FSpatialGDKEditorToolbarModule::GenerateDefaultLaunchConfig(const FString& 
 					Writer->WriteValue(TEXT("name"), TEXT("bridge_soft_handover_enabled"));
 					Writer->WriteValue(TEXT("value"), TEXT("false"));
 				Writer->WriteObjectEnd();
+				Writer->WriteObjectStart();
+					Writer->WriteValue(TEXT("name"), TEXT("enable_chunk_interest"));
+					Writer->WriteValue(TEXT("value"), TEXT("false"));
+				Writer->WriteObjectEnd();
 			Writer->WriteArrayEnd();
 			Writer->WriteObjectStart(TEXT("snapshots"));
 				Writer->WriteValue(TEXT("snapshot_write_period_seconds"), 0);
