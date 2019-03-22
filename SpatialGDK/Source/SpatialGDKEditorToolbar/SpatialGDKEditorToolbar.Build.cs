2176eb5726f05d1ebf4d57ba6c44bec6641d69a9
Boyan Asenov <boyan@improbable.io>
2019-03-22 14:30:54 +0000

    Now tabs to spaces :)

--- SpatialGDK/Source/SpatialGDKEditorToolbar/SpatialGDKEditorToolbar.Build.cs
+++ SpatialGDK/Source/SpatialGDKEditorToolbar/SpatialGDKEditorToolbar.Build.cs
@@ -17,7 +17,7 @@ public class SpatialGDKEditorToolbar : ModuleRules
                 "Core",
                 "CoreUObject",
                 "DesktopPlatform",
-				"DesktopWidgets",
+                "DesktopWidgets",
                 "Engine",
                 "EngineSettings",
                 "InputCore",
