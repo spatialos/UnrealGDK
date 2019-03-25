6c5a797ef8669f37212006a9c0e3dcb3d2c5e313
Daniel Hall <danielhall@improbable.io>
2019-03-25 13:51:34 +0000

    Fixed broken simulated players build

--- SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/Build/Build.cs
+++ SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/Build/Build.cs
@@ -233,7 +233,7 @@ exit /b !ERRORLEVEL!
                     Console.WriteLine("worker coordinator path did not exist");
                 }
 
-                var archiveFileName = gameName + "SimulatedPlayer@Linux.zip";
+                var archiveFileName = "UnrealSimulatedPlayer@Linux.zip";
                 Common.RunRedirected(@"%UNREAL_HOME%\Engine\Build\BatchFiles\RunUAT.bat", new[]
                 {
                     "ZipUtils",
