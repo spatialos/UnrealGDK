09bfa8ac59f5b0715d651f9a1108b10f28343971
WVerlaek <wouter@improbable.io>
2019-03-28 14:50:14 +0000

    Add deployment launcher .bat script

---
+++ SpatialGDK/Build/Scripts/DeploymentLauncher.bat
@@ -0,0 +1,16 @@
+@echo off
+
+pushd "%~dp0..\..\..\..\..\"
+
+set DEPLOYMENT_LAUNCHER_EXE_PATH="Plugins\UnrealGDK\SpatialGDK\Binaries\ThirdParty\Improbable\Programs\DeploymentLauncher\DeploymentLauncher.exe"
+
+if not exist %DEPLOYMENT_LAUNCHER_EXE_PATH% (
+	echo Error: Deployment launcher executable not found! Please run Setup.bat in your UnrealGDK root to generate it.
+	exit /b 1
+)
+
+%DEPLOYMENT_LAUNCHER_EXE_PATH% %*
+
+popd
+
+exit /b %ERRORLEVEL%
