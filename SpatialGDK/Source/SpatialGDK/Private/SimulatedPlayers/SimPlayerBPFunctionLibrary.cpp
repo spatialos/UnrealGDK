2882abd984caab2830b2932425d0897273399c71
WVerlaek <wouter@improbable.io>
2019-05-08 15:24:13 +0100

    Refactor IsSimulatedPlayer to use GameplayStatics

---
+++ SpatialGDK/Source/SpatialGDK/Private/SimulatedPlayers/SimPlayerBPFunctionLibrary.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "SimPlayerBPFunctionLibrary.h"
+
+#include "Engine/GameInstance.h"
+#include "Kismet/GameplayStatics.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogSimulatedPlayer, Log, All);
+
+bool USimPlayerBPFunctionLibrary::IsSimulatedPlayer(UObject* WorldContextObject)
+{
+	UGameInstance* GameInstance = UGameplayStatics::GetGameInstance(WorldContextObject);
+	if (GameInstance == nullptr) {
+		UE_LOG(LogSimulatedPlayer, Warning, TEXT("Cannot check if player is simulated: cannot get GameInstance. Defaulting to false."));
+		return false;
+	}
+
+	return GameInstance->IsSimulatedPlayer();
+}
