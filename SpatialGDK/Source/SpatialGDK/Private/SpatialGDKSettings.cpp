a507414a7e78850b7dfce6e7e08c5b4a6545238b
danielimprobable <danielhall@improbable.io>
2019-05-16 14:42:46 +0100

    Changed default value

--- SpatialGDK/Source/SpatialGDK/Private/SpatialGDKSettings.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/SpatialGDKSettings.cpp
@@ -21,7 +21,7 @@ USpatialGDKSettings::USpatialGDKSettings(const FObjectInitializer& ObjectInitial
 	, EntityCreationRateLimit(0)
 	, OpsUpdateRate(30.0f)
 	, bUsingQBI(true)
-	, bUsingOffloading(true)
+	, bUsingOffloading(false)
 	, PositionUpdateFrequency(1.0f)
 	, PositionDistanceThreshold(100.0f) // 1m (100cm)
 	, bEnableMetrics(true)
