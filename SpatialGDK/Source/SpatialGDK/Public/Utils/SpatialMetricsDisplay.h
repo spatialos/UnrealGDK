d65583822cbd7ea9e7383fa6bbb68ecd0e8482a6
Joshua Huburn <joshuahuburn@improbable.io>
2019-05-29 13:47:13 +0100

    Added category to SpatialMetricsDisplay

--- SpatialGDK/Source/SpatialGDK/Public/Utils/SpatialMetricsDisplay.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/SpatialMetricsDisplay.h
@@ -37,7 +37,7 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Destroyed() override;
 
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(Category = "SpatialGDK", BlueprintCallable)
 	void ToggleStatDisplay();
 
 private:
