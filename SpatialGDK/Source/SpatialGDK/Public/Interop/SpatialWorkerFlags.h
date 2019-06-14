d9feef9577611345d64e14941e46dfc87e29a57c
chrisfields0 <christopherfields@improbable.io>
2019-06-14 15:48:16 +0100

    Update SpatialGDK/Source/SpatialGDK/Public/Interop/SpatialWorkerFlags.h
    Co-Authored-By: Sahil Dhanju <Vatyx@users.noreply.github.com>

--- SpatialGDK/Source/SpatialGDK/Public/Interop/SpatialWorkerFlags.h
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/SpatialWorkerFlags.h
@@ -6,6 +6,7 @@
 #include "SpatialWorkerFlags.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnWorkerFlagsUpdated, FString, FlagName, FString, FlagValue);
+
 UCLASS()
 class SPATIALGDK_API USpatialWorkerFlags : public UBlueprintFunctionLibrary
 {
