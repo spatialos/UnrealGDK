3cea6de70ec628788be54ce23b73a9d89662eb3c
Matt Young <matthewyoung@improbable.io>
2019-06-18 15:05:19 +0100

    Remove old function signature

--- SpatialGDK/Source/SpatialGDK/Public/SpatialGDKSettings.h
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialGDKSettings.h
@@ -24,9 +24,6 @@ public:
 
 	void ValidateOffloadingSettings();
 
-	void DoValidate(TMap<FName, FActorClassSet> OldActorGroups, TMap<FName, FActorClassSet>* ActorGroups,
-		TSet<FName> OldWorkerTypes, TSet<FName>* WorkerTypes, FWorkerAssociation& WorkerAssociation);
-
 #endif
 
 	virtual void PostInitProperties() override;
