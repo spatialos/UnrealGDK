4da582d4b8bfb2c4cae3ebed5f7ce5b2e24ec05f
Sang Tian <sangtian@improbable.io>
2019-06-20 21:27:31 +0800

    * Fix variables naming to capital

--- SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialWorkerConnection.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialWorkerConnection.cpp
@@ -151,8 +151,8 @@ void USpatialWorkerConnection::OnPlayerIdentityToken(void* UserData, const Worke
 	Connection->LocatorConfig.PlayerIdentityToken = UTF8_TO_TCHAR(PIToken->player_identity_token);
 	Worker_Alpha_LoginTokensRequest LTParams{};
 	LTParams.player_identity_token = PIToken->player_identity_token;
-	FTCHARToUTF8 worker_type(*Connection->LocatorConfig.WorkerType);
-	LTParams.worker_type = worker_type.Get();
+	FTCHARToUTF8 WorkerType(*Connection->LocatorConfig.WorkerType);
+	LTParams.worker_type = WorkerType.Get();
 	LTParams.use_insecure_connection = false;
 
 	if (Worker_Alpha_LoginTokensResponseFuture* LTFuture = Worker_Alpha_CreateDevelopmentLoginTokensAsync(TCHAR_TO_UTF8(*Connection->LocatorConfig.LocatorHost), SpatialConstants::LOCATOR_PORT, &LTParams))
@@ -164,10 +164,10 @@ void USpatialWorkerConnection::OnPlayerIdentityToken(void* UserData, const Worke
 void USpatialWorkerConnection::StartDevelopmentAuth(FString DevAuthToken)
 {
 	Worker_Alpha_PlayerIdentityTokenRequest PITParams{};
-	FTCHARToUTF8 dev_auth_token(*DevAuthToken);
-	FTCHARToUTF8 player_id(*SpatialConstants::DEVELOPMENT_AUTH_PLAYER_ID);
-	PITParams.development_authentication_token = dev_auth_token.Get();
-	PITParams.player_id = player_id.Get();
+	FTCHARToUTF8 DAToken(*DevAuthToken);
+	FTCHARToUTF8 PlayerId(*SpatialConstants::DEVELOPMENT_AUTH_PLAYER_ID);
+	PITParams.development_authentication_token = DAToken.Get();
+	PITParams.player_id = PlayerId.Get();
 	PITParams.display_name = "";
 	PITParams.metadata = "";
 	PITParams.use_insecure_connection = false;
