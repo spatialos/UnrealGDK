8791542dc5c28e64677cbb5a5397d78a83c7c73d
Sang Tian <sangtian@improbable.io>
2019-06-20 19:52:14 +0800

    * If the default receptionist is not localhost, set useExternalIp to true;

--- SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/ConnectionConfig.h
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/ConnectionConfig.h
@@ -78,6 +78,10 @@ struct FReceptionistConfig : public FConnectionConfig
 			{
 				// If an IP is not specified then use default.
 				ReceptionistHost = GetDefault<USpatialGDKSettings>()->DefaultReceptionistHost;
+				if (ReceptionistHost.Compare(SpatialConstants::LOCAL_HOST) != 0)
+				{
+					UseExternalIp = true;
+				}
 			}
 		}
 
