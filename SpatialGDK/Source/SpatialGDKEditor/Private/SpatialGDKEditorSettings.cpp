dd88c6add291da9b5e36f2d2374f657e498c3044
danielimprobable <danielhall@improbable.io>
2019-06-21 12:27:42 +0100

    Removed unused include

--- SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorSettings.cpp
+++ SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorSettings.cpp
@@ -4,7 +4,6 @@
 #include "Misc/MessageDialog.h"
 #include "Modules/ModuleManager.h"
 #include "Settings/LevelEditorPlaySettings.h"
-#include "SpatialGDKSettings.h"
 
 
 USpatialGDKEditorSettings::USpatialGDKEditorSettings(const FObjectInitializer& ObjectInitializer)
