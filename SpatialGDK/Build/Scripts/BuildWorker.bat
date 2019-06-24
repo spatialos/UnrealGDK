9c34464dbd9f32b71f9de0bf80a9059ace8807dc
Joshua Huburn <joshuahuburn@improbable.io>
2019-06-24 13:37:54 +0100

    Update BuildWorker.bat

--- SpatialGDK/Build/Scripts/BuildWorker.bat
+++ SpatialGDK/Build/Scripts/BuildWorker.bat
@@ -1,11 +1,22 @@
 @echo off
 
+rem Generate worker configs
 pushd "%~dp0..\..\..\..\..\..\spatial"
-
 spatial worker build build-config
-
 popd
 
+rem Generate schema descriptor
+set SCHEMA_COMPILER="%~dp0..\..\Binaries\ThirdParty\Improbable\Programs\schema_compiler.exe"
+set PROJECT_SCHEMA="%~dp0..\..\..\..\..\..\spatial\schema"
+set GDK_SCHEMA="%~dp0..\..\Extras\schema"
+set STANDARD_LIBRARY_SCHEMA="%~dp0..\..\Build\core_sdk\schema\standard_library"
+set SCHEMA_DESCRIPTOR_OUT="%~dp0..\..\..\..\..\..\spatial\build\assembly\schema\schema.descriptor"
+
+echo Building schema descriptor
+
+call %SCHEMA_COMPILER% --schema_path=%PROJECT_SCHEMA% --schema_path=%GDK_SCHEMA% --schema_path=%STANDARD_LIBRARY_SCHEMA% --descriptor_set_out=%SCHEMA_DESCRIPTOR_OUT% --load_all_schema_on_schema_path
+
+rem Build Unreal project
 pushd "%~dp0..\..\..\..\..\"
 
 set BUILD_EXE_PATH="Plugins\UnrealGDK\SpatialGDK\Binaries\ThirdParty\Improbable\Programs\Build.exe"
@@ -16,6 +27,8 @@ if not exist %BUILD_EXE_PATH% (
 )
 
 %BUILD_EXE_PATH% %*
+
+
 
 popd
 
