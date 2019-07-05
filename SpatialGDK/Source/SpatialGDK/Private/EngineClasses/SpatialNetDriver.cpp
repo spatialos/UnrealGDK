4374942bc28d3ba582debeec7478f658078bbe22
Alex Moroz <oleksandrmoroz@improbable.io>
2019-07-05 17:53:12 +0100

    Removed commented code

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
@@ -1739,9 +1739,6 @@ uint32 USpatialNetDriver::GetNextReliableRPCId(AActor* Actor, ESchemaComponentTy
 
 void USpatialNetDriver::OnReceivedReliableRPC(AActor* Actor, ESchemaComponentType RPCType, FString WorkerId, uint32 RPCId, UObject* TargetObject, UFunction* Function)
 {
-	// Commented out since this code is out of date and will assert
-	//check(!WorkerId.IsEmpty());
-
 	if (!ReliableRPCIdMap.Contains(Actor))
 	{
 		ReliableRPCIdMap.Add(Actor);
