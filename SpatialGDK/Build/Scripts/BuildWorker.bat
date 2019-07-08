f9645df63f9c5a32de3a6ed64a229028dcd34b6e
Joshua Huburn <joshuahuburn@improbable.io>
2019-07-08 15:06:34 +0100

    Working with engine and project plugin

--- SpatialGDK/Build/Scripts/BuildWorker.bat
+++ SpatialGDK/Build/Scripts/BuildWorker.bat
@@ -5,9 +5,11 @@ rem Usage: <GameName> <Platform> <Configuration> <game.uproject> [-nocompile] <A
 rem Try and build as a project plugin first, check for a project plugin structure.
 set UnrealProjectDir="%~dp0..\..\..\..\..\"
 
-for /f "delims=" %%A in (' powershell -Command "(Get-ChildItem -Path "%UnrealProjectDir%" *.uproject).FullName" ') do set UPROJECT="%%A"
+for /f "delims=" %%A in (' powershell -Command "(Get-ChildItem -Path "%UnrealProjectDir%" *.uproject).FullName" ') do set FoundUproject="%%A"
 
-if %UPROJECT%=="" (
+echo Found uproject: %FoundUproject%
+
+if "%FoundUproject%"=="" (
 	goto :BuildAsEnginePlugin
 )
 
