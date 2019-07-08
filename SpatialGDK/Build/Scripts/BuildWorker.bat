cbcd827f748b1c1063cd413fc845aadd1137c479
Joshua Huburn <joshuahuburn@improbable.io>
2019-07-08 17:39:58 +0100

    Review comments

--- SpatialGDK/Build/Scripts/BuildWorker.bat
+++ SpatialGDK/Build/Scripts/BuildWorker.bat
@@ -3,18 +3,18 @@
 if [%4] == [] goto :MissingParams
 
 rem Try and build as a project plugin first, check for a project plugin structure.
-set UnrealProjectDir="%~dp0..\..\..\..\..\"
-set FoundUProject=""
+set UNREAL_PROJECT_DIR="%~dp0..\..\..\..\..\"
+set FOUND_UPROJECT=""
 
-for /f "delims=" %%A in (' powershell -Command "(Get-ChildItem -Path "%UnrealProjectDir%" *.uproject).FullName" ') do set FoundUProject="%%A"
+for /f "delims=" %%A in (' powershell -Command "(Get-ChildItem -Path "%UNREAL_PROJECT_DIR%" *.uproject).FullName" ') do set FOUND_UPROJECT="%%A"
 
-if %FoundUProject%=="" (
+if %FOUND_UPROJECT%=="" (
 	goto :BuildAsEnginePlugin
 )
 
 rem If we are running as a project plugin then this will be the path to the spatial directory.
 echo Building as project plugin
-set SpatialDir="%~dp0..\..\..\..\..\..\spatial"
+set SPATIAL_DIR="%~dp0..\..\..\..\..\..\spatial"
 set BUILD_EXE_PATH="Plugins\UnrealGDK\SpatialGDK\Binaries\ThirdParty\Improbable\Programs\Build.exe"
 goto :Build
 
@@ -24,39 +24,39 @@ rem If we are running as an Engine plugin then we need a full path to the .uproj
 set UPROJECT=%4
 
 if not exist %UPROJECT% (
-	echo To use BuildWorker.bat with an Engine plugin installation you must provide a full path to your .uproject file.
+	echo To use BuildWorker.bat with an Engine plugin installation you must provide the full path to your .uproject file.
 	exit /b 1
 )
 
 rem Grab the project path from the .uproject file.
 for %%i in (%UPROJECT%) do (
 	rem file drive + file directory
-	set UnrealProjectDir="%%~di%%~pi"
+	set UNREAL_PROJECT_DIR="%%~di%%~pi"
 )
 
 rem Path to the SpatialGDK build tool as an Engine plugin.
 set BUILD_EXE_PATH="%~dp0..\..\Binaries\ThirdParty\Improbable\Programs\Build.exe"
-set SpatialDir=%UnrealProjectDir%..\spatial\
+set SPATIAL_DIR=%UNREAL_PROJECT_DIR%..\spatial\
 
 
 :Build
 
-if not exist %SpatialDir% (
-	echo Could not find the projects 'spatial' directory! Please ensure your input arguments are correct and your GDK is correctly installed.
+if not exist %SPATIAL_DIR% (
+	echo Could not find the project's 'spatial' directory! Please ensure your input arguments are correct and your GDK is correctly installed.
 	exit /b 1
 )
 
-echo Building using project directory: 	%UnrealProjectDir%
-echo Building using spatial directory: 	%SpatialDir%
+echo Building using project directory: 	%UNREAL_PROJECT_DIR%
+echo Building using spatial directory: 	%SPATIAL_DIR%
 echo Building using uproject file:		%4
 
 rem First build the spatial worker configs
-pushd "%SpatialDir%"
+pushd "%SPATIAL_DIR%"
 spatial worker build build-config
 popd
 
 rem We pushd to the location of the project file to allow specifying just the .uproject name and not the full path (both are allowed for project plugins).
-pushd %UnrealProjectDir%
+pushd %UNREAL_PROJECT_DIR%
 
 if not exist %BUILD_EXE_PATH% (
 	echo Error: Build executable not found! Please run Setup.bat in your UnrealGDK root to generate it.
