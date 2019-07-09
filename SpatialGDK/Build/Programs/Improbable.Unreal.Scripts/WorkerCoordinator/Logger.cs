71ac5f9d379749d5c973f393e62999982542e3c3
WVerlaek <wouter@improbable.io>
2019-07-09 10:16:58 +0100

    Convert tabs to spaces

--- SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/WorkerCoordinator/Logger.cs
+++ SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/WorkerCoordinator/Logger.cs
@@ -5,54 +5,54 @@ using Improbable.Worker;
 
 namespace Improbable.WorkerCoordinator
 {
-	public class Logger
-	{
-		private readonly string LogPath;
+    public class Logger
+    {
+        private readonly string LogPath;
         private readonly string LoggerName;
         private Connection Connection = null;
 
-		public Logger(string logPath, string loggerName)
-		{
-			LogPath = logPath;
+        public Logger(string logPath, string loggerName)
+        {
+            LogPath = logPath;
             LoggerName = loggerName;
-		}
+        }
 
         public void EnableSpatialOSLogging(Connection connection)
         {
             Connection = connection;
         }
 
-		public void ClearLog()
-		{
-			File.WriteAllText(LogPath, string.Empty);
-		}
+        public void ClearLog()
+        {
+            File.WriteAllText(LogPath, string.Empty);
+        }
 
-		public void WriteLog(string logMessage, bool logToConnectionIfExists = true)
-		{
+        public void WriteLog(string logMessage, bool logToConnectionIfExists = true)
+        {
             WriteLogToFile(logMessage);
             if (logToConnectionIfExists && Connection != null)
             {
                 Connection.SendLogMessage(LogLevel.Info, LoggerName, logMessage);
             }
-		}
+        }
 
-		public void WriteWarning(string logMessage, bool logToConnectionIfExists = true)
-		{
-			WriteLogToFile("Warning: " + logMessage);
+        public void WriteWarning(string logMessage, bool logToConnectionIfExists = true)
+        {
+            WriteLogToFile("Warning: " + logMessage);
             if (logToConnectionIfExists && Connection != null)
             {
                 Connection.SendLogMessage(LogLevel.Warn, LoggerName, logMessage);
             }
-		}
+        }
 
-		public void WriteError(string logMessage, bool logToConnectionIfExists = true)
-		{
-			WriteLogToFile("Error: " + logMessage);
+        public void WriteError(string logMessage, bool logToConnectionIfExists = true)
+        {
+            WriteLogToFile("Error: " + logMessage);
             if (logToConnectionIfExists && Connection != null)
             {
                 Connection.SendLogMessage(LogLevel.Error, LoggerName, logMessage);
             }
-		}
+        }
 
         private void WriteLogToFile(string logMessage)
         {
@@ -61,5 +61,5 @@ namespace Improbable.WorkerCoordinator
                 file.WriteLine(logMessage);
             }
         }
-	}
+    }
 }
