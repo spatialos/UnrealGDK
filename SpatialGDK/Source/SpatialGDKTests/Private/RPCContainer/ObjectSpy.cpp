5f16cc626e764b841735a8f36284284adaf08252
Alex Moroz <oleksandrmoroz@improbable.io>
2019-07-19 13:12:12 +0100

    Moved assert to the top of the file

--- SpatialGDK/Source/SpatialGDKTests/Private/RPCContainer/ObjectSpy.cpp
+++ SpatialGDK/Source/SpatialGDKTests/Private/RPCContainer/ObjectSpy.cpp
@@ -2,20 +2,18 @@
 
 #include "ObjectSpy.h"
 
+// If this assertion fails, then TypeToArray and ArrayToType
+// functions have to be updated correspondingly
+static_assert(sizeof(ESchemaComponentType) == sizeof(int32), "");
+
 TArray<uint8> SpyUtils::TypeToArray(ESchemaComponentType Type)
 {
-	// Make sure that if Type value changes - there will be an explicit error
-	static_assert(sizeof(ESchemaComponentType) == sizeof(int32), "");
-
 	int32 ConvertedType = static_cast<int32>(Type);
 	return TArray<uint8>(reinterpret_cast<uint8*>(&ConvertedType), sizeof(ConvertedType));
 }
 
 ESchemaComponentType SpyUtils::ArrayToType(const TArray<uint8>& Array)
 {
-	// Make sure that if Type value changes - there will be an explicit error
-	static_assert(sizeof(ESchemaComponentType) == sizeof(int32), "");
-
 	return ESchemaComponentType(*reinterpret_cast<const int32*>(&Array[0]));
 }
 
