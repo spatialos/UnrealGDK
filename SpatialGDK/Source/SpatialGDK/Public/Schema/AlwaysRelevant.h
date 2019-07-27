bed82e1571755ae12761ec13621ffbd9553a67bf
Michael Samiec <michaelsamiec@improbable.io>
2019-07-27 19:21:33 +0100

    Initial AlwaysRelevant support

---
+++ SpatialGDK/Source/SpatialGDK/Public/Schema/AlwaysRelevant.h
@@ -0,0 +1,33 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "Schema/Component.h"
+#include "SpatialConstants.h"
+
+#include <WorkerSDK/improbable/c_schema.h>
+#include <WorkerSDK/improbable/c_worker.h>
+
+namespace SpatialGDK
+{
+
+struct AlwaysRelevant : Component
+{
+	static const Worker_ComponentId ComponentId = SpatialConstants::ALWAYS_RELEVANT_COMPONENT_ID;
+
+	AlwaysRelevant() = default;
+	AlwaysRelevant(const Worker_ComponentData& Data)
+	{
+	}
+
+	FORCEINLINE Worker_ComponentData CreateData()
+	{
+		Worker_ComponentData Data = {};
+		Data.component_id = ComponentId;
+		Data.schema_type = Schema_CreateComponentData(ComponentId);
+
+		return Data;
+	}
+};
+
+} // namespace SpatialGDK
