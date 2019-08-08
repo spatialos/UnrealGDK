f167ceb59ae3e5350c8b1c21b0f2e12d7e6b5623
samiwh <sami@improbable.io>
2019-08-08 15:49:13 +0100

    Started changing the dispatcher.

--- SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialDispatcher.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialDispatcher.cpp
@@ -117,6 +117,127 @@ void USpatialDispatcher::ProcessOps(Worker_OpList* OpList)
 	Receiver->FlushRetryRPCs();
 }
 
+void USpatialDispatcher::ProcessOps(const gdk::SpatialOsWorker& Worker)
+{
+	Receiver->OnCriticalSection(true);
+	for (auto& Entity : Worker.GetEntitiesAdded())
+	{
+		Receiver->OnAddEntity(Entity.GetEntityId());
+	}
+	for (auto& EntityId : Worker.GetEntitiesRemoved())
+	{
+		Receiver->OnRemoveEntity(EntityId);
+		StaticComponentView->OnRemoveEntity(EntityId);
+	}
+	for (auto& entityComponent : Worker.GetComponentsAdded(1))
+	{
+		StaticComponentView->OnAddComponent(entityComponent);
+		Receiver->OnAddComponent(entityComponent);
+	}
+	for (auto& entityComponent : Worker.GetComponentsRemoved(1))
+	{
+		Receiver->OnRemoveComponent(entityComponent);
+	}
+	for (auto& entityComponent : Worker.GetComponentUpdates(1))
+	{
+		Receiver->OnComponentUpdate(entityComponent);
+	}
+	for (size_t i = 0; i < Worker->op_count; ++i)
+	{
+		Worker_Op* Op = &Worker->ops[i];
+
+		if (OpsToSkip.Num() != 0 &&
+			OpsToSkip.Contains(Op))
+		{
+			OpsToSkip.Remove(Op);
+			continue;
+		}
+
+		if (IsExternalSchemaOp(Op))
+		{
+			ProcessExternalSchemaOp(Op);
+			continue;
+		}
+
+		switch (Op->op_type)
+		{
+		// Critical Section
+		case WORKER_OP_TYPE_CRITICAL_SECTION:
+			Receiver->OnCriticalSection(Op->critical_section.in_critical_section != 0);
+			break;
+
+		// Entity Lifetime
+		case WORKER_OP_TYPE_ADD_ENTITY:
+			Receiver->OnAddEntity(Op->add_entity);
+			break;
+		case WORKER_OP_TYPE_REMOVE_ENTITY:
+			Receiver->OnRemoveEntity(Op->remove_entity);
+			StaticComponentView->OnRemoveEntity(Op->remove_entity.entity_id);
+			Receiver->RemoveComponentOpsForEntity(Op->remove_entity.entity_id);
+			break;
+
+		// Components
+		case WORKER_OP_TYPE_ADD_COMPONENT:
+			StaticComponentView->OnAddComponent(Op->add_component);
+			Receiver->OnAddComponent(Op->add_component);
+			break;
+		case WORKER_OP_TYPE_REMOVE_COMPONENT:
+			Receiver->OnRemoveComponent(Op->remove_component);
+			break;
+		case WORKER_OP_TYPE_COMPONENT_UPDATE:
+			StaticComponentView->OnComponentUpdate(Op->component_update);
+			Receiver->OnComponentUpdate(Op->component_update);
+			break;
+
+		// Commands
+		case WORKER_OP_TYPE_COMMAND_REQUEST:
+			Receiver->OnCommandRequest(Op->command_request);
+			break;
+		case WORKER_OP_TYPE_COMMAND_RESPONSE:
+			Receiver->OnCommandResponse(Op->command_response);
+			break;
+
+		// Authority Change
+		case WORKER_OP_TYPE_AUTHORITY_CHANGE:
+			Receiver->OnAuthorityChange(Op->authority_change);
+			break;
+
+		// World Command Responses
+		case WORKER_OP_TYPE_RESERVE_ENTITY_IDS_RESPONSE:
+			Receiver->OnReserveEntityIdsResponse(Op->reserve_entity_ids_response);
+			break;
+		case WORKER_OP_TYPE_CREATE_ENTITY_RESPONSE:
+			Receiver->OnCreateEntityResponse(Op->create_entity_response);
+			break;
+		case WORKER_OP_TYPE_DELETE_ENTITY_RESPONSE:
+			break;
+		case WORKER_OP_TYPE_ENTITY_QUERY_RESPONSE:
+			Receiver->OnEntityQueryResponse(Op->entity_query_response);
+			break;
+
+		case WORKER_OP_TYPE_FLAG_UPDATE:
+			USpatialWorkerFlags::ApplyWorkerFlagUpdate(Op->flag_update);
+			break;
+		case WORKER_OP_TYPE_LOG_MESSAGE:
+			UE_LOG(LogSpatialView, Log, TEXT("SpatialOS Worker Log: %s"), UTF8_TO_TCHAR(Op->log_message.message));
+			break;
+		case WORKER_OP_TYPE_METRICS:
+			break;
+
+		case WORKER_OP_TYPE_DISCONNECT:
+			Receiver->OnDisconnect(Op->disconnect);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	Receiver->FlushRemoveComponentOps();
+	Receiver->FlushRetryRPCs();
+
+}
+
 bool USpatialDispatcher::IsExternalSchemaOp(Worker_Op* Op) const
 {
 	Worker_ComponentId ComponentId = SpatialGDK::GetComponentId(Op);
--- SpatialGDK/Source/SpatialGDK/Public/Interop/SpatialDispatcher.h
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/SpatialDispatcher.h
@@ -15,6 +15,10 @@
 
 #include "SpatialDispatcher.generated.h"
 
+namespace gdk {
+	class SpatialOsWorker;
+}   // namespace gdk
+
 DECLARE_LOG_CATEGORY_EXTERN(LogSpatialView, Log, All);
 
 class USpatialMetrics;
@@ -31,7 +35,7 @@ public:
 
 	void Init(USpatialReceiver* InReceiver, USpatialStaticComponentView* InStaticComponentView, USpatialMetrics* InSpatialMetrics);
 	void ProcessOps(Worker_OpList* OpList);
-
+	void ProcessOps(const gdk::SpatialOsWorker& Worker);
 	// The following 2 methods should *only* be used by the Startup OpList Queueing flow
 	// from the SpatialNetDriver, and should be temporary since an alternative solution will be available via the Worker SDK soon.
 	void MarkOpToSkip(const Worker_Op* Op);
