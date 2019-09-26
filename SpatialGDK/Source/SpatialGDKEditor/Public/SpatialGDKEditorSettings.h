d785e96d29ad2f82cc4d39b5748d4fe8fcbd6aa2
Vlad <vladbarvinko@improbable.io>
2019-09-26 10:22:06 +0100

    improve settings tooltip

--- SpatialGDK/Source/SpatialGDKEditor/Public/SpatialGDKEditorSettings.h
+++ SpatialGDK/Source/SpatialGDKEditor/Public/SpatialGDKEditorSettings.h
@@ -261,11 +261,11 @@ private:
 	FFilePath SpatialOSLaunchConfig;
 
 public:
-	/** Expose the runtime on a particular IP address when it is running on this machine. Changes are applied on next deployment startup. */
+	/** Expose the runtime on a particular IP address when it is running on this machine. Changes are applied on next local deployment startup. */
 	UPROPERTY(EditAnywhere, config, Category = "Launch", meta = (ConfigRestartRequired = false, DisplayName = "Expose local runtime"))
 	bool bExposeRuntimeIP;
 
-	/** If the runtime is set to be exposed, specify on which IP address it should be reachable. Changes are applied on next deployment startup. */
+	/** If the runtime is set to be exposed, specify on which IP address it should be reachable. Changes are applied on next local deployment startup. */
 	UPROPERTY(EditAnywhere, config, Category = "Launch", meta = (EditCondition = "bExposeRuntimeIP", ConfigRestartRequired = false, DisplayName = "Exposed local runtime IP address"))
 	FString ExposedRuntimeIP;
 
