635c5c2ebeaf45f269983212f66cbfd0e6950edc
Tilman Schmidt <tilmanschmidt+github@improbable.io>
2019-09-27 16:09:38 +0100

    First test to use setup.bat

--- ci/setup-build-test-gdk.ps1
+++ ci/setup-build-test-gdk.ps1
@@ -17,22 +17,20 @@ Start-Event "setup-gdk" "command"
 &$PSScriptRoot"\setup-gdk.ps1"
 Finish-Event "setup-gdk" "command"
 
-# Build the GDK plugin
-Start-Event "build-gdk" "command"
-&$PSScriptRoot"\build-gdk.ps1" -target_platform $($target_platform)
-Finish-Event "build-gdk" "command"
+# # Build the GDK plugin
+# Start-Event "build-gdk" "command"
+# &$PSScriptRoot"\build-gdk.ps1" -target_platform $($target_platform)
+# Finish-Event "build-gdk" "command"
 
-Write-Log "built plugin is at $($gdk_home)/SpatialGDK/Intermediate/BuildPackage/Win64"
-Write-Log "built plugin folder contents: $(Get-ChildItem $gdk_home/SpatialGDK/Intermediate/BuildPackage/Win64)"
+# Write-Log "built plugin is at $($gdk_home)/SpatialGDK/Intermediate/BuildPackage/Win64"
+# Write-Log "built plugin folder contents: $(Get-ChildItem $gdk_home/SpatialGDK/Intermediate/BuildPackage/Win64)"
 
-$unreal_path = "$($gdk_home)\UnrealEngine"
+# $unreal_path = "$($gdk_home)\UnrealEngine"
 
-Start-Event "setup-tests" "command"
-&$PSScriptRoot"\setup-tests.ps1" -project_clone_path "$($unreal_path)\Samples"
-Finish-Event "setup-tests" "command"
+# Start-Event "setup-tests" "command"
+# &$PSScriptRoot"\setup-tests.ps1" -project_clone_path "$($unreal_path)\Samples"
+# Finish-Event "setup-tests" "command"
 
-tree . /a /f > root_dir.txt
-
-Start-Event "test-gdk" "command"
-&$PSScriptRoot"\run-tests.ps1" -ue_path "$($unreal_path)\Engine\Binaries\Win64\UE4Editor.exe" -uproject_path "$($unreal_path)\Samples\UnrealGDKExampleProject\Game\GDKShooter.uproject" -output_dir "TestResults" -log_file_name "tests.log"
-Finish-Event "test-gdk" "command"
+# Start-Event "test-gdk" "command"
+# &$PSScriptRoot"\run-tests.ps1" -ue_path "$($unreal_path)\Engine\Binaries\Win64\UE4Editor.exe" -uproject_path "$($unreal_path)\Samples\UnrealGDKExampleProject\Game\GDKShooter.uproject" -output_dir "TestResults" -log_file_name "tests.log"
+# Finish-Event "test-gdk" "command"
