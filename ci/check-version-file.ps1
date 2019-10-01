a6d73ccc9a5f7bf1d995dd9a661097b23e3a49dd
Tilman Schmidt <tilmanschmidt+github@improbable.io>
2019-10-01 11:01:31 +0100

    Look for version file in the right directory

--- ci/check-version-file.ps1
+++ ci/check-version-file.ps1
@@ -1,12 +1,15 @@
+# Expects $gdk_home
 param(
     $pinned_branches = @("master", "release", "feature/ci-use-latest-engine-fake-master")
 )
 
-$branch_name = (Get-Item -Path env:BUILDKITE_BRANCH).Value
-if ($pinned_branches.Contains($branch_name)) {
-    $unreal_version = Get-Content -Path "unreal-engine.version" -Raw
+pushd "$($gdk_home)\ci"
+    $branch_name = (Get-Item -Path env:BUILDKITE_BRANCH).Value
+    if ($pinned_branches.Contains($branch_name)) {
+        $unreal_version = Get-Content -Path "unreal-engine.version" -Raw
 
-    if ($unreal_version.StartsWith("HEAD ")) {
-        Throw "Branches $($pinned_branches -join ", ") are required to have a pinned engine version. (I.e. of the form UnrealEngine-<commit hash>)"
+        if ($unreal_version.StartsWith("HEAD ")) {
+            Throw "Branches $($pinned_branches -join ", ") are required to have a pinned engine version. (I.e. of the form UnrealEngine-<commit hash>)"
+        }
     }
-}
+popd
