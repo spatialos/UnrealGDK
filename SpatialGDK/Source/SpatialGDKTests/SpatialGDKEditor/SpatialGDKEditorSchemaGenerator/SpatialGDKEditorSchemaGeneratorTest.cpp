1fc33590dfab64f32bad466c720754cbe8ff09e0
aleximprobable <48994762+aleximprobable@users.noreply.github.com>
2019-10-09 14:43:26 +0100

    Moved SpatialGDKTests folders (#1417)

---
+++ SpatialGDK/Source/SpatialGDKTests/SpatialGDKEditor/SpatialGDKEditorSchemaGenerator/SpatialGDKEditorSchemaGeneratorTest.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "TestDefinitions.h"
+
+#include "CoreMinimal.h"
+
+#define SCHEMA_GENERATOR_TEST(TestName) \
+	TEST(SpatialGDKEditor, SchemaGenerator, TestName)
+
