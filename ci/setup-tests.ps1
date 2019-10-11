a3200a6935c6a8969a2d805cb47b3b53a13ca51a
Miron Zelina <mironzelina@improbable.io>
2019-10-11 10:18:52 +0100

    Rename unreal_home to unreal_path

--- ci/setup-tests.ps1
+++ ci/setup-tests.ps1
@@ -2,8 +2,7 @@
 param(
     [string] $build_output_dir,
     [string] $project_path,
-    [string] $output_dir,
-    [string] $ue_home
+    [string] $unreal_path
 )
 
 # Copy the built files back into the SpatialGDK folder, to have a complete plugin
@@ -19,4 +18,4 @@ New-Item -ItemType Junction -Path "$project_path\Plugins\UnrealGDK" -Target "$gd
 New-Item -Path "$PSScriptRoot" -Name "TestResults" -ItemType "directory" -ErrorAction SilentlyContinue
 
 # Disable tutorials, otherwise the closing of the window will crash the editor due to some graphic context reason
-Add-Content -Path "$ue_home\Engine\Config\BaseEditorSettings.ini" -Value "`r`n[/Script/IntroTutorials.TutorialStateSettings]`r`nTutorialsProgress=(Tutorial=/Engine/Tutorial/Basics/LevelEditorAttract.LevelEditorAttract_C,CurrentStage=0,bUserDismissed=True)`r`n"
+Add-Content -Path "$unreal_path\Engine\Config\BaseEditorSettings.ini" -Value "`r`n[/Script/IntroTutorials.TutorialStateSettings]`r`nTutorialsProgress=(Tutorial=/Engine/Tutorial/Basics/LevelEditorAttract.LevelEditorAttract_C,CurrentStage=0,bUserDismissed=True)`r`n"
