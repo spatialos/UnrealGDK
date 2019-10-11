c9e9a5cbdb168504e904a5ee3fdf4eda6851702a
Miron Zelina <mironzelina@improbable.io>
2019-10-11 14:34:14 +0100

    Hopefully fix error report

--- ci/setup-build-test-gdk.ps1
+++ ci/setup-build-test-gdk.ps1
@@ -36,7 +36,7 @@ if ($target_platform -eq "Win64") {
   Finish-Event "test-gdk" "command"
 
   Start-Event "report-tests" "command"
-  &$PSScriptRoot"\report-tests.ps1"
+  &$PSScriptRoot"\report-tests.ps1" -test_result_dir "$PSScriptRoot\TestResults"
   Finish-Event "report-tests" "command"
 }
 
