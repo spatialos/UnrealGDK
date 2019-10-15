122c65e369abb8574e3f1c412d5078ec7cacd170
Miron Zelina <mironzelina@improbable.io>
2019-10-15 13:37:27 +0100

    Try to fix symlinks

--- ci/setup-tests.ps1
+++ ci/setup-tests.ps1
@@ -12,7 +12,7 @@ Copy-Item -Path "$build_output_dir\*" -Destination "$gdk_home\SpatialGDK\" -Recu
 # The Plugin does not get recognised as an Engine plugin, because we are using a pre-built version of the engine
 # copying the plugin into the project's folder bypasses the issue
 New-Item -Path "$project_path" -Name "Plugins" -ItemType "directory" -ErrorAction SilentlyContinue
-New-Item -ItemType Junction -Path "$project_path\Plugins\UnrealGDK" -Target "$gdk_home" -ErrorAction SilentlyContinue
+New-Item -ItemType Junction -Name "UnrealGDK" -Path "$project_path\Plugins" -Target "$gdk_home"
 
 # Create the TestResults directory if it does not exist, for storing results
 New-Item -Path "$PSScriptRoot" -Name "TestResults" -ItemType "directory" -ErrorAction SilentlyContinue
