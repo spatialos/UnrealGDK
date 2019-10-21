6c0f402ba36500e4142546b49d78ab876d779232
Alex Moroz <oleksandrmoroz@improbable.io>
2019-10-21 14:43:34 +0100

    Updated includes

--- SpatialGDK/Source/SpatialGDKTests/SpatialGDKEditor/SpatialGDKEditorSchemaGenerator/SchemaGenObjectStub.h
+++ SpatialGDK/Source/SpatialGDKTests/SpatialGDKEditor/SpatialGDKEditorSchemaGenerator/SchemaGenObjectStub.h
@@ -1,11 +1,7 @@
 // Copyright (c) Improbable Worlds Ltd, All Rights Reserved
 
 #pragma once
-
-#include "Utils/RPCContainer.h"
-
 #include "CoreMinimal.h"
-
 #include "SchemaGenObjectStub.generated.h"
 
 UCLASS()
