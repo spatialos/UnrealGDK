d901d94e665700596d1a0d301f8b3ea4b2242a27
Vlad <vladbarvinko@improbable.io>
2019-10-25 16:22:11 +0100

    revert deleting

--- ci/cleanup.ps1
+++ ci/cleanup.ps1
@@ -5,12 +5,12 @@ $gdk_in_engine = "$unreal_path\Engine\Plugins\UnrealGDK"
 
 # Clean up the symlinks
 if (Test-Path "$gdk_in_engine") {
-    (Get-Item "$gdk_in_engine").Delete($true)
+    (Get-Item "$gdk_in_engine").Delete()
 }
 if (Test-Path "$unreal_path\Samples\UnrealGDKCITestProject\Plugins\UnrealGDK") {
     (Get-Item "$unreal_path\Samples\UnrealGDKCITestProject\Plugins\UnrealGDK").Delete() # TODO needs to stay in sync with setup-tests
 }
 if (Test-Path "$unreal_path") {
-    (Get-Item "$unreal_path").Delete($true)
+    (Get-Item "$unreal_path").Delete()
 }
 
