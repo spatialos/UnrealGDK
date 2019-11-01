458a1909639f6e6ac04e7ff48d45ab19b4b37910
Alex Moroz <oleksandrmoroz@improbable.io>
2019-11-01 14:28:22 +0000

    Added Testing guidelines as SpatialGDKTestGuidelines.h

---
+++ SpatialGDK/Source/SpatialGDKTests/Examples/SpatialGDKTestGuidelines.h
@@ -0,0 +1,51 @@
+/*
+0. Session Frontend and cmd (how to run tests)
+
+1. Folder structure
+	- Tests are located in a separate SpatialGDKTests module.
+	So for every component that needs to be tested - it has to be exposed via corresponding macro.
+	E.g. `class FRPCContainer` -> `class SPATIALGDK_API FRPCContainer`, to make FRPCContainer testable.
+
+	- The folder structure inside SpatialGDKTests should resemble the folder structure for components being tested.
+	E.g. If file tested is
+	`UnrealGDK\SpatialGDK\Source\SpatialGDK\Public\Utils\RPCContainer.cpp`,
+	then the corresponding test should be located in
+	`UnrealGDK\SpatialGDK\Source\SpatialGDKTests\SpatialGDK\Utils\RPCContainer\RPCContainerTest.cpp`.
+	There should be a folder with the same name as component tested. It should include the test and all the supporting files.
+
+2. Test definitions (check TestDefinitions.h for more info)
+    - We have defined 3 types of Macro to be used when writing tests:
+	(https://docs.unrealengine.com/en-US/Programming/Automation/TechnicalGuide/index.html has more information)
+	TEST
+	COMPLEX_TEST
+	DEFINE_LATENT_COMMAND...
+
+	- There are 5 types of mock objects we can use in tests:
+	Dummy objects
+		are passed around but never actually used. Usually they are just used to fill parameter lists.
+
+	Fake objects
+		actually have working implementations, but usually take some shortcut which makes them not suitable for production (an InMemoryTestDatabase is a good example).
+
+	Stubs
+		provide canned answers to calls made during the test, usually not responding at all to anything outside what's programmed in for the test.
+
+	Spies
+		are stubs that also record some information based on how they were called.
+		One form of this might be an email service that records how many messages it was sent.
+
+	Mocks
+		are pre-programmed with expectations which form a specification of the calls they are expected to receive.
+		They can throw an exception if they receive a call they don't expect and are checked during verification to ensure they got all the calls they were expecting.
+
+3. Test naming convention
+	- `GIVEN_WHEN_THEN` should be used.
+	E.g. `GIVEN_one_and_two_WHEN_summed_THEN_the_sum_is_three`
+	Tests should contain only one TestTrue/TestFalse statement.
+
+5. Test fixtures (to perform the setup and cleanup before and after test correspondingly)
+	- There are no Test Fixtures out of the box in Unreal Automation Testing Framework
+	The solution for now is to instantiate an object at the beginning of test,
+	that sets up the environment in the constructor and cleans it up in the destructor
+	(however, that cannot be used with Latent Commands).
+*/
--- SpatialGDK/Source/SpatialGDKTests/Public/TestDefinitions.h
+++ SpatialGDK/Source/SpatialGDKTests/Public/TestDefinitions.h
@@ -23,21 +23,3 @@
 #define DEFINE_LATENT_COMMAND_TWO_PARAMETERS(CommandName, ParamType1, Param1, ParamType2, Param2) \
 	DEFINE_LATENT_AUTOMATION_COMMAND_TWO_PARAMETER(CommandName, ParamType1, Param1, ParamType2, Param2); \
 	bool CommandName::Update()
-/*
-Dummy objects
-	are passed around but never actually used. Usually they are just used to fill parameter lists.
-
-Fake objects
-	actually have working implementations, but usually take some shortcut which makes them not suitable for production (an InMemoryTestDatabase is a good example).
-
-Stubs
-	provide canned answers to calls made during the test, usually not responding at all to anything outside what's programmed in for the test.
-
-Spies
-	are stubs that also record some information based on how they were called.
-	One form of this might be an email service that records how many messages it was sent.
-
-Mocks
-	are pre-programmed with expectations which form a specification of the calls they are expected to receive.
-	They can throw an exception if they receive a call they don't expect and are checked during verification to ensure they got all the calls they were expecting.
-*/
