7b52e24074fd0c6fb7c0e5f3dad0d7024e0715cf
cmsmithio <christophersmith@improbable.io>
2019-11-04 06:35:23 -0700

    Feature/unr 2034 spatial debugger (#1421)
    * Add raw textures
    
    * Allow SpatialGDK to contain Content
    
    * Add delegates for OnEntityAdded/Removed
    
    * Add ability to retrieve current list of known entity ids
    
    * Add first pass SpatialDebugger
    
    * Sort actors once per tick by range to PlayerPawn
    
    * Pass console commands to SpatialDebugger
    
    * add icon textures
    
    * Reference SpatialDebuggerClass from SpatialGDKSettings
    
    * Add blueprint-side BP_SpatialDebugger
    
    * - Fix bug with lock icon
    
    - Remove stacking for now
    - Cleanup spacing in the header
    
    * Variable horizontal offsets for icons/text depending on which ones are enabled
    
    *  - Remove some magic numbers
    
     - Expose WorldSpaceActorTagOffset via UPROPERTY
    
    *  - Rename constant to match it's now more restricted usage
    
    * Moving away from member initializer list toward more modern initialization style.  Also, UObjects are zero'd by default, so we only need to set deltas from that
    
    * supertick
    
    * bunch more little fixes
    
    * Switch to using FSoftClassPath for SpatialDebuggerClassPath as a default can then be provided
    
    * Don't render tags that project to 0,0
    
    * Set defaults up correctly so we aren't rendering data that isn't available yet
    
    * final cleanup pass
    
    * Naming consistency for the delegates in SpatialReceiver
    
    * Add comment to make it clear what this method is for
    
    * Linux should compile too
    
    * Add placeholder changelog entry
    
    * Add box icon
    
    * Reference new box texture asset
    
    * Add box texture and address review feedback
    
    * Address review feedback
    
    * Enable SchemaGen for types in non-Game (ex. Plugin) folders
    
    * Addressing PR feedback

--- SpatialGDK/Source/SpatialGDK/Private/SpatialGDKSettings.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/SpatialGDKSettings.cpp
@@ -40,6 +40,7 @@ USpatialGDKSettings::USpatialGDKSettings(const FObjectInitializer& ObjectInitial
 	, bEnableOffloading(false)
 	, ServerWorkerTypes({ SpatialConstants::DefaultServerWorkerType })
 	, WorkerLogLevel(ESettingsWorkerLogVerbosity::Warning)
+	, SpatialDebuggerClassPath(TEXT("/SpatialGDK/SpatialDebugger/BP_SpatialDebugger.BP_SpatialDebugger_C"))
 {
 	DefaultReceptionistHost = SpatialConstants::LOCAL_HOST;
 }
