5027f5ad35de12b3e22f88c20cf81b62a9089e48
Joshua Huburn <joshuahuburn@improbable.io>
2019-11-06 16:33:17 +0000

    Disable deprecated warnings in NetSerializeStruct

--- SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialFastArrayNetSerialize.h
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialFastArrayNetSerialize.h
@@ -12,6 +12,7 @@ class USpatialNetDriver;
 
 namespace SpatialGDK
 {
+PRAGMA_DISABLE_DEPRECATION_WARNINGS // TODO: UNR-2371 - Remove when we update our usage of FNetDeltaSerializeInfo
 
 class SpatialFastArrayNetSerializeCB : public INetSerializeCB
 {
@@ -19,10 +20,9 @@ public:
 	SpatialFastArrayNetSerializeCB(USpatialNetDriver* InNetDriver)
 		: NetDriver(InNetDriver)
 	{ }
-
 	virtual void NetSerializeStruct(UScriptStruct* Struct, FBitArchive& Ar, UPackageMap* PackageMap, void* Data, bool& bHasUnmapped);
 #if ENGINE_MINOR_VERSION >= 23
-	//TODO (UNR-2371): Look at whether we need to implement these - added check(false)s here to guarantee no entry
+	//TODO: UNR-2371 - Look at whether we need to implement these and implement 'NetSerializeStruct(FNetDeltaSerializeInfo& Params)'.
 	virtual void NetSerializeStruct(FNetDeltaSerializeInfo& Params) { checkf(false, TEXT("The GDK does not support the new version of NetSerializeStruct yet.")); };
 
 	virtual void GatherGuidReferencesForFastArray(struct FFastArrayDeltaSerializeParams& Params) { checkf(false, TEXT("GatherGuidReferencesForFastArray called - the GDK currently does not support delta serialization of structs within fast arrays.")); };
@@ -45,5 +45,7 @@ struct FSpatialNetDeltaSerializeInfo : FNetDeltaSerializeInfo
 	static bool DeltaSerializeRead(USpatialNetDriver* NetDriver, FSpatialNetBitReader& Reader, UObject* Object, int32 ArrayIndex, UProperty* ParentProperty, UScriptStruct* NetDeltaStruct);
 	static bool DeltaSerializeWrite(USpatialNetDriver* NetDriver, FSpatialNetBitWriter& Writer, UObject* Object, int32 ArrayIndex, UProperty* ParentProperty, UScriptStruct* NetDeltaStruct);
 };
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS // TODO: UNR-2371 - Remove when we update our usage of FNetDeltaSerializeInfo
 
 } // namespace SpatialGDK
