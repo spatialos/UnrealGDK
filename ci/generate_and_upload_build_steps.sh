c4f75cc5617f87b47c4edf4adadabb4487eecca7
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-06 14:20:49 +0000

    test new engine version

--- ci/generate_and_upload_build_steps.sh
+++ ci/generate_and_upload_build_steps.sh
@@ -2,7 +2,4 @@
 set -euo pipefail
 
 cat ci/inserted_steps.yaml >> ci/stage_2_premerge.steps.yaml
-
-buildkite-agent artifact upload ci/stage_2_premerge.steps.yaml
-
 buildkite-agent pipeline upload ci/stage_2_premerge.steps.yaml
