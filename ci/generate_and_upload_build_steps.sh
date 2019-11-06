039ced73d900dac45a5149cf001e5c6143e9abc2
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-06 14:14:38 +0000

    fix file appending

--- ci/generate_and_upload_build_steps.sh
+++ ci/generate_and_upload_build_steps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-cat ci/inserted_steps.yaml >> ci/stage_2_premerge.steps
+cat ci/inserted_steps.yaml >> ci/stage_2_premerge.steps.yaml
 
-buildkite-agent artifact upload ci/stage_2_premerge.steps
+buildkite-agent artifact upload ci/stage_2_premerge.steps.yaml
 
 buildkite-agent pipeline upload ci/stage_2_premerge.steps.yaml
