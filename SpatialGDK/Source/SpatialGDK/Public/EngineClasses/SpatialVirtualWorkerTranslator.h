3ef37865dc5bc2d67f2c43a4d11fe42a2fb73dbf
Jennifer Harkness <jennifer@improbable.io>
2019-11-06 10:03:06 +0000

    Update the translator to query for connection entities

--- SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialVirtualWorkerTranslator.h
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialVirtualWorkerTranslator.h
@@ -21,22 +21,49 @@ public:
 
 	void Init(USpatialNetDriver* InNetDriver);
 
+	// Returns true if the Translator has received the information needed to map virtual workers to physical workers.
+	// Currently that is only the number of virtual workers desired.
+	bool IsReady() const { return bIsReady; }
+
+	void SetDesiredVirtualWorkerCount(uint32 NumberOfVirtualWorkers);
+
 	// Returns the name of the worker currently assigned to VirtualWorkerId id or nullptr if there is
 	// no worker assigned.
 	// TODO(harkness): Do we want to copy this data? Otherwise it's only guaranteed to be valid until
 	// the next mapping update.
 	const FString* GetPhysicalWorkerForVirtualWorker(VirtualWorkerId id);
 
-	// On receiving an update to the translation state, apply that to the internal mapping.
-	void ApplyVirtualWorkerManagerData(const Worker_ComponentData& Data);
+	// On receiving an version of the translation state, apply that to the internal mapping.
+	void ApplyVirtualWorkerManagerData(Schema_Object* ComponentObject);
 
 	void OnComponentUpdated(const Worker_ComponentUpdateOp& Op);
+
+	// Authority may change on one of two components we care about:
+	// 1) The translation component, in which case this worker is now authoritative on the virtual to physical worker translation.
+	// 2) The ACL component for some entity, in which case this worker is now authoritative for the entity and will be
+	// responsible for updating the ACL in the future if this worker loses authority.
+	void AuthorityChanged(const Worker_AuthorityChangeOp& AuthChangeOp);
 
 private:
 	USpatialNetDriver* NetDriver;
 
 	TMap<VirtualWorkerId, PhysicalWorkerName>  VirtualToPhysicalWorkerMapping;
+	TQueue<VirtualWorkerId> UnassignedVirtualWorkers;
+
+	bool bWorkerEntityQueryInFlight;
+
+	bool bIsReady;
+	uint32 DesiredVirtualWorkerCount;
+
+	// The WorkerId of this worker, for logging purposes.
+	FString WorkerId;
 
 	void ApplyMappingFromSchema(Schema_Object* Object);
 	void WriteMappingToSchema(Schema_Object* Object);
-}; 
+
+	void QueryForWorkerEntities();
+	void ConstructVirtualWorkerMappingFromQueryResponse(const Worker_EntityQueryResponseOp& Op);
+	void SendVirtualWorkerMappingUpdate();
+
+	void AssignWorker(const FString& WorkerId);
+};
