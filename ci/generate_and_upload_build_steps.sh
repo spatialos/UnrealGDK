66f0a86f743b8f38792c660837cd87e65296bccb
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-06 15:25:10 +0000

    add engine hash substitution

--- ci/generate_and_upload_build_steps.sh
+++ ci/generate_and_upload_build_steps.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-cat ci/inserted_steps.yaml >> ci/stage_2_premerge.steps.yaml
-buildkite-agent pipeline upload ci/stage_2_premerge.steps.yaml
+IFS=$'\n'
+for commit_hash in $(cat < ci/unreal-engine.version); do
+    echo "hash: $commit_hash"
+    echo "s/INSERT_ENGINE_COMMIT_HASH/$commit_hash/g"
+done
+
+for commit_hash in $(cat < ci/unreal-engine.version); do
+    sed "s/INSERT_ENGINE_COMMIT_HASH/$commit_hash/g" ci/generated_steps.steps.yaml >> ci/generated_base.steps.yaml
+done
+
+buildkite-agent pipeline upload ci/generated_base.steps.yaml
