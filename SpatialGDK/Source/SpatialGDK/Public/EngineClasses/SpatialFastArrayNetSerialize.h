316ea4f86251c4eacf221d592521a64e3530a1a0
Joshua Huburn <31517089+joshuahuburn@users.noreply.github.com>
2019-11-07 23:13:18 +0000

    [UNR-1997] 4.23.1 Upgrade (#1454)
    * Merge changes from 4.22 master merge with 4.23
    
    * Fix new problems from merge
    
    * Update engine version
    
    * Add guards around new GDK code
    
    * Add missed guards
    
    * Add replacement for InitShadowData, fix crash on close
    
    * Add missing guard and hopefully enable 4.22 build
    
    * Remove RepLayout hack in favor of an Epic-provided workaround
    
    * Fix compile errors on 4.22
    
    * Refactorings
    
    * Refactoring and updated engine version
    
    * Use FNameLexicalLess for sorting, add no entry checks to fast array serialization helpers
    
    * Added ShadowData ticket and changed FRepHandleIterator to RepLayout Owner
    
    * Disable new version of NetSerializeStruct, add ticket TODO
    
    * Add ticket to track bMaySendProperties workaround in SpatialNetDriver
    
    * Reduce changeset in TypeStructure.cpp
    
    * Address PR feedback - update changelog, cosmetic changes to code
    
    * Disable deprecated warnings in NetSerializeStruct
    
    * Added overrides
    
    * Remove the need for SpatialNetworking when running Schema Generation
    
    * Add guards around the need for spatial networking in schema gen
    
    * Enable CI against 4.22
    
    * Update SPATIAL_GDK_VERSION
    
    * Update engine version file for 4.23

--- SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialFastArrayNetSerialize.h
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialFastArrayNetSerialize.h
@@ -12,6 +12,7 @@ class USpatialNetDriver;
 
 namespace SpatialGDK
 {
+PRAGMA_DISABLE_DEPRECATION_WARNINGS // TODO: UNR-2371 - Remove when we update our usage of FNetDeltaSerializeInfo
 
 class SpatialFastArrayNetSerializeCB : public INetSerializeCB
 {
@@ -19,8 +20,16 @@ public:
 	SpatialFastArrayNetSerializeCB(USpatialNetDriver* InNetDriver)
 		: NetDriver(InNetDriver)
 	{ }
+	virtual void NetSerializeStruct(UScriptStruct* Struct, FBitArchive& Ar, UPackageMap* PackageMap, void* Data, bool& bHasUnmapped) override;
+#if ENGINE_MINOR_VERSION >= 23
+	//TODO: UNR-2371 - Look at whether we need to implement these and implement 'NetSerializeStruct(FNetDeltaSerializeInfo& Params)'.
+	virtual void NetSerializeStruct(FNetDeltaSerializeInfo& Params) override { checkf(false, TEXT("The GDK does not support the new version of NetSerializeStruct yet.")); };
 
-	virtual void NetSerializeStruct(UScriptStruct* Struct, FBitArchive& Ar, UPackageMap* PackageMap, void* Data, bool& bHasUnmapped);
+	virtual void GatherGuidReferencesForFastArray(struct FFastArrayDeltaSerializeParams& Params) override { checkf(false, TEXT("GatherGuidReferencesForFastArray called - the GDK currently does not support delta serialization of structs within fast arrays.")); };
+	virtual bool MoveGuidToUnmappedForFastArray(struct FFastArrayDeltaSerializeParams& Params) override { checkf(false, TEXT("MoveGuidToUnmappedForFastArray called - the GDK currently does not support delta serialization of structs within fast arrays.")); return false; };
+	virtual void UpdateUnmappedGuidsForFastArray(struct FFastArrayDeltaSerializeParams& Params) override { checkf(false, TEXT("UpdateUnmappedGuidsForFastArray called - the GDK currently does not support delta serialization of structs within fast arrays.")); };
+	virtual bool NetDeltaSerializeForFastArray(struct FFastArrayDeltaSerializeParams& Params) override { checkf(false, TEXT("NetDeltaSerializeForFastArray called - the GDK currently does not support delta serialization of structs within fast arrays.")); return false; };
+#endif
 
 private:
 	USpatialNetDriver* NetDriver;
@@ -36,5 +45,7 @@ struct FSpatialNetDeltaSerializeInfo : FNetDeltaSerializeInfo
 	static bool DeltaSerializeRead(USpatialNetDriver* NetDriver, FSpatialNetBitReader& Reader, UObject* Object, int32 ArrayIndex, UProperty* ParentProperty, UScriptStruct* NetDeltaStruct);
 	static bool DeltaSerializeWrite(USpatialNetDriver* NetDriver, FSpatialNetBitWriter& Writer, UObject* Object, int32 ArrayIndex, UProperty* ParentProperty, UScriptStruct* NetDeltaStruct);
 };
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS // TODO: UNR-2371 - Remove when we update our usage of FNetDeltaSerializeInfo
 
 } // namespace SpatialGDK
