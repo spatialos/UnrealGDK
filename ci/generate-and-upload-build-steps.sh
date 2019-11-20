f3ad01546d2447bf96a0bc9e38882acde43453b1
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-20 11:19:04 +0000

    generate steps for build configurations

--- ci/generate-and-upload-build-steps.sh
+++ ci/generate-and-upload-build-steps.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-function Generate-Build-Configuration-Steps {
-    param( $engine_commit_hash )
+generate_build_configuration_steps () {
     if [ -z "${NIGHTLY_BUILD}" ]; then 
         echo "This is a nightly build. Generating the appropriate steps..."
         for build_target_suffix in "" "Editor" "Server" "SimulatedPlayer"; do
             for build_state in "DebugGame" "Development" "Shipping" "Test"; do
                 cat "ci/gdk_build.template.steps.yaml" | \
-                sed "s|ENGINE_COMMIT_HASH_PLACEHOLDER|$engine_commit_hash|g" | \
+                sed "s|ENGINE_COMMIT_HASH_PLACEHOLDER|$1|g" | \
                 sed "s|BUILD_TARGET_SUFFIX_PLACEHOLDER|$build_target_suffix|g" | \
                 sed "s|BUILD_STATE_PLACEHOLDER|$build_state|g" | \
                 buildkite-agent pipeline upload
@@ -17,7 +16,7 @@ function Generate-Build-Configuration-Steps {
     else
         echo "This is not a nightly build. Generating the appropriate steps..."
         cat "ci/gdk_build.template.steps.yaml" | \
-        sed "s|ENGINE_COMMIT_HASH_PLACEHOLDER|$engine_commit_hash|g" | \
+        sed "s|ENGINE_COMMIT_HASH_PLACEHOLDER|$1|g" | \
         sed "s|BUILD_TARGET_SUFFIX_PLACEHOLDER|Editor|g" | \
         sed "s|BUILD_STATE_PLACEHOLDER|Development|g" | \
         buildkite-agent pipeline upload
@@ -29,9 +28,9 @@ if [ -z "${ENGINE_VERSION}" ]; then
     echo "Generating build steps for each engine version listed in unreal-engine.version"  
     IFS=$'\n'
     for commit_hash in $(cat < ci/unreal-engine.version); do
-        Generate-Build-Configuration-Steps -engine_commit_hash "$commit_hash"
+        Generate-Build-Configuration-Steps "$commit_hash"
     done
 else
     echo "Generating steps for the specified engine version: $ENGINE_VERSION" 
-    Generate-Build-Configuration-Steps -engine_commit_hash "$ENGINE_VERSION"
+    Generate-Build-Configuration-Steps "$ENGINE_VERSION"
 fi;
