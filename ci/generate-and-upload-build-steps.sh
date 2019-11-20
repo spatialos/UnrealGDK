9edbf72d79e654ba7592c27140b89d91249f7c87
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-20 11:11:56 +0000

    generate steps for build configurations

--- ci/generate-and-upload-build-steps.sh
+++ ci/generate-and-upload-build-steps.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 set -euo pipefail
 
+function Generate-Build-Configuration-Steps {
+    param( $engine_commit_hash )
+    if [ -z "${NIGHTLY_BUILD}" ]; then 
+        echo "This is a nightly build. Generating the appropriate steps..."
+        for build_target_suffix in "" "Editor" "Server" "SimulatedPlayer"; do
+            for build_state in "DebugGame" "Development" "Shipping" "Test"; do
+                cat "ci/gdk_build.template.steps.yaml" | \
+                sed "s|ENGINE_COMMIT_HASH_PLACEHOLDER|$engine_commit_hash|g" | \
+                sed "s|BUILD_TARGET_SUFFIX_PLACEHOLDER|$build_target_suffix|g" | \
+                sed "s|BUILD_STATE_PLACEHOLDER|$build_state|g" | \
+                buildkite-agent pipeline upload
+            done
+        done
+    else
+        echo "This is not a nightly build. Generating the appropriate steps..."
+        cat "ci/gdk_build.template.steps.yaml" | \
+        sed "s|ENGINE_COMMIT_HASH_PLACEHOLDER|$engine_commit_hash|g" | \
+        sed "s|BUILD_TARGET_SUFFIX_PLACEHOLDER|Editor|g" | \
+        sed "s|BUILD_STATE_PLACEHOLDER|Development|g" | \
+        buildkite-agent pipeline upload
+    fi;
+}
+
 # This script generates steps for each engine version listed in unreal-engine.version, based on the gdk_build.template.steps.yaml template
 if [ -z "${ENGINE_VERSION}" ]; then 
     echo "Generating build steps for each engine version listed in unreal-engine.version"  
     IFS=$'\n'
     for commit_hash in $(cat < ci/unreal-engine.version); do
-        sed "s|ENGINE_COMMIT_HASH_PLACEHOLDER|$commit_hash|g" ci/gdk_build.template.steps.yaml | buildkite-agent pipeline upload
+        Generate-Build-Configuration-Steps -engine_commit_hash "$commit_hash"
     done
 else
     echo "Generating steps for the specified engine version: $ENGINE_VERSION" 
-    sed "s|ENGINE_COMMIT_HASH_PLACEHOLDER|$ENGINE_VERSION|g" ci/gdk_build.template.steps.yaml | buildkite-agent pipeline upload
+    Generate-Build-Configuration-Steps -engine_commit_hash "$ENGINE_VERSION"
 fi;
