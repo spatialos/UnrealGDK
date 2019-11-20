a07984867ff1a5f324abdcc1737cc301a6af57a1
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-20 11:37:07 +0000

    fix empty variable checks

--- ci/generate-and-upload-build-steps.sh
+++ ci/generate-and-upload-build-steps.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 generate_build_configuration_steps () {
-    if [ -z "${NIGHTLY_BUILD}" ]; then 
+    if [ -z ${NIGHTLY_BUILD+x} ]; then 
         echo "This is a nightly build. Generating the appropriate steps..."
         for build_target_suffix in "" "Editor" "Server" "SimulatedPlayer"; do
             for build_state in "DebugGame" "Development" "Shipping" "Test"; do
@@ -24,7 +24,7 @@ generate_build_configuration_steps () {
 }
 
 # This script generates steps for each engine version listed in unreal-engine.version, based on the gdk_build.template.steps.yaml template
-if [ -z "${ENGINE_VERSION}" ]; then 
+if [ -z ${ENGINE_VERSION+x} ]; then 
     echo "Generating build steps for each engine version listed in unreal-engine.version"  
     IFS=$'\n'
     for commit_hash in $(cat < ci/unreal-engine.version); do
