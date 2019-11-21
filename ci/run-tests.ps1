3a545c1498ea011d3a8c1305c2e35e2ce6a90030
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-21 17:38:09 +0000

    change command order

--- ci/run-tests.ps1
+++ ci/run-tests.ps1
@@ -36,7 +36,7 @@ $cmd_args_list = @( `
 
 Write-Log "Running $($ue_path_absolute) $($cmd_args_list)"
 
-$run_tests_proc = Start-Process -PassThru -Wait -NoNewWindow $ue_path_absolute -ArgumentList $cmd_args_list
+$run_tests_proc = Start-Process $ue_path_absolute -PassThru -Wait -NoNewWindow -ArgumentList $cmd_args_list
 Wait-Process -Id (Get-Process -InputObject $run_tests_proc).id
 If ($run_tests_proc.ExitCode -ne 0) {
     throw "Failed to run tests."
