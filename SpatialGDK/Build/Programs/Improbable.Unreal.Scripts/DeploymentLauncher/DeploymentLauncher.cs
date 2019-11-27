88cf1271c0ef8b64f175d20c9dd793f212f41717
Danny Birch <danielbirch@improbable.io>
2019-11-27 14:09:34 +0000

    Update SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/DeploymentLauncher/DeploymentLauncher.cs
    Co-Authored-By: Michael Samiec <michaelsamiec@improbable.io>

--- SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/DeploymentLauncher/DeploymentLauncher.cs
+++ SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/DeploymentLauncher/DeploymentLauncher.cs
@@ -78,6 +78,7 @@ namespace Improbable
 
             return confirmUploadResponse.Snapshot.Id;
         }
+        
         private static PlatformApiEndpoint GetApiEndpoint(string region)
         {
             if (region == "CN")
