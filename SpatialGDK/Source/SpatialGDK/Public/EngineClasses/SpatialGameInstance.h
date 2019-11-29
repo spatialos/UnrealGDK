6c8ba6807bfebd75c4386ee5aacdaa848136ce5a
Michael Samiec <michaelsamiec@improbable.io>
2019-11-29 17:52:49 +0000

    Merge resolution

--- SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialGameInstance.h
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialGameInstance.h
@@ -34,9 +34,6 @@ public:
 	virtual void Init() override;
 	//~ End UGameInstance Interface
 
-	// bResponsibleForSnapshotLoading exists to have persistent knowledge if this worker has authority over the GSM during ServerTravel.
-	bool bResponsibleForSnapshotLoading = false;
-
 	// The SpatialWorkerConnection must always be owned by the SpatialGameInstance and so must be created here to prevent TrimMemory from deleting it during Browse.
 	void CreateNewSpatialWorkerConnection();
 
