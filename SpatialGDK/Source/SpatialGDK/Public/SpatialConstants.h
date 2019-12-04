063c4e0cef082265bdafcdb9b9383c082597eec6
Danny Birch <57946331+improbable-danny@users.noreply.github.com>
2019-12-04 10:20:26 +0000

    Update worker SDK ports (#1549)

--- SpatialGDK/Source/SpatialGDK/Public/SpatialConstants.h
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialConstants.h
@@ -199,7 +199,7 @@ namespace SpatialConstants
 	const FString SPATIALOS_METRICS_DYNAMIC_FPS = TEXT("Dynamic.FPS");
 
 	const FString LOCATOR_HOST = TEXT("locator.improbable.io");
-	const uint16 LOCATOR_PORT = 444;
+	const uint16 LOCATOR_PORT = 443;
 
 	const FString DEVELOPMENT_AUTH_PLAYER_ID = TEXT("Player Id");
 }
