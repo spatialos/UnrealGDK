25de41c83c7e822252f2dd717f59d84738471212
Joshua Huburn <31517089+joshuahuburn@users.noreply.github.com>
2019-12-06 14:55:10 +0000

    Update SpatialGDK/Source/SpatialGDKServices/Private/LocalDeploymentManager.cpp

--- SpatialGDK/Source/SpatialGDKServices/Private/LocalDeploymentManager.cpp
+++ SpatialGDK/Source/SpatialGDKServices/Private/LocalDeploymentManager.cpp
@@ -14,7 +14,6 @@
 
 DEFINE_LOG_CATEGORY(LogSpatialDeploymentManager);
 
-static const FString SpatialExe(TEXT("spatial.exe"));
 static const FString SpatialServiceVersion(TEXT("20191128.003423.475a3c1edb"));
 
 namespace
