68871e467277e0eee12117624deb057c1381fdaf
Joshua Huburn <joshuahuburn@improbable.io>
2019-12-07 19:12:41 +0000

    Fix build

--- SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorCloudLauncher.cpp
+++ SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorCloudLauncher.cpp
@@ -59,6 +59,8 @@ bool SpatialGDKCloudStop()
 	UE_LOG(LogSpatialGDKEditorCloudLauncher, Error, TEXT("Function not available"));
 	return false;
 
+	const USpatialGDKEditorSettings* SpatialGDKSettings = GetDefault<USpatialGDKEditorSettings>();
+
 	// TODO: UNR-2435 - Add a Stop Deployment button and fix the code below:
 	// get and provide deployment-id to stop the deployment as one of the LauncherStopArguments
 	const FString LauncherStopArguments = FString::Printf(
