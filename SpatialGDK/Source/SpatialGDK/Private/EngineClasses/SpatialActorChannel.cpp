e1b3db46902fc52eb803682d8fe7b2938720ca99
samiwh <sami@improbable.io>
2019-12-11 14:42:19 +0000

    Actor channel now checks for the authority intent as well as authority.

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialActorChannel.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialActorChannel.cpp
@@ -562,7 +562,8 @@ int64 USpatialActorChannel::ReplicateActor()
 		NetDriver->LoadBalanceStrategy != nullptr &&
 		// TODO: the 'bWroteSomethingImportant' check causes problems for actors that need to transition in groups (ex. Character, PlayerController, PlayerState),
 		// so disabling it for now.  Figure out a way to deal with this to recover the perf lost by calling ShouldChangeAuthority() frequently. [UNR-2387]
-		Actor->HasAuthority() &&
+		NetDriver->StaticComponentView->HasAuthority(EntityId, SpatialConstants::AUTHORITY_INTENT_COMPONENT_ID) &&
+		!NetDriver->LockingPolicy->IsLocked(Actor) &&
 		NetDriver->LoadBalanceStrategy->ShouldRelinquishAuthority(*Actor))
 	{
 		const VirtualWorkerId NewAuthVirtualWorkerId = NetDriver->LoadBalanceStrategy->WhoShouldHaveAuthority(*Actor);
