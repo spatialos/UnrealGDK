f9e6ae007a6a4bedf82fc125742d1a8dfffc5ff0
Ally <alastairdglennie@gmail.com>
2019-12-16 15:24:52 +0000

    Update SpatialGDK/Source/SpatialGDK/Public/LoadBalancing/AbstractLockingPolicy.h
    Co-Authored-By: Tencho Tenev <tencho@improbable.io>

--- SpatialGDK/Source/SpatialGDK/Public/LoadBalancing/AbstractLockingPolicy.h
+++ SpatialGDK/Source/SpatialGDK/Public/LoadBalancing/AbstractLockingPolicy.h
@@ -13,6 +13,6 @@ class SPATIALGDK_API UAbstractLockingPolicy : public UObject
 
 public:
 	virtual ActorLockToken AcquireLock(AActor* Actor, FString LockName = "") PURE_VIRTUAL(UAbstractLockingPolicy::AcquireLock, return SpatialConstants::INVALID_ACTOR_LOCK_TOKEN;);
-	virtual void ReleaseLock(ActorLockToken) PURE_VIRTUAL(UAbstractLockingPolicy::ReleaseLock, return;);
+	virtual void ReleaseLock(ActorLockToken Token) PURE_VIRTUAL(UAbstractLockingPolicy::ReleaseLock, return;);
 	virtual bool IsLocked(const AActor* Actor) const PURE_VIRTUAL(UAbstractLockingPolicy::IsLocked, return false;);
 };
