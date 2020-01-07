bce0be050180aed5d3766ec971422c5155a828f9
Victor Buldakov <victorbuldakov@improbable.io>
2020-01-07 13:18:54 +0000

    UNR-2485: update ci scripts for the networking tests

--- ci/setup-build-test-gdk.ps1
+++ ci/setup-build-test-gdk.ps1
@@ -3,11 +3,12 @@ param(
   [string] $gcs_publish_bucket = "io-internal-infra-unreal-artifacts-production/UnrealEngine",
   [string] $msbuild_exe = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe",
   [string] $build_home = (Get-Item "$($PSScriptRoot)").parent.parent.FullName, ## The root of the entire build. Should ultimately resolve to "C:\b\<number>\".
-  [string] $unreal_path = "$build_home\UnrealEngine",
-  [string] $test_repo_branch = "master",
-  [string] $test_repo_url = "https://github.com/spatialos/UnrealGDKTestGyms.git",
-  [string] $test_repo_relative_uproject_path = "Game\GDKTestGyms.uproject",
-  [string] $test_repo_map = "EmptyGym"
+  [string] $unreal_path = "$build_home\UnrealEngine"
+)
+
+$tests = @(
+  ("https://github.com/spatialos/UnrealGDKTestGyms.git", "master", "Game\GDKTestGyms.uproject", "EmptyGym"),
+  ("https://github.com/improbable/UnrealGDKEngineNetTest.git", "master", "Game\EngineNetTest.uproject", "NetworkingMap")
 )
 
 # Allow overriding testing branch via environment variable
@@ -30,33 +31,42 @@ Start-Event "setup-gdk" "command"
 & $PSScriptRoot"\setup-gdk.ps1" -gdk_path "$gdk_in_engine" -msbuild_path "$msbuild_exe"
 Finish-Event "setup-gdk" "command"
 
-# Build the testing project
-Start-Event "build-project" "command"
-& $PSScriptRoot"\build-project.ps1" `
-    -unreal_path "$unreal_path" `
-    -test_repo_branch "$test_repo_branch" `
-    -test_repo_url "$test_repo_url" `
-    -test_repo_uproject_path "$build_home\TestProject\$test_repo_relative_uproject_path" `
-    -test_repo_path "$build_home\TestProject" `
-    -msbuild_exe "$msbuild_exe" `
-    -gdk_home "$gdk_home" `
-    -build_platform "$env:BUILD_PLATFORM" `
-    -build_state "$env:BUILD_STATE" `
-    -build_target "$env:BUILD_TARGET"
-Finish-Event "build-project" "command"
+foreach ($test in $tests) {
+  
+  $test_repo_url = $test[0]
+  $test_repo_branch = $test[1]
+  $test_repo_relative_uproject_path = $test[2]
+  $test_repo_map = $test[3]
 
-# Only run tests on Windows, as we do not have a linux agent - should not matter
-if ($env:BUILD_PLATFORM -eq "Win64" -And $env:BUILD_TARGET -eq "Editor" -And $env:BUILD_STATE -eq "Development") {
-  Start-Event "test-gdk" "command"
-  & $PSScriptRoot"\run-tests.ps1" `
-      -unreal_editor_path "$unreal_path\Engine\Binaries\Win64\UE4Editor.exe" `
-      -uproject_path "$build_home\TestProject\$test_repo_relative_uproject_path" `
+  # Build the testing project
+  Start-Event "build-project" "command"
+  & $PSScriptRoot"\build-project.ps1" `
+      -unreal_path "$unreal_path" `
+      -test_repo_branch "$test_repo_branch" `
+      -test_repo_url "$test_repo_url" `
+      -test_repo_uproject_path "$build_home\TestProject\$test_repo_relative_uproject_path" `
       -test_repo_path "$build_home\TestProject" `
-      -log_file_path "$PSScriptRoot\TestResults\tests.log" `
-      -test_repo_map "$test_repo_map"
-  Finish-Event "test-gdk" "command"
+      -msbuild_exe "$msbuild_exe" `
+      -gdk_home "$gdk_home" `
+      -build_platform "$env:BUILD_PLATFORM" `
+      -build_state "$env:BUILD_STATE" `
+      -build_target "$env:BUILD_TARGET"
+  Finish-Event "build-project" "command"
 
-  Start-Event "report-tests" "command"
-  & $PSScriptRoot"\report-tests.ps1" -test_result_dir "$PSScriptRoot\TestResults" -target_platform "$env:BUILD_PLATFORM"
-  Finish-Event "report-tests" "command"
+  # Only run tests on Windows, as we do not have a linux agent - should not matter
+  if ($env:BUILD_PLATFORM -eq "Win64" -And $env:BUILD_TARGET -eq "Editor" -And $env:BUILD_STATE -eq "Development") {
+    Start-Event "test-gdk" "command"
+    & $PSScriptRoot"\run-tests.ps1" `
+        -unreal_editor_path "$unreal_path\Engine\Binaries\Win64\UE4Editor.exe" `
+        -uproject_path "$build_home\TestProject\$test_repo_relative_uproject_path" `
+        -test_repo_path "$build_home\TestProject" `
+        -log_file_path "$PSScriptRoot\TestResults\tests.log" `
+        -test_repo_map "$test_repo_map"
+    Finish-Event "test-gdk" "command"
+
+    Start-Event "report-tests" "command"
+    & $PSScriptRoot"\report-tests.ps1" -test_result_dir "$PSScriptRoot\TestResults" -target_platform "$env:BUILD_PLATFORM"
+    Finish-Event "report-tests" "command"
+  }
 }
+
