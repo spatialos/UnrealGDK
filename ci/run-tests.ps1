c4b23e81401d8fce6d5cb7301f0a1e2af0d8c72e
Miron Zelina <mironzelina@improbable.io>
2020-01-09 17:49:49 +0000

    Remove wrong whitespace

--- ci/run-tests.ps1
+++ ci/run-tests.ps1
@@ -86,7 +86,7 @@ Echo "Running $($ue_path_absolute) $($cmd_args_list)"
 
 $run_tests_proc = Start-Process $ue_path_absolute -PassThru -NoNewWindow -ArgumentList $cmd_args_list
 Start-Sleep -s 10
-$tail_proc = Start-Process "powershell" -PassThru -ArgumentList @("Get-Content", $log_file_path,  "-Wait")
+$tail_proc = Start-Process "powershell" -PassThru -ArgumentList @("Get-Content", $log_file_path, "-Wait")
 Wait-Process -Id (Get-Process -InputObject $run_tests_proc).id
 Stop-Process -InputObject $tail_proc
 
