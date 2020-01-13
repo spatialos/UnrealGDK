bfc50d761aa3ac065d48abdf2ae46aee5996e3db
Miron Zelina <mironzelina@improbable.io>
2020-01-13 12:31:28 +0000

    Add timeout for running tests

--- ci/run-tests.ps1
+++ ci/run-tests.ps1
@@ -83,4 +83,4 @@ Echo "Running $($ue_path_absolute) $($cmd_args_list)"
 $run_tests_proc = Start-Process $ue_path_absolute -PassThru -NoNewWindow -ArgumentList $cmd_args_list
 Start-Sleep -s 6
 Get-Content $log_file_path -Wait
-Wait-Process -Id (Get-Process -InputObject $run_tests_proc).id
+Wait-Process -Timeout 1200 -InputObject $run_tests_proc
