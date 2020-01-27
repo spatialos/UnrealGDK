a77807b146adeec9fc0368bd7aa47c2531429ff0
Ally <alastairdglennie@gmail.com>
2020-01-27 14:37:25 +0000

    switched implementation to use delegates

--- SpatialGDK/Source/SpatialGDK/Private/Utils/SpatialAbilitySystemComponent.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Utils/SpatialAbilitySystemComponent.cpp
@@ -4,82 +4,90 @@
 
 #include "EngineClasses/SpatialNetDriver.h"
 #include "SpatialConstants.h"
-#include "GeneralProjectSettings.h"
+#include "SpatialGDKSettings.h"
+
+#include "GameplayAbility.h"
 #include "GameplayAbilitySpec.h"
+#include "GameplayAbilityTypes.h"
+#include "GeneralProjectSettings.h"
 
 DEFINE_LOG_CATEGORY(LogSpatialAbilitySystemComponent);
 
 void USpatialAbilitySystemComponent::BeginPlay()
 {
+	Super::BeginPlay();
 
-	bSpatialNetworkingEnabled = GetDefault<UGeneralProjectSettings>()->UsesSpatialNetworking();
-	LockingPolicy = bSpatialNetworkingEnabled ? Cast<USpatialNetDriver>(GetWorld()->NetDriver)->LockingPolicy : nullptr;
+	bool bSpatialNetworkingEnabled = GetDefault<UGeneralProjectSettings>()->UsesSpatialNetworking();
+	bool bEnableUnrealLoadBalancer = GetDefault<USpatialGDKSettings>()->bEnableUnrealLoadBalancer;
+	USpatialNetDriver* NetDriver = Cast<USpatialNetDriver>(GetWorld()->NetDriver);
+
+	if (!bSpatialNetworkingEnabled || !bEnableUnrealLoadBalancer || !NetDriver->IsServer())
+	{
+		return;
+	}
+
+	LockingPolicy = NetDriver->LockingPolicy;
+	AbilityActivatedCallbacks.AddUObject(this, &USpatialAbilitySystemComponent::OnAbilityActivateCallback);
+	AbilityEndedCallbacks.AddUObject(this, &USpatialAbilitySystemComponent::OnAbilityEndedCallback);
 }
 
-bool USpatialAbilitySystemComponent::InternalTryActivateAbility(FGameplayAbilitySpecHandle AbilityToActivate, FPredictionKey InPredictionKey, UGameplayAbility ** OutInstancedAbility, FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate, const FGameplayEventData* TriggerEventData)
+void USpatialAbilitySystemComponent::OnAbilityActivateCallback(UGameplayAbility* ActivatedAbility)
 {
-	ABILITYLIST_SCOPE_LOCK(); // make sure our spec doesn't get removed during activation in the super call
-
-	bool bAbilitySuccessfullyActivated = Super::InternalTryActivateAbility(AbilityToActivate, InPredictionKey, OutInstancedAbility, OnGameplayAbilityEndedDelegate, TriggerEventData);
-
-	// If we failed the ability or we're using Unreal Networking we can just return the result early and not care about locking.
-	if (!bAbilitySuccessfullyActivated || !bSpatialNetworkingEnabled)
+	if (ActivatedAbility->GetInstancingPolicy() == EGameplayAbilityInstancingPolicy::NonInstanced)
 	{
-		return bAbilitySuccessfullyActivated;
+		UE_LOG(LogSpatialAbilitySystemComponent, Error, TEXT("Failed to lock Actor for non-instanced ability"));
+		return;
 	}
 
-	//todo: may want to get the activation info from the UGameplayAbility instance when running an instanced ability, might be more reliable.
-	// though really prediction keys may not be the best mapping to use so this will likely change
-	FGameplayAbilitySpec* Spec = FindAbilitySpecFromHandle(AbilityToActivate);
-	FPredictionKey ActivationPredictionKey = Spec->ActivationInfo.GetActivationPredictionKey();
+	const FGameplayAbilityActivationInfo& ActivationInfo = ActivatedAbility->GetCurrentActivationInfoRef();
+	FPredictionKey ActivationPredictionKey = ActivationInfo.GetActivationPredictionKey();
 
-	//todo: check whether the ability activated and ended immediately. In that case, don't lock at all
-
-	UE_LOG(LogSpatialAbilitySystemComponent, Warning, TEXT("AbilitySystemComponent::InternalTryActivateAbility returned true"));
-	bool LockSucceeded = LockAbilityInstance(ActivationPredictionKey);
-	if (LockSucceeded)
+	bool LockSucceeded = LockAbilityInstance(ActivationPredictionKey.Current);
+	if (!LockSucceeded)
 	{
-		BindUnlockToAbilityEnd(ActivationPredictionKey, OnGameplayAbilityEndedDelegate);
+		UE_LOG(LogSpatialAbilitySystemComponent, Error, TEXT("Failed to lock Actor for ability instance"));
 	}
-
-	return true; // for now we'll just let the ability continued regardless of lock success, we print an error in the method below
 }
 
-bool USpatialAbilitySystemComponent::LockAbilityInstance(FPredictionKey InPredictionKey)
+void USpatialAbilitySystemComponent::OnAbilityEndedCallback(UGameplayAbility* AbilityThatEnded)
+{
+	if (AbilityThatEnded->GetInstancingPolicy() == EGameplayAbilityInstancingPolicy::NonInstanced)
+	{
+		UE_LOG(LogSpatialAbilitySystemComponent, Error, TEXT("Failed to unlock Actor for non-instanced ability"));
+		return;
+	}
+
+	const FGameplayAbilityActivationInfo& ActivationInfo = AbilityThatEnded->GetCurrentActivationInfoRef();
+	FPredictionKey ActivationPredictionKey = ActivationInfo.GetActivationPredictionKey();
+
+	UnlockAfterAbilityEnded(ActivationPredictionKey.Current);
+}
+
+bool USpatialAbilitySystemComponent::LockAbilityInstance(AbilityInstanceIdentifier AbilityInstanceId)
 {
 	check(LockingPolicy != nullptr);
-	ActorLockToken LockToken = LockingPolicy->AcquireLock(GetOwner(), FString::Printf(TEXT("Ability instance lock. Prediction key Id: %d"), InPredictionKey.Current));
+	ActorLockToken LockToken = LockingPolicy->AcquireLock(GetOwner(), FString::Printf(TEXT("Ability instance lock. Ability Id: %d"), AbilityInstanceId));
 	if (LockToken == SpatialConstants::INVALID_ACTOR_LOCK_TOKEN)
 	{
 		// Could be worth trying to pass in the ability name here for debugging this error.
-		UE_LOG(LogSpatialAbilitySystemComponent, Error, TEXT("Failed to lock Actor for ability instance. Prediction key Id: %d"), InPredictionKey.Current);
+		UE_LOG(LogSpatialAbilitySystemComponent, Error, TEXT("Failed to lock Actor for ability instance. Ability Id: %d"), AbilityInstanceId);
 		return false;
 	}
 	check(!AbilityInstanceToLockTokens.Contains(LockToken));
-	AbilityInstanceToLockTokens.Add(InPredictionKey.Current, LockToken);
+	AbilityInstanceToLockTokens.Add(AbilityInstanceId, LockToken);
 	return true;
 }
 
-void USpatialAbilitySystemComponent::BindUnlockToAbilityEnd(FPredictionKey InPredictionKey, FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate)
+bool USpatialAbilitySystemComponent::UnlockAfterAbilityEnded(AbilityInstanceIdentifier AbilityInstanceId)
 {
-	check(OnGameplayAbilityEndedDelegate != nullptr);
-	OnGameplayAbilityEndedDelegate->BindLambda([this, InPredictionKey](UGameplayAbility* GameplayAbility)
+	if (!AbilityInstanceToLockTokens.Contains(AbilityInstanceId))
 	{
-		this->UnlockAfterAbilityEnded(InPredictionKey);
-	});
-}
-
-bool USpatialAbilitySystemComponent::UnlockAfterAbilityEnded(FPredictionKey InPredictionKey)
-{
-	uint16 KeyId = InPredictionKey.Current;
-	if (!AbilityInstanceToLockTokens.Contains(KeyId))
-	{
-		UE_LOG(LogSpatialAbilitySystemComponent, Error, TEXT("Tried to unlock after an ability ended but no lock token was found for the prediction key. Prediction key Id: %d"), InPredictionKey.Current);
+		UE_LOG(LogSpatialAbilitySystemComponent, Error, TEXT("Tried to unlock after an ability ended but no lock token was found for the ability Id. Ability Id: %d"), AbilityInstanceId);
 		return false;
 	}
 
+	ActorLockToken LockToken = AbilityInstanceToLockTokens.FindAndRemoveChecked(AbilityInstanceId);
 	check(LockingPolicy != nullptr);
-	ActorLockToken LockToken = AbilityInstanceToLockTokens.FindAndRemoveChecked(KeyId);
 	LockingPolicy->ReleaseLock(LockToken);
 	return true;
 }
--- SpatialGDK/Source/SpatialGDK/Public/Utils/SpatialAbilitySystemComponent.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/SpatialAbilitySystemComponent.h
@@ -5,15 +5,17 @@
 #include "LoadBalancing/AbstractLockingPolicy.h"
 #include "SpatialCommonTypes.h"
 
-#include "GameplayAbilities/Public/AbilitySystemComponent.h"
 #include "Components/ActorComponent.h"
+#include "AbilitySystemComponent.h"
 #include "GameplayPrediction.h"
 
 #include "SpatialAbilitySystemComponent.generated.h"
 
+class UGameplayAbility;
+
 DECLARE_LOG_CATEGORY_EXTERN(LogSpatialAbilitySystemComponent, Log, All);
 
-UCLASS(ClassGroup = AbilitySystem, hidecategories = (Object, LOD, Lighting, Transform, Sockets, TextureStreaming), editinlinenew, meta = (BlueprintSpawnableComponent))
+UCLASS()
 class SPATIALGDK_API USpatialAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
@@ -22,17 +24,16 @@ public:
 	// To identify unique ability activations we use the prediction key, which is the ID used by the server to cancel / rewind
 	// CSP'd ability activations after the fact. It's a uint16 which wraps round, ability activations are so short-lived this
 	// should never cause us an issue with collisions, or something else in Unreal would probably break if so anyway.
-	using AbilityInstanceIdentifiers = FPredictionKey::KeyType;
+	using AbilityInstanceIdentifier = FPredictionKey::KeyType;
 
 	virtual void BeginPlay() override;
-	virtual bool InternalTryActivateAbility(FGameplayAbilitySpecHandle AbilityToActivate, FPredictionKey InPredictionKey = FPredictionKey(), UGameplayAbility ** OutInstancedAbility = nullptr, FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate = nullptr, const FGameplayEventData* TriggerEventData = nullptr) override;
+	void OnAbilityActivateCallback(UGameplayAbility* ActivatedAbility);
+	void OnAbilityEndedCallback(UGameplayAbility* AbilityThatEnded);
 
 private:
-	bool bSpatialNetworkingEnabled;
 	UAbstractLockingPolicy* LockingPolicy;
-	TMap<AbilityInstanceIdentifiers, ActorLockToken> AbilityInstanceToLockTokens;
+	TMap<AbilityInstanceIdentifier, ActorLockToken> AbilityInstanceToLockTokens;
 
-	bool LockAbilityInstance(FPredictionKey InPredictionKey);
-	void BindUnlockToAbilityEnd(FPredictionKey InPredictionKey, FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate);
-	bool UnlockAfterAbilityEnded(FPredictionKey InPredictionKey);
+	bool LockAbilityInstance(AbilityInstanceIdentifier AbilityInstanceId);
+	bool UnlockAfterAbilityEnded(AbilityInstanceIdentifier AbilityInstanceId);
 };
