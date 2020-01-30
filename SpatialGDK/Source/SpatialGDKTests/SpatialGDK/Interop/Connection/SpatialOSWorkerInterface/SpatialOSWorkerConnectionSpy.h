9bf9d82bdc6070a205d854afbba859b608f3fd08
Jennifer Harkness <jennifer@improbable.io>
2020-01-30 11:58:38 +0000

    Feature/unr 2734 translation manager testing (#1721)
    * Mocked SpatialOSWorkerInterface
    * Added interface and mock for dispatching functionality of SpatialReceiver
    * Cleaned up and documented the mocks. Added more tests for the translation manager

---
+++ SpatialGDK/Source/SpatialGDKTests/SpatialGDK/Interop/Connection/SpatialOSWorkerInterface/SpatialOSWorkerConnectionSpy.h
@@ -0,0 +1,50 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "Interop/Connection/SpatialOSWorkerInterface.h"
+
+#include "Interop/Connection/OutgoingMessages.h"
+#include "SpatialCommonTypes.h"
+#include "Utils/SpatialLatencyTracer.h"
+
+#include <WorkerSDK/improbable/c_schema.h>
+#include <WorkerSDK/improbable/c_worker.h>
+
+// The SpatialOSWorkerConnectionSpy is intended to be a very minimal implementation of a WorkerConnection which just records then swallows
+// any data sent through it. It is intended to be extended with methods to query for what data has been sent through it.
+//
+// Only a few methods have meaningful implementations. You are intended to extend the implementations whenever needed
+// for testing code which uses the WorkerConnection.
+
+class SpatialOSWorkerConnectionSpy : public SpatialOSWorkerInterface
+{
+public:
+	SpatialOSWorkerConnectionSpy();
+
+	virtual TArray<Worker_OpList*> GetOpList() override;
+	virtual Worker_RequestId SendReserveEntityIdsRequest(uint32_t NumOfEntities) override;
+	virtual Worker_RequestId SendCreateEntityRequest(TArray<Worker_ComponentData>&& Components, const Worker_EntityId* EntityId) override;
+	virtual Worker_RequestId SendDeleteEntityRequest(Worker_EntityId EntityId) override;
+	virtual void SendAddComponent(Worker_EntityId EntityId, Worker_ComponentData* ComponentData, const TraceKey Key = USpatialLatencyTracer::InvalidTraceKey) override;
+	virtual void SendRemoveComponent(Worker_EntityId EntityId, Worker_ComponentId ComponentId) override;
+	virtual void SendComponentUpdate(Worker_EntityId EntityId, const Worker_ComponentUpdate* ComponentUpdate, const TraceKey Key = USpatialLatencyTracer::InvalidTraceKey) override;
+	virtual Worker_RequestId SendCommandRequest(Worker_EntityId EntityId, const Worker_CommandRequest* Request, uint32_t CommandId) override;
+	virtual void SendCommandResponse(Worker_RequestId RequestId, const Worker_CommandResponse* Response) override;
+	virtual void SendCommandFailure(Worker_RequestId RequestId, const FString& Message) override;
+	virtual void SendLogMessage(uint8_t Level, const FName& LoggerName, const TCHAR* Message) override;
+	virtual void SendComponentInterest(Worker_EntityId EntityId, TArray<Worker_InterestOverride>&& ComponentInterest) override;
+	virtual Worker_RequestId SendEntityQueryRequest(const Worker_EntityQuery* EntityQuery) override;
+	virtual void SendMetrics(const SpatialGDK::SpatialMetrics& Metrics) override;
+
+	// The following methods are used to query for state in tests.
+	const Worker_EntityQuery* GetLastEntityQuery();
+	void ClearLastEntityQuery();
+
+	Worker_RequestId GetLastRequestId();
+
+private:
+	Worker_RequestId NextRequestId;
+
+	const Worker_EntityQuery* LastEntityQuery;
+};
