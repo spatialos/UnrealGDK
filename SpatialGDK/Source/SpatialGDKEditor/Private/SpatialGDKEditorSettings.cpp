5c6125f4a1297a79ebde20c25dda30655a300bd7
Nicolas Colombe <nicolascolombe@improbable.io>
2020-02-07 13:50:32 +0000

    Add Runtime version pinning to the GDK + settings to control this behaviour
    Add additional setting for cloud deployment runtime version, along with UI in the deployment panel.

--- SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorSettings.cpp
+++ SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorSettings.cpp
@@ -20,6 +20,7 @@ USpatialGDKEditorSettings::USpatialGDKEditorSettings(const FObjectInitializer& O
 	, bShowSpatialServiceButton(false)
 	, bDeleteDynamicEntities(true)
 	, bGenerateDefaultLaunchConfig(true)
+	, bUseGDKPinnedRuntimeVersion(true)
 	, bExposeRuntimeIP(false)
 	, ExposedRuntimeIP(TEXT(""))
 	, bStopSpatialOnExit(false)
@@ -32,6 +33,24 @@ USpatialGDKEditorSettings::USpatialGDKEditorSettings(const FObjectInitializer& O
 	SpatialOSLaunchConfig.FilePath = GetSpatialOSLaunchConfig();
 	SpatialOSSnapshotToSave = GetSpatialOSSnapshotToSave();
 	SpatialOSSnapshotToLoad = GetSpatialOSSnapshotToLoad();
+}
+
+const FString& USpatialGDKEditorSettings::GetSpatialOSRuntimeVersionForLocal() const
+{
+	if (bUseGDKPinnedRuntimeVersion)
+	{
+		return SpatialGDKServicesConstants::SpatialOSRuntimePinnedVersion;
+	}
+	return LocalRuntimeVersion;
+}
+
+const FString& USpatialGDKEditorSettings::GetSpatialOSRuntimeVersionForCloud() const
+{
+	if (bUseGDKPinnedRuntimeVersion)
+	{
+		return SpatialGDKServicesConstants::SpatialOSRuntimePinnedVersion;
+	}
+	return CloudRuntimeVersion;
 }
 
 void USpatialGDKEditorSettings::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
@@ -211,6 +230,18 @@ void USpatialGDKEditorSettings::SetSimulatedPlayerRegionCode(const ERegionCode::
 void USpatialGDKEditorSettings::SetSimulatedPlayersEnabledState(bool IsEnabled)
 {
 	bSimulatedPlayersIsEnabled = IsEnabled;
+	SaveConfig();
+}
+
+void USpatialGDKEditorSettings::SetUseGDKPinnedRuntimeVersion(bool Use)
+{
+	bUseGDKPinnedRuntimeVersion = Use;
+	SaveConfig();
+}
+
+void USpatialGDKEditorSettings::SetCustomCloudSpatialOSRuntimeVersion(const FString& Version)
+{
+	CloudRuntimeVersion = Version;
 	SaveConfig();
 }
 
