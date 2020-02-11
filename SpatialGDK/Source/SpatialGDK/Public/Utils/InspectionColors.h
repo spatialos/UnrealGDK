15fb69a2d9dd4a6966cb0408631ccb46e471549f
Jacques Elliott <jacques@improbable.io>
2020-02-11 11:55:58 +0000

    expose colors to blueprints

--- SpatialGDK/Source/SpatialGDK/Public/Utils/InspectionColors.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/InspectionColors.h
@@ -6,7 +6,6 @@
 #include "Math/Color.h"
 
 // Mimicking Inspector V2 coloring from platform/js/console/src/inspector-v2/styles/colors.ts
-
 namespace SpatialGDK
 {
 	// Argument expected in the form: UnrealWorker1a2s3d4f...
