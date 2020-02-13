e8f2b6013637182792c956b2ccc6208ba42b2be4
Jacques Elliott <jacques@improbable.io>
2020-02-13 15:27:43 +0000

    Feature/unr 2815 split server to server to server (#1795)
    * schema change
    
    * split component
    
    * reverse silly testing things
    
    * release note
    
    * remove ready and all checks
    
    * accidnet
    
    * rename legacy->command
    
    * bug
    
    * remove unnecessary file
    
    * someone made another component

--- SpatialGDK/Source/SpatialGDK/Public/SpatialConstants.h
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialConstants.h
@@ -114,6 +114,7 @@ const Worker_ComponentId SERVER_ENDPOINT_COMPONENT_ID					= 9977;
 const Worker_ComponentId MULTICAST_RPCS_COMPONENT_ID					= 9976;
 const Worker_ComponentId SPATIAL_DEBUGGING_COMPONENT_ID					= 9975;
 const Worker_ComponentId SERVER_WORKER_COMPONENT_ID						= 9974;
+const Worker_ComponentId SERVER_TO_SERVER_COMMAND_ENDPOINT_COMPONENT_ID = 9973;
 
 const Worker_ComponentId STARTING_GENERATED_COMPONENT_ID				= 10000;
 
@@ -304,9 +305,8 @@ const TArray<Worker_ComponentId> REQUIRED_COMPONENTS_FOR_NON_AUTH_SERVER_INTERES
 	MULTICAST_RPCS_COMPONENT_ID,
 	NETMULTICAST_RPCS_COMPONENT_ID_LEGACY,
 
-	// Required for server to server RPCs. TODO(UNR-2815): split server to server RPCs into its own component
+	// Required for server to server RPCs.
 	SERVER_ENDPOINT_COMPONENT_ID,
-	SERVER_RPC_ENDPOINT_COMPONENT_ID_LEGACY,
 
 	// Global state components
 	SINGLETON_MANAGER_COMPONENT_ID,
@@ -336,7 +336,7 @@ FORCEINLINE Worker_ComponentId RPCTypeToWorkerComponentIdLegacy(ERPCType RPCType
 	{
 	case ERPCType::CrossServer:
 	{
-		return SpatialConstants::SERVER_RPC_ENDPOINT_COMPONENT_ID_LEGACY;
+		return SpatialConstants::SERVER_TO_SERVER_COMMAND_ENDPOINT_COMPONENT_ID;
 	}
 	case ERPCType::NetMulticast:
 	{
@@ -365,7 +365,7 @@ FORCEINLINE Worker_ComponentId GetClientAuthorityComponent(bool bUsingRingBuffer
 
 FORCEINLINE Worker_ComponentId GetCrossServerRPCComponent(bool bUsingRingBuffers)
 {
-	return bUsingRingBuffers ? SERVER_ENDPOINT_COMPONENT_ID : SERVER_RPC_ENDPOINT_COMPONENT_ID_LEGACY;
+	return bUsingRingBuffers ? SERVER_ENDPOINT_COMPONENT_ID : SERVER_TO_SERVER_COMMAND_ENDPOINT_COMPONENT_ID;
 }
 
 } // ::SpatialConstants
