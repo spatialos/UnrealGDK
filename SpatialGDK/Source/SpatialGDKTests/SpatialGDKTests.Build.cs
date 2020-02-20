67adbf0af5f214f13131ac72d157310fa0d4fbe3
Miron Zelina <mironzelina@improbable.io>
2020-02-20 18:43:42 +0000

    Fix bug with using unity builds in 4.24

--- SpatialGDK/Source/SpatialGDKTests/SpatialGDKTests.Build.cs
+++ SpatialGDK/Source/SpatialGDKTests/SpatialGDKTests.Build.cs
@@ -8,7 +8,11 @@ public class SpatialGDKTests : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 #pragma warning disable 0618
-        bFasterWithoutUnity = true; // Deprecated in 4.24, replace with bUseUnity = true; once we drop support for 4.23
+        bFasterWithoutUnity = true;             // Deprecated in 4.24, replace with bUseUnity = false; once we drop support for 4.23
+        if (Target.Version.MinorVersion == 24)  // Due to a bug in 4.24, bFasterWithoutUnity is inversed, fixed in master, so should hopefully roll into the next release, remove this once it does
+        {
+            bFasterWithoutUnity = false;
+        }
 #pragma warning restore 0618
 
         PrivateDependencyModuleNames.AddRange(
