2b0129655ede96754e236c42c41fc6740377095a
Miron Zelina <mironzelina@improbable.io>
2020-02-27 18:10:42 +0000

    Fix spacing issues

--- SpatialGDK/Source/SpatialGDKServices/SpatialGDKServices.Build.cs
+++ SpatialGDK/Source/SpatialGDKServices/SpatialGDKServices.Build.cs
@@ -8,14 +8,14 @@ public class SpatialGDKServices : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 #pragma warning disable 0618
-        bFasterWithoutUnity = true;             // Deprecated in 4.24, replace with bUseUnity = false; once we drop support for 4.23
-        if (Target.Version.MinorVersion == 24)  // Due to a bug in 4.24, bFasterWithoutUnity is inversed, fixed in master, so should hopefully roll into the next release, remove this once it does
-        {
-            bFasterWithoutUnity = false;
-        }
+		bFasterWithoutUnity = true;             // Deprecated in 4.24, replace with bUseUnity = false; once we drop support for 4.23
+		if (Target.Version.MinorVersion == 24)  // Due to a bug in 4.24, bFasterWithoutUnity is inversed, fixed in master, so should hopefully roll into the next release, remove this once it does
+		{
+			bFasterWithoutUnity = false;
+		}
 #pragma warning restore 0618
 
-        PrivateDependencyModuleNames.AddRange(
+		PrivateDependencyModuleNames.AddRange(
 			new string[] {
 				"EditorStyle",
 				"Engine",
