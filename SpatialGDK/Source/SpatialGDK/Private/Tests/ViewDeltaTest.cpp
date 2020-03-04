4284c4e7cf9056c028ca7b28c33dad90c53261ac
Alex Moroz <oleksandrmoroz@improbable.io>
2020-03-04 13:06:31 +0000

    Added more WorkerView tests

--- SpatialGDK/Source/SpatialGDK/Private/Tests/ViewDeltaTest.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Tests/ViewDeltaTest.cpp
@@ -11,44 +11,70 @@ using namespace SpatialGDK;
 
 VIEWDELTA_TEST(GIVEN_empty_ViewDelta_WHEN_GetCreateEntityResponses_called_THEN_no_CreateEntityResponse_responses_returned)
 {
+	// GIVEN
+	// WHEN
+	// THEN
 	return true;
 }
 
 VIEWDELTA_TEST(GIVEN_empty_ViewDelta_WHEN_GeneratedLegacyOpList_called_THEN_no_ops_returned_returned)
 {
+	// GIVEN
 	ViewDelta Delta;
+
+	// WHEN
 	auto OpList = Delta.GenerateLegacyOpList();
 
+	// THEN
 	TestTrue("Valid OpList returned", OpList != nullptr);
+
 	return true;
 }
 
 VIEWDELTA_TEST(GIVEN_ViewDelta_with_one_CreateEntityResponse_added_WHEN_GetCreateEntityResponse_called_THEN_one_CreateEntityResponse_returned)
 {
+	// GIVEN
+	// WHEN
+	// THEN
 	return true;
 }
 
 VIEWDELTA_TEST(GIVEN_ViewDelta_with_multiple_CreateEntityResponse_added_WHEN_GetCreateEntityResponse_called_THEN_multiple_CreateEntityResponses_returned)
 {
+	// GIVEN
+	// WHEN
+	// THEN
 	return true;
 }
 
 VIEWDELTA_TEST(GIVEN_ViewDelta_with_one_CreateEntityResponse_added_WHEN_GenerateLegacyOpList_called_THEN_one_op_returned)
 {
+	// GIVEN
+	// WHEN
+	// THEN
 	return true;
 }
 
 VIEWDELTA_TEST(GIVEN_ViewDelta_with_multiple_CreateEntityResponse_added_WHEN_GenerateLegacyOpList_called_THEN_multiple_ops_returned)
 {
+	// GIVEN
+	// WHEN
+	// THEN
 	return true;
 }
 
 VIEWDELTA_TEST(GIVEN_non_empty_ViewDelta_WHEN_Clear_called_THEN_GetCreateEntityResponse_returns_no_items)
 {
+	// GIVEN
+	// WHEN
+	// THEN
 	return true;
 }
 
 VIEWDELTA_TEST(GIVEN_non_empty_ViewDelta_WHEN_Clear_called_THEN_GenerateLegacyOpList_returns_no_items)
 {
+	// GIVEN
+	// WHEN
+	// THEN
 	return true;
 }
--- SpatialGDK/Source/SpatialGDK/Private/Tests/WorkerViewTest.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Tests/WorkerViewTest.cpp
@@ -3,11 +3,24 @@
 #include "Tests/TestDefinitions.h"
 
 #include "WorkerView.h"
+#include "OpList/ViewDeltaLegacyOpList.h"
 
 #define WORKERVIEW_TEST(TestName) \
 	GDK_TEST(Core, WorkerView, TestName)
 
 using namespace SpatialGDK; 
+
+namespace
+{
+	Worker_Op CreateEmptyCreateEntityResponseOp()
+	{
+		Worker_Op Op{};
+		Op.op_type = WORKER_OP_TYPE_CREATE_ENTITY_RESPONSE;
+		Op.op.create_entity_response = Worker_CreateEntityResponseOp{};
+		return Op;
+	}
+
+} // anonymous namespace
 
 WORKERVIEW_TEST(GIVEN_empty_WorkerView_WHEN_GenerateViewDelta_called_THEN_ViewDelta_returned)
 {
@@ -18,22 +31,75 @@ WORKERVIEW_TEST(GIVEN_empty_WorkerView_WHEN_GenerateViewDelta_called_THEN_ViewDe
 	return true;
 }
 
-WORKERVIEW_TEST(GIVEN_WorkerView_with_one_CreateEntityRequest_WHEN_FlushLocalChanges_called_THEN_one_MessagesToSend_returned)
+WORKERVIEW_TEST(GIVEN_WorkerView_with_one_CreateEntityRequest_WHEN_FlushLocalChanges_called_THEN_one_CreateEntityRequest_returned)
 {
+	// GIVEN
+	WorkerView View;
+	CreateEntityRequest Request;
+	View.SendCreateEntityRequest(Request);
+
+	// WHEN
+	auto Messages = View.FlushLocalChanges();
+
+	// THEN
+	TestTrue("WorkerView has one CreateEntityRequest", Messages->CreateEntityRequests.Num() == 1);
+
 	return true;
 }
 
-WORKERVIEW_TEST(GIVEN_WorkerView_with_multiple_CreateEntityRequest_WHEN_FlushLocalChanges_called_THEN_mutliple_MessagesToSend_returned)
+WORKERVIEW_TEST(GIVEN_WorkerView_with_multiple_CreateEntityRequest_WHEN_FlushLocalChanges_called_THEN_mutliple_CreateEntityRequests_returned)
 {
+	// GIVEN
+	WorkerView View;
+	CreateEntityRequest Request;
+	View.SendCreateEntityRequest(Request);
+	View.SendCreateEntityRequest(Request);
+
+	auto Messages = View.FlushLocalChanges();
+
+	// THEN
+	TestTrue("WorkerView has multiple CreateEntityRequest", Messages->CreateEntityRequests.Num() > 1);
+
 	return true;
 }
 
 WORKERVIEW_TEST(GIVEN_WorkerView_with_one_op_enqued_WHEN_GenerateViewDelta_called_THEN_ViewDelta_with_one_op_returned)
 {
+	// GIVEN
+	WorkerView View;
+	// TODO(Alex): what type should it be
+	TArray<Worker_Op> Ops;
+	Ops.Push(CreateEmptyCreateEntityResponseOp());
+	auto OpList = MakeUnique<ViewDeltaLegacyOpList>(Ops);
+	View.EnqueueOpList(MoveTemp(OpList));
+
+	// WHEN
+	auto ViewDelta = View.GenerateViewDelta();
+
+	// THEN
+	// TODO(Alex): should it be using LegacyOpList?
+	TestTrue("ViewDelta has one op", ViewDelta->GenerateLegacyOpList()->GetCount() == 1);
+
 	return true;
 }
 
 WORKERVIEW_TEST(GIVEN_WorkerView_with_multiple_ops_engued_WHEN_GenerateViewDelta_called_THEN_ViewDelta_with_multiple_ops_returned)
 {
+	// GIVEN
+	WorkerView View;
+	// TODO(Alex): what type should it be
+	TArray<Worker_Op> Ops;
+	Ops.Push(CreateEmptyCreateEntityResponseOp());
+	Ops.Push(CreateEmptyCreateEntityResponseOp());
+	auto OpList = MakeUnique<ViewDeltaLegacyOpList>(Ops);
+	View.EnqueueOpList(MoveTemp(OpList));
+
+	// WHEN
+	auto ViewDelta = View.GenerateViewDelta();
+
+	// THEN
+	// TODO(Alex): should it be using LegacyOpList?
+	TestTrue("ViewDelta has multiple ops", ViewDelta->GenerateLegacyOpList()->GetCount() > 1);
+
 	return true;
 }
