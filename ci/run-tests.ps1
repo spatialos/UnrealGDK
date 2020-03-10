66846f58d782ba02fcc252900078d0ddbef8330f
Miron Zelina <mironzelina@improbable.io>
2020-03-10 18:23:41 +0000

    Fix path to project

--- ci/run-tests.ps1
+++ ci/run-tests.ps1
@@ -49,7 +49,7 @@ $ue_path_absolute = Force-ResolvePath $unreal_editor_path
 $uproject_path_absolute = Force-ResolvePath $uproject_path
 $output_dir_absolute = Force-ResolvePath $output_dir
 
-Add-Content -Path "$uproject_path_absolute\Game\Config\DefaultSpatialGDKSettings.ini" -Value "`r`n`r`n$additional_gdk_options"
+Add-Content -Path "$PSScriptRoot\Game\Config\DefaultSpatialGDKSettings.ini" -Value "`r`n`r`n$additional_gdk_options"
 
 $cmd_args_list = @( `
     "`"$uproject_path_absolute`"", # We need some project to run tests in, but for unit tests the exact project shouldn't matter
