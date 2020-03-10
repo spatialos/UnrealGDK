35f79d9e861dda4f74cbb8f55d5f93a70fefeb06
Miron Zelina <mironzelina@improbable.io>
2020-03-10 19:50:55 +0000

    Fix incorrect requirement for additional_gdk_options

--- ci/setup-build-test-gdk.ps1
+++ ci/setup-build-test-gdk.ps1
@@ -14,7 +14,7 @@ class TestSuite {
     [ValidateNotNullOrEmpty()][string]$test_project_name
     [ValidateNotNullOrEmpty()][string]$test_results_dir
     [ValidateNotNullOrEmpty()][string]$tests_path
-    [ValidateNotNullOrEmpty()][string]$additional_gdk_options
+    [ValidateNotNull()]       [string]$additional_gdk_options
     [bool]                            $run_with_spatial
 
     TestSuite([string] $test_repo_url, [string] $test_repo_branch, [string] $test_repo_relative_uproject_path, [string] $test_repo_map, [string] $test_project_name, [string] $test_results_dir, [string] $tests_path, [string] $additional_gdk_options, [string] $run_with_spatial) {
@@ -49,7 +49,7 @@ if ((Test-Path env:TEST_CONFIG) -And ($env:TEST_CONFIG -eq "Native")) {
 }
 else {
     $tests += [TestSuite]::new("$test_repo_url", "$test_repo_branch", "$test_repo_relative_uproject_path", "$test_repo_map", "$test_project_name", "TestResults", "SpatialGDK+/Game/SpatialNetworkingMap", "", $True)
-    $tests += [TestSuite]::new("$test_repo_url", "$test_repo_branch", "$test_repo_relative_uproject_path", "$test_repo_map", "$test_project_name", "LoadbalancerTestResults", "/Game/Spatial_ZoningMap_1S_2C", "bEnableUnrealLoadBalancer=true;`r`nLoadBalancingWorkerType=(WorkerTypeName=`"UnrealWorker`");", $True)
+    $tests += [TestSuite]::new("$test_repo_url", "$test_repo_branch", "$test_repo_relative_uproject_path", "$test_repo_map", "$test_project_name", "LoadbalancerTestResults", "/Game/Spatial_ZoningMap_1S_2C", "bEnableUnrealLoadBalancer=true;LoadBalancingWorkerType=(WorkerTypeName=`"UnrealWorker`")", $True)
 }
 
 if ($slow_networking_tests) {
