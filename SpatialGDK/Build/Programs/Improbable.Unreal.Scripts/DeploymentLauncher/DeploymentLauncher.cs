e2663cbab1af727bd7354d846217a246f7398d57
Matthew Sandford <matthewsandford@improbable.io>
2020-03-11 11:15:59 +0000

    Revert "[UNR-3064][MS] catching this exception and printing error."
    This reverts commit 8cb0f5b9ecfaa062933b4b7532b9eba0c5bc0999.

--- SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/DeploymentLauncher/DeploymentLauncher.cs
+++ SpatialGDK/Build/Programs/Improbable.Unreal.Scripts/DeploymentLauncher/DeploymentLauncher.cs
@@ -207,12 +207,6 @@ namespace Improbable
                         $"Unable to launch the deployment(s). This is likely because the project '{projectName}' or assembly '{assemblyName}' doesn't exist.");
                     return 1;
                 }
-                else if (e.Status.StatusCode == Grpc.Core.StatusCode.ResourceExhausted)
-                {
-                    Console.WriteLine(
-                        $"Unable to launch the deployment(s). Cloud cluster resources exhausted, Detail: '{e.Status.Detail}'" );
-                    return 1;
-                }
                 else
                 {
                     throw;
