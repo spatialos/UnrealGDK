d7ac021511ab882e2e3d21260028467a574dae45
Ally <alastairdglennie@gmail.com>
2020-03-13 14:06:10 +0000

    typo

--- SpatialGDK/Source/SpatialGDK/Private/Utils/EntityFactory.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Utils/EntityFactory.cpp
@@ -415,7 +415,7 @@ TArray<FWorkerComponentData> EntityFactory::CreateEntityComponents(USpatialActor
 
 		// Populate the ComponentPresence component with the list of initially present component IDs.
 		TArray<Worker_ComponentId> ComponentPresenceList;
-		ComponentPresenceList.Init(ComponentData.Num());
+		ComponentPresenceList.Init(ComponentDatas.Num());
 		for (int i = 0; i < ComponentDatas.Num(); i++)
 		{
 			ComponentPresenceList[i] = ComponentDatas[i].component_id;
