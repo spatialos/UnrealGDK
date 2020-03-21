9f0f1f4c55e9f4eeca505071c730096a62e323de
Jessica Falk <jessica@improbable.io>
2020-03-21 08:21:07 +0000

    let's not try to stop spatial for now

--- ci/cleanup.sh
+++ ci/cleanup.sh
@@ -10,7 +10,7 @@ pushd "$(dirname "$0")"
 
     # Workaround for UNR-2156 and UNR-2076, where spatiald / runtime processes sometimes never close, or where runtimes are orphaned
     # Clean up any spatiald and java (i.e. runtime) processes that may not have been shut down
-    spatial service stop
+    #spatial service stop
     pkill -9 -f java
 
     rm -f ${UNREAL_PATH}
