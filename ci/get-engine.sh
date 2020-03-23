62c7b63b2f72f427d913ddf24ceff2c83615da20
Jessica Falk <jessica@improbable.io>
2020-03-23 09:32:13 +0000

    fixing up check for HEAD

--- ci/get-engine.sh
+++ ci/get-engine.sh
@@ -28,14 +28,14 @@ pushd "$(dirname "$0")"
             # This is specified by putting "HEAD name/of-a-branch" in the unreal-engine.version file
             # If so, retrieve the version of the latest build from GCS, and use that going forward.
             HEAD_VERSION_PREFIX="HEAD "
-            if [[ "${VERSION_DESCRIPTION}" = "${HEAD_VERSION_PREFIX}*" ]]; then
+            if [[ "${VERSION_DESCRIPTION}" == ${HEAD_VERSION_PREFIX}* ]]; then
                 VERSION_BRANCH=${VERSION_DESCRIPTION#"${HEAD_VERSION_PREFIX}"} # Remove the prefix to just get the branch name
-                VERSION_BRANCH= ${VERSION_BRANCH///_} # Replace / with _ since / is treated as the folder seperator in GCS
+                VERSION_BRANCH=$(echo ${VERSION_BRANCH} | tr "/" "_") # Replace / with _ since / is treated as the folder seperator in GCS
 
                 # Download the head pointer file for the given branch, which contains the latest built version of the engine from that branch
                 HEAD_POINTER_GCS_PATH="gs://${GCS_PUBLISH_BUCKET}/HEAD/mac-${VERSION_BRANCH}.version"
                 UNREAL_VERSION=$(gsutil cp "${HEAD_POINTER_GCS_PATH}" -) # the '-' at the end instructs gsutil to download the file and output the contents to stdout
-            else 
+            else
                 UNREAL_VERSION="${VERSION_DESCRIPTION}"
             fi
         popd
