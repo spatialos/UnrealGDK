639ff0c07bb123d8da783e374c1e6ebe0cc031d3
Danny Birch <danielbirch@improbable.io>
2020-03-27 18:27:22 +0000

    Fix SetupIncTraceLibs flake

--- SetupIncTraceLibs.bat
+++ SetupIncTraceLibs.bat
@@ -22,6 +22,9 @@ call :MarkStartOfBlock "Retrieve dependencies"
 	spatial package retrieve internal        trace-dynamic-x86_64-gcc510-linux          14.3.0-b2647-85717ee-WORKER-SNAPSHOT "%CORE_SDK_DIR%\trace_lib\trace-linux.zip"
 call :MarkEndOfBlock "Retrieve dependencies"
 
+rem There seems to be a race with spatial package and the unzip command below
+timeout /t 1
+
 call :MarkStartOfBlock "Unpack dependencies"
     powershell -Command "Expand-Archive -Path \"%CORE_SDK_DIR%\trace_lib\trace-win32.zip\"	-DestinationPath \"%BINARIES_DIR%\Win64\" -Force;"^
 						"Expand-Archive -Path \"%CORE_SDK_DIR%\trace_lib\trace-linux.zip\"	-DestinationPath \"%BINARIES_DIR%\Linux\" -Force;"
