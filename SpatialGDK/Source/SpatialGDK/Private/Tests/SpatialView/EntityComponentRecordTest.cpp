8411d3bd4b4fe0f189e7807a99518999816826bc
Alex Moroz <oleksandrmoroz@improbable.io>
2020-04-02 15:06:38 +0100

    Added test files

---
+++ SpatialGDK/Source/SpatialGDK/Private/Tests/SpatialView/EntityComponentRecordTest.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "Tests/TestDefinitions.h"
+
+#include "SpatialView/EntityComponentRecord.h"
+
+#define ENTITYCOMPONENTRECORD_TEST(TestName) \
+	GDK_TEST(Core, EntityComponentRecord, TestName)
+
+using namespace SpatialGDK;
