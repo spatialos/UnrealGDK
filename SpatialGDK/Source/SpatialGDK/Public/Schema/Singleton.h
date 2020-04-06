f23d621fefc0aba88db3e3af701a950286c63f29
Ally <alastairdglennie@gmail.com>
2020-04-06 15:08:51 +0100

    delet

--- SpatialGDK/Source/SpatialGDK/Public/Schema/Singleton.h
+++ SpatialGDK/Source/SpatialGDK/Public/Schema/Singleton.h
@@ -2,11 +2,8 @@
 
 #pragma once
 
-#include "Interop/SpatialStaticComponentView.h"
-#include "LoadBalancing/AbstractLBStrategy.h"
 #include "Schema/Component.h"
 #include "SpatialConstants.h"
-#include "SpatialGDKSettings.h"
 
 #include <WorkerSDK/improbable/c_schema.h>
 #include <WorkerSDK/improbable/c_worker.h>
@@ -30,15 +27,6 @@ struct Singleton : Component
 		Data.schema_type = Schema_CreateComponentData();
 
 		return Data;
-	}
-
-	static bool ShouldHaveLocalWorkerAuthority(AActor* SingletonActor, UAbstractLBStrategy* LBStrategy, USpatialStaticComponentView* ComponentView)
-	{
-		const bool bLoadBalancerEnabled = GetDefault<USpatialGDKSettings>()->bEnableUnrealLoadBalancer;
-		// We should check here because if we're calling this before GSM entity data is received it will effectively be giving a false negative.
-		check(ComponentView->HasComponent(SpatialConstants::INITIAL_GLOBAL_STATE_MANAGER_ENTITY_ID, SpatialConstants::STARTUP_ACTOR_MANAGER_COMPONENT_ID));
-		const bool bHasGSMAuthority = ComponentView->HasAuthority(SpatialConstants::INITIAL_GLOBAL_STATE_MANAGER_ENTITY_ID, SpatialConstants::STARTUP_ACTOR_MANAGER_COMPONENT_ID);
-		return (bLoadBalancerEnabled && LBStrategy->ShouldHaveAuthority(*SingletonActor)) || (!bLoadBalancerEnabled && bHasGSMAuthority);
 	}
 };
 
