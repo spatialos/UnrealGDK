81e63bbc03b839d4b3d4dea78f180db9e017050d
Danny Birch <danielbirch@improbable.io>
2020-04-06 15:28:04 +0100

    Fix check

--- SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialReceiver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialReceiver.cpp
@@ -2105,7 +2105,7 @@ void USpatialReceiver::ProcessQueuedActorRPCsOnEntityCreation(AActor* Actor, RPC
 		UFunction* Function = Info.RPCs[RPC.Index];
 		const FRPCInfo& RPCInfo = ClassInfoManager->GetRPCInfo(Actor, Function);
 		const FUnrealObjectRef ObjectRef(PackageMap->GetEntityIdFromObject(Actor), RPC.Offset);
-		check(ObjectRef != FUnrealObjectRef::UNRESOLVED_OBJECT_REF);
+		check(ObjectRef.bNoLoadOnClient != SpatialConstants::INVALID_ENTITY_ID);
 
 		ProcessOrQueueIncomingRPC(ObjectRef, MoveTemp(RPC));
 	}
