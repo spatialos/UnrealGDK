40492e3652aa8a544e9c3e6d10e9635384589a26
jessicafalk <31853332+jessicafalk@users.noreply.github.com>
2020-04-09 20:56:39 +0100

    Update SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorLayoutDetails.cpp
    Co-Authored-By: improbable-valentyn <valentyn@improbable.io>

--- SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorLayoutDetails.cpp
+++ SpatialGDK/Source/SpatialGDKEditor/Private/SpatialGDKEditorLayoutDetails.cpp
@@ -123,7 +123,8 @@ FReply FSpatialGDKEditorLayoutDetails::GenerateDevAuthToken()
 
 	FString AuthResult;
 	FString DevAuthTokenResult;
-	if (!CreateDevAuthTokenResult.TrimEnd().Split(TEXT("\n"), &AuthResult, &DevAuthTokenResult, ESearchCase::IgnoreCase, ESearchDir::FromEnd) || DevAuthTokenResult.IsEmpty())
+	bool bFoundNewline = CreateDevAuthTokenResult.TrimEnd().Split(TEXT("\n"), &AuthResult, &DevAuthTokenResult, ESearchCase::IgnoreCase, ESearchDir::FromEnd);
+	if (!bFoundNewline || DevAuthTokenResult.IsEmpty())
 	{
 		// This is necessary because depending on whether you are already authenticated against spatial, it will either return two json structs or one.
 		DevAuthTokenResult = CreateDevAuthTokenResult;
