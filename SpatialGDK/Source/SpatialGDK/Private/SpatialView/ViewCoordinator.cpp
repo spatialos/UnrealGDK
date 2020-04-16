d77ab4083d914bdd924d2ecb8b0748119505ef03
samiwh <sami@improbable.io>
2020-04-16 18:32:45 +0100

    Plumbed in component messages and some missing command functions to the view coordinator.

--- SpatialGDK/Source/SpatialGDK/Private/SpatialView/ViewCoordinator.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/SpatialView/ViewCoordinator.cpp
@@ -27,6 +27,21 @@ void ViewCoordinator::FlushMessagesToSend()
 	ConnectionHandler->SendMessages(View.FlushLocalChanges());
 }
 
+void ViewCoordinator::SendAddComponent(Worker_EntityId EntityId, ComponentData Data)
+{
+	View.SendAddComponent(EntityId, MoveTemp(Data));
+}
+
+void ViewCoordinator::SendComponentUpdate(Worker_EntityId EntityId, ComponentUpdate Update)
+{
+	View.SendComponentUpdate(EntityId, MoveTemp(Update));
+}
+
+void ViewCoordinator::SendRemoveComponent(Worker_EntityId EntityId, Worker_ComponentId ComponentId)
+{
+	View.SendRemoveComponent(EntityId, ComponentId);
+}
+
 const TArray<CreateEntityResponse>& ViewCoordinator::GetCreateEntityResponses() const
 {
 	return Delta->GetCreateEntityResponses();
@@ -47,9 +62,29 @@ const TArray<EntityComponentId>& ViewCoordinator::GetAuthorityLostTemporarily() 
 	return Delta->GetAuthorityLostTemporarily();
 }
 
+const TArray<EntityComponentData>& ViewCoordinator::GetComponentsAdded() const
+{
+	return Delta->GetComponentsAdded();
+}
+
+const TArray<EntityComponentId>& ViewCoordinator::GetComponentsRemoved() const
+{
+	return Delta->GetComponentsRemoved();
+}
+
+const TArray<EntityComponentUpdate>& ViewCoordinator::GetUpdates() const
+{
+	return Delta->GetUpdates();
+}
+
+const TArray<EntityComponentCompleteUpdate>& ViewCoordinator::GetCompleteUpdates() const
+{
+	return Delta->GetCompleteUpdates();
+}
+
 TUniquePtr<AbstractOpList> ViewCoordinator::GenerateLegacyOpList() const
 {
 	return Delta->GenerateLegacyOpList();
 }
 
-}  // SpatialView
+}  // namespace SpatialGDK
