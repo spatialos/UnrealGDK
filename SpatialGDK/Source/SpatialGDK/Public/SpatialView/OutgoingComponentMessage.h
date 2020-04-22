f857a69455946f8e18fad6bbde506eac87706303
samiwh <sami@improbable.io>
2020-04-22 17:39:12 +0100

    Fixing deletion errors with outgoing component messages.

--- SpatialGDK/Source/SpatialGDK/Public/SpatialView/OutgoingComponentMessage.h
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/OutgoingComponentMessage.h
@@ -9,55 +9,90 @@ namespace SpatialGDK
 {
 
 // Represents one of a component addition, update, or removal.
-// Interally schema data is stored using raw pointers. However the interface exclusively uses explicitly owning objects to denote ownership.
-// Not safe to use after being moved from.
+// Internally schema data is stored using raw pointers. However the interface exclusively uses explicitly owning objects to denote ownership.
 class OutgoingComponentMessage
 {
 public:
-	enum MessageType {ADD, UPDATE, REMOVE};
+	enum MessageType {NONE, ADD, UPDATE, REMOVE};
+
+	explicit OutgoingComponentMessage()
+		: EntityId(0), ComponentId(0), Type(NONE)
+	{
+	}
 
 	explicit OutgoingComponentMessage(Worker_EntityId EntityId, ComponentData ComponentAdded)
 		: EntityId(EntityId), ComponentId(ComponentAdded.GetComponentId()), ComponentAdded(MoveTemp(ComponentAdded).Release()), Type(ADD)
 	{
 	}
 
-	OutgoingComponentMessage(Worker_EntityId EntityId, ComponentUpdate ComponentUpdated)
+	explicit OutgoingComponentMessage(Worker_EntityId EntityId, ComponentUpdate ComponentUpdated)
 		: EntityId(EntityId), ComponentId(ComponentUpdated.GetComponentId()), ComponentUpdated(MoveTemp(ComponentUpdated).Release()), Type(UPDATE)
 	{
 	}
 
-	OutgoingComponentMessage(Worker_EntityId EntityId, Worker_ComponentId RemovedComponentId)
+	explicit OutgoingComponentMessage(Worker_EntityId EntityId, Worker_ComponentId RemovedComponentId)
 		: EntityId(EntityId), ComponentId(RemovedComponentId), Type(REMOVE)
 	{
 	}
 
 	~OutgoingComponentMessage()
 	{
-		// As data is stored in owning raw pointers we need to make sure resources are realsed.
-		switch (Type)
-		{
-		case ADD:
-			if (ComponentAdded)
-			{
-				Schema_DestroyComponentData(ComponentAdded);
-			}
-			break;
-		case UPDATE:
-			if (ComponentUpdated)
-			{
-				Schema_DestroyComponentUpdate(ComponentUpdated);
-			}
-			break;
-		case REMOVE:
-			break;
-		}
+		// As data is stored in owning raw pointers we need to make sure resources are released.
+		DeleteSchemaObjects();
 	}
 
 	// Moveable, not copyable.
 	OutgoingComponentMessage(const OutgoingComponentMessage&) = delete;
-	OutgoingComponentMessage(OutgoingComponentMessage&& Other) = default;
 	OutgoingComponentMessage& operator=(const OutgoingComponentMessage& Other) = delete;
-	OutgoingComponentMessage& operator=(OutgoingComponentMessage&& Other) = default;
+
+	OutgoingComponentMessage(OutgoingComponentMessage&& Other) noexcept
+		: EntityId(Other.EntityId), ComponentId(Other.ComponentId), Type(Other.Type)
+	{
+		switch (Other.Type)
+		{
+		case NONE:
+			break;
+		case ADD:
+			ComponentAdded = Other.ComponentAdded;
+			Other.ComponentAdded = nullptr;
+			break;
+		case UPDATE:
+			ComponentUpdated = Other.ComponentUpdated;
+			Other.ComponentUpdated = nullptr;
+			break;
+		case REMOVE:
+			break;
+		}
+		Other.Type = NONE;
+	}
+
+	OutgoingComponentMessage& operator=(OutgoingComponentMessage&& Other) noexcept {
+
+		EntityId = Other.EntityId;
+		ComponentId = Other.ComponentId;
+
+		// As data is stored in owning raw pointers we need to make sure resources are released.
+		DeleteSchemaObjects();
+		switch (Other.Type)
+		{
+		case NONE:
+			break;
+		case ADD:
+			ComponentAdded = Other.ComponentAdded;
+			Other.ComponentAdded = nullptr;
+			break;
+		case UPDATE:
+			ComponentUpdated = Other.ComponentUpdated;
+			Other.ComponentUpdated = nullptr;
+			break;
+		case REMOVE:
+			break;
+		}
+
+		Other.Type = NONE;
+
+		return *this;
+	}
 
 	MessageType GetType() const
 	{
@@ -84,6 +119,23 @@ public:
 	Worker_ComponentId ComponentId;
 
 private:
+	void DeleteSchemaObjects()
+	{
+		switch (Type)
+		{
+		case NONE:
+			break;
+		case ADD:
+			Schema_DestroyComponentData(ComponentAdded);
+			break;
+		case UPDATE:
+			Schema_DestroyComponentUpdate(ComponentUpdated);
+			break;
+		case REMOVE:
+			break;
+		}
+	}
+
 	union
 	{
 		Schema_ComponentData* ComponentAdded;
