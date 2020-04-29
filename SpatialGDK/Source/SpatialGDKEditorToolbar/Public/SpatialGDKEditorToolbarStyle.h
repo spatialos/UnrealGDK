57222a37324136066bed5987ff73d998807b034b
余君庭 <kenyu@improbable.io>
2020-04-29 10:02:57 +0800

    Update SpatialGDK/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbarStyle.h
    Co-Authored-By: improbable-valentyn <valentyn@improbable.io>

--- SpatialGDK/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbarStyle.h
+++ SpatialGDK/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbarStyle.h
@@ -14,7 +14,8 @@ public:
 	static void Shutdown();
 	static void ReloadTextures();
 	static const ISlateStyle& Get();
-	static FName GetStyleSetName();	
+	static FName GetStyleSetName();
+
 private:
 	static TSharedRef<FSlateStyleSet> Create();
 	static TSharedPtr<FSlateStyleSet> StyleInstance;
