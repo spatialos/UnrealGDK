7f5e9d5e4bdc1a094e5973712d81d46e00fc0950
improbable-valentyn <valentyn@improbable.io>
2020-04-30 12:20:32 +0100

    Update pinned runtime version

--- SpatialGDK/Source/SpatialGDKServices/Public/SpatialGDKServicesConstants.h
+++ SpatialGDK/Source/SpatialGDKServices/Public/SpatialGDKServicesConstants.h
@@ -29,6 +29,6 @@ namespace SpatialGDKServicesConstants
 	const FString SpotExe = CreateExePath(GDKProgramPath, TEXT("spot"));
 	const FString SchemaCompilerExe = CreateExePath(GDKProgramPath, TEXT("schema_compiler"));
 	const FString SpatialOSDirectory = FPaths::ConvertRelativePathToFull(FPaths::Combine(FPaths::ProjectDir(), TEXT("/../spatial/")));
-	const FString SpatialOSRuntimePinnedVersion("14.5.1");
+	const FString SpatialOSRuntimePinnedVersion("14.6.1");
 	const FString SpatialOSConfigFileName = TEXT("spatialos.json");
 }
