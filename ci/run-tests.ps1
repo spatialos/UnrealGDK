4fd562782b3c39346ee19fdb7e9b98923213becc
Miron Zelina <mironzelina@improbable.io>
2020-05-04 17:50:51 +0100

    Add editor options

--- ci/run-tests.ps1
+++ ci/run-tests.ps1
@@ -7,6 +7,7 @@ param(
     [string] $report_output_path,
     [string] $tests_path = "SpatialGDK",
     [string] $additional_gdk_options = "",
+    [string] $additional_gdk_editor_options = "",
     [bool]   $run_with_spatial = $False
 )
 
@@ -17,6 +18,22 @@ function Force-ResolvePath {
         [string] $path
     )
     return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($path)
+}
+
+function Parse-UnrealOptions {
+    param (
+        [string] $raw_options
+        [string] $category
+    )
+    $options_arr = $raw_options.Split(";")
+    $options_result = ""
+    foreach ($option in $options_arr) {
+        if ($options_result -ne "") {
+            $options_result += ","
+        }
+        $options_result += "$category:$additional_gdk_option"
+    }
+    return $options_result
 }
 
 . "$PSScriptRoot\common.ps1"
@@ -51,14 +68,8 @@ $ue_path_absolute = Force-ResolvePath $unreal_editor_path
 $uproject_path_absolute = Force-ResolvePath $uproject_path
 $output_dir_absolute = Force-ResolvePath $output_dir
 
-$additional_gdk_options_arr = $additional_gdk_options.Split(";")
-$additional_gdk_options = ""
-Foreach ($additional_gdk_option in $additional_gdk_options_arr) {
-    if ($additional_gdk_options -ne "") {
-        $additional_gdk_options += ","
-    }
-    $additional_gdk_options += "[/Script/SpatialGDK.SpatialGDKSettings]:$additional_gdk_option"
-}
+$additional_gdk_options = Parse-UnrealOptions "$additional_gdk_options" "[/Script/SpatialGDK.SpatialGDKSettings]"
+$additional_gdk_editor_options = Parse-UnrealOptions "$additional_gdk_editor_options" "[/Script/SpatialGDKEditor.SpatialGDKEditorSettings]"
 
 $cmd_args_list = @( `
     "`"$uproject_path_absolute`"", # We need some project to run tests in, but for unit tests the exact project shouldn't matter
@@ -73,6 +84,7 @@ $cmd_args_list = @( `
     "-nullRHI", # Hard to find documentation for, but seems to indicate that we want something akin to a headless (i.e. no UI / windowing) editor
     "-stdout", # Print to output
     "-ini:SpatialGDKSettings:$additional_gdk_options" # Pass changes to configuration files from above
+    "-ini:SpatialGDKEditorSettings:$additional_gdk_editor_options" # Pass changes to editor configuration files from above
     "-OverrideSpatialNetworking=$run_with_spatial" # A parameter to switch beetween different networking implementations
 )
 
