811c2dd662cc0d6917bf25eec2e39b7baf59eff6
Andreas Krügersen <andreaskrugersen@improbable.io>
2020-05-04 16:51:35 +0100

    Interfaces are now included when collecting remote functions

--- SpatialGDK/Source/SpatialGDK/Public/Utils/RepLayoutUtils.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/RepLayoutUtils.h
@@ -87,7 +87,7 @@ inline void RepLayout_SendPropertiesForRPC(FRepLayout& RepLayout, FNetBitWriter&
 		if (!Cast<UBoolProperty>(Parent.Property))
 		{
 			// check for a complete match, including arrays
-			// (we're comparing against zero data here, since 
+			// (we're comparing against zero data here, since
 			// that's the default.)
 			bSend = !Parent.Property->Identical_InContainer(Data, NULL, Parent.ArrayIndex);
 
@@ -162,7 +162,9 @@ inline TArray<UFunction*> GetClassRPCFunctions(const UClass* Class)
 	// Get all remote functions from the class. This includes parents super functions and child override functions.
 	TArray<UFunction*> AllClassFunctions;
 
-	for (TFieldIterator<UFunction> RemoteFunction(Class); RemoteFunction; ++RemoteFunction)
+	TFieldIterator<UFunction> RemoteFunction(Class, EFieldIteratorFlags::IncludeSuper, EFieldIteratorFlags::IncludeDeprecated,
+		EFieldIteratorFlags::IncludeInterfaces);
+	for (; RemoteFunction; ++RemoteFunction)
 	{
 		if (RemoteFunction->FunctionFlags & FUNC_NetClient ||
 			RemoteFunction->FunctionFlags & FUNC_NetServer ||
