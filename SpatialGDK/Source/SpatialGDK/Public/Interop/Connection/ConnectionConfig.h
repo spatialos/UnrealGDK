58bd3526d2d91b809af3c0a811ac1ad69ff8a9eb
Andreas Krügersen <andreaskrugersen@improbable.io>
2020-05-13 18:21:22 +0100

    Read parameter at correct location

--- SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/ConnectionConfig.h
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/ConnectionConfig.h
@@ -25,7 +25,6 @@ struct FConnectionConfig
 		const TCHAR* CommandLine = FCommandLine::Get();
 
 		FParse::Value(CommandLine, TEXT("workerId"), WorkerId);
-		FParse::Bool(CommandLine, *SpatialConstants::URL_USE_EXTERNAL_IP_FOR_BRIDGE_OPTION, UseExternalIp);
 		FParse::Bool(CommandLine, TEXT("enableProtocolLogging"), EnableProtocolLoggingAtStartup);
 		FParse::Value(CommandLine, TEXT("protocolLoggingPrefix"), ProtocolLoggingPrefix);
 
@@ -179,6 +178,7 @@ public:
 
 		// Get command line options first since the URL handling will modify the CommandLine string
 		FParse::Value(CommandLine, TEXT("receptionistPort"), ReceptionistPort);
+		FParse::Bool(CommandLine, *SpatialConstants::URL_USE_EXTERNAL_IP_FOR_BRIDGE_OPTION, UseExternalIp);
 
 		// Parse the command line for receptionistHost, if it exists then use this as the host IP.
 		FString Host;
