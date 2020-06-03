0f0da8d961863e1759e2b5ecf37bbf68e69018b3
Oliver Balaam <oliverbalaam@improbable.io>
2020-06-03 17:59:23 +0100

    fix {} bracket pairs

--- ci/ReleaseTool/PrepCommand.cs
+++ ci/ReleaseTool/PrepCommand.cs
@@ -175,8 +175,14 @@ namespace ReleaseTool
                             }
 
                             throw;
+                        }
                     }
-
+                    
+                    else
+                    {
+                        Logger.Info("A PR has already been opened from candidate branch into release branch: {0}", pullRequest.HtmlUrl);
+                    }
+                    
 
                     BuildkiteAgent.SetMetaData($"{options.GitRepoName}-{options.SourceBranch}-pr-url", pullRequest.HtmlUrl);
 
