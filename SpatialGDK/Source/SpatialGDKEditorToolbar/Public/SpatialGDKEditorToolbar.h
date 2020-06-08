78129494d27456e4ad3d86deb5851760824b32c3
Evi Kalogiannidou <evmorfiakalogiannidou@improbable.io>
2020-06-08 15:36:19 +0100

    Add line back

--- SpatialGDK/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbar.h
+++ SpatialGDK/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbar.h
@@ -61,6 +61,7 @@ public:
 	bool IsSimulatedPlayersEnabled() const;
 	/** Delegate called when the user either clicks the simulated players checkbox */
 	void OnCheckedSimulatedPlayers();
+
 	bool IsBuildClientWorkerEnabled() const;
 	void OnCheckedBuildClientWorker();
 
