c4f7677a2ae4d05a9af87fc811109d692b064b96
Joshua Huburn <joshuahuburn@improbable.io>
2020-06-09 13:01:01 +0100

    Add inital logging support for new runtime

--- SpatialGDK/Source/SpatialGDKServices/Public/SpatialGDKServicesConstants.h
+++ SpatialGDK/Source/SpatialGDKServices/Public/SpatialGDKServicesConstants.h
@@ -29,7 +29,7 @@ namespace SpatialGDKServicesConstants
 	const FString SpotExe = CreateExePath(GDKProgramPath, TEXT("spot"));
 	const FString SchemaCompilerExe = CreateExePath(GDKProgramPath, TEXT("schema_compiler"));
 	const FString SpatialOSDirectory = FPaths::ConvertRelativePathToFull(FPaths::Combine(FPaths::ProjectDir(), TEXT("/../spatial/")));
-	const FString SpatialOSRuntimePinnedStandardVersion("0.4.0-preview-2");
+	const FString SpatialOSRuntimePinnedStandardVersion("0.4.0-preview-5");
 	const FString SpatialOSRuntimePinnedCompatbilityModeVersion("14.5.1");
 	const FString SpatialOSConfigFileName = TEXT("spatialos.json");
 	const FString ChinaEnvironmentArgument = TEXT(" --environment=cn-production");
