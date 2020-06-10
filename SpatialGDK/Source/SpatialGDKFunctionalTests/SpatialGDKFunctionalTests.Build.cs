4e248d8d020f9a178ad7159a8073f04f897b297b
Jose Pinhao <jose@improbable.io>
2020-06-10 18:48:16 +0100

    Ad FunctionalTesting module dependency

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDKFunctionalTests.Build.cs
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDKFunctionalTests.Build.cs
@@ -21,7 +21,8 @@ public class SpatialGDKFunctionalTests : ModuleRules
                 "SpatialGDK",
                 "Core",
                 "CoreUObject",
-                "Engine"
+                "Engine",
+                "FunctionalTesting"
             });
     }
 }
