414ef2227277f5e8a3ddc74f3fa9d9df3a1f7429
Oliver Balaam <oliverbalaam@improbable.io>
2020-06-10 18:30:43 +0100

    quick var declare fix

--- ci/ReleaseTool/ReleaseCommand.cs
+++ ci/ReleaseTool/ReleaseCommand.cs
@@ -211,6 +211,7 @@ namespace ReleaseTool
                     Thread.Sleep(TimeSpan.FromMinutes(1));
                 }
 
+                mergeResult result = null;
                 while (true)
                 {
                     // Merge into release
