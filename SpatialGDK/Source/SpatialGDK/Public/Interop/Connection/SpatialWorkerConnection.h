e8f5c8e47bcaefe01079b60df74e33e04a0678d2
samiwh <sami@improbable.io>
2020-07-02 00:57:52 +0100

    Split the worker connection into a legacy and spatial view one.

--- SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/SpatialWorkerConnection.h
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/SpatialWorkerConnection.h
@@ -2,89 +2,35 @@
 
 #pragma once
 
-#include "Containers/Queue.h"
-#include "HAL/Event.h"
-#include "HAL/Runnable.h"
-#include "HAL/ThreadSafeBool.h"
 #include "Interop/Connection/OutgoingMessages.h"
 #include "Interop/Connection/SpatialOSWorkerInterface.h"
-#include "Interop/Connection/WorkerConnectionCoordinator.h"
 #include "SpatialCommonTypes.h"
-#include "UObject/WeakObjectPtr.h"
-
-#include <WorkerSDK/improbable/c_schema.h>
-#include <WorkerSDK/improbable/c_worker.h>
 
 #include "SpatialWorkerConnection.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogSpatialWorkerConnection, Log, All);
 
-UCLASS()
-class SPATIALGDK_API USpatialWorkerConnection : public UObject, public FRunnable, public SpatialOSWorkerInterface
+UCLASS(abstract)
+class SPATIALGDK_API USpatialWorkerConnection : public UObject, public SpatialOSWorkerInterface
 {
 	GENERATED_BODY()
 
 public:
-	void SetConnection(Worker_Connection* WorkerConnectionIn);
-	virtual void FinishDestroy() override;
-	void DestroyConnection();
+	virtual void SetConnection(Worker_Connection* WorkerConnectionIn) PURE_VIRTUAL(USpatialWorkerConnection::SetConnection, return;);
+	virtual void FinishDestroy() override
+	{
+		Super::FinishDestroy();
+	}
+	virtual void DestroyConnection() PURE_VIRTUAL(USpatialWorkerConnection::DestroyConnection, return;);
 
-	// Worker Connection Interface
-	virtual TArray<Worker_OpList*> GetOpList() override;
-	virtual Worker_RequestId SendReserveEntityIdsRequest(uint32_t NumOfEntities) override;
-	virtual Worker_RequestId SendCreateEntityRequest(TArray<FWorkerComponentData>&& Components, const Worker_EntityId* EntityId) override;
-	virtual Worker_RequestId SendDeleteEntityRequest(Worker_EntityId EntityId) override;
-	virtual void SendAddComponent(Worker_EntityId EntityId, FWorkerComponentData* ComponentData) override;
-	virtual void SendRemoveComponent(Worker_EntityId EntityId, Worker_ComponentId ComponentId) override;
-	virtual void SendComponentUpdate(Worker_EntityId EntityId, const FWorkerComponentUpdate* ComponentUpdate) override;
-	virtual Worker_RequestId SendCommandRequest(Worker_EntityId EntityId, const Worker_CommandRequest* Request, uint32_t CommandId) override;
-	virtual void SendCommandResponse(Worker_RequestId RequestId, const Worker_CommandResponse* Response) override;
-	virtual void SendCommandFailure(Worker_RequestId RequestId, const FString& Message) override;
-	virtual void SendLogMessage(uint8_t Level, const FName& LoggerName, const TCHAR* Message) override;
-	virtual void SendComponentInterest(Worker_EntityId EntityId, TArray<Worker_InterestOverride>&& ComponentInterest) override;
-	virtual Worker_RequestId SendEntityQueryRequest(const Worker_EntityQuery* EntityQuery) override;
-	virtual void SendMetrics(const SpatialGDK::SpatialMetrics& Metrics) override;
+	virtual PhysicalWorkerName GetWorkerId() const PURE_VIRTUAL(USpatialWorkerConnection::GetWorkerId, return PhysicalWorkerName(););
+	virtual const TArray<FString>& GetWorkerAttributes() const PURE_VIRTUAL(USpatialWorkerConnection::GetWorkerAttributes, return ReturnValuePlaceholder;);
 
-	PhysicalWorkerName GetWorkerId() const;
-	const TArray<FString>& GetWorkerAttributes() const;
-
-	DECLARE_MULTICAST_DELEGATE_OneParam(FOnEnqueueMessage, const SpatialGDK::FOutgoingMessage*);
-	FOnEnqueueMessage OnEnqueueMessage;
-
-	DECLARE_MULTICAST_DELEGATE_OneParam(FOnDequeueMessage, const SpatialGDK::FOutgoingMessage*);
-	FOnDequeueMessage OnDequeueMessage;
-
-	void QueueLatestOpList();
-	void ProcessOutgoingMessages();
-	void MaybeFlush();
-	void Flush();
+	virtual void ProcessOutgoingMessages() PURE_VIRTUAL(USpatialWorkerConnection::ProcessOutgoingMessages, return;);
+	virtual void MaybeFlush() PURE_VIRTUAL(USpatialWorkerConnection::MaybeFlush, return;);
+	virtual void Flush() PURE_VIRTUAL(USpatialWorkerConnection::Flush, return;);
 
 private:
-	void CacheWorkerAttributes();
-
-	// Begin FRunnable Interface
-	virtual uint32 Run() override;
-	virtual void Stop() override;
-	// End FRunnable Interface
-
-	void InitializeOpsProcessingThread();
-
-	template <typename T, typename... ArgsType>
-	void QueueOutgoingMessage(ArgsType&&... Args);
-
-	Worker_Connection* WorkerConnection;
-
-	TArray<FString> CachedWorkerAttributes;
-
-	FRunnableThread* OpsProcessingThread;
-	FThreadSafeBool KeepRunning = true;
-
-	TQueue<Worker_OpList*> OpListQueue;
-	TQueue<TUniquePtr<SpatialGDK::FOutgoingMessage>> OutgoingMessagesQueue;
-
-	// RequestIds per worker connection start at 0 and incrementally go up each command sent.
-	Worker_RequestId NextRequestId = 0;
-
-	// Coordinates the async worker ops thread.
-	TOptional<WorkerConnectionCoordinator> ThreadWaitCondition;
+	// Exists for the sake of having PURE_VIRTUAL functions returning a const ref.
+	TArray<FString> ReturnValuePlaceholder;
 };
