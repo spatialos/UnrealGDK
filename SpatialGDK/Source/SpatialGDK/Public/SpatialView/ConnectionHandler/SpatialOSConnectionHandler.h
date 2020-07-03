2cd16b9e6bf3c4094594d574d9f54dc76a80af70
samiwh <sami@improbable.io>
2020-07-03 15:55:00 +0100

    Changed the abstract connection handler interface, added the spatialos connection handler and removed the previously planned shim one.

---
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/ConnectionHandler/SpatialOSConnectionHandler.h
@@ -0,0 +1,36 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "SpatialView/ConnectionHandler/AbstractConnectionHandler.h"
+#include "SpatialView/OpList/OpList.h"
+#include "SpatialView/OpList/WorkerConnectionOpList.h"
+
+namespace SpatialGDK
+{
+
+class SpatialOSConnectionHandler : public AbstractConnectionHandler
+{
+public:
+	explicit SpatialOSConnectionHandler(Worker_Connection* Connection);
+
+	void Advance() override;
+	uint32 GetOpListCount() override;
+	OpList GetNextOpList() override;
+	void SendMessages(TUniquePtr<MessagesToSend> Messages) override;
+	const FString& GetWorkerId() const override;
+	const TArray<FString>& GetWorkerAttributes() const override;
+
+private:
+	struct ConnectionDeleter
+	{
+		void operator()(Worker_Connection* Connection) const noexcept;
+	};
+
+	TUniquePtr<Worker_Connection, ConnectionDeleter> Connection;
+	TMap<int64, int64> InternalToUserRequestId;
+	FString WorkerId;
+	TArray<FString> WorkerAttributes;
+};
+
+}  // namespace SpatialGDK
