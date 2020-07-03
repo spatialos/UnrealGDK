6acbecba461b9d8cb9d2641067277b55e09037ae
samiwh <sami@improbable.io>
2020-07-03 15:54:59 +0100

    Added the entity presence record.

---
+++ SpatialGDK/Source/SpatialGDK/Private/SpatialView/EntityPresenceRecord.cpp
@@ -0,0 +1,38 @@
+﻿#include "SpatialView/EntityPresenceRecord.h"
+
+namespace SpatialGDK
+{
+
+void EntityPresenceRecord::AddEntity(Worker_EntityId EntityId)
+{
+	if (!EntitiesRemoved.RemoveSwap(EntityId))
+	{
+		EntitiesAdded.Add(EntityId);
+	}
+}
+
+void EntityPresenceRecord::RemoveEntity(Worker_EntityId EntityId)
+{
+	if (!EntitiesAdded.RemoveSingleSwap(EntityId))
+	{
+		EntitiesRemoved.Add(EntityId);
+	}
+}
+
+void EntityPresenceRecord::Clear()
+{
+	EntitiesAdded.Empty();
+	EntitiesRemoved.Empty();
+}
+
+const TArray<Worker_EntityId>& EntityPresenceRecord::GetEntitiesAdded() const
+{
+	return EntitiesAdded;
+}
+
+const TArray<Worker_EntityId>& EntityPresenceRecord::GetEntitiesRemoved() const
+{
+	return EntitiesRemoved;
+}
+
+}  // namespace SpatialGDK
