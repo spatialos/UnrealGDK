245185efbe7e57c08b26bc03b7c037433ace9373
samiwh <sami@improbable.io>
2020-07-03 15:54:59 +0100

    Added command and response wrappers.

---
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/CommandRequest.h
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "Templates/UniquePtr.h"
+#include <improbable/c_worker.h>
+#include <improbable/c_schema.h>
+
+namespace SpatialGDK
+{
+
+struct CommandRequestDeleter
+{
+	void operator()(Schema_CommandRequest* CommandRequest) const noexcept
+	{
+		if (CommandRequest)
+		{
+			Schema_DestroyCommandRequest(CommandRequest);
+		}
+	}
+};
+
+using OwningCommandRequestPtr = TUniquePtr<Schema_CommandRequest, CommandRequestDeleter>;
+
+// An RAII wrapper for component data.
+class CommandRequest
+{
+public:
+	// Creates a new component data.
+	explicit CommandRequest(Worker_ComponentId ComponentId, Worker_CommandIndex CommandIndex);
+	// Takes ownership of component data.
+	explicit CommandRequest(OwningCommandRequestPtr Data, Worker_ComponentId ComponentId, Worker_CommandIndex CommandIndex);
+
+	~CommandRequest() = default;
+
+	// Moveable, not copyable.
+	CommandRequest(const CommandRequest&) = delete;
+	CommandRequest(CommandRequest&&) = default;
+	CommandRequest& operator=(const CommandRequest&) = delete;
+	CommandRequest& operator=(CommandRequest&&) = default;
+
+	static CommandRequest CreateCopy(const Schema_CommandRequest* Data, Worker_ComponentId ComponentId, Worker_CommandIndex CommandIndex);
+
+	// Creates a copy of the command request.
+	CommandRequest DeepCopy() const;
+	// Releases ownership of the command request.
+	Schema_CommandRequest* Release() &&;
+
+	Schema_Object* GetRequestObject() const;
+
+	Schema_CommandRequest* GetUnderlying() const;
+
+	Worker_ComponentId GetComponentId() const;
+	Worker_CommandIndex GetCommandIndex() const;
+
+private:
+	Worker_ComponentId ComponentId;
+	Worker_CommandIndex CommandIndex;
+	OwningCommandRequestPtr Data;
+};
+
+} // namespace SpatialGDK
