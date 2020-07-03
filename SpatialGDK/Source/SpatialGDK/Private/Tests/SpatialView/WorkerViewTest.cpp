5d2f469fe963503a8dba0946cc6aa30299b6356b
samiwh <sami@improbable.io>
2020-07-03 15:55:00 +0100

    Removed and fixed tests.

--- SpatialGDK/Source/SpatialGDK/Private/Tests/SpatialView/WorkerViewTest.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Tests/SpatialView/WorkerViewTest.cpp
@@ -8,7 +8,7 @@
 #define WORKERVIEW_TEST(TestName) \
 	GDK_TEST(Core, WorkerView, TestName)
 
-using namespace SpatialGDK; 
+using namespace SpatialGDK;
 
 namespace
 {
@@ -26,11 +26,11 @@ WORKERVIEW_TEST(GIVEN_WorkerView_with_one_CreateEntityRequest_WHEN_FlushLocalCha
 {
 	// GIVEN
 	WorkerView View;
-	CreateEntityRequest Request;
-	View.SendCreateEntityRequest(Request);
+	CreateEntityRequest Request = {};
+	View.SendCreateEntityRequest(MoveTemp(Request));
 
 	// WHEN
-	auto Messages = View.FlushLocalChanges();
+	const auto Messages = View.FlushLocalChanges();
 
 	// THEN
 	TestTrue("WorkerView has one CreateEntityRequest", Messages->CreateEntityRequests.Num() == 1);
@@ -42,51 +42,14 @@ WORKERVIEW_TEST(GIVEN_WorkerView_with_multiple_CreateEntityRequest_WHEN_FlushLoc
 {
 	// GIVEN
 	WorkerView View;
-	CreateEntityRequest Request;
-	View.SendCreateEntityRequest(Request);
-	View.SendCreateEntityRequest(Request);
+	CreateEntityRequest Request = {};
+	View.SendCreateEntityRequest(MoveTemp(Request));
+	View.SendCreateEntityRequest(MoveTemp(Request));
 
 	auto Messages = View.FlushLocalChanges();
 
 	// THEN
 	TestTrue("WorkerView has multiple CreateEntityRequest", Messages->CreateEntityRequests.Num() > 1);
-
-	return true;
-}
-
-WORKERVIEW_TEST(GIVEN_WorkerView_with_one_op_enqued_WHEN_GenerateViewDelta_called_THEN_ViewDelta_with_one_op_returned)
-{
-	// GIVEN
-	WorkerView View;
-	TArray<Worker_Op> Ops;
-	Ops.Push(CreateEmptyCreateEntityResponseOp());
-	auto OpList = MakeUnique<ViewDeltaLegacyOpList>(Ops);
-	View.EnqueueOpList(MoveTemp(OpList));
-
-	// WHEN
-	auto ViewDelta = View.GenerateViewDelta();
-
-	// THEN
-	TestTrue("ViewDelta has one op", ViewDelta->GenerateLegacyOpList()->GetCount() == 1);
-
-	return true;
-}
-
-WORKERVIEW_TEST(GIVEN_WorkerView_with_multiple_ops_engued_WHEN_GenerateViewDelta_called_THEN_ViewDelta_with_multiple_ops_returned)
-{
-	// GIVEN
-	WorkerView View;
-	TArray<Worker_Op> Ops;
-	Ops.Push(CreateEmptyCreateEntityResponseOp());
-	Ops.Push(CreateEmptyCreateEntityResponseOp());
-	auto OpList = MakeUnique<ViewDeltaLegacyOpList>(Ops);
-	View.EnqueueOpList(MoveTemp(OpList));
-
-	// WHEN
-	auto ViewDelta = View.GenerateViewDelta();
-
-	// THEN
-	TestTrue("ViewDelta has multiple ops", ViewDelta->GenerateLegacyOpList()->GetCount() > 1);
 
 	return true;
 }
