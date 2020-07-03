a57a74b64f70302b425e52a1f699be1fd4f7f008
samiwh <sami@improbable.io>
2020-07-03 15:55:00 +0100

    Added flushing on exit to the view coordinator and made it work better with the shim layer.

--- SpatialGDK/Source/SpatialGDK/Public/SpatialView/ViewCoordinator.h
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/ViewCoordinator.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "SpatialView/WorkerView.h"
-#include "SpatialView/ConnectionHandlers/AbstractConnectionHandler.h"
+#include "SpatialView/ConnectionHandler/AbstractConnectionHandler.h"
 #include "Templates/UniquePtr.h"
 
 namespace SpatialGDK
@@ -14,28 +14,39 @@ class ViewCoordinator
 public:
 	explicit ViewCoordinator(TUniquePtr<AbstractConnectionHandler> ConnectionHandler);
 
-	void Advance();
+	~ViewCoordinator();
+
+	// Moveable, not copyable.
+	ViewCoordinator(const ViewCoordinator&) = delete;
+	ViewCoordinator(ViewCoordinator&&) = delete;
+	ViewCoordinator& operator=(const ViewCoordinator&) = delete;
+	ViewCoordinator& operator=(ViewCoordinator&&) = delete;
+
+	OpList Advance();
 	void FlushMessagesToSend();
+
+	const FString& GetWorkerId() const;
+	const TArray<FString>& GetWorkerAttributes() const;
 
 	void SendAddComponent(Worker_EntityId EntityId, ComponentData Data);
 	void SendComponentUpdate(Worker_EntityId EntityId, ComponentUpdate Update);
 	void SendRemoveComponent(Worker_EntityId EntityId, Worker_ComponentId ComponentId);
-
-	const TArray<CreateEntityResponse>& GetCreateEntityResponses() const;
-	const TArray<EntityComponentId>& GetAuthorityGained() const;
-	const TArray<EntityComponentId>& GetAuthorityLost() const;
-	const TArray<EntityComponentId>& GetAuthorityLostTemporarily() const;
-	const TArray<EntityComponentData>& GetComponentsAdded() const;
-	const TArray<EntityComponentId>& GetComponentsRemoved() const;
-	const TArray<EntityComponentUpdate>& GetUpdates() const;
-	const TArray<EntityComponentCompleteUpdate>& GetCompleteUpdates() const;
-
-	TUniquePtr<AbstractOpList> GenerateLegacyOpList() const;
+	Worker_RequestId SendReserveEntityIdsRequest(uint32 NumberOfEntityIds, TOptional<uint32> TimeoutMillis = {});
+	Worker_RequestId SendCreateEntityRequest(TArray<ComponentData> EntityComponents,
+		TOptional<Worker_EntityId> EntityId, TOptional<uint32> TimeoutMillis = {});
+	Worker_RequestId SendDeleteEntityRequest(Worker_EntityId EntityId, TOptional<uint32> TimeoutMillis = {});
+	Worker_RequestId SendEntityQueryRequest(EntityQuery Query, TOptional<uint32> TimeoutMillis = {});
+	Worker_RequestId SendEntityCommandRequest(Worker_EntityId EntityId, CommandRequest Request,
+		TOptional<uint32> TimeoutMillis = {});
+	void SendEntityCommandResponse(Worker_RequestId RequestId, CommandResponse Response);
+	void SendEntityCommandFailure(Worker_RequestId RequestId, FString Message);
+	void SendMetrics(SpatialMetrics Metrics);
+	void SendLogMessage(Worker_LogLevel Level, const FName& LoggerName, FString Message);
 
 private:
-	const ViewDelta* Delta;
 	WorkerView View;
 	TUniquePtr<AbstractConnectionHandler> ConnectionHandler;
+	Worker_RequestId NextRequestId;
 };
 
 } // namespace SpatialGDK
