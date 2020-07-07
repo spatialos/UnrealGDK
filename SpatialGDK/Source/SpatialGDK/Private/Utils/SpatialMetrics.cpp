f240790ef1cd41c42c642396cb22977b1ea9483e
Danny Birch <danielbirch@improbable.io>
2020-07-07 17:02:06 +0100

    Build fix

--- SpatialGDK/Source/SpatialGDK/Private/Utils/SpatialMetrics.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Utils/SpatialMetrics.cpp
@@ -11,8 +11,6 @@
 
 DEFINE_LOG_CATEGORY(LogSpatialMetrics);
 
-USpatialMetrics::WorkerMetricsDelegate USpatialMetrics::WorkerMetricsRecieved;
-
 void USpatialMetrics::Init(USpatialWorkerConnection* InConnection, float InNetServerMaxTickRate, bool bInIsServer)
 {
 	Connection = InConnection;
