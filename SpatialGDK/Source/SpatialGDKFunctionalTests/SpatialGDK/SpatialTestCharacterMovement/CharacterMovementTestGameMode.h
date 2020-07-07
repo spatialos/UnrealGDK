30dbb27879e944a98cab3330066dbf9e1e4a8682
Andrei Lazar <andreilazar@improbable.io>
2020-07-07 17:16:28 +0100

    Added Character Movement Test (#2317)

---
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestCharacterMovement/CharacterMovementTestGameMode.h
@@ -0,0 +1,17 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CharacterMovementTestGameMode.generated.h"
+
+/**
+ * GameMode used for the SpatialTestCharacterMovementMap
+ */
+UCLASS(Blueprintable)
+class ACharacterMovementTestGameMode : public AGameModeBase
+{
+	GENERATED_UCLASS_BODY()
+	
+};
---
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestCharacterMovement/TestMovementCharacter.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+
+#include "TestMovementCharacter.h"
+#include "Engine/Classes/Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Materials/Material.h"
+#include "Components/CapsuleComponent.h"
+
+ATestMovementCharacter::ATestMovementCharacter()
+{
+	bReplicates = true;
+	bReplicateMovement = true;
+
+	GetCapsuleComponent()->InitCapsuleSize(38.0f, 38.0f);
+
+	SphereComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetStaticMesh(LoadObject<UStaticMesh>(nullptr, TEXT("StaticMesh'/Engine/BasicShapes/Sphere.Sphere'")));
+	SphereComponent->SetMaterial(0, LoadObject<UMaterial>(nullptr, TEXT("Material'/Engine/BasicShapes/BasicShapeMaterial.BasicShapeMaterial'")));
+	SphereComponent->SetVisibility(true);
+	SphereComponent->SetupAttachment(GetCapsuleComponent());
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->bAbsoluteLocation = false;
+	CameraComponent->bAbsoluteRotation = false;
+	CameraComponent->RelativeLocation = FVector(300.0f, 0.0f, 75.0f);
+	CameraComponent->RelativeRotation = FRotator::MakeFromEuler(FVector(0.0f, -10.0f, 180.0f));
+	CameraComponent->SetupAttachment(GetCapsuleComponent());
+}
