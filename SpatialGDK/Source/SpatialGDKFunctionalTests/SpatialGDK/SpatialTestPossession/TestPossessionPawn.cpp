4f71f0b5108748f1c2495a129cf4001da0a75b79
nafonso <nafonso@gmail.com>
2020-07-10 10:14:39 +0100

    Finxing warnings in Test classes

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestPossession/TestPossessionPawn.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestPossession/TestPossessionPawn.cpp
@@ -18,8 +18,18 @@ ATestPossessionPawn::ATestPossessionPawn()
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
 	CameraComponent->SetupAttachment(RootComponent);
 
+	FVector CameraLocation = FVector(300.0f, 0.0f, 75.0f);
+	FRotator CameraRotation = FRotator::MakeFromEuler(FVector(0.0f, -10.0f, 180.0f));
+
+#if ENGINE_MINOR_VERSION < 24
 	CameraComponent->bAbsoluteLocation = false;
 	CameraComponent->bAbsoluteRotation = false;
-	CameraComponent->RelativeLocation = FVector(300.0f, 0.0f, 75.0f);
-	CameraComponent->RelativeRotation = FRotator::MakeFromEuler(FVector(0.0f, -10.0f, 180.0f));
+	CameraComponent->RelativeLocation = CameraLocation;
+	CameraComponent->RelativeRotation = CameraRotation;
+#else
+	CameraComponent->SetUsingAbsoluteLocation(false);
+	CameraComponent->SetUsingAbsoluteRotation(false);
+	CameraComponent->SetRelativeLocation(CameraLocation);
+	CameraComponent->SetRelativeRotation(CameraRotation);
+#endif
 }
