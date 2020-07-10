68c686c1a36dbfa2b2810be18ea81ade3ee6b12d
Matt Young <matthewyoung@improbable.io>
2020-07-10 15:42:22 +0100

    Make speedy schema gen

--- SpatialGDK/Source/SpatialGDKEditorCommandlet/Private/Commandlets/GenerateSchemaCommandlet.cpp
+++ SpatialGDK/Source/SpatialGDKEditorCommandlet/Private/Commandlets/GenerateSchemaCommandlet.cpp
@@ -5,8 +5,61 @@
 #include "SpatialGDKEditor.h"
 #include "SpatialGDKEditorCommandletPrivate.h"
 #include "SpatialGDKEditorSchemaGenerator.h"
+#include "Modules/ModuleManager.h"
+#include "Engine/World.h"
+#include "AssetRegistryModule.h"
 
 using namespace SpatialGDKEditor::Schema;
+
+DEFINE_LOG_CATEGORY(LogGenerateSchemaCommandlet);
+
+struct FObjectListener : public FUObjectArray::FUObjectCreateListener
+{
+public:
+	void StartListening(TSet<FSoftClassPath>* ClassesFound)
+	{
+		VisitedClasses = ClassesFound;
+		GUObjectArray.AddUObjectCreateListener(this);
+	}
+
+	void StopListening()
+	{
+		GUObjectArray.RemoveUObjectCreateListener(this);
+	}
+
+	virtual void NotifyUObjectCreated(const UObjectBase* Object, int32 Index) override
+	{
+		FSoftClassPath SoftClass = FSoftClassPath(Object->GetClass());
+		if (UnsupportedClasses.Contains(SoftClass))
+		{
+			return;
+		}
+		if (!VisitedClasses->Contains(SoftClass))
+		{
+			if (IsSupportedClass(Object->GetClass()))
+			{
+				UE_LOG(LogGenerateSchemaCommandlet, Verbose, TEXT("Object [%s] Created, Consider Class [%s] For Schema."),
+					*Object->GetFName().ToString(), *GetPathNameSafe(Object->GetClass()));
+				VisitedClasses->Add(SoftClass);
+			}
+			else
+			{
+				UnsupportedClasses.Add(SoftClass);
+			}
+		}
+	}
+
+#if ENGINE_MINOR_VERSION >= 23
+	virtual void OnUObjectArrayShutdown() override
+	{
+		GUObjectArray.RemoveUObjectCreateListener(this);
+	}
+#endif
+
+private:
+	TSet<FSoftClassPath>* VisitedClasses;
+	TSet<FSoftClassPath> UnsupportedClasses;
+};
 
 UGenerateSchemaCommandlet::UGenerateSchemaCommandlet()
 {
@@ -16,43 +69,193 @@ UGenerateSchemaCommandlet::UGenerateSchemaCommandlet()
 	LogToConsole = true;
 }
 
-bool UGenerateSchemaCommandlet::HandleOptions(const TArray<FString>& Switches)
+void UGenerateSchemaCommandlet::CompileBlueprint(UBlueprint* Blueprint)
 {
-	if (Switches.Contains("delete-schema-db"))
-	{
-		if (DeleteSchemaDatabase(SpatialConstants::SCHEMA_DATABASE_FILE_PATH))
-		{
-			UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("Deleted schema database"));
-		}
-		else
-		{
-			UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("Failed to delete schema database"));
-			return false;
-		}
-	}
-	return true;
+	// Don't actually compile anything
+	return;
 }
 
-int32 UGenerateSchemaCommandlet::Main(const FString& Args)
+void UGenerateSchemaCommandlet::LoadLevelBlueprints()
 {
-	UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("Schema Generation Commandlet Started"));
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+	TArray<FAssetData> AssetData;
+	FARFilter Filter;
+	Filter.bRecursivePaths = true;
+	Filter.ClassNames.Add(UWorld::StaticClass()->GetFName());
+	for (const FString MapPackagePath : MapPackagePaths)
+	{
+		Filter.PackagePaths.Add(FName(*MapPackagePath));
+	}
+	
+	AssetRegistryModule.Get().GetAssets(Filter, AssetData);
+
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Found Maps:"));
+	for (FAssetData Asset : AssetData)
+	{
+		UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("- %s"), *Asset.ObjectPath.ToString());
+		FString PackageName, ObjectName;
+
+		if (Asset.ObjectPath.ToString().Split(TEXT("."), &PackageName, &ObjectName, ESearchCase::IgnoreCase, ESearchDir::FromEnd))
+		{
+			UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("  Making Linker to look for %s_C in %s"), *ObjectName, *PackageName);
+			FLinkerLoad* Linker = LoadPackageLinker(nullptr, *PackageName, LOAD_NoVerify);
+			if (Linker)
+			{
+				UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("  Linker Valid, searching %d exports"), Linker->ExportMap.Num());
+				FName ObjectFName = FName(*FString::Printf(TEXT("%s_C"), *ObjectName));
+				for (FObjectExport Export : Linker->ExportMap)
+				{
+					if (Export.ObjectName == ObjectFName)
+					{
+						UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("  Found %s, loading"), *ObjectFName.ToString());
+						UBlueprintGeneratedClass* LevelBP = LoadObject<UBlueprintGeneratedClass>(nullptr, *FString::Printf(TEXT("%s_C"), *Asset.ObjectPath.ToString()), nullptr,
+							LOAD_NoWarn | LOAD_NoVerify | LOAD_Quiet);
+						if (LevelBP)
+						{
+							UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Loaded Level BP Class: %s"), *GetPathNameSafe(LevelBP));
+						}
+						break;
+					}
+				}
+			}
+		}
+	}	
+}
+
+void UGenerateSchemaCommandlet::InitCmdLine(const FString& CmdLineParams)
+{
+	TArray<FString> Tokens;
+	TArray<FString> Switches;
+	TMap<FString, FString> SwitchParams;
+	ParseCommandLine(*CmdLineParams, Tokens, Switches, SwitchParams);
+
+	MapPackagePaths.Empty();
+	if (SwitchParams.Contains(TEXT("MapPackagePaths")))
+	{
+		const FString& AllMapPackagePaths = SwitchParams[TEXT("MapPackagePaths")];
+		TArray<FString> ParsedMapPackagePaths;
+		AllMapPackagePaths.ParseIntoArray(ParsedMapPackagePaths, TEXT(","));
+
+		for (const FString& MapPackagePath : ParsedMapPackagePaths)
+		{
+			MapPackagePaths.Add(MapPackagePath.TrimQuotes());
+		}
+	}
+}
+
+int32 UGenerateSchemaCommandlet::Main(const FString& CmdLineParams)
+{
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Generate Schema Started."));
 
 	TGuardValue<bool> UnattendedScriptGuard(GIsRunningUnattendedScript, GIsRunningUnattendedScript || IsRunningCommandlet());
 
-	TArray<FString> Tokens;
-	TArray<FString> Switches;
-	TMap<FString, FString> Params;
-	ParseCommandLine(*Args, Tokens, Switches);
+	InitCmdLine(CmdLineParams);
 
-	if (!HandleOptions(Switches))
+#if ENGINE_MINOR_VERSION <= 22
+	// Force spatial networking
+	GetMutableDefault<UGeneralProjectSettings>()->SetUsesSpatialNetworking(true);
+#endif
+
+	FObjectListener ObjectListener;
+	TSet<FSoftClassPath> ReferencedClasses;
+	ObjectListener.StartListening(&ReferencedClasses);
+
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Try Load Schema Database."));
+	if (IsAssetReadOnly(SpatialConstants::SCHEMA_DATABASE_FILE_PATH))
 	{
-		UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("Schema generation aborted"));
-		return 1;
+		UE_LOG(LogGenerateSchemaCommandlet, Error, TEXT("Failed to load Schema Database."));
+		return 0;
 	}
 
-	UE_LOG(LogSpatialGDKEditorCommandlet, Error, TEXT("Commandlet GenerateSchema has been deprecated in favor of CookAndGenerateSchemaCommandlet."));
-	
-	UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("Schema Generation Commandlet Complete"));
+	if (!LoadGeneratorStateFromSchemaDatabase(SpatialConstants::SCHEMA_DATABASE_FILE_PATH))
+	{
+		ResetSchemaGeneratorStateAndCleanupFolders();
+	}
 
-	return false;
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Finding supported C++ and in-memory Classes."));
+
+	TArray<UObject*> AllClasses;
+	GetObjectsOfClass(UClass::StaticClass(), AllClasses);
+	for (const auto& SupportedClass : GetAllSupportedClasses(AllClasses))
+	{
+		ReferencedClasses.Add(FSoftClassPath(SupportedClass));
+	}
+
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Starting Recompile Command."));
+	int32 ParentResult = Super::Main(CmdLineParams);
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Recompile Command Completed."));
+
+	int ClassesFoundDuringRecompile = ReferencedClasses.Num();
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Discovered %d Classes during recompile."), ClassesFoundDuringRecompile);
+
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Starting Level Blueprint Loads."));
+	LoadLevelBlueprints();
+	
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Starting Level Blueprint Loads."));
+
+	int32 ClassesFoundLoadingLevelBlueprints = ReferencedClasses.Num() - ClassesFoundDuringRecompile;
+
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Discovered %d Classes loading level blueprints."), ClassesFoundLoadingLevelBlueprints);
+	ObjectListener.StopListening();
+
+	// Sort classes here so that batching does not have an effect on ordering.
+	ReferencedClasses.Sort([](const FSoftClassPath& A, const FSoftClassPath& B)
+	{
+#if ENGINE_MINOR_VERSION <= 22
+		return A.GetAssetPathName() < B.GetAssetPathName();
+#else
+		return FNameLexicalLess()(A.GetAssetPathName(), B.GetAssetPathName());
+#endif
+	});
+
+	for (FSoftClassPath SoftClassPath : ReferencedClasses)
+	{
+		UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("- %s"), *SoftClassPath.ToString());
+	}
+
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Start Schema Generation for discovered assets."));
+	FDateTime StartTime = FDateTime::Now();
+	TSet<UClass*> Classes;
+	const int BatchSize = 100;
+	for (const FSoftClassPath& SoftPath : ReferencedClasses)
+	{
+		if (UClass* LoadedClass = SoftPath.TryLoadClass<UObject>())
+		{
+			UE_LOG(LogGenerateSchemaCommandlet, Verbose, TEXT("Reloaded %s, adding to batch"), *GetPathNameSafe(LoadedClass));
+			Classes.Add(LoadedClass);
+			if (Classes.Num() >= BatchSize)
+			{
+				SpatialGDKGenerateSchemaForClasses(Classes);
+				Classes.Empty(BatchSize);
+			}
+		}
+		else
+		{
+			UE_LOG(LogGenerateSchemaCommandlet, Warning, TEXT("Failed to reload %s"), *SoftPath.ToString());
+		}
+	}
+	SpatialGDKGenerateSchemaForClasses(Classes);
+
+	GenerateSchemaForSublevels();
+	GenerateSchemaForRPCEndpoints();
+	GenerateSchemaForNCDs();
+
+	FTimespan Duration = FDateTime::Now() - StartTime;
+
+	UE_LOG(LogGenerateSchemaCommandlet, Display, TEXT("Schema Generation Finished in %.2f seconds"), Duration.GetTotalSeconds());
+
+	if (!RunSchemaCompiler())
+	{
+		UE_LOG(LogGenerateSchemaCommandlet, Error, TEXT("Failed to run schema compiler."));
+		return 0;
+	}
+
+	if (!SaveSchemaDatabase(SpatialConstants::SCHEMA_DATABASE_ASSET_PATH))
+	{
+		UE_LOG(LogGenerateSchemaCommandlet, Error, TEXT("Failed to save schema database."));
+		return 0;
+	}
+
+	// Maybe this should always pass?
+	return 0;
 }
--- SpatialGDK/Source/SpatialGDKEditorCommandlet/Private/Commandlets/GenerateSchemaCommandlet.h
+++ SpatialGDK/Source/SpatialGDKEditorCommandlet/Private/Commandlets/GenerateSchemaCommandlet.h
@@ -2,21 +2,36 @@
 
 #pragma once
 
-#include "Commandlets/Commandlet.h"
-
+#include "Commandlets/CompileAllBlueprintsCommandlet.h"
 #include "GenerateSchemaCommandlet.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogGenerateSchemaCommandlet, Log, All);
+
+struct FObjectListener;
+/**
+ * -- EXPERIMENTAL --
+ * - Use CompileAllBlueprintsCommandlet to find all blueprints, skipping compile though.
+ * - Use ObjectArrayListener to detect supported blueprints.
+ * - Use FLinkerLoad to try and load level blueprints from inside maps without loading them...
+ */
 UCLASS()
-class UGenerateSchemaCommandlet : public UCommandlet
+class UGenerateSchemaCommandlet : public UCompileAllBlueprintsCommandlet
 {
 	GENERATED_BODY()
+
+private:
+	void InitCmdLine(const FString& Params);
+
+	UPROPERTY()
+	TArray<FString> MapPackagePaths;
 
 public:
 	UGenerateSchemaCommandlet();
 
-public:
-	virtual int32 Main(const FString& Params) override;
+	virtual int32 Main(const FString& CmdLineParams) override;
 
 protected:
-	virtual bool HandleOptions(const TArray<FString>& Switches);
+	virtual void CompileBlueprint(UBlueprint* Blueprint) override;
+
+	void LoadLevelBlueprints();
 };
