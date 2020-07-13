57b890fa38f1555e9fbcc5215de26da58e52a689
Sang Tian <sangtian@improbable.io>
2020-07-13 14:38:23 +0800

    Added ActorCounter of each worker to DynamicLBSInfo

--- SpatialGDK/Source/SpatialGDK/Private/LoadBalancing/DynamicLBSInfo.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/LoadBalancing/DynamicLBSInfo.cpp
@@ -4,6 +4,8 @@
 #include "LoadBalancing/GridBasedLBStrategy.h"
 #include "Net/UnrealNetwork.h"
 
+DEFINE_LOG_CATEGORY(LogDynamicLBStrategy);
+
 using namespace SpatialGDK;
 
 ADynamicLBSInfo::ADynamicLBSInfo(const FObjectInitializer& ObjectInitializer)
@@ -23,6 +25,7 @@ void ADynamicLBSInfo::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutL
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	DOREPLIFETIME_CONDITION(ADynamicLBSInfo, DynamicWorkerCells, COND_SimulatedOnly);
+	DOREPLIFETIME_CONDITION(ADynamicLBSInfo, ActorCounters, COND_SimulatedOnly);
 }
 
 void ADynamicLBSInfo::BeginPlay()
@@ -37,10 +40,14 @@ void ADynamicLBSInfo::BeginPlay()
 		{
 			const UGridBasedLBStrategy::LBStrategyRegions LBStrategyRegions = GridBasedLBStrategy->GetLBStrategyRegions();
 			DynamicWorkerCells.SetNum(LBStrategyRegions.Num());
+			ActorCounters.SetNum(LBStrategyRegions.Num());
+
 			for (int i = 0; i < LBStrategyRegions.Num(); i++)
 			{
 				const TPair<VirtualWorkerId, FBox2D>& LBStrategyRegion = LBStrategyRegions[i];
 				DynamicWorkerCells[i] = FBox2D(LBStrategyRegion.Value.Min, LBStrategyRegion.Value.Max);
+
+				//ActorCounters[i] = 0;
 			}
 		}
 	}
@@ -54,4 +61,51 @@ void ADynamicLBSInfo::OnAuthorityGained()
 void ADynamicLBSInfo::OnRep_DynamicWorkerCells()
 {
 	//UE_LOG(LogTemp, Warning, TEXT("[DynamicLBSInfo] worker cells changed!!!"));
+}
+
+void ADynamicLBSInfo::OnRep_ActorCounters()
+{
+	UE_LOG(LogTemp, Warning, TEXT("[DynamicLBSInfo] actor counters changed!!!"));
+}
+
+uint32 ADynamicLBSInfo::GetActorCounter(const VirtualWorkerId TargetWorkerId)
+{
+	return ActorCounters[TargetWorkerId - 1];
+}
+
+void ADynamicLBSInfo::IncreseActorCounter(const VirtualWorkerId TargetWorkerId)
+{
+	uint32 ActorCounter;
+	if (TargetWorkerId > (uint32)ActorCounters.Num())
+	{
+		ActorCounters.SetNum(TargetWorkerId);
+		ActorCounter = 1;
+	}
+	else
+	{
+		ActorCounter = GetActorCounter(TargetWorkerId);
+		ActorCounter++;
+	}
+	ActorCounters[TargetWorkerId - 1] = ActorCounter;
+	UE_LOG(LogDynamicLBStrategy, Log, TEXT("VirtualWorker[%d] increased actor counter to: %d"), TargetWorkerId, ActorCounter);
+}
+
+void ADynamicLBSInfo::DecreaseActorCounter(const VirtualWorkerId TargetWorkerId)
+{
+	uint32 ActorCounter;
+	if (TargetWorkerId > (uint32)ActorCounters.Num())
+	{
+		ActorCounters.SetNum(TargetWorkerId);
+		ActorCounter = 0;
+	}
+	else
+	{
+		ActorCounter = GetActorCounter(TargetWorkerId);
+		if (ActorCounter > 0)
+		{
+			ActorCounter--;
+		}
+	}
+	ActorCounters[TargetWorkerId - 1] = ActorCounter;
+	UE_LOG(LogDynamicLBStrategy, Log, TEXT("VirtualWorker[%d] decreased actor counter to: %d"), TargetWorkerId, ActorCounter);
 }
