9ca723bab63e3204b41a51efc90e6f52ef756e00
Vix2021 <victoriabloom@improbable.io>
2020-07-15 12:00:01 +0300

    Code cleanup

--- SpatialGDK/Source/SpatialGDKFunctionalTests/Private/AutoDestroyComponent.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/Private/AutoDestroyComponent.cpp
@@ -3,9 +3,5 @@
 
 #include "AutoDestroyComponent.h"
 
-// Sets default values for this component's properties
-UAutoDestroyComponent::UAutoDestroyComponent()
-{
-}
 
 
--- SpatialGDK/Source/SpatialGDKFunctionalTests/Public/AutoDestroyComponent.h
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/Public/AutoDestroyComponent.h
@@ -7,12 +7,11 @@
 #include "AutoDestroyComponent.generated.h"
 
 
+/*
+* Empty component to be added to actors so that they can be automatically destroyed when the tests finish
+*/
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SPATIALGDKFUNCTIONALTESTS_API UAutoDestroyComponent : public USceneComponent
 {
 	GENERATED_BODY()
-
-public:	
-	UAutoDestroyComponent();
-	
 };
