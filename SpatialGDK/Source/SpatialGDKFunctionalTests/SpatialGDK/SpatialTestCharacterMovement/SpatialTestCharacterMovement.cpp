5f1172202ccb8df7f66293a862913a8f69a6599d
Andrei Lazar <andreilazar@improbable.io>
2020-07-16 11:11:07 +0100

    Refactored the test to remove the timer that was previously used

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestCharacterMovement/SpatialTestCharacterMovement.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestCharacterMovement/SpatialTestCharacterMovement.cpp
@@ -50,110 +50,108 @@ void ASpatialTestCharacterMovement::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// Universal setup step to create the TriggerBox and to set the 2 helper variables
-	AddUniversalStep(TEXT("UniversalSetupStep"), nullptr, [](ASpatialFunctionalTest* NetTest) {
-		ASpatialTestCharacterMovement* Test = Cast<ASpatialTestCharacterMovement>(NetTest);
-		Test->bCharacterReachedDestination = false;
-		Test->ElapsedTime = 0.0f;
-
-		ATriggerBox* TriggerBox = Test->GetWorld()->SpawnActor<ATriggerBox>(FVector(232.0f, 0.0f, 40.0f), FRotator::ZeroRotator, FActorSpawnParameters());
-
-		UBoxComponent* BoxComponent = Cast<UBoxComponent>(TriggerBox->GetCollisionComponent());
-		if (BoxComponent)
+	// Universal setup step to create the TriggerBox and to set the helper variable
+	AddUniversalStep(TEXT("UniversalSetupStep"), nullptr, [this](ASpatialFunctionalTest* NetTest)
 		{
-			BoxComponent->SetBoxExtent(FVector(10.0f, 1.0f, 1.0f));
-		}
+			bCharacterReachedDestination = false;
 
-		TriggerBox->OnActorBeginOverlap.AddDynamic(Test, &ASpatialTestCharacterMovement::OnOverlapBegin);
+			ATriggerBox* TriggerBox = GetWorld()->SpawnActor<ATriggerBox>(FVector(232.0f, 0.0f, 40.0f), FRotator::ZeroRotator, FActorSpawnParameters());
 
-		Test->FinishStep();
+			UBoxComponent* BoxComponent = Cast<UBoxComponent>(TriggerBox->GetCollisionComponent());
+			if (BoxComponent)
+			{
+				BoxComponent->SetBoxExtent(FVector(10.0f, 1.0f, 1.0f));
+			}
+
+			TriggerBox->OnActorBeginOverlap.AddDynamic(this, &ASpatialTestCharacterMovement::OnOverlapBegin);
+
+			FinishStep();
 		});
 
 	// The server checks if the clients received a TestCharacterMovement and moves them to the mentioned locations
-	AddServerStep(TEXT("SpatialTestCharacterMovementServerSetupStep"), 1, nullptr, [this](ASpatialFunctionalTest* NetTest) {
-		ASpatialTestCharacterMovement* Test = Cast<ASpatialTestCharacterMovement>(NetTest);
-
-		for (ASpatialFunctionalTestFlowController* FlowController : NetTest->GetFlowControllers())
+	AddServerStep(TEXT("SpatialTestCharacterMovementServerSetupStep"), 1, nullptr, [this](ASpatialFunctionalTest* NetTest)
 		{
-			if (FlowController->ControllerType == ESpatialFunctionalTestFlowControllerType::Server)
+			for (ASpatialFunctionalTestFlowController* FlowController : GetFlowControllers())
 			{
-				continue;
+				if (FlowController->ControllerType == ESpatialFunctionalTestFlowControllerType::Server)
+				{
+					continue;
+				}
+
+				AController* PlayerController = Cast<AController>(FlowController->GetOwner());
+				ATestMovementCharacter* PlayerCharacter = Cast<ATestMovementCharacter>(PlayerController->GetPawn());
+
+				checkf(PlayerCharacter, TEXT("Client did not receive a TestMovementCharacter"));
+
+				if (FlowController->ControllerInstanceId == 1)
+				{
+					PlayerCharacter->SetActorLocation(FVector(0.0f, 0.0f, 50.0f));
+				}
+				else
+				{
+					PlayerCharacter->SetActorLocation(FVector(100.0f, 300.0f, 50.0f));
+				}
 			}
 
-			AController* PlayerController = Cast<AController>(FlowController->GetOwner());
-			ATestMovementCharacter* PlayerCharacter = Cast<ATestMovementCharacter>(PlayerController->GetPawn());
-
-			checkf(PlayerCharacter, TEXT("Client did not receive a TestMovementCharacter"));
-
-			if (FlowController->ControllerInstanceId == 1)
-			{
-				PlayerCharacter->SetActorLocation(FVector(0.0f, 0.0f, 50.0f));
-			}
-			else
-			{
-				PlayerCharacter->SetActorLocation(FVector(100.0f, 300.0f, 50.0f));
-			}
-		}
-
-		Test->FinishStep();
+			FinishStep();
 		});
 
 	// Client 1 moves his character and asserts that it reached the Destination locally.
 	AddClientStep(TEXT("SpatialTestCharacterMovementClient1Move"), 1,
-		[](ASpatialFunctionalTest* NetTest) -> bool {
+		[](ASpatialFunctionalTest* NetTest) -> bool
+		{
 			AController* PlayerController = Cast<AController>(NetTest->GetLocalFlowController()->GetOwner());
-			// Since the character is simulating gravity, it will drop from the original position close to (0, 0, 40), depending on the size of the CapsuleComponent in the TestMovementCharacter
-			return IsValid(PlayerController->GetPawn()) && PlayerController->GetPawn()->GetActorLocation().Equals(FVector(0.0f,0.0f,40.0f), 0.5f);
-		}, 
-		nullptr,
-		[](ASpatialFunctionalTest* NetTest, float DeltaTime) {
-			ASpatialTestCharacterMovement* Test = Cast<ASpatialTestCharacterMovement>(NetTest);
-			AController* PlayerController = Cast<AController>(Test->GetLocalFlowController()->GetOwner());
 			ATestMovementCharacter* PlayerCharacter = Cast<ATestMovementCharacter>(PlayerController->GetPawn());
 
-			//  Apply movement input for half a second
-			if (Test->ElapsedTime < 0.5f)
-			{ 
-				Test->ElapsedTime += DeltaTime;
-				PlayerCharacter->AddMovementInput(PlayerCharacter->GetActorForwardVector(), 1.0f);
-			}
-			else
-			{
-				Test->AssertTrue(Test->bCharacterReachedDestination, TEXT("Player character has reached the destination on the autonomous proxy."));
+			// Since the character is simulating gravity, it will drop from the original position close to (0, 0, 40), depending on the size of the CapsuleComponent in the TestMovementCharacter
+			return IsValid(PlayerCharacter) && PlayerCharacter->GetActorLocation().Equals(FVector(0.0f,0.0f,40.0f), 0.5f);
+		}, 
+		nullptr,
+		[this](ASpatialFunctionalTest* NetTest, float DeltaTime)
+		{
+			AController* PlayerController = Cast<AController>(GetLocalFlowController()->GetOwner());
+			ATestMovementCharacter* PlayerCharacter = Cast<ATestMovementCharacter>(PlayerController->GetPawn());
 
-				Test->FinishStep();
+			PlayerCharacter->AddMovementInput(PlayerCharacter->GetActorForwardVector(), 1.0f);
+
+			if(bCharacterReachedDestination)
+			{
+				AssertTrue(bCharacterReachedDestination, TEXT("Player character has reached the destination on the autonomous proxy."));
+				FinishStep();
 			}
-		});
+		}, 1.0f);
 
 	// Server asserts that the character of client 1 has reached the Destination.
-	AddServerStep(TEXT("SpatialTestChracterMovementServerCheckMovementVisibility"), 1, nullptr, nullptr, [](ASpatialFunctionalTest* NetTest, float DeltaTime) {
-		ASpatialTestCharacterMovement* Test = Cast<ASpatialTestCharacterMovement>(NetTest);
-
-		Test->AssertTrue(Test->bCharacterReachedDestination, TEXT("Player character has reached the destination on the server."));
-
-		Test->FinishStep();
-		});
+	AddServerStep(TEXT("SpatialTestChracterMovementServerCheckMovementVisibility"), 1, nullptr, nullptr, [this](ASpatialFunctionalTest* NetTest, float DeltaTime)
+		{
+			if (bCharacterReachedDestination)
+			{
+				AssertTrue(bCharacterReachedDestination, TEXT("Player character has reached the destination on the server."));
+				FinishStep();
+			}
+		}, 1.0f);
 
 	// Client 2 asserts that the character of client 1 has reached the Destination.
-	AddClientStep(TEXT("SpatialTestCharacterMovementClient2CheckMovementVisibility"), 2, nullptr, [](ASpatialFunctionalTest* NetTest) {
-		ASpatialTestCharacterMovement* Test = Cast<ASpatialTestCharacterMovement>(NetTest);
-
-		Test->AssertTrue(Test->bCharacterReachedDestination, TEXT("Player character has reached the destination on the simulated proxy"));
-
-		Test->FinishStep();
-		});
-
+	AddClientStep(TEXT("SpatialTestCharacterMovementClient2CheckMovementVisibility"), 2, nullptr, nullptr, [this](ASpatialFunctionalTest* NetTest, float DeltaTime)
+		{
+			if (bCharacterReachedDestination)
+			{
+				AssertTrue(bCharacterReachedDestination, TEXT("Player character has reached the destination on the simulated proxy"));
+				FinishStep();
+			}
+		}, 1.0f);
 
 	// Universal clean-up step to delete the TriggerBox from all connected clients and servers		
-	AddUniversalStep(TEXT("SpatialTestCharacterMovementUniversalCleanUp"), nullptr, [](ASpatialFunctionalTest* NetTest) {
-		TArray<AActor*> FoundTriggers;
-		UGameplayStatics::GetAllActorsOfClass(NetTest->GetWorld(), ATriggerBox::StaticClass(), FoundTriggers);
-
-		for (auto TriggerToDestroy : FoundTriggers)
+	AddUniversalStep(TEXT("SpatialTestCharacterMovementUniversalCleanUp"), nullptr, [this](ASpatialFunctionalTest* NetTest)
 		{
-			TriggerToDestroy->Destroy();
-		}
+			TArray<AActor*> FoundTriggers;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATriggerBox::StaticClass(), FoundTriggers);
 
-		NetTest->FinishStep();
+			for (auto TriggerToDestroy : FoundTriggers)
+			{
+				TriggerToDestroy->Destroy();
+			}
+
+			FinishStep();
 		});
 }
