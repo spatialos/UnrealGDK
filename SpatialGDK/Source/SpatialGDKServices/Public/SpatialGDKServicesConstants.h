9b88424a35bcbd0cc5fbef4a87762bf7cd4d4eac
Miron Zelina <mironzelina@improbable.io>
2020-08-03 12:37:15 +0100

    Update to preview runtime version

--- SpatialGDK/Source/SpatialGDKServices/Public/SpatialGDKServicesConstants.h
+++ SpatialGDK/Source/SpatialGDKServices/Public/SpatialGDKServicesConstants.h
@@ -32,7 +32,7 @@ namespace SpatialGDKServicesConstants
 	const FString SpatialOSConfigFileName = TEXT("spatialos.json");
 	const FString ChinaEnvironmentArgument = TEXT(" --environment=cn-production");
 
-	const FString SpatialOSRuntimePinnedStandardVersion         = TEXT("0.4.3");
+	const FString SpatialOSRuntimePinnedStandardVersion         = TEXT("0.5.0-preview-1");
 	const FString SpatialOSRuntimePinnedCompatbilityModeVersion = TEXT("14.5.4");
 
 	const FString InspectorURL   = TEXT("http://localhost:31000/inspector");
