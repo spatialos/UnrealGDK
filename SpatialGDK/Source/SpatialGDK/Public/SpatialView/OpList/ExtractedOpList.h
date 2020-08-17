5b310c67f491e3cc992d28cea738eb7012e77bd4
samiwh <sami@improbable.io>
2020-08-17 11:51:11 +0100

    Op list and connection handler changes.

---
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/OpList/ExtractedOpList.h
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+#include "OpList.h"
+
+#include "Containers/Array.h"
+
+namespace SpatialGDK
+{
+struct ExtractedOpListData : OpListData
+{
+	TArray<Worker_Op> ExtractedOps;
+
+	void AddOp(OpList& Ops, uint32 Index)
+	{
+		Worker_Op& Op = Ops.Ops[Index];
+		ExtractedOps.Push(Op);
+		Op.op_type = 0;
+	}
+};
+
+} // namespace SpatialGDK
