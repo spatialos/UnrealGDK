bb15f522db7df94fe150c870712cd4b8c6fb52aa
samiwh <sami@improbable.io>
2020-08-17 15:22:18 +0100

    Added in a entity delta dispatcher.

---
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/Callbacks.h
@@ -0,0 +1,176 @@
+﻿#pragma once
+
+#include "Containers/Array.h"
+#include "Templates/Function.h"
+
+namespace SpatialGDK
+{
+using CallbackId = int32;
+
+template <typename T>
+class TCallbacks
+{
+public:
+	using CallbackType = TFunction<void(const T&)>;
+
+	bool IsEmpty() const { return Callbacks.Num() == 0; }
+
+	void Register(CallbackId CallbackId, CallbackType Callback)
+	{
+		if (CallbackDepth)
+		{
+			CallbacksToAdd.Push({ std::move(Callback), CallbackId });
+		}
+		else
+		{
+			Callbacks.Push({ std::move(Callback), CallbackId });
+		}
+	}
+
+	void Remove(CallbackId Id)
+	{
+		if (CallbackDepth)
+		{
+			CallbacksToRemove.Emplace(Id);
+		}
+		else
+		{
+			CallbackAndId* Element = Callbacks.FindByPredicate([Id](const CallbackAndId& E) {
+				return E.Id == Id;
+			});
+			if (Element != nullptr)
+			{
+				Callbacks.RemoveAt(Element - Callbacks.GetData());
+			}
+		}
+	}
+
+	void Invoke(const T& Value)
+	{
+		++CallbackDepth;
+		for (const auto& Callback : Callbacks)
+		{
+			if (CallbacksToRemove.Contains(Callback.Id))
+			{
+				continue;
+			}
+			Callback.Callback(Value);
+		}
+		--CallbackDepth;
+		if (CallbackDepth)
+		{
+			return;
+		}
+
+		// Sort out pending adds and removes.
+		if (CallbacksToAdd.Num() > 0)
+		{
+			Callbacks.Append(MoveTemp(CallbacksToAdd));
+			CallbacksToAdd.Empty();
+		}
+		if (CallbacksToRemove.Num() > 0)
+		{
+			Callbacks.RemoveAll([this](const CallbackAndId& E) {
+				return CallbacksToRemove.Contains(E.Id);
+			});
+			CallbacksToRemove.Empty();
+		}
+	}
+
+private:
+	struct CallbackAndId
+	{
+		CallbackType Callback;
+		CallbackId Id;
+	};
+
+	TArray<CallbackAndId> Callbacks;
+	int32 CallbackDepth = 0;
+	TArray<CallbackAndId> CallbacksToAdd;
+	TArray<CallbackId> CallbacksToRemove;
+};
+
+template <>
+class TCallbacks<void>
+{
+public:
+	using CallbackType = TFunction<void()>;
+
+	bool IsEmpty() const { return Callbacks.Num() == 0; }
+
+	void Register(CallbackId CallbackId, CallbackType Callback)
+	{
+		if (CallbackDepth)
+		{
+			CallbacksToAdd.Push({ std::move(Callback), CallbackId });
+		}
+		else
+		{
+			Callbacks.Push({ std::move(Callback), CallbackId });
+		}
+	}
+
+	void Remove(CallbackId Id)
+	{
+		if (CallbackDepth)
+		{
+			CallbacksToRemove.Emplace(Id);
+		}
+		else
+		{
+			CallbackAndId* Element = Callbacks.FindByPredicate([Id](const CallbackAndId& E) {
+				return E.Id == Id;
+			});
+			if (Element != nullptr)
+			{
+				Callbacks.RemoveAt(Element - Callbacks.GetData());
+			}
+		}
+	}
+
+	void Invoke()
+	{
+		++CallbackDepth;
+		for (const auto& Callback : Callbacks)
+		{
+			if (CallbacksToRemove.Contains(Callback.Id))
+			{
+				continue;
+			}
+			Callback.Callback();
+		}
+		--CallbackDepth;
+		if (CallbackDepth)
+		{
+			return;
+		}
+
+		// Sort out pending adds and removes.
+		if (CallbacksToAdd.Num() > 0)
+		{
+			Callbacks.Append(MoveTemp(CallbacksToAdd));
+			CallbacksToAdd.Empty();
+		}
+		if (CallbacksToRemove.Num() > 0)
+		{
+			Callbacks.RemoveAll([this](const CallbackAndId& E) {
+				return CallbacksToRemove.Contains(E.Id);
+			});
+			CallbacksToRemove.Empty();
+		}
+	}
+
+private:
+	struct CallbackAndId
+	{
+		CallbackType Callback;
+		CallbackId Id;
+	};
+
+	TArray<CallbackAndId> Callbacks;
+	int32 CallbackDepth = 0;
+	TArray<CallbackAndId> CallbacksToAdd;
+	TArray<CallbackId> CallbacksToRemove;
+};
+
+} // namespace SpatialGDK
---
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/Dispatcher.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+
+#include "SpatialView/Callbacks.h"
+#include "SpatialView/ViewDelta.h"
+
+#include "Containers/Array.h"
+#include "Templates/Function.h"
+
+namespace SpatialGDK
+{
+struct FEntityComponentChange
+{
+	Worker_EntityId EntityId;
+	const ComponentChange& Change;
+};
+
+using FEntityCallback = TCallbacks<Worker_EntityId>::CallbackType;
+using FComponentValueCallback = TCallbacks<FEntityComponentChange>::CallbackType;
+
+class FDispatcher
+{
+public:
+	explicit FDispatcher(const EntityView* View);
+
+	void InvokeCallbacks(const TArray<EntityDelta>& Deltas);
+
+	CallbackId RegisterComponentAddedCallback(Worker_ComponentId ComponentId, FComponentValueCallback Callback);
+	CallbackId RegisterComponentRemovedCallback(Worker_ComponentId ComponentId, FComponentValueCallback Callback);
+	CallbackId RegisterComponentValueCallback(Worker_ComponentId ComponentId, FComponentValueCallback Callback);
+	CallbackId RegisterAuthorityGainedCallback(Worker_ComponentId ComponentId, FEntityCallback Callback);
+	CallbackId RegisterAuthorityLostCallback(Worker_ComponentId ComponentId, FEntityCallback Callback);
+	CallbackId RegisterAuthorityLostTempCallback(Worker_ComponentId ComponentId, FEntityCallback Callback);
+
+	void RemoveCallback(CallbackId Id);
+
+private:
+	struct FComponentCallbacks
+	{
+		explicit FComponentCallbacks(Worker_ComponentId Id)
+			: Id(Id)
+		{
+		}
+		Worker_ComponentId Id;
+		TCallbacks<FEntityComponentChange> ComponentAddedCallbacks;
+		TCallbacks<FEntityComponentChange> ComponentRemovedCallbacks;
+		TCallbacks<FEntityComponentChange> ComponentValueCallbacks;
+
+		struct ComponentIdComparator
+		{
+			bool operator()(const FComponentCallbacks& Callbacks, Worker_ComponentId Id) const { return Callbacks.Id < Id; }
+		};
+	};
+
+	struct FAuthorityCallbacks
+	{
+		explicit FAuthorityCallbacks(Worker_ComponentId Id)
+			: Id(Id)
+		{
+		}
+		Worker_ComponentId Id;
+		TCallbacks<Worker_EntityId> AuthorityGainedCallbacks;
+		TCallbacks<Worker_EntityId> AuthorityLostCallbacks;
+		TCallbacks<Worker_EntityId> AuthorityLostTemporarilyCallbacks;
+
+		struct ComponentIdComparator
+		{
+			bool operator()(const FAuthorityCallbacks& Callbacks, Worker_ComponentId Id) const { return Callbacks.Id < Id; }
+		};
+	};
+
+	void InvokeWithExistingAuthority(Worker_ComponentId ComponentId, const FEntityCallback& Callback) const;
+	void InvokeWithExistingValues(Worker_ComponentId ComponentId, const FComponentValueCallback& Callback) const;
+	void HandleComponentPresenceChanges(Worker_EntityId EntityId, const ComponentSpan<ComponentChange>& ComponentChanges,
+										TCallbacks<FEntityComponentChange> FComponentCallbacks::*Callbacks);
+	void HandleComponentValueChanges(Worker_EntityId EntityId, const ComponentSpan<ComponentChange>& ComponentChanges);
+	void HandleAuthorityChange(Worker_EntityId EntityId, const ComponentSpan<AuthorityChange>& AuthorityChanges,
+							   TCallbacks<Worker_EntityId> FAuthorityCallbacks::*Callbacks);
+
+	const EntityView* View;
+	// Component callbacks sorted by component ID;
+	TArray<FComponentCallbacks> ComponentCallbacks;
+	// Authority callbacks sorted by component ID;
+	TArray<FAuthorityCallbacks> AuthorityCallbacks;
+	CallbackId NextCallbackId;
+};
+} // namespace SpatialGDK
