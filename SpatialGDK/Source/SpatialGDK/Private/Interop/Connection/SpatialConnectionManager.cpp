9739f7dbb1a128743006986b31a2a27c3fb21886
samiwh <sami@improbable.io>
2020-08-17 11:51:12 +0100

    Changed the SpatialWorkerConnection to not be abstract and to mirror the SpatialViewWorkerConnection.

--- SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialConnectionManager.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialConnectionManager.cpp
@@ -386,14 +386,7 @@ void USpatialConnectionManager::FinishConnecting(Worker_ConnectionFuture* Connec
 			if (Worker_Connection_IsConnected(NewCAPIWorkerConnection))
 			{
 				const USpatialGDKSettings* Settings = GetDefault<USpatialGDKSettings>();
-				if (Settings->bUseSpatialView)
-				{
-					SpatialConnectionManager->WorkerConnection = NewObject<USpatialViewWorkerConnection>();
-				}
-				else
-				{
-					SpatialConnectionManager->WorkerConnection = NewObject<ULegacySpatialWorkerConnection>();
-				}
+				SpatialConnectionManager->WorkerConnection = NewObject<USpatialWorkerConnection>();
 				SpatialConnectionManager->WorkerConnection->SetConnection(NewCAPIWorkerConnection);
 				SpatialConnectionManager->OnConnectionSuccess();
 			}
