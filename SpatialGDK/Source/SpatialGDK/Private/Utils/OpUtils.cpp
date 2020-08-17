cf0adaa1a97428b83987294bee1806659d8436a0
samiwh <sami@improbable.io>
2020-08-17 11:51:12 +0100

    Changed the connection interface to return entity deltas and worker messages.

--- SpatialGDK/Source/SpatialGDK/Private/Utils/OpUtils.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Utils/OpUtils.cpp
@@ -5,22 +5,22 @@
 
 namespace SpatialGDK
 {
-Worker_ComponentId GetComponentId(const Worker_Op* Op)
+Worker_ComponentId GetComponentId(const Worker_Op& Op)
 {
-	switch (Op->op_type)
+	switch (Op.op_type)
 	{
 	case WORKER_OP_TYPE_ADD_COMPONENT:
-		return Op->op.add_component.data.component_id;
+		return Op.op.add_component.data.component_id;
 	case WORKER_OP_TYPE_REMOVE_COMPONENT:
-		return Op->op.remove_component.component_id;
+		return Op.op.remove_component.component_id;
 	case WORKER_OP_TYPE_COMPONENT_UPDATE:
-		return Op->op.component_update.update.component_id;
+		return Op.op.component_update.update.component_id;
 	case WORKER_OP_TYPE_AUTHORITY_CHANGE:
-		return Op->op.authority_change.component_id;
+		return Op.op.authority_change.component_id;
 	case WORKER_OP_TYPE_COMMAND_REQUEST:
-		return Op->op.command_request.request.component_id;
+		return Op.op.command_request.request.component_id;
 	case WORKER_OP_TYPE_COMMAND_RESPONSE:
-		return Op->op.command_response.response.component_id;
+		return Op.op.command_response.response.component_id;
 	default:
 		return SpatialConstants::INVALID_COMPONENT_ID;
 	}
