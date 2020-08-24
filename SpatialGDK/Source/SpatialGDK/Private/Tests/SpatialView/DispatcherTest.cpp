80f1dc8c77fd063c5fe07b2c1e420eb68e444c45
Jacques Elliott <jacques@improbable.io>
2020-08-24 17:15:49 +0100

    working tests

--- SpatialGDK/Source/SpatialGDK/Private/Tests/SpatialView/DispatcherTest.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Tests/SpatialView/DispatcherTest.cpp
@@ -14,48 +14,51 @@
 
 namespace
 {
-	constexpr Worker_ComponentId COMPONENT_ID = 1000;
-	constexpr Worker_EntityId ENTITY_ID = 1;
-	constexpr double COMPONENT_VALUE = 2;
+constexpr Worker_ComponentId COMPONENT_ID = 1000;
+constexpr Worker_EntityId ENTITY_ID = 1;
+constexpr double COMPONENT_VALUE = 2;
 
-	struct ViewDispatcher
+struct ViewDispatcher
+{
+	TUniquePtr<SpatialGDK::EntityView> View;
+	SpatialGDK::FDispatcher Dispatcher;
+
+	ViewDispatcher(TUniquePtr<SpatialGDK::EntityView>& View, SpatialGDK::FDispatcher Dispatcher)
+		: View(MoveTemp(View))
+		, Dispatcher(Dispatcher)
 	{
-		TUniquePtr<SpatialGDK::EntityView> View;
-		SpatialGDK::FDispatcher Dispatcher;
-
-		ViewDispatcher(TUniquePtr<SpatialGDK::EntityView>& View, SpatialGDK::FDispatcher Dispatcher)
-		: View(MoveTemp(View)), Dispatcher(Dispatcher) {}
-	};
-
-	ViewDispatcher CreateViewAndDispatcher()
-	{
-		auto View = MakeUnique<SpatialGDK::EntityView>();
-		SpatialGDK::FDispatcher Dispatcher(View.Get());
-		return ViewDispatcher(View, Dispatcher);
 	}
+};
 
-	TUniquePtr<SpatialGDK::ComponentChange> CreateComponentAddedChange(const Worker_ComponentId ComponentId,
-		const double Value)
-	{
-		SpatialGDK::ComponentData Data = SpatialGDK::CreateTestComponentData(ComponentId, Value);
-		return MakeUnique<SpatialGDK::ComponentChange>(ComponentId, MoveTemp(Data).Release());
-	}
+ViewDispatcher CreateViewAndDispatcher()
+{
+	auto View = MakeUnique<SpatialGDK::EntityView>();
+	SpatialGDK::FDispatcher Dispatcher(View.Get());
+	return ViewDispatcher(View, Dispatcher);
+}
 
-	TArray<SpatialGDK::EntityDelta> CreateComponentAddedDeltaWithChange(const Worker_EntityId EntityId, const TUniquePtr<SpatialGDK::ComponentChange>& Change)
-	{
-		SpatialGDK::EntityDelta Delta = {};
-		Delta.EntityId = EntityId;
-		Delta.ComponentsAdded = SpatialGDK::ComponentSpan<SpatialGDK::ComponentChange>(Change.Get(), 1);
-		return TArray<SpatialGDK::EntityDelta>{ Delta };
-	}
+TUniquePtr<SpatialGDK::ComponentChange> CreateComponentAddedChange(const Worker_ComponentId ComponentId, const double Value)
+{
+	SpatialGDK::ComponentData Data = SpatialGDK::CreateTestComponentData(ComponentId, Value);
+	return MakeUnique<SpatialGDK::ComponentChange>(ComponentId, MoveTemp(Data).Release());
+}
 
-	void AddComponentToViewWithValue(TUniquePtr<SpatialGDK::EntityView>& View, const Worker_EntityId EntityId,
-		const Worker_ComponentId ComponentId, const double Value)
-	{
-		SpatialGDK::ComponentData Data = SpatialGDK::CreateTestComponentData(ComponentId, Value);
-		auto *Element = &View->Emplace(EntityId, SpatialGDK::EntityViewElement{});
-		Element->Components.Emplace(MoveTemp(Data));
-	}
+TArray<SpatialGDK::EntityDelta> CreateComponentAddedDeltaWithChange(const Worker_EntityId EntityId,
+																	const TUniquePtr<SpatialGDK::ComponentChange>& Change)
+{
+	SpatialGDK::EntityDelta Delta = {};
+	Delta.EntityId = EntityId;
+	Delta.ComponentsAdded = SpatialGDK::ComponentSpan<SpatialGDK::ComponentChange>(Change.Get(), 1);
+	return TArray<SpatialGDK::EntityDelta>{ Delta };
+}
+
+void AddComponentToViewWithValue(TUniquePtr<SpatialGDK::EntityView>& View, const Worker_EntityId EntityId,
+								 const Worker_ComponentId ComponentId, const double Value)
+{
+	SpatialGDK::ComponentData Data = SpatialGDK::CreateTestComponentData(ComponentId, Value);
+	auto* Element = &View->Emplace(EntityId, SpatialGDK::EntityViewElement{});
+	Element->Components.Emplace(MoveTemp(Data));
+}
 } // anonymous namespace
 
 DISPATCHER_TEST(GIVEN_Dispatcher_WHEN_Callback_Added_Then_Invoked_THEN_Callback_Invoked_With_Correct_Values)
@@ -63,8 +66,7 @@ DISPATCHER_TEST(GIVEN_Dispatcher_WHEN_Callback_Added_Then_Invoked_THEN_Callback_
 	bool Invoked = false;
 	SpatialGDK::FDispatcher Dispatcher = CreateViewAndDispatcher().Dispatcher;
 
-	const SpatialGDK::FComponentValueCallback Callback = [&Invoked](SpatialGDK::FEntityComponentChange Change)
-	{
+	const SpatialGDK::FComponentValueCallback Callback = [&Invoked](SpatialGDK::FEntityComponentChange Change) {
 		if (Change.EntityId == ENTITY_ID)
 		{
 			Invoked = true;
@@ -85,8 +87,7 @@ DISPATCHER_TEST(GIVEN_Dispatcher_With_Callback_WHEN_Callback_Removed_THEN_Callba
 	bool Invoked = false;
 	SpatialGDK::FDispatcher Dispatcher = CreateViewAndDispatcher().Dispatcher;
 
-	const SpatialGDK::FComponentValueCallback Callback = [&Invoked](SpatialGDK::FEntityComponentChange)
-	{
+	const SpatialGDK::FComponentValueCallback Callback = [&Invoked](SpatialGDK::FEntityComponentChange) {
 		Invoked = true;
 	};
 
@@ -113,8 +114,7 @@ DISPATCHER_TEST(GIVEN_Dispatcher_WHEN_Callback_Added_And_Invoked_THEN_Callback_I
 	SpatialGDK::FDispatcher Dispatcher = ViewAndDispatcher.Dispatcher;
 	TUniquePtr<SpatialGDK::EntityView>& View = ViewAndDispatcher.View;
 
-	const SpatialGDK::FComponentValueCallback Callback = [&Invoked](SpatialGDK::FEntityComponentChange Change)
-	{
+	const SpatialGDK::FComponentValueCallback Callback = [&Invoked](SpatialGDK::FEntityComponentChange Change) {
 		if (Change.EntityId == ENTITY_ID)
 		{
 			Invoked = true;
