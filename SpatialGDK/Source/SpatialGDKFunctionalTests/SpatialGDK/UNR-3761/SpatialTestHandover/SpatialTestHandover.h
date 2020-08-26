4e4980789eaf2b979dc0cfaac0e8c9d1df8a23c7
Andrei Lazar <andreilazar@improbable.io>
2020-08-26 18:14:12 +0100

    UNR-3761/Handover-Gym-Automation (#2492)
    * Added an automated test for the Handover Gym
    
    * Addressed PR feedback
    
    * The test now fails if no handover happens
    
    * Addressed PR feedback
    
    Co-authored-by: Michael Samiec <michaelsamiec@improbable.io>
    
    Co-authored-by: Michael Samiec <michaelsamiec@improbable.io>

---
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/UNR-3761/SpatialTestHandover/SpatialTestHandover.h
@@ -0,0 +1,39 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpatialFunctionalTest.h"
+#include "SpatialTestHandover.generated.h"
+
+class AHandoverCube;
+class ULayeredLBStrategy;
+
+UCLASS()
+class SPATIALGDKFUNCTIONALTESTS_API ASpatialTestHandover : public ASpatialFunctionalTest
+{
+	GENERATED_BODY()
+
+public:
+	ASpatialTestHandover();
+
+	virtual void BeginPlay() override;
+
+private:
+	AHandoverCube* HandoverCube;
+
+	// The location where the HandoverCube will be spawned at.
+	FVector SpawnLocation;
+
+	// Index used to store what Location the HandoverCube should be moved to at each step in the test.
+	int LocationIndex;
+
+	// Array holding the Locations where the HandoverCube will be moved throughout the test.
+	TArray<FVector> TestLocations;
+
+	// The Load Balancing used by the test, needed to decide what Server should have authority over the HandoverCube.
+	ULayeredLBStrategy* LoadBalancingStrategy;
+
+	// Int holding the number of authority changes the HandoverCube had throughout the test.
+	int AuthorityChanges;
+};
