3ddaf5c7942e9335c0341e316c0cdd0f50111526
Jacques Elliott <jacques@improbable.io>
2020-09-04 00:49:03 +0100

    lint

--- SpatialGDK/Source/SpatialGDK/Public/SpatialView/SubView.h
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/SubView.h
@@ -8,36 +8,34 @@
 
 namespace SpatialGDK
 {
-	class SubView
-	{
-	public:
-		SubView(const Worker_ComponentId TagComponentId,
-			const TFunction<bool(const Worker_EntityId, const EntityViewElement&)> Filter,
-			const EntityView& View,
-			FDispatcher& Dispatcher);
+class SubView
+{
+public:
+	SubView(const Worker_ComponentId TagComponentId, const TFunction<bool(const Worker_EntityId, const EntityViewElement&)> Filter,
+			const EntityView& View, FDispatcher& Dispatcher);
 
-		void TagQuery(Query& QueryToTag) const;
-		void TagEntity(TArray<FWorkerComponentData>& Components) const;
+	void TagQuery(Query& QueryToTag) const;
+	void TagEntity(TArray<FWorkerComponentData>& Components) const;
 
-		void AdvanceViewDelta(const ViewDelta& Delta);
-		const ViewDelta& GetViewDelta() const;
-		void RefreshEntity(const Worker_EntityId EntityId);
+	void AdvanceViewDelta(const ViewDelta& Delta);
+	const ViewDelta& GetViewDelta() const;
+	void RefreshEntity(const Worker_EntityId EntityId);
 
-	private:
-		void OnTaggedEntityAdded(const Worker_EntityId EntityId);
-		void OnTaggedEntityRemoved(const Worker_EntityId EntityId);
-		void CheckEntityAgainstFilter(const Worker_EntityId EntityId);
+private:
+	void OnTaggedEntityAdded(const Worker_EntityId EntityId);
+	void OnTaggedEntityRemoved(const Worker_EntityId EntityId);
+	void CheckEntityAgainstFilter(const Worker_EntityId EntityId);
 
-		const Worker_ComponentId TagComponentId;
-		const TFunction<bool(const Worker_EntityId, const EntityViewElement&)> Filter;
-		const EntityView& View;
+	const Worker_ComponentId TagComponentId;
+	const TFunction<bool(const Worker_EntityId, const EntityViewElement&)> Filter;
+	const EntityView& View;
 
-		ViewDelta SubDelta;
+	ViewDelta SubDelta;
 
-		TSet<Worker_EntityId> TaggedEntities;
-		TSet<Worker_EntityId> CompleteEntities;
-		TSet<Worker_EntityId> NewlyCompleteEntities;
-		TSet<Worker_EntityId> NewlyIncompleteEntities;
-	};
+	TSet<Worker_EntityId> TaggedEntities;
+	TSet<Worker_EntityId> CompleteEntities;
+	TSet<Worker_EntityId> NewlyCompleteEntities;
+	TSet<Worker_EntityId> NewlyIncompleteEntities;
+};
 
 } // namespace SpatialGDK
