ec06f55a209855c289e0e589d4bc7702e066f8bc
Nicolas Colombe <nicolascolombe@improbable.io>
2020-09-03 15:01:38 +0100

    Re add check between WITH_EDITOR guards.

--- SpatialGDK/Source/SpatialGDK/Private/LoadBalancing/LayeredLBStrategy.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/LoadBalancing/LayeredLBStrategy.cpp
@@ -223,7 +223,7 @@ UAbstractLBStrategy* ULayeredLBStrategy::GetLBStrategyForVisualRendering() const
 {
 	// The default strategy is guaranteed to exist as long as the strategy is ready.
 	checkf(LayerNameToLBStrategy.Contains(SpatialConstants::DefaultLayer),
-		TEXT("Load balancing strategy does not contain default layer which is needed to render worker debug visualization. "
+		   TEXT("Load balancing strategy does not contain default layer which is needed to render worker debug visualization. "
 				"Default layer presence should be enforced by MultiWorkerSettings edit validation. Class: %s"),
 		   *GetNameSafe(this));
 	return GetLBStrategyForLayer(SpatialConstants::DefaultLayer);
@@ -231,6 +231,11 @@ UAbstractLBStrategy* ULayeredLBStrategy::GetLBStrategyForVisualRendering() const
 
 UAbstractLBStrategy* ULayeredLBStrategy::GetLBStrategyForLayer(FName Layer) const
 {
+	// Editor has the option to display the load balanced zones and could query the strategy anytime.
+#ifndef WITH_EDITOR
+	check(IsReady());
+#endif
+
 	if (UAbstractLBStrategy* const* Entry = LayerNameToLBStrategy.Find(Layer))
 	{
 		return *Entry;
