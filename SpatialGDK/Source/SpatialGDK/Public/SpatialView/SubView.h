be97f91b9f8c4a216a45773ecc77a9a8a586aa51
jacquese <jacques@improbable.io>
2020-09-11 00:28:31 +0100

    actual subviews part

---
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/SubView.h
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "Dispatcher.h"
+#include "EntityView.h"
+#include "Schema/Interest.h"
+#include "Templates/Function.h"
+
+using FFilterPredicate = TFunction<bool(const Worker_EntityId, const SpatialGDK::EntityViewElement&)>;
+using FRefreshCallback = TFunction<void(const Worker_EntityId)>;
+using FDispatcherRefreshCallback = TFunction<void(const FRefreshCallback)>;
+using FComponentChangeRefreshPredicate = TFunction<bool(SpatialGDK::FEntityComponentChange)>;
+using FAuthorityChangeRefreshPredicate = TFunction<bool(Worker_EntityId)>;
+
+namespace SpatialGDK
+{
+class FSubView
+{
+public:
+	// The subview constructor takes the filter and the dispatcher refresh callbacks for the subview, rather than
+	// adding them to the subview later. This is to maintain the invariant that a subview always has the correct
+	// full set of complete entities. During construction, it calculates the initial set of complete entities,
+	// and registers the passed dispatcher callbacks in order to ensure all possible changes which could change
+	// the state of completeness for any entity are picked up by the subview to maintain this invariant.
+	FSubView(const Worker_ComponentId& TagComponentId, FFilterPredicate Filter, const EntityView* View, FDispatcher& Dispatcher,
+			 const TArray<FDispatcherRefreshCallback>& DispatcherRefreshCallbacks);
+
+	~FSubView() = default;
+
+	// Non-copyable, non-movable
+	FSubView(const FSubView&) = delete;
+	FSubView(FSubView&&) = default;
+	FSubView& operator=(const FSubView&) = delete;
+	FSubView& operator=(FSubView&&) = default;
+
+	void TagQuery(Query& QueryToTag) const;
+	void TagEntity(TArray<FWorkerComponentData>& Components) const;
+
+	void Advance(const ViewDelta& Delta);
+	const FSubViewDelta& GetViewDelta() const;
+	void RefreshEntity(const Worker_EntityId& EntityId);
+
+	// Helper functions for creating dispatcher refresh callbacks for use when constructing a subview.
+	// Takes an optional predicate argument to further filter what causes a refresh. Example: Only trigger
+	// a refresh if the received component change has a change for a certain field.
+	static FDispatcherRefreshCallback CreateComponentExistenceRefreshCallback(FDispatcher& Dispatcher,
+																			  const Worker_ComponentId& ComponentId,
+																			  const FComponentChangeRefreshPredicate& RefreshPredicate);
+	static FDispatcherRefreshCallback CreateComponentChangedRefreshCallback(FDispatcher& Dispatcher, const Worker_ComponentId& ComponentId,
+																			const FComponentChangeRefreshPredicate& RefreshPredicate);
+	static FDispatcherRefreshCallback CreateAuthorityChangeRefreshCallback(FDispatcher& Dispatcher, const Worker_ComponentId& ComponentId,
+																		   const FAuthorityChangeRefreshPredicate& RefreshPredicate);
+
+private:
+	void RegisterTagCallbacks(FDispatcher& Dispatcher);
+	void RegisterRefreshCallbacks(const TArray<FDispatcherRefreshCallback>& DispatcherRefreshCallbacks);
+	void OnTaggedEntityAdded(const Worker_EntityId& EntityId);
+	void OnTaggedEntityRemoved(const Worker_EntityId& EntityId);
+	void CheckEntityAgainstFilter(const Worker_EntityId& EntityId);
+	void EntityComplete(const Worker_EntityId& EntityId);
+	void EntityIncomplete(const Worker_EntityId& EntityId);
+
+	Worker_ComponentId TagComponentId;
+	FFilterPredicate Filter;
+	const EntityView* View;
+
+	FSubViewDelta SubViewDelta;
+
+	TArray<Worker_EntityId> TaggedEntities;
+	TArray<Worker_EntityId> CompleteEntities;
+	TArray<Worker_EntityId> NewlyCompleteEntities;
+	TArray<Worker_EntityId> NewlyIncompleteEntities;
+	TArray<Worker_EntityId> TemporarilyIncompleteEntities;
+};
+
+} // namespace SpatialGDK
