5f01096efb1c07ad07e2524771b2519f077fd411
Evi Kalogiannidou <evmorfiakalogiannidou@improbable.io>
2020-09-28 15:38:51 +0100

    Add automated test

---
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/DynamicSubobjectsTest/DynamicSubObjectsTest.h
@@ -0,0 +1,34 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpatialFunctionalTest.h"
+#include "DynamicSubobjectsTest.generated.h"
+
+class ATestMovementCharacter;
+class AReplicatedGASTestActor;
+
+UCLASS()
+class SPATIALGDKFUNCTIONALTESTS_API ADynamicSubobjectsTest : public ASpatialFunctionalTest
+{
+	GENERATED_BODY()
+
+public:
+	ADynamicSubobjectsTest();
+
+	virtual void BeginPlay() override;
+
+	// A reference to the Default Pawn of Client 1 to allow for repossession in the final step of the test.
+	APawn* ClientOneDefaultPawn;
+
+	ATestMovementCharacter* ClientOneSpawnedPawn;
+
+	AReplicatedGASTestActor* TestActor;
+
+	// The spawn location for Client 1's Pawn;
+	FVector CharacterSpawnLocation;
+
+	// A remote location where Client 1's Pawn will be moved in order to not see the AReplicatedVisibilityTestActor.
+	FVector CharacterRemoteLocation;
+};
