992d9c0446001ca7623498ded88467e12c2b2b56
Tilman Schmidt <tilmanschmidt+github@improbable.io>
2020-10-02 14:13:26 +0100

    Gather hierarchy actors

--- SpatialGDK/Source/SpatialGDK/Private/Utils/SpatialDebugger.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Utils/SpatialDebugger.cpp
@@ -607,19 +607,15 @@ void ASpatialDebugger::DrawDebugLocalPlayer(UCanvas* Canvas)
 		return;
 	}
 
-	TArray<const AActor*> ActorsToDisplay = { LocalPlayerState.Get(), LocalPlayerController.Get(), LocalPawn.Get() };
-
-	if (bShowPlayerHierarchy)
-	{
-		GetReplicatedActorsInHierarchy(LocalPlayerController.Get(), ActorsToDisplay);
-	}
+	TArray<const AActor*> PlayerHierarchyActors = { LocalPlayerState.Get(), LocalPlayerController.Get(), LocalPawn.Get() };
+	GetReplicatedActorsInHierarchy(LocalPlayerController.Get(), PlayerHierarchyActors);
 
 	FVector2D ScreenLocation(PlayerPanelStartX, PlayerPanelStartY);
 
-	for (int32 i = 0; i < ActorsToDisplay.Num(); ++i)
+	for (int32 i = 0; i < PlayerHierarchyActors.Num(); ++i)
 	{
-		const Worker_EntityId EntityId = NetDriver->PackageMap->GetEntityIdFromObject(ActorsToDisplay[i]);
-		DrawTag(Canvas, ScreenLocation, EntityId, ActorsToDisplay[i]->GetName());
+		const Worker_EntityId EntityId = NetDriver->PackageMap->GetEntityIdFromObject(PlayerHierarchyActors[i]);
+		DrawTag(Canvas, ScreenLocation, EntityId, PlayerHierarchyActors[i]->GetName());
 		ScreenLocation.Y += PLAYER_TAG_VERTICAL_OFFSET;
 	}
 }
