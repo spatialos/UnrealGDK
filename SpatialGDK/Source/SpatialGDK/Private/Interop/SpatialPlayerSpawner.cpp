5731f9ec850442411c8e6edd4e57e4326e7c3735
Danny Birch <danielbirch@improbable.io>
2020-10-05 14:00:00 +0100

    Revert log change

--- SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialPlayerSpawner.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialPlayerSpawner.cpp
@@ -361,10 +361,8 @@ void USpatialPlayerSpawner::ReceiveForwardedPlayerSpawnRequest(const Worker_Comm
 	}
 	else
 	{
-		UE_LOG(
-			LogSpatialPlayerSpawner, Log,
-			TEXT("PlayerStart Actor was null object ref in forward spawn request. This is intentional when handing request to the correct "
-				 "load balancing layer. Attempting to find a player start again."));
+		UE_LOG(LogSpatialPlayerSpawner, Error, UE_LOG(LogSpatialPlayerSpawner, Error,
+			TEXT("PlayerStart Actor UnrealObjectRef was invalid on forwarded player spawn request worker: %s"), *ClientWorkerId);
 		FindPlayerStartAndProcessPlayerSpawn(PlayerSpawnData, ClientWorkerId);
 	}
 
