95c61634913a37b477099f542d4cde69fe9581c2
samiwh <sami@improbable.io>
2020-10-10 14:25:05 +0100

    Split out critical section filtering from the worker view.

--- SpatialGDK/Source/SpatialGDK/Private/SpatialView/WorkerView.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/SpatialView/WorkerView.cpp
@@ -15,11 +15,9 @@ WorkerView::WorkerView(SpatialEventTracer* InEventTracer)
 {
 }
 
-void WorkerView::AdvanceViewDelta()
+void WorkerView::AdvanceViewDelta(TArray<OpList> OpLists)
 {
-	Delta.Clear();
-	Delta.SetFromOpList(MoveTemp(QueuedOps), View);
-	QueuedOps.Empty();
+	Delta.SetFromOpList(MoveTemp(OpLists), View);
 }
 
 const ViewDelta& WorkerView::GetViewDelta() const
@@ -30,52 +28,6 @@ const ViewDelta& WorkerView::GetViewDelta() const
 const EntityView& WorkerView::GetView() const
 {
 	return View;
-}
-
-void WorkerView::EnqueueOpList(OpList Ops)
-{
-	// Ensure that we only process closed critical sections.
-	// Scan backwards looking for critical sections ops.
-	for (uint32 i = Ops.Count; i > 0; --i)
-	{
-		Worker_Op& Op = Ops.Ops[i - 1];
-		if (Op.op_type != WORKER_OP_TYPE_CRITICAL_SECTION)
-		{
-			continue;
-		}
-
-		// There can only be one critical section open at a time.
-		// So any previous open critical section must now be closed.
-		for (OpList& OpenCriticalSection : OpenCriticalSectionOps)
-		{
-			QueuedOps.Add(MoveTemp(OpenCriticalSection));
-		}
-		OpenCriticalSectionOps.Empty();
-
-		// If critical section op is opening the section then enqueue any ops before this point and store the open critical section.
-		if (Op.op.critical_section.in_critical_section)
-		{
-			SplitOpListPair SplitOpLists(MoveTemp(Ops), i);
-			QueuedOps.Add(MoveTemp(SplitOpLists.Head));
-			OpenCriticalSectionOps.Add(MoveTemp(SplitOpLists.Tail));
-		}
-		// If critical section op is closing the section then enqueue all ops.
-		else
-		{
-			QueuedOps.Add(MoveTemp(Ops));
-		}
-		return;
-	}
-
-	// If no critical section is present then either add this to existing open section ops if there are any or enqueue if not.
-	if (OpenCriticalSectionOps.Num())
-	{
-		OpenCriticalSectionOps.Push(MoveTemp(Ops));
-	}
-	else
-	{
-		QueuedOps.Push(MoveTemp(Ops));
-	}
 }
 
 TUniquePtr<MessagesToSend> WorkerView::FlushLocalChanges()
---
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialView/CriticalSectionFilter.h
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "Containers/Array.h"
+#include "OpList/OpList.h"
+
+namespace SpatialGDK
+{
+class FCriticalSectionFilter
+{
+public:
+	void AddOpList(OpList Ops);
+	TArray<OpList> GetReadyOpLists();
+
+private:
+	TArray<OpList> ReadyOps;
+	TArray<OpList> OpenCriticalSectionOps;
+};
+} // namespace SpatialGDK
