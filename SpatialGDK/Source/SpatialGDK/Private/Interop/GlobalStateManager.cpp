c10e2dfc2b38726b28739b17967749ea6bd27167
Danny Birch <danielbirch@improbable.io>
2020-10-14 18:16:07 +0100

    Non-replicated actors don't need roles

--- SpatialGDK/Source/SpatialGDK/Private/Interop/GlobalStateManager.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/GlobalStateManager.cpp
@@ -389,13 +389,16 @@ void UGlobalStateManager::HandleActorBasedOnLoadBalancer(AActor* Actor) const
 		return;
 	}
 
-	// Non-replicated Actors should always be authoritative.
-	bool bAuthoritative = !Actor->GetIsReplicated();
+	if (!Actor->GetIsReplicated())
+	{
+		return; 
+	}
+
 	// Replicated level Actors should only be initially authority if:
 	//  - these are workers starting as part of a fresh deployment (tracked by the bCanSpawnWithAuthority bool),
 	//  - these actors are marked as NotPersistent and we're loading from a saved snapshot (which means bCanSpawnWithAuthority is false)
 	//  - the load balancing strategy says this server should be authoritative (as opposed to some other server).
-	bAuthoritative |= (bCanSpawnWithAuthority || Actor->GetClass()->HasAnySpatialClassFlags(SPATIALCLASS_NotPersistent))
+	bool bAuthoritative = (bCanSpawnWithAuthority || Actor->GetClass()->HasAnySpatialClassFlags(SPATIALCLASS_NotPersistent))
 					  && NetDriver->LoadBalanceStrategy->ShouldHaveAuthority(*Actor);
 
 	Actor->Role = bAuthoritative ? ROLE_Authority : ROLE_SimulatedProxy;
