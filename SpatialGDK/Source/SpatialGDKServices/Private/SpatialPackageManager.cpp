506389069acb4f2044f2f4b655507e3281b6acf3
mimistiv <mariaivanova@improbable.io>
2020-10-15 10:54:20 +0100

    retrieve runtime logic added to the class

--- SpatialGDK/Source/SpatialGDKServices/Private/SpatialPackageManager.cpp
+++ SpatialGDK/Source/SpatialGDKServices/Private/SpatialPackageManager.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) Improbable Worlds Ltd, All Rights Reserved
 
 #include "SpatialPackageManager.h"
-
 #include "SpatialGDKServicesConstants.h"
 
 DEFINE_LOG_CATEGORY(LogSpatialPackageManager);
@@ -16,10 +15,30 @@ bool FSpatialPackageManager::TryFetchRuntimeBinary()
 {
 	UE_LOG(LogSpatialPackageManager, Error, TEXT("UNR-4334 - Starting to fetch the Runtime binary"));
 
+	FString RuntimePath = FString::Printf(TEXT("%s/runtime.exe"), *SpatialGDKServicesConstants::GDKProgramPath);
+
 	// Check if the binary already exists for a given version
 
-	// UnrealGDK\SpatialGDK\Binaries\ThirdParty\Improbable\Programs
-	FString RuntimePath = SpatialGDKServicesConstants::GDKProgramPath;
+	bool bSuccess = FPaths::FileExists(RuntimePath);
+
+	if (bSuccess) {
+		UE_LOG(LogSpatialPackageManager, Error, TEXT("RUNTIME BINARIES ALREADY EXIST"));
+	}
+
+	// spatial package retrieve runtime x86_64-win32 0.5.1 runtime.zip
+	else
+	{
+		FString RuntimeRetrieveArgs = FString::Printf(TEXT("package retrieve runtime x86_64-win32 0.5.1 runtime.zip"));
+		FString RuntimeRetrieveResult;
+		int32 ExitCode;
+		FSpatialGDKServicesModule::ExecuteAndReadOutput(SpatialGDKServicesConstants::SpatialExe, RuntimeRetrieveArgs,
+														SpatialGDKServicesConstants::GDKProgramPath, RuntimeRetrieveResult, ExitCode);
+		const int32 exitCodeSuccess = 0;
+		UE_LOG(LogSpatialPackageManager, Log, TEXT("OUTPUT %s"), *RuntimeRetrieveResult);
+		return (ExitCode == exitCodeSuccess);
+		
+	}
+
 
 	// If it does not exist then fetch the binary using `spatial worker package retrieve`
 	// Download the zip to // UnrealGDK\SpatialGDK\Binaries\ThirdParty\Improbable\Programs\Runtime\*version*
