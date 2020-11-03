c666fac3dbb776f71745e386c39a6be89ee66dc3
Jessica Falk <jessica@improbable.io>
2020-11-03 16:51:10 +0000

    cross server rpc handler and sender

---
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/CrossServerRPCHandler.cpp
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "Interop/CrossServerRPCHandler.h"
+
+#include "Interop/Connection/SpatialWorkerConnection.h"
+#include "SpatialGDKSettings.h"
+
+using namespace SpatialGDK;
+
+CrossServerRPCHandler::CrossServerRPCHandler(ViewCoordinator& InCoordinator, TUniquePtr<RPCExecutorInterface> InRPCExecutor)
+	: Coordinator(InCoordinator)
+	, RPCExecutor(MoveTemp(InRPCExecutor))
+{
+	CommandRetryTime = GetDefault<USpatialGDKSettings>()->QueuedIncomingRPCRetryTime;
+}
+
+void CrossServerRPCHandler::ProcessOps(const float TimeAdvancedS, const TArray<Worker_Op>& WorkerMessages)
+{
+	for (auto& Op : WorkerMessages)
+	{
+		if (Op.op_type == WORKER_OP_TYPE_COMMAND_REQUEST
+			&& Op.op.command_request.request.command_index == SpatialConstants::UNREAL_RPC_ENDPOINT_COMMAND_ID
+			&& Op.op.command_request.request.component_id == SpatialConstants::SERVER_TO_SERVER_COMMAND_ENDPOINT_COMPONENT_ID)
+		{
+			HandleWorkerOp(Op);
+		}
+	}
+
+	TimeElapsedS += TimeAdvancedS;
+	if (CommandRetryTime < TimeElapsedS)
+	{
+		ProcessPendingCommandOps();
+		TimeElapsedS = 0.0;
+	}
+}
+
+void CrossServerRPCHandler::ProcessPendingCommandOps()
+{
+	TArray<Worker_EntityId> EmptyRPCQueues;
+	for (auto& CrossServerRPCs : QueuedCrossServerRPCs)
+	{
+		int32 ProcessedRPCs = 0;
+		for (auto& Command : CrossServerRPCs.Value)
+		{
+			if (!TryExecuteCommandRequest(Command))
+			{
+				break;
+			}
+
+			++ProcessedRPCs;
+		}
+
+		CrossServerRPCs.Value.RemoveAt(0, ProcessedRPCs);
+		if (CrossServerRPCs.Value.Num() == 0)
+		{
+			EmptyRPCQueues.Add(CrossServerRPCs.Key);
+		}
+	}
+	for (auto EntityId : EmptyRPCQueues)
+	{
+		QueuedCrossServerRPCs.Remove(EntityId);
+	}
+}
+
+void CrossServerRPCHandler::HandleWorkerOp(const Worker_Op& Op)
+{
+	const Worker_CommandRequestOp& CommandOp = Op.op.command_request;
+	FCrossServerRPCParams Params = RPCExecutor->TryRetrieveCrossServerRPCParams(Op);
+	if (Params.RequestId == -1)
+	{
+		Coordinator.SendEntityCommandFailure(CommandOp.request_id, "Failed to parse cross server RPC", Op.span_id);
+		return;
+	}
+
+	if (!QueuedCrossServerRPCs.Contains(CommandOp.entity_id))
+	{
+		// No Command Requests of this type queued so far. Let's try to process it:
+		if (TryExecuteCommandRequest(Params))
+		{
+			return;
+		}
+	}
+
+	// Unable to process command request. Let's queue it up:
+	if (!QueuedCrossServerRPCs.Contains(CommandOp.entity_id))
+	{
+		QueuedCrossServerRPCs.Add(CommandOp.entity_id, TArray<FCrossServerRPCParams>());
+	}
+
+	QueuedCrossServerRPCs[CommandOp.entity_id].Add(MoveTemp(Params));
+}
+
+bool CrossServerRPCHandler::TryExecuteCommandRequest(const FCrossServerRPCParams& Params)
+{
+	if (RPCExecutor->ExecuteCommand(Params))
+	{
+		Coordinator.SendEntityCommandResponse(Params.RequestId,
+											  CommandResponse(SpatialConstants::SERVER_TO_SERVER_COMMAND_ENDPOINT_COMPONENT_ID,
+															  SpatialConstants::UNREAL_RPC_ENDPOINT_COMMAND_ID),
+											  Params.SpanId);
+		return true;
+	}
+
+	return false;
+}
+
+void CrossServerRPCHandler::DropQueueForEntity(const Worker_EntityId_Key EntityId)
+{
+	QueuedCrossServerRPCs.Remove(EntityId);
+}
---
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/CrossServerRPCHandler.h
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "RPCExecutorInterface.h"
+#include "SpatialView/ViewCoordinator.h"
+
+namespace SpatialGDK
+{
+DECLARE_DELEGATE_RetVal_OneParam(bool, FProcessCrossServerRPC, const FCrossServerRPCParams&);
+DECLARE_DELEGATE_RetVal_OneParam(FCrossServerRPCParams, FTryRetrieveCrossServerRPCParams, const Worker_CommandRequestOp&);
+
+class CrossServerRPCHandler
+{
+public:
+	CrossServerRPCHandler(ViewCoordinator& Coordinator, TUniquePtr<RPCExecutorInterface> RPCExecutor);
+
+	void ProcessOps(const float TimeAdvancedS, const TArray<Worker_Op>& WorkerMessages);
+	void ProcessPendingCommandOps();
+
+private:
+	ViewCoordinator& Coordinator;
+	TUniquePtr<RPCExecutorInterface> RPCExecutor;
+
+	double TimeElapsedS = 0.0;
+	double CommandRetryTime = 0.0;
+	TMap<Worker_EntityId_Key, TArray<FCrossServerRPCParams>> QueuedCrossServerRPCs;
+	FProcessCrossServerRPC ProcessCrossServerRPCDelegate;
+	FTryRetrieveCrossServerRPCParams TryRetrieveCrossServerRPCParamsDelegate;
+
+	void HandleWorkerOp(const Worker_Op& Op);
+	bool TryExecuteCommandRequest(const FCrossServerRPCParams& Params);
+	void DropQueueForEntity(const Worker_EntityId_Key EntityId);
+};
+} // namespace SpatialGDK
