f13d3858c8d6db861c3a63d361f26be4a2348bbf
Jessica Falk <jessica@improbable.io>
2020-11-05 13:07:03 +0000

    cross server rpc handler and sender

---
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/CrossServerRPCSender.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "Interop/CrossServerRPCSender.h"
+
+#include "Interop/Connection/SpatialWorkerConnection.h"
+#include "Utils/SpatialMetrics.h"
+
+using namespace SpatialGDK;
+
+CrossServerRPCSender::CrossServerRPCSender(ViewCoordinator& InCoordinator, USpatialMetrics* InSpatialMetrics)
+	: Coordinator(InCoordinator)
+	, SpatialMetrics(InSpatialMetrics)
+{
+}
+
+void CrossServerRPCSender::SendCommand(const FUnrealObjectRef& InTargetObjectRef, UObject* TargetObject, UFunction* Function,
+									   RPCPayload&& InPayload, FRPCInfo Info, const TOptional<Trace_SpanId>& SpanId) const
+{
+	if (Function == nullptr || TargetObject == nullptr || InTargetObjectRef.Entity == SpatialConstants::INVALID_ENTITY_ID
+		|| Info.Type != ERPCType::CrossServer)
+	{
+		return;
+	}
+
+	auto SchemaType = Schema_CreateCommandRequest();
+	Schema_Object* RequestObject = Schema_GetCommandRequestObject(SchemaType);
+	RPCPayload::WriteToSchemaObject(RequestObject, InTargetObjectRef.Offset, Info.Index, InPayload.PayloadData.GetData(),
+									InPayload.PayloadData.Num());
+	CommandRequest CommandRequest(OwningCommandRequestPtr(SchemaType), SpatialConstants::SERVER_TO_SERVER_COMMAND_ENDPOINT_COMPONENT_ID,
+								  SpatialConstants::UNREAL_RPC_ENDPOINT_COMMAND_ID);
+
+	if (Function->HasAnyFunctionFlags(FUNC_NetReliable))
+	{
+		Coordinator.SendEntityCommandRequest(InTargetObjectRef.Entity, MoveTemp(CommandRequest), RETRY_MAX_TIMES, SpanId);
+	}
+	else
+	{
+		Coordinator.SendEntityCommandRequest(InTargetObjectRef.Entity, MoveTemp(CommandRequest), TOptional<uint32>(), SpanId);
+	}
+
+#if !UE_BUILD_SHIPPING
+	SpatialMetrics->TrackSentRPC(Function, ERPCType::CrossServer, InPayload.PayloadData.Num());
+#endif // !UE_BUILD_SHIPPING
+}
---
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/CrossServerRPCSender.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "Interop/SpatialClassInfoManager.h"
+#include "Schema/RPCPayload.h"
+#include "SpatialView/CommandRetryHandler.h"
+#include "Utils/SpatialMetrics.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogCrossServerRPCSender, Log, All);
+
+namespace SpatialGDK
+{
+class ViewCoordinator;
+constexpr FRetryData RETRY_MAX_TIMES = { SpatialConstants::MAX_NUMBER_COMMAND_ATTEMPTS, 0, 0.1f, 5.0f, 0 };
+
+class CrossServerRPCSender
+{
+public:
+	CrossServerRPCSender(ViewCoordinator& Coordinator, USpatialMetrics* SpatialMetrics);
+	void SendCommand(const FUnrealObjectRef& InTargetObjectRef, UObject* TargetObject, UFunction* Function, RPCPayload&& InPayload,
+					 FRPCInfo Info, const TOptional<Trace_SpanId>& SpanId) const;
+
+private:
+	ViewCoordinator& Coordinator;
+	USpatialMetrics* SpatialMetrics;
+};
+} // namespace SpatialGDK
