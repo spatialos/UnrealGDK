a7efd5af9fcccab577b1a3f180548a2725db6247
Thomas Yung <thomasyung@improbable.io>
2020-11-11 11:35:06 +0000

    refactor: format

--- SpatialGDK/Source/SpatialGDK/Public/Utils/WorkerVersionCheck.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/WorkerVersionCheck.h
@@ -6,12 +6,13 @@
 
 #define WORKER_SDK_VERSION "14.8.0"
 
-constexpr bool StringsEqual(char const * A, char const * B) {
-    return *A == *B && (*A == '\0' || StringsEqual(A + 1, B + 1));
+constexpr bool StringsEqual(char const* A, char const* B)
+{
+	return *A == *B && (*A == '\0' || StringsEqual(A + 1, B + 1));
 }
 
 // Check if the Worker SDK is compatible with the current version of Unreal Engine
 // WORKER_SDK_VERSION is incremented in engine when breaking changes
 // are made that make previous versions of the SDK incompatible
 static_assert(StringsEqual(WORKER_API_VERSION_STR, WORKER_SDK_VERSION),
-              "Worker SDK Version is incompatible with the Engine Version. Check both the Worker SDK and Engine are up to date");
+			  "Worker SDK Version is incompatible with the Engine Version. Check both the Worker SDK and Engine are up to date");
