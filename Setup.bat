e604afc8023c367bdff24de170a20b0be1e81202
Thomas Yung <thomasyung@improbable.io>
2020-11-12 14:15:11 +0000

    fix: undo drive-by log

--- Setup.bat
+++ Setup.bat
@@ -9,11 +9,7 @@ pushd "%~dp0"
 call :MarkStartOfBlock "%~0"
 
 call :MarkStartOfBlock "Setup the git hooks"
-    if not exist .git\hooks (
-      rem Not a critical error
-      echo Skipping git hooks: .git\hooks not found
-      goto SkipGitHooks
-    )
+    if not exist .git\hooks goto SkipGitHooks
 
     rem Remove the old post-checkout hook.
     if exist .git\hooks\post-checkout del .git\hooks\post-checkout
