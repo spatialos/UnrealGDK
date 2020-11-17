4ba33023055fd3d8a5c08b83e1c4938ecd07ae3b
Michael Samiec <michaelsamiec@improbable.io>
2020-11-17 22:38:58 +0000

    Fix trace libs

--- SetupIncTraceLibs.bat
+++ SetupIncTraceLibs.bat
@@ -33,6 +33,8 @@ call :MarkStartOfBlock "Unpack dependencies"
 	set LEGACY_FOLDER=%WORKER_SDK_DIR%\improbable\legacy\
 	set TRACE_HEADER="%LEGACY_FOLDER%trace.h"
 	powershell -Command "(Get-Content '%TRACE_HEADER%').replace('#include <improbable/c_trace.h>', '#include <improbable/legacy/c_trace.h>') | Set-Content -Force '%TRACE_HEADER%'"
+	powershell -Command "(Get-Content '%TRACE_HEADER%').replace('void SetIntervalMillis', 'inline void SetIntervalMillis') | Set-Content -Force '%TRACE_HEADER%'"
+	powershell -Command "(Get-Content '%TRACE_HEADER%').replace('void SetBatchSize', 'inline void SetBatchSize') | Set-Content -Force '%TRACE_HEADER%'"
 	
 call :MarkEndOfBlock "Unpack dependencies"
 
