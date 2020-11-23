50aa68c9aed083f5732dc16cb9105fa8e25557b5
Matthew Sandford <matthewsandford@improbable.io>
2020-11-23 11:26:05 +0000

    Updates
    # Conflicts:
    #	SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialEventTracer.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialEventTracerUserInterface.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialGDKSpanId.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialWorkerConnection.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialReceiver.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialSender.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/SpatialView/ReceivedOpEventHandler.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/SpatialView/ViewCoordinator.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/SpatialView/WorkerView.cpp
    #	SpatialGDK/Source/SpatialGDK/Private/Utils/ComponentReader.cpp
    #	SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/OutgoingMessages.h
    #	SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/SpatialEventTracer.h
    #	SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/SpatialGDKSpanId.h
    #	SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/SpatialOSWorkerInterface.h
    #	SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/SpatialWorkerConnection.h
    #	SpatialGDK/Source/SpatialGDK/Public/Interop/SpatialSender.h
    #	SpatialGDK/Source/SpatialGDK/Public/SpatialGDKSettings.h
    #	SpatialGDK/Source/SpatialGDK/Public/SpatialView/OutgoingComponentMessage.h
    #	SpatialGDK/Source/SpatialGDK/Public/SpatialView/OutgoingMessages.h
    #	SpatialGDK/Source/SpatialGDK/Public/SpatialView/ViewCoordinator.h
    #	SpatialGDK/Source/SpatialGDK/Public/SpatialView/WorkerView.h
    #	SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/EventTracingTests/EventTracingTest.cpp
    #	SpatialGDK/Source/SpatialGDKTests/SpatialGDK/Interop/Connection/SpatialOSWorkerInterface/SpatialOSWorkerConnectionSpy.cpp
    #	SpatialGDK/Source/SpatialGDKTests/SpatialGDK/Interop/Connection/SpatialOSWorkerInterface/SpatialOSWorkerConnectionSpy.h

--- SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialGDKSpanId.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/Connection/SpatialGDKSpanId.cpp
@@ -2,79 +2,23 @@
 
 #include "Interop/Connection/SpatialGDKSpanId.h"
 
-// ----- FSpatialGDKSpanId -----
-
-FSpatialGDKSpanId::FSpatialGDKSpanId()
-	: bIsValid(false)
-{
-}
-
-FSpatialGDKSpanId::FSpatialGDKSpanId(bool bInIsValid)
-	: bIsValid(bInIsValid)
-{
-	if (bIsValid)
-	{
-		WriteData(Trace_SpanId_Null());
-	}
-}
-
-FSpatialGDKSpanId::FSpatialGDKSpanId(const Trace_SpanIdType* TraceSpanId)
-	: bIsValid(true)
-{
-	WriteData(TraceSpanId);
-}
-
-void FSpatialGDKSpanId::WriteData(const Trace_SpanIdType* TraceSpanId)
-{
-	FMemory::Memcpy(Data, TraceSpanId, TRACE_SPAN_ID_SIZE_BYTES);
-}
-
-Trace_SpanIdType* FSpatialGDKSpanId::GetData()
-{
-	return bIsValid ? Data : nullptr;
-}
-
-const Trace_SpanIdType* FSpatialGDKSpanId::GetConstData() const
-{
-	return bIsValid ? Data : nullptr;
-}
-
-FString FSpatialGDKSpanId::ToString() const
-{
-	return bIsValid ? ToString(Data) : TEXT("");
-}
-
-FString FSpatialGDKSpanId::ToString(const Trace_SpanIdType* TraceSpanId)
-{
-	FString HexStr;
-	for (int i = 0; i < TRACE_SPAN_ID_SIZE_BYTES; i++)
-	{
-		HexStr += FString::Printf(TEXT("%02x"), TraceSpanId[i]);
-	}
-	return HexStr;
-}
-
-// ----- FMultiGDKSpanIdAllocator -----
-
 FMultiGDKSpanIdAllocator::FMultiGDKSpanIdAllocator(const FSpatialGDKSpanId& A, const FSpatialGDKSpanId& B)
-	: Buffer(nullptr)
-	, NumSpanIds(2)
+	: NumSpanIds(2)
 	, NumBytes(NumSpanIds * TRACE_SPAN_ID_SIZE_BYTES)
 {
 	Buffer = Allocator.allocate(NumBytes);
-	WriteToBuffer(0, A);
-	WriteToBuffer(1, B);
+	Allocate(0, A);
+	Allocate(1, B);
 }
 
 FMultiGDKSpanIdAllocator::FMultiGDKSpanIdAllocator(const TArray<FSpatialGDKSpanId>& SpanIds)
-	: Buffer(nullptr)
-	, NumSpanIds(SpanIds.Num())
+	: NumSpanIds(SpanIds.Num())
 	, NumBytes(NumSpanIds * TRACE_SPAN_ID_SIZE_BYTES)
 {
 	Buffer = Allocator.allocate(NumBytes);
 	for (int32 SpanIndex = 0; SpanIndex < SpanIds.Num(); ++SpanIndex)
 	{
-		WriteToBuffer(SpanIndex, SpanIds[SpanIndex]);
+		Allocate(SpanIndex, SpanIds[SpanIndex]);
 	}
 }
 
@@ -83,13 +27,11 @@ FMultiGDKSpanIdAllocator::~FMultiGDKSpanIdAllocator()
 	Allocator.deallocate(Buffer, NumBytes);
 }
 
-void FMultiGDKSpanIdAllocator::WriteToBuffer(const int32 SpanIndex, const FSpatialGDKSpanId& SpanId)
+void FMultiGDKSpanIdAllocator::Allocate(const int32 SpanIndex, const FSpatialGDKSpanId& SpanId)
 {
-	const Trace_SpanIdType* Data = SpanId.GetConstData();
-	const int32 NumElements = TRACE_SPAN_ID_SIZE_BYTES / sizeof(Trace_SpanIdType);
-	for (int32 ElementIndex = 0; ElementIndex < NumElements; ++ElementIndex)
+	for (int32 ElementIndex = 0; ElementIndex < TRAC_SPAN_ID_SIZE_BYTES; ++ElementIndex)
 	{
-		const int32 BufferIndex = SpanIndex * TRACE_SPAN_ID_SIZE_BYTES + ElementIndex * sizeof(Trace_SpanIdType);
-		Allocator.construct(Buffer + BufferIndex, Data[ElementIndex]);
+		const int32 BufferIndex = SpanIndex * TRACE_SPAN_ID_SIZE_BYTES + ElementIndex;
+		Allocator.construct(Buffer + BufferIndex, SpanId.Data[ElementIndex]);
 	}
 }
--- SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/SpatialGDKSpanId.h
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/Connection/SpatialGDKSpanId.h
@@ -7,24 +7,20 @@
 
 #include <WorkerSDK/improbable/c_trace.h>
 
-struct SPATIALGDK_API FSpatialGDKSpanId
+struct FSpatialGDKSpanId
 {
-	FSpatialGDKSpanId();
-	explicit FSpatialGDKSpanId(bool bInIsValid);
-	explicit FSpatialGDKSpanId(const Trace_SpanIdType* TraceSpanId);
+	explicit FSpatialGDKSpanId() {};
+	explicit FSpatialGDKSpanId(const Trace_SpanIdType* TraceSpanId)
+	{
+		const int32 Size = TRACE_SPAN_ID_SIZE_BYTES * sizeof(Trace_SpanIdType);
+		FMemory::Memcpy(Data, TraceSpanId, Size);
+	}
 
-	FString ToString() const;
-	static FString ToString(const Trace_SpanIdType* TraceSpanId);
+	bool IsValid() const
+	{
+		return Trace_SpanId_IsNull(Data);
+	}
 
-	bool IsNull() const { return Trace_SpanId_IsNull(Data); }
-	bool IsValid() const { return bIsValid; }
-
-	void WriteData(const Trace_SpanIdType* TraceSpanId);
-	Trace_SpanIdType* GetData();
-	const Trace_SpanIdType* GetConstData() const;
-
-private:
-	bool bIsValid;
 	Trace_SpanIdType Data[TRACE_SPAN_ID_SIZE_BYTES];
 };
 
@@ -40,10 +36,13 @@ public:
 	int32 GetNumSpanIds() const { return NumSpanIds; }
 
 private:
-	void WriteToBuffer(const int32 SpanIndex, const FSpatialGDKSpanId& TraceSpanId);
 
-	Trace_SpanIdType* Buffer;
+	void Allocate(const int32 SpanIndex, const FSpatialGDKSpanId& TraceSpanId);
+
 	std::allocator<Trace_SpanIdType> Allocator;
 	const int32 NumSpanIds;
 	const int32 NumBytes;
+	Trace_SpanIdType* Buffer;
 };
+
+
