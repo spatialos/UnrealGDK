df3bbcb36a5ab2c4f8fbb1f005370807edd7df1e
Danny Birch <danielbirch@improbable.io>
2020-11-25 16:24:47 +0000

    Update SpatialGDK/Source/SpatialGDK/Private/Interop/RPCs/SpatialRPCService.cpp

--- SpatialGDK/Source/SpatialGDK/Private/Interop/RPCs/SpatialRPCService.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/RPCs/SpatialRPCService.cpp
@@ -70,11 +70,6 @@ EPushRPCResult SpatialRPCService::PushRPC(const Worker_EntityId EntityId, const 
 		PendingPayload.SpanId = SpanId;
 	}
 
-	if (Function->GetName() == TEXT("ServerAttack"))
-	{
-		int z = 0;
-	}
-
 #if TRACE_LIB_ACTIVE
 	TraceKey Trace = Payload.Trace;
 #endif
