59a552ab2ac6f172e7d786ca2b4e2bf1be327aff
Vix2021 <victoriabloom@improbable.io>
2020-12-04 18:57:05 +0200

    Update log with status code

--- SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialReceiver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/SpatialReceiver.cpp
@@ -1917,7 +1917,8 @@ void USpatialReceiver::OnCommandResponse(const Worker_Op& Op)
 
 		if (CommandResponseOp.status_code != WORKER_STATUS_CODE_SUCCESS)
 		{
-			UE_LOG(LogSpatialReceiver, Warning, TEXT("Migration diaganostic log failed."));
+			UE_LOG(LogSpatialReceiver, Warning, TEXT("Migration diaganostic log failed, status code %i."),
+				   CommandResponseOp.status_code);
 			return;
 		}
 
