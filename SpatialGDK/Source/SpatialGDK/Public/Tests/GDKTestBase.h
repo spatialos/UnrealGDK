165ed53914eef724b88f70cb9339b20b1794dd50
Thomas Yung <thomasyung@improbable.io>
2020-12-07 15:55:05 +0000

    [broken] format

--- SpatialGDK/Source/SpatialGDK/Public/Tests/GDKTestBase.h
+++ SpatialGDK/Source/SpatialGDK/Public/Tests/GDKTestBase.h
@@ -15,45 +15,50 @@ public:
 	}
 };
 
-#define	IMPLEMENT_GDK_AUTOMATION_TEST(TestName, PrettyName) \
-	class TestName : public FGDKTestBase \
-	{ \
-	public: \
-		TestName( const FString& InName ) \
-			: FGDKTestBase( InName, false ) {\
-		} \
-		virtual uint32 GetTestFlags() const override { return EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::EngineFilter; } \
-		virtual bool IsStressTest() const { return false; } \
-		virtual uint32 GetRequiredDeviceNum() const override { return 1; } \
-		virtual FString GetTestSourceFileName() const override { return __FILE__; } \
-		virtual int32 GetTestSourceFileLine() const override { return __LINE__; } \
-	protected: \
-		void SetUp() \
-		{ \
-			FLocalDeploymentManager* LocalDeploymentManager = GetLocalDeploymentManager(); \
-			if (LocalDeploymentManager->IsLocalDeploymentRunning() || LocalDeploymentManager->IsDeploymentStarting()) \
-			{ \
-				UE_LOG(LogGDKTestBase, Warning, TEXT("Deployment found! (Was this left over from another test?)")) \
-				UE_LOG(LogGDKTestBase, Warning, TEXT("Ending PIE session")) \
-				GEditor->RequestEndPlayMap(); \
-				ADD_LATENT_AUTOMATION_COMMAND(FWaitForDeployment(this, EDeploymentState::IsNotRunning)); \
-			} \
-		} \
-		virtual void GetTests(TArray<FString>& OutBeautifiedNames, TArray <FString>& OutTestCommands) const override \
-		{ \
-			OutBeautifiedNames.Add(PrettyName); \
-			OutTestCommands.Add(FString()); \
-		} \
-		virtual bool RunTest(const FString& Parameters) override \
-		{ \
-			SetUp(); \
-			return RunGDKTest(Parameters); \
-		} \
-		virtual bool RunGDKTest(const FString& Parameters); \
-		virtual FString GetBeautifiedTestName() const override { return TEXT(PrettyName); } \
-	}; \
-	namespace\
-	{\
-		TestName TestName##AutomationTestInstance(TEXT(PrettyName)); \
-	} \
+#define IMPLEMENT_GDK_AUTOMATION_TEST(TestName, PrettyName)                                                                                \
+	class TestName : public FGDKTestBase                                                                                                   \
+	{                                                                                                                                      \
+	public:                                                                                                                                \
+		TestName(const FString& InName)                                                                                                    \
+			: FGDKTestBase(InName, false)                                                                                                  \
+		{                                                                                                                                  \
+		}                                                                                                                                  \
+		virtual uint32 GetTestFlags() const override                                                                                       \
+		{                                                                                                                                  \
+			return EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::EngineFilter;                                      \
+		}                                                                                                                                  \
+		virtual bool IsStressTest() const { return false; }                                                                                \
+		virtual uint32 GetRequiredDeviceNum() const override { return 1; }                                                                 \
+		virtual FString GetTestSourceFileName() const override { return __FILE__; }                                                        \
+		virtual int32 GetTestSourceFileLine() const override { return __LINE__; }                                                          \
+                                                                                                                                           \
+	protected:                                                                                                                             \
+		void SetUp()                                                                                                                       \
+		{                                                                                                                                  \
+			FLocalDeploymentManager* LocalDeploymentManager = GetLocalDeploymentManager();                                                 \
+			if (LocalDeploymentManager->IsLocalDeploymentRunning() || LocalDeploymentManager->IsDeploymentStarting())                      \
+			{                                                                                                                              \
+				UE_LOG(LogGDKTestBase, Warning, TEXT("Deployment found! (Was this left over from another test?)"))                         \
+				UE_LOG(LogGDKTestBase, Warning, TEXT("Ending PIE session"))                                                                \
+				GEditor->RequestEndPlayMap();                                                                                              \
+				ADD_LATENT_AUTOMATION_COMMAND(FWaitForDeployment(this, EDeploymentState::IsNotRunning));                                   \
+			}                                                                                                                              \
+		}                                                                                                                                  \
+		virtual void GetTests(TArray<FString>& OutBeautifiedNames, TArray<FString>& OutTestCommands) const override                        \
+		{                                                                                                                                  \
+			OutBeautifiedNames.Add(PrettyName);                                                                                            \
+			OutTestCommands.Add(FString());                                                                                                \
+		}                                                                                                                                  \
+		virtual bool RunTest(const FString& Parameters) override                                                                           \
+		{                                                                                                                                  \
+			SetUp();                                                                                                                       \
+			return RunGDKTest(Parameters);                                                                                                 \
+		}                                                                                                                                  \
+		virtual bool RunGDKTest(const FString& Parameters);                                                                                \
+		virtual FString GetBeautifiedTestName() const override { return TEXT(PrettyName); }                                                \
+	};                                                                                                                                     \
+	namespace                                                                                                                              \
+	{                                                                                                                                      \
+	TestName TestName##AutomationTestInstance(TEXT(PrettyName));                                                                           \
+	}                                                                                                                                      \
 	bool TestName::RunGDKTest(const FString& Parameters)
