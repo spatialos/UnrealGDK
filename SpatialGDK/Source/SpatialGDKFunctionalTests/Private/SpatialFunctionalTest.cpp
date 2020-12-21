ebb4d2c279903bddafc0c95192c7f91e621624d4
Tilman Schmidt <tilmanschmidt+github@improbable.io>
2020-12-21 10:44:21 +0000

    Fix off-by-one error in AddStepSetTagDelegation (#2783)
    ServerWorkerIDs are starting at 1 (see the ensure above this change), so the largest allowed worker ID is equal to the expected server number.
    
    Includes a drive-by formatting fix

--- SpatialGDK/Source/SpatialGDKFunctionalTests/Private/SpatialFunctionalTest.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/Private/SpatialFunctionalTest.cpp
@@ -410,7 +410,8 @@ void ASpatialFunctionalTest::RegisterFlowController(ASpatialFunctionalTestFlowCo
 	{
 		if (LocalFlowController != nullptr)
 		{
-			checkf(LocalFlowController == FlowController, TEXT("OwningTest already had different LocalFlowController, this shouldn't happen"));
+			checkf(LocalFlowController == FlowController,
+				   TEXT("OwningTest already had different LocalFlowController, this shouldn't happen"));
 			return;
 		}
 
@@ -833,11 +834,12 @@ void ASpatialFunctionalTest::KeepActorOnCurrentWorker(AActor* Actor)
 
 void ASpatialFunctionalTest::AddStepSetTagDelegation(FName Tag, int32 ServerWorkerId /*= 1*/)
 {
+	// Valid ServerWorkerIDs range from 1 to NumExpectedServers, inclusive
 	if (!ensureMsgf(ServerWorkerId > 0, TEXT("Invalid Server Worker Id")))
 	{
 		return;
 	}
-	if (ServerWorkerId >= GetNumExpectedServers())
+	if (ServerWorkerId > GetNumExpectedServers())
 	{
 		ServerWorkerId = 1; // Support for single worker environments.
 	}
