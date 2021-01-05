0936a5e19467c072939e55bdc3a360161a55e4bd
Miron Zelina <mironzelina@improbable.io>
2021-01-05 19:47:34 +0000

    Add test for Unreal Metadata component

--- SpatialGDK/Source/SpatialGDKTests/SpatialGDK/Utils/EntityFactory/EntityFactoryTest.cpp
+++ SpatialGDK/Source/SpatialGDKTests/SpatialGDK/Utils/EntityFactory/EntityFactoryTest.cpp
@@ -29,6 +29,11 @@ ENTITYFACTORY_TEST(GIVEN_an_actor_WHEN_creating_skeleton_entity_components_THEN_
 	// component
 	TestTrue("Has position component", ComponentDatas.ContainsByPredicate(ContainsComponent{ SpatialConstants::POSITION_COMPONENT_ID }));
 
+	// We want the UnrealMetadata component to be present, as it has been deemed essential enough to be included in the skeleton of an
+	// entity (mainly due to containing the stably named path for actors placed in the level)
+	TestTrue("Has Unreal Metadata component",
+			 ComponentDatas.ContainsByPredicate(ContainsComponent{ SpatialConstants::UNREAL_METADATA_COMPONENT_ID }));
+
 	// Actor tags
 	// This is here so that we can verify we are adding the expected tags (debatable as this is basically an interface test)
 	TestTrue("Has actor auth tag component",
