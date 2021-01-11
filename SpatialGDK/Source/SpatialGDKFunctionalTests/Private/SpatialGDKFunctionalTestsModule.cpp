e26ea1506f0f0eeb2c23c3535ff107435b5f8031
Tilman Schmidt <tilmanschmidt+github@improbable.io>
2021-01-11 11:03:35 +0000

    Add LogSpatialFunctionalTest

--- SpatialGDK/Source/SpatialGDKFunctionalTests/Private/SpatialGDKFunctionalTestsModule.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/Private/SpatialGDKFunctionalTestsModule.cpp
@@ -3,10 +3,12 @@
 #include "SpatialGDKFunctionalTestsModule.h"
 
 #include "SpatialGDKFunctionalTestsPrivate.h"
+#include "LogSpatialFunctionalTest.h"
 
 #define LOCTEXT_NAMESPACE "FSpatialGDKFunctionalTestsModule"
 
 DEFINE_LOG_CATEGORY(LogSpatialGDKFunctionalTests);
+DEFINE_LOG_CATEGORY(LogSpatialFunctionalTest);
 
 IMPLEMENT_MODULE(FSpatialGDKFunctionalTestsModule, SpatialGDKFunctionalTests);
 
