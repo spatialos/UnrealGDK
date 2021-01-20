258b97e0b4b59e8cfa018abf7e9d71eb00568837
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-01-20 18:10:01 +0000

    UNR-4447: Added the same check for replication as well

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestHandoverReplication/DynamicReplicationHandoverCube.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestHandoverReplication/DynamicReplicationHandoverCube.cpp
@@ -2,6 +2,8 @@
 
 #include "DynamicReplicationHandoverCube.h"
 
+#include "Net/UnrealNetwork.h"
+
 ADynamicReplicationHandoverCube::ADynamicReplicationHandoverCube()
 {
 	bReplicates = false;
@@ -12,6 +14,14 @@ void ADynamicReplicationHandoverCube::OnAuthorityGained()
 	if (ShouldResetValueToDefaultCounter == 1)
 	{
 		HandoverTestProperty = BasicTestPropertyValue;
+		ReplicatedTestProperty = BasicTestPropertyValue;
 		ShouldResetValueToDefaultCounter = 2;
 	}
+}
+
+void ADynamicReplicationHandoverCube::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ADynamicReplicationHandoverCube, ReplicatedTestProperty);
 }
