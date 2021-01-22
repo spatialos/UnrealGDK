e4c560057f5d22fda0354cd69631a12c298e6dc4
Vix2021 <victoriabloom@improbable.io>
2021-01-22 16:12:32 +0200

    Review feedback

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
@@ -1615,10 +1615,7 @@ void USpatialNetDriver::ProcessRPC(AActor* Actor, UObject* SubObject, UFunction*
 		if (PackageMap->GetEntityIdFromObject(CallingObject) == SpatialConstants::INVALID_ENTITY_ID)
 		{
 			AActor* TargetActor = Cast<AActor>(CallingObject);
-			if (TargetActor == nullptr)
-			{
-				TargetActor = Cast<AActor>(CallingObject->GetOuter());
-			}
+			
 			check(TargetActor != nullptr);
 			if (!TargetActor->HasAuthority() && TargetActor->IsNameStableForNetworking() && TargetActor->GetIsReplicated())
 			{
