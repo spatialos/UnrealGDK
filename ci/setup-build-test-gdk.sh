aa9e1528a77ab050e5205c3de738a4b15cae3207
Simon Sarginson <simonsarginson@improbable.io>
2021-01-29 09:49:43 +0000

    fixed up shell script to take into account test gym branch overrides

--- ci/setup-build-test-gdk.sh
+++ ci/setup-build-test-gdk.sh
@@ -7,6 +7,27 @@ fi
 
 source /opt/improbable/environment
 
+# Download the unreal-engine.version file from the GDK repo so we can run the example project builds on the same versions the GDK was run against.
+# This is not the pinnacle of engineering, as we rely on GitHub's web interface to download the file, but it seems like GitHub disallows git archive
+# which would be our other option for downloading a single file.
+# Also resolve the GDK branch to run against. The order of priority is:
+# TEST_REPO_BRANCH envvar > same-name branch as the branch we are currently on > UnrealGDKVersion.txt > "master".
+GDK_BRANCH_LOCAL="${TEST_REPO_BRANCH:-}"
+if [ -z "${GDK_BRANCH_LOCAL}" ]; then
+    GDK_REPO_HEADS=$(git ls-remote --heads "git@github.com:spatialos/UnrealGDK.git" "${BUILDKITE_BRANCH}")
+    EXAMPLEPROJECT_REPO_HEAD="refs/heads/${BUILDKITE_BRANCH}"
+    if echo "${GDK_REPO_HEADS}" | grep -qF "${EXAMPLEPROJECT_REPO_HEAD}"; then
+        GDK_BRANCH_LOCAL="${BUILDKITE_BRANCH}"
+    else
+        GDK_VERSION=$(cat UnrealGDKVersion.txt)
+        if [ -z "${GDK_VERSION}" ]; then
+            GDK_BRANCH_LOCAL="master"
+        else
+            GDK_BRANCH_LOCAL="${GDK_VERSION}"
+        fi
+    fi
+fi
+
 pushd "$(dirname "$0")"
     GDK_HOME="${1:-"$(pwd)/.."}"
     GCS_PUBLISH_BUCKET="${2:-io-internal-infra-unreal-artifacts-production/UnrealEngine}"
@@ -17,7 +38,7 @@ pushd "$(dirname "$0")"
     TEST_REPO_URL="git@github.com:spatialos/UnrealGDKTestGyms.git"
     TEST_REPO_MAP="EmptyGym"
     TEST_PROJECT_NAME="GDKTestGyms"
-    CHOSEN_TEST_REPO_BRANCH="${TEST_REPO_BRANCH:-master}"
+    CHOSEN_TEST_REPO_BRANCH="${GDK_BRANCH_LOCAL}"
 
     # Download Unreal Engine
     echo "--- get-unreal-engine"
