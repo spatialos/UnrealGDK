1d1710ce9f789b1827925f01934d2e36964f71e3
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-02-05 17:21:57 +0000

    UNR-4355: Added a test to check attached component replication

---
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestAttachedComponentReplication/SpatialTestAttachedComponentReplication.cpp
@@ -0,0 +1,105 @@
+﻿// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "SpatialTestAttachedComponentReplication.h"
+
+#include "Algo/Copy.h"
+#include "Algo/Count.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+#include "SpatialFunctionalTestStep.h"
+
+struct FSpatialTestComponentReplicationValues
+{
+};
+
+ASpatialTestAttachedComponentReplicationActor::ASpatialTestAttachedComponentReplicationActor()
+{
+	bReplicates = true;
+}
+
+USpatialTestAttachedComponentReplicationComponent::USpatialTestAttachedComponentReplicationComponent()
+{
+	SetIsReplicatedByDefault(true);
+}
+
+void USpatialTestAttachedComponentReplicationComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, ReplicatedValue);
+}
+
+ASpatialTestAttachedComponentReplication::ASpatialTestAttachedComponentReplication()
+{
+	Author = TEXT("Dmitrii Kozlov <dmitriikozlov@improbable.io>");
+	Description =
+		TEXT("Check different types of attached component replication:" LINE_TERMINATOR "* Component present on a C++ actor" LINE_TERMINATOR
+			 "* Component present on a BP actor" LINE_TERMINATOR "* Component dynamically added to a C++ actor" LINE_TERMINATOR
+			 "* Component dynamically added to a BP actor" LINE_TERMINATOR "* Component added in a map to a C++ actor" LINE_TERMINATOR
+			 "* Component added in a map to a BP actor" LINE_TERMINATOR);
+}
+
+void ASpatialTestAttachedComponentReplication::PrepareTest()
+{
+	Super::PrepareTest();
+
+	auto AddWaitingStep = [this](const FString& StepDescription, const FWorkerDefinition& WorkerDefinition,
+								 TFunction<void()> TickFunction) {
+		AddStep(
+			StepDescription, WorkerDefinition, nullptr, nullptr,
+			[this, TickFunction](float) {
+				TickFunction();
+				FinishStep();
+			},
+			10.0f);
+	};
+
+	AddWaitingStep(TEXT("Retrieve the level placed actor"), FWorkerDefinition::AllWorkers, [this] {
+		TArray<AActor*> LevelPlacedActors;
+		UGameplayStatics::GetAllActorsOfClass(this, ASpatialTestAttachedComponentReplicationActor::StaticClass(), LevelPlacedActors);
+		RequireEqual_Int(LevelPlacedActors.Num(), 1, TEXT("Received one level placed actor"));
+		AssertTrue(LevelPlacedActors.Num() <= 1, TEXT("No more than one level placed actors are allowed"));
+		if (LevelPlacedActors.Num() == 1)
+		{
+			LevelPlacedActor = Cast<ASpatialTestAttachedComponentReplicationActor>(LevelPlacedActors[0]);
+		}
+		RequireTrue(IsValid(LevelPlacedActor), TEXT("Level placed actor is valid"));
+
+		RequireCompare_Float(TimeRunningStep, EComparisonMethod::Greater_Than, 3.0f, TEXT("Step ran for 3 secs"));
+	});
+
+	AddWaitingStep(TEXT("Retrieve the attached component"), FWorkerDefinition::AllWorkers, [this] {
+		const TSet<UActorComponent*>& AttachedComponents = LevelPlacedActor->GetComponents();
+
+		TArray<UActorComponent*> AttachedTypedComponents;
+
+		Algo::CopyIf(AttachedComponents, AttachedTypedComponents, [](const UActorComponent* Component) {
+			return Component->IsA<USpatialTestAttachedComponentReplicationComponent>();
+		});
+
+		RequireEqual_Int(AttachedTypedComponents.Num(), 1, TEXT("One attached component on the actor"));
+		AssertTrue(AttachedTypedComponents.Num() <= 1, TEXT("No more than one attached components are allowed"));
+
+		if (AttachedTypedComponents.Num() == 1)
+		{
+			AttachedComponent = Cast<USpatialTestAttachedComponentReplicationComponent>(AttachedTypedComponents[0]);
+		}
+
+		UE_LOG(LogTemp, Log, TEXT("%s %d -> %f"),
+			   GetLocalWorkerType() == ESpatialFunctionalTestWorkerType::Server ? TEXT("Server") : TEXT("Client"), GetLocalWorkerId(),
+			   TimeRunningStep);
+
+		RequireCompare_Float(TimeRunningStep, EComparisonMethod::Greater_Than, 3.0f, TEXT("Step ran for 3 secs"));
+	});
+
+	AddStep(TEXT("Modify replicated value on the component"), FWorkerDefinition::Server(1), nullptr, [this] {
+		AssertTrue(LevelPlacedActor->HasAuthority(), TEXT("Server 1 has authority over the actor"));
+		AttachedComponent->ReplicatedValue = SpatialTestAttachedComponentReplicationValues::ChangedValue;
+		FinishStep();
+	});
+
+	AddWaitingStep(TEXT("Check that the updated value is received"), FWorkerDefinition::AllWorkers, [this] {
+		RequireEqual_Int(AttachedComponent->ReplicatedValue, SpatialTestAttachedComponentReplicationValues::ChangedValue,
+						 TEXT("Updated value received"));
+	});
+}
---
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestAttachedComponentReplication/SpatialTestAttachedComponentReplication.h
@@ -0,0 +1,53 @@
+﻿// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "SpatialFunctionalTest.h"
+
+#include "SpatialTestAttachedComponentReplication.generated.h"
+
+namespace SpatialTestAttachedComponentReplicationValues
+{
+constexpr int InitialValue = 0;
+constexpr int ChangedValue = 100;
+} // namespace SpatialTestAttachedComponentReplicationValues
+
+UCLASS(BlueprintType)
+class ASpatialTestAttachedComponentReplicationActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASpatialTestAttachedComponentReplicationActor();
+};
+
+UCLASS(BlueprintType, meta = (BlueprintSpawnableComponent))
+class USpatialTestAttachedComponentReplicationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	USpatialTestAttachedComponentReplicationComponent();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(Replicated)
+	int ReplicatedValue = SpatialTestAttachedComponentReplicationValues::InitialValue;
+};
+
+UCLASS()
+class ASpatialTestAttachedComponentReplication : public ASpatialFunctionalTest
+{
+	GENERATED_BODY()
+public:
+	ASpatialTestAttachedComponentReplication();
+
+	virtual void PrepareTest() override;
+
+private:
+	UPROPERTY()
+	ASpatialTestAttachedComponentReplicationActor* LevelPlacedActor;
+
+	UPROPERTY()
+	USpatialTestAttachedComponentReplicationComponent* AttachedComponent;
+};
