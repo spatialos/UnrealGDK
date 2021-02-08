6fae64b8744b6567efb7a39a424bb8600c8c1acc
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-02-08 14:58:58 +0000

    UNR-4355: Improved test code

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestAttachedComponentReplication/SpatialTestAttachedComponentReplication.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestAttachedComponentReplication/SpatialTestAttachedComponentReplication.cpp
@@ -8,10 +8,6 @@
 #include "Net/UnrealNetwork.h"
 #include "SpatialFunctionalTestStep.h"
 
-struct FSpatialTestComponentReplicationValues
-{
-};
-
 ASpatialTestAttachedComponentReplicationActor::ASpatialTestAttachedComponentReplicationActor()
 {
 	bReplicates = true;
@@ -65,102 +61,92 @@ void ASpatialTestAttachedComponentReplication::PrepareTest()
 			10.0f);
 	};
 
-	auto AddGenericTestCases = [this, &AddWaitingStep](TFunction<void()> Setup) {
-		AddStep(TEXT("Setup"), FWorkerDefinition::AllWorkers, nullptr, [this, Setup] {
-			LevelPlacedActor = nullptr;
-			AttachedComponent = nullptr;
-
-			FinishStep();
-		});
-
-		AddStep(TEXT("Setup 2"), FWorkerDefinition::Server(1), nullptr, [Setup, this] {
-			Setup();
-			FinishStep();
-		});
-
-		AddWaitingStep(TEXT("Retrieve the level placed actor"), FWorkerDefinition::AllWorkers, [this] {
-			TArray<AActor*> LevelPlacedActors;
-			UGameplayStatics::GetAllActorsOfClass(this, ASpatialTestAttachedComponentReplicationActor::StaticClass(), LevelPlacedActors);
-			TArray<AActor*> Actors;
-			Algo::CopyIf(LevelPlacedActors, Actors, [this](const AActor* Actor) {
-				return (Actor == SceneActor) == (bIsWorkingWithSceneActor == ESpatialTestAttachedComponentReplicationType::LevelPlaced);
-			});
-			// RequireEqual_Int(LevelPlacedActors.Num(), 1, TEXT("Received one level placed actor"));
-			// AssertTrue(LevelPlacedActors.Num() <= 1, TEXT("No more than one level placed actors are allowed"));
-			LevelPlacedActors = Actors;
-			if (LevelPlacedActors.Num() > 1)
-			{
-				AddError(FString::Printf(TEXT("Received %d actors while only 1 expected"), LevelPlacedActors.Num()));
-			}
-			auto ttt = LevelPlacedActors.FindByPredicate([this](const AActor* Actor) {
-				return (Actor == SceneActor) == (bIsWorkingWithSceneActor == ESpatialTestAttachedComponentReplicationType::LevelPlaced);
-			});
-			if (ttt != nullptr)
-			{
-				LevelPlacedActor = Cast<ASpatialTestAttachedComponentReplicationActor>(*ttt);
-			}
-			RequireTrue(IsValid(LevelPlacedActor), TEXT("Level placed actor is valid"));
-
-			RequireCompare_Float(TimeRunningStep2, EComparisonMethod::Greater_Than, 3.0f, TEXT("Step ran for 3 secs"));
-		});
-
-		AddWaitingStep(TEXT("Retrieve the attached component"), FWorkerDefinition::AllWorkers, [this] {
-			const TSet<UActorComponent*>& AttachedComponents = LevelPlacedActor->GetComponents();
-
-			TArray<UActorComponent*> AttachedTypedComponents;
-
-			Algo::CopyIf(AttachedComponents, AttachedTypedComponents, [](const UActorComponent* Component) {
-				return Component->IsA<USpatialTestAttachedComponentReplicationComponent>();
-			});
-
-			RequireEqual_Int(AttachedTypedComponents.Num(), 1, TEXT("One attached component on the actor"));
-			if (AttachedTypedComponents.Num() > 1)
-			{
-				LogStep(ELogVerbosity::Error,
-						FString::Printf(TEXT("Received %d components while only 1 expected"), AttachedTypedComponents.Num()));
-			}
-
-			if (AttachedTypedComponents.Num() == 1)
-			{
-				AttachedComponent = Cast<USpatialTestAttachedComponentReplicationComponent>(AttachedTypedComponents[0]);
-			}
-
-			RequireCompare_Float(TimeRunningStep2, EComparisonMethod::Greater_Than, 3.0f, TEXT("Step ran for 3 secs"));
-		});
-
-		AddStep(TEXT("Modify replicated value on the component"), FWorkerDefinition::Server(1), nullptr, [this] {
-			AssertTrue(LevelPlacedActor->HasAuthority(), TEXT("Server 1 has authority over the actor"));
-			AttachedComponent->ReplicatedValue = SpatialTestAttachedComponentReplicationValues::ChangedValue;
-			FinishStep();
-		});
-
-		AddWaitingStep(TEXT("Check that the updated value is received"), FWorkerDefinition::AllWorkers, [this] {
-			RequireEqual_Int(AttachedComponent->ReplicatedValue, SpatialTestAttachedComponentReplicationValues::ChangedValue,
-							 TEXT("Updated value received"));
-		});
-	};
-
-	AddGenericTestCases([this] {
-		switch (bIsWorkingWithSceneActor)
-		{
-		case ESpatialTestAttachedComponentReplicationType::LevelPlaced:
-			break;
-		case ESpatialTestAttachedComponentReplicationType::DynamicallySpawnedWithDynamicComponent:
-		{
+	switch (bIsWorkingWithSceneActor)
+	{
+	case ESpatialTestAttachedComponentReplicationType::LevelPlaced:
+		break;
+	case ESpatialTestAttachedComponentReplicationType::DynamicallySpawnedWithDynamicComponent:
+		AddStep(TEXT("Spawn a test actor and attach dynamic component to it"), FWorkerDefinition::Server(1), nullptr, [this] {
 			auto ttttt = GetWorld()->SpawnActor<ASpatialTestAttachedComponentReplicationActor>();
 			auto trterter = NewObject<USpatialTestAttachedComponentReplicationComponent>(ttttt);
 			trterter->RegisterComponent();
 			RegisterAutoDestroyActor(ttttt);
-		}
+			FinishStep();
+		});
 		break;
-		case ESpatialTestAttachedComponentReplicationType::DynamicallySpawnedWithDefaultComponent:
-		{
+	case ESpatialTestAttachedComponentReplicationType::DynamicallySpawnedWithDefaultComponent:
+		AddStep(TEXT("Spawn a test actor with an attached default component"), FWorkerDefinition::Server(1), nullptr, [this] {
 			auto ttttt = GetWorld()->SpawnActor<ASpatialTestAttachedComponentReplicationActorWithDefaultComponent>();
 			RegisterAutoDestroyActor(ttttt);
-		}
+			FinishStep();
+		});
 		break;
-		default:
-			checkNoEntry();
+	default:
+		checkNoEntry();
+	}
+
+	AddStep(TEXT("Setup 2"), FWorkerDefinition::Server(1), nullptr, [this] {
+		FinishStep();
+	});
+
+	AddWaitingStep(TEXT("Retrieve the level placed actor"), FWorkerDefinition::AllWorkers, [this] {
+		TArray<AActor*> LevelPlacedActors;
+		UGameplayStatics::GetAllActorsOfClass(this, ASpatialTestAttachedComponentReplicationActor::StaticClass(), LevelPlacedActors);
+
+		{
+			// HACK: Filtering out the level placed actor.
+			TArray<AActor*> Actors;
+			Algo::CopyIf(LevelPlacedActors, Actors, [this](const AActor* Actor) {
+				return (Actor == SceneActor) == (bIsWorkingWithSceneActor == ESpatialTestAttachedComponentReplicationType::LevelPlaced);
+			});
+			LevelPlacedActors = Actors;
 		}
+
+		if (LevelPlacedActors.Num() > 1)
+		{
+			AddError(FString::Printf(TEXT("Received %d actors while only 1 expected"), LevelPlacedActors.Num()));
+		}
+
+		if (LevelPlacedActors.Num() == 1)
+		{
+			LevelPlacedActor = Cast<ASpatialTestAttachedComponentReplicationActor>(LevelPlacedActors[0]);
+		}
+		RequireTrue(IsValid(LevelPlacedActor), TEXT("Level placed actor is valid"));
+
+		RequireCompare_Float(TimeRunningStep2, EComparisonMethod::Greater_Than, 3.0f, TEXT("Step ran for 3 secs"));
+	});
+
+	AddWaitingStep(TEXT("Retrieve the attached component"), FWorkerDefinition::AllWorkers, [this] {
+		const TSet<UActorComponent*>& AttachedComponents = LevelPlacedActor->GetComponents();
+
+		TArray<UActorComponent*> AttachedTypedComponents;
+
+		Algo::CopyIf(AttachedComponents, AttachedTypedComponents, [](const UActorComponent* Component) {
+			return Component->IsA<USpatialTestAttachedComponentReplicationComponent>();
+		});
+
+		if (AttachedTypedComponents.Num() > 1)
+		{
+			AddError(FString::Printf(TEXT("Received %d components while only 1 expected"), AttachedTypedComponents.Num()));
+		}
+
+		if (AttachedTypedComponents.Num() == 1)
+		{
+			AttachedComponent = Cast<USpatialTestAttachedComponentReplicationComponent>(AttachedTypedComponents[0]);
+		}
+		RequireTrue(IsValid(AttachedComponent), TEXT("Received attached component"));
+
+		RequireCompare_Float(TimeRunningStep2, EComparisonMethod::Greater_Than, 3.0f, TEXT("Step ran for 3 secs"));
+	});
+
+	AddStep(TEXT("Modify replicated value on the component"), FWorkerDefinition::Server(1), nullptr, [this] {
+		AssertTrue(LevelPlacedActor->HasAuthority(), TEXT("Server 1 has authority over the actor"));
+		AttachedComponent->ReplicatedValue = SpatialTestAttachedComponentReplicationValues::ChangedValue;
+		FinishStep();
+	});
+
+	AddWaitingStep(TEXT("Check that the updated value is received"), FWorkerDefinition::AllWorkers, [this] {
+		RequireEqual_Int(AttachedComponent->ReplicatedValue, SpatialTestAttachedComponentReplicationValues::ChangedValue,
+						 TEXT("Updated value received"));
 	});
 }
