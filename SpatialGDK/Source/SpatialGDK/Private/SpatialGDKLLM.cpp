aec349fe5feeb41fdf3eeff26967042861ba26cc
Junting Yu <kenyu@improbable.io>
2021-02-20 18:48:18 +0800

    fix issues of spatial llm

--- SpatialGDK/Source/SpatialGDK/Private/SpatialGDKLLM.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/SpatialGDKLLM.cpp
@@ -10,18 +10,39 @@ struct FLLMTagInfoSpatial
 	FName SummaryStatName; // shows in the LLM summary stat group
 };
 
-DECLARE_LLM_MEMORY_STAT(TEXT("Spatial Receiver"), STAT_ReceiverLLM, STATGROUP_LLMPlatform);
-DECLARE_LLM_MEMORY_STAT(TEXT("Spatial Sender"), STAT_SenderLLM, STATGROUP_LLMPlatform);
-DECLARE_LLM_MEMORY_STAT(TEXT("Spatial NetDriver"), STAT_NetDriverLLM, STATGROUP_LLMPlatform);
-DECLARE_LLM_MEMORY_STAT(TEXT("Spatial RPCService"), STAT_RPCServiceLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial Receiver"), STAT_SpatialReceiverLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial Sender"), STAT_SpatialSenderLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial NetDriver"), STAT_SpatialNetDriverLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial RPCService"), STAT_SpatialRPCServiceLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial DebugCtx"), STAT_SpatialDebugCtxLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial ClientConnectionManager"), STAT_SpatialClientConnectionManagerLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial ActorSystem"), STAT_SpatialActorSystemLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial DebuggerSystem"), STAT_SpatialDebuggerSystemLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial Dispatcher"), STAT_SpatialDispatcherLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial CrossServerRPCHandler"), STAT_SpatialCrossServerRPCHandlerLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial WellKnownEntitySystem"), STAT_SpatialWellKnownEntitySystemLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial RoutingSystem"), STAT_SpatialRoutingSystemLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial SpatialMetrics"), STAT_SpatialSpatialMetricsLLM, STATGROUP_LLMPlatform);
+DECLARE_LLM_MEMORY_STAT(TEXT("Spatial AsyncPackageLoadFilter"), STAT_SpatialAsyncPackageLoadFilterLLM, STATGROUP_LLMPlatform);
 
 // *** order must match ELLMTagSpatialGDK enum ***
 const FLLMTagInfoSpatial ELLMTagNamesSpatial[] = {
 	// csv name						// stat name						// summary stat name
-	{ TEXT("Spatial Receiver"), GET_STATFNAME(STAT_ReceiverLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
-	{ TEXT("Spatial Sender"), GET_STATFNAME(STAT_SenderLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
-	{ TEXT("Spatial NetDriver"), GET_STATFNAME(STAT_NetDriverLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
-	{ TEXT("Spatial RPCService"), GET_STATFNAME(STAT_RPCServiceLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial Receiver"), GET_STATFNAME(STAT_SpatialReceiverLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial Sender"), GET_STATFNAME(STAT_SpatialSenderLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial NetDriver"), GET_STATFNAME(STAT_SpatialNetDriverLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial RPCService"), GET_STATFNAME(STAT_SpatialRPCServiceLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial DebugCtx"), GET_STATFNAME(STAT_SpatialDebugCtxLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial ClientConnectionManager"), GET_STATFNAME(STAT_SpatialClientConnectionManagerLLM),
+	  GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial ActorSystem"), GET_STATFNAME(STAT_SpatialActorSystemLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial DebuggerSystem"), GET_STATFNAME(STAT_SpatialDebuggerSystemLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial Dispatcher"), GET_STATFNAME(STAT_SpatialDispatcherLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial CrossServerRPCHandler"), GET_STATFNAME(STAT_SpatialCrossServerRPCHandlerLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial WellKnownEntitySystem"), GET_STATFNAME(STAT_SpatialWellKnownEntitySystemLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial RoutingSystem"), GET_STATFNAME(STAT_SpatialRoutingSystemLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial SpatialMetrics"), GET_STATFNAME(STAT_SpatialSpatialMetricsLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
+	{ TEXT("Spatial AsyncPackageLoadFilter"), GET_STATFNAME(STAT_SpatialAsyncPackageLoadFilterLLM), GET_STATFNAME(STAT_EngineSummaryLLM) },
 };
 
 void SpatialGDKLLM::Initialise()
