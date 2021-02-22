0cff65b4e0cb39ca0e090be9e0a8a212825d14a4
Valentyn Nykoliuk <valentyn@improbable.io>
2021-02-22 14:34:36 +0000

    Add generated map for SpatialTestNetReference

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/UNR-3761/SpatialTestNetReference/SpatialTestNetReference.h
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/UNR-3761/SpatialTestNetReference/SpatialTestNetReference.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "SpatialFunctionalTest.h"
+#include "TestMaps/GeneratedTestMap.h"
+
 #include "SpatialTestNetReference.generated.h"
 
 UCLASS()
@@ -33,4 +35,16 @@ public:
 	TPair<AController*, APawn*> OriginalPawn;
 
 	float PreviousMaximumDistanceThreshold;
+};
+
+UCLASS()
+class SPATIALGDKFUNCTIONALTESTS_API USpatialTestNetReferenceMap : public UGeneratedTestMap
+{
+	GENERATED_BODY()
+
+public:
+	USpatialTestNetReferenceMap();
+
+protected:
+	virtual void CreateCustomContentForMap() override;
 };
