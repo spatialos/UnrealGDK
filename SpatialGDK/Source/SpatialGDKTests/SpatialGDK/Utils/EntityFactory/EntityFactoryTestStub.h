2ea9fe8c9ec44fdbe85cac392bd86465ab24bd55
Martin Kift <martinkift@improbable.io>
2021-02-23 12:44:27 +0000

    Added notplaceable and notblueprintable to code-test actors

--- SpatialGDK/Source/SpatialGDKTests/SpatialGDK/Utils/EntityFactory/EntityFactoryTestStub.h
+++ SpatialGDK/Source/SpatialGDKTests/SpatialGDK/Utils/EntityFactory/EntityFactoryTestStub.h
@@ -6,73 +6,73 @@
 
 #include "EntityFactoryTestStub.generated.h"
 
-UCLASS(SpatialType)
+UCLASS(SpatialType, notplaceable, NotBlueprintable)
 class APersistentByDefaultSpatialActor : public AActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS(SpatialType = Persistent)
+UCLASS(SpatialType = Persistent, notplaceable, NotBlueprintable)
 class AExplicitPersistentSpatialActor : public AActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS(SpatialType = NotPersistent)
+UCLASS(SpatialType = NotPersistent, notplaceable, NotBlueprintable)
 class ANonPersistentSpatialActor : public AActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS()
+UCLASS(notplaceable, NotBlueprintable)
 class AActorSubclassOfPersistentByDefaultSpatialActor : public APersistentByDefaultSpatialActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS()
+UCLASS(notplaceable, NotBlueprintable)
 class AActorSubclassOfExplicitPersistentSpatialActor : public AExplicitPersistentSpatialActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS()
+UCLASS(notplaceable, NotBlueprintable)
 class AActorSubclassOfNonPersistentSpatialActor : public ANonPersistentSpatialActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS(SpatialType = NotPersistent)
+UCLASS(SpatialType = NotPersistent, notplaceable, NotBlueprintable)
 class ANonPersistentActorSubclassOfPersistentByDefaultSpatialActor : public APersistentByDefaultSpatialActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS(SpatialType = NotPersistent)
+UCLASS(SpatialType = NotPersistent, notplaceable, NotBlueprintable)
 class ANonPersistentActorSubclassOfExpicitPersistentSpatialActor : public AExplicitPersistentSpatialActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS(SpatialType = Persistent)
+UCLASS(SpatialType = Persistent, notplaceable, NotBlueprintable)
 class APersistentActorSubclassOfNonPersistentSpatialActor : public ANonPersistentSpatialActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS(SpatialType = Persistent)
+UCLASS(SpatialType = Persistent, notplaceable, NotBlueprintable)
 class APersistentActorSubclassOfActorSubclassOfNonPersistentSpatialActor : public AActorSubclassOfNonPersistentSpatialActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS(SpatialType = NotPersistent)
+UCLASS(SpatialType = NotPersistent, notplaceable, NotBlueprintable)
 class ANonPersistentActorSubclassOfActorSubclassOfPersistentByDefaultSpatialActor : public AActorSubclassOfPersistentByDefaultSpatialActor
 {
 	GENERATED_BODY()
 };
 
-UCLASS(SpatialType = NotPersistent)
+UCLASS(SpatialType = NotPersistent, notplaceable, NotBlueprintable)
 class ANonPersistentActorSubclassOfActorSubclassOfExplicitPersistentSpatialActor : public AActorSubclassOfExplicitPersistentSpatialActor
 {
 	GENERATED_BODY()
