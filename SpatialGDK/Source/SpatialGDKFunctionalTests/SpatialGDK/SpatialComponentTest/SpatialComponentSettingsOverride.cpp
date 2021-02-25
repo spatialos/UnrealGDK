04b30dde4341dd9fd9e8e11acd89878b817703c5
Miron Zelina <mironzelina@improbable.io>
2021-02-25 14:15:18 +0000

    Small fix

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialComponentTest/SpatialComponentSettingsOverride.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialComponentTest/SpatialComponentSettingsOverride.cpp
@@ -47,7 +47,7 @@ void ASpatialComponentSettingsOverride::PrepareTest()
 		TEXT("Check Editor Peformance Settings"), FWorkerDefinition::AllServers, nullptr,
 		[this]() {
 			bool bThrottleCPUWhenNotForeground = GetDefault<UEditorPerformanceSettings>()->bThrottleCPUWhenNotForeground;
-			RequireFalse(bThrottleCPUWhenNotForeground, TEXT("Expected bSpatialNetworking to be False"));
+			RequireFalse(bThrottleCPUWhenNotForeground, TEXT("Expected bThrottleCPUWhenNotForeground to be False"));
 
 			FinishStep();
 		},
