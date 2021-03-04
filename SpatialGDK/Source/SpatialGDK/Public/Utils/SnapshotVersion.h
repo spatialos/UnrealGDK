09340a69e6fe46b87de315dd298a61ac425368b1
Martin Kift <martinkift@improbable.io>
2021-03-04 09:54:27 +0000

    Clang format (again)

--- SpatialGDK/Source/SpatialGDK/Public/Utils/SnapshotVersion.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/SnapshotVersion.h
@@ -7,8 +7,8 @@
 // If changes are made to the schema or content of snapshots then:
 //	(a) (optional) if the schema has changed, update SPATIAL_SNAPSHOT_SCHEMA_HASH,
 //			see the test
-//'GIVEN_snapshot_affecting_schema_files_WHEN_hash_of_file_contents_is_generated_THEN_hash_matches_expected_snapshot_version_hash' 			which
-//will provide the hash on failure
+//'GIVEN_snapshot_affecting_schema_files_WHEN_hash_of_file_contents_is_generated_THEN_hash_matches_expected_snapshot_version_hash'
+//which will provide the hash on failure
 //  (b) (mandatory) increment SPATIAL_SNAPSHOT_VERSION_INC
 
 constexpr uint32 SPATIAL_SNAPSHOT_SCHEMA_HASH = 958184518;
