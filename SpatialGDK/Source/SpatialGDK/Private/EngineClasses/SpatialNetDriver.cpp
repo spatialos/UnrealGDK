3655f8d535f8dbaa0535931b87ed246abfa77326
jacquese <jacques@improbable.io>
2021-03-05 13:05:49 +0000

    whoops

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
@@ -512,7 +512,7 @@ void USpatialNetDriver::CreateAndInitializeCoreClasses()
 		ClientConnectionManager = MakeUnique<SpatialGDK::ClientConnectionManager>(SystemEntitySubview, this);
 
 		Dispatcher->Init(Receiver, StaticComponentView, SpatialMetrics, SpatialWorkerFlags);
-		Sender->Init(this, &TimerManager, RPCService.Get(), Connection->GetEventTracer());
+		Sender->Init(this, &TimerManager, Connection->GetEventTracer());
 		Receiver->Init(this, Connection->GetEventTracer());
 		GlobalStateManager->Init(this);
 		SnapshotManager->Init(Connection, GlobalStateManager, Receiver);
