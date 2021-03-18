ac875eed0b8dfaaadccb1ca2f19263771a87b0b3
Nolan Beck <nolanbeck@improbable.io>
2021-03-18 09:50:29 -0600

    Add Custom Persistence Component
    Add component for objects with custom persistence that can be dervied
    from and defined by users.

---
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/Components/CustomPersistenceComponent.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "EngineClasses/Components/CustomPersistenceComponent.h"
+
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+#include "Runtime/Launch/Resources/Version.h"
+#include "TimerManager.h"
+
+UCustomPersistenceComponent::UCustomPersistenceComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+
+#if ENGINE_MINOR_VERSION <= 23
+	bReplicates = true;
+#else
+	SetIsReplicatedByDefault(true);
+#endif
+}
---
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/Components/CustomPersistenceComponent.h
@@ -0,0 +1,20 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "CoreMinimal.h"
+#include "CustomPersistenceComponent.generated.h"
+
+
+
+
+/*
+ Offers a configurable means of persisting variables.
+ This component should be attached to an actor.
+ */
+UCLASS(ClassGroup = (SpatialGDK), Meta = (BlueprintSpawnableComponent))
+class SPATIALGDK_API UCustomPersistenceComponent : public UActorComponent
+{
+	GENERATED_UCLASS_BODY()
+};
