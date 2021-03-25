523ce60b20fdf72accd7b204343548d7423b59c2
Danny Birch <danielbirch@improbable.io>
2021-03-25 12:55:00 +0000

    Build fix

--- SpatialGDK/Source/SpatialGDK/Private/Utils/SpatialLatencyTracerMinimal.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/Utils/SpatialLatencyTracerMinimal.cpp
@@ -6,24 +6,23 @@
 static_assert(sizeof(uint32) == sizeof(Schema_FieldId), "Expected size match");
 static_assert(sizeof(int32) == sizeof(TraceKey), "Expected size match");
 
-int32 FSpatialLatencyTracerMinimal::ReadTraceFromSchemaObject(Schema_Object* Obj, uint32 FieldId)
+int32 FSpatialLatencyTracerMinimal::ReadTraceFromSchemaObject(worker::c::Schema_Object* Obj, uint32 FieldId)
 {
 #if TRACE_LIB_ACTIVE
 	if (USpatialLatencyTracer* Tracer = USpatialLatencyTracer::GetTracer(nullptr))
 	{
-		Trace = Tracer->ReadTraceFromSchemaObject(Obj, (Schema_FieldId)FieldId);
-		return Trace;
+		return Tracer->ReadTraceFromSchemaObject(Obj, (Schema_FieldId)FieldId);
 	}
 #endif
 	return static_cast<int32>(InvalidTraceKey);
 }
 
-void FSpatialLatencyTracerMinimal::WriteTraceToSchemaObject(int32 Key, Schema_Object* Obj, uint32 FieldId)
+void FSpatialLatencyTracerMinimal::WriteTraceToSchemaObject(int32 Key, worker::c::Schema_Object* Obj, uint32 FieldId)
 {
 #if TRACE_LIB_ACTIVE
 	if (USpatialLatencyTracer* Tracer = USpatialLatencyTracer::GetTracer(nullptr))
 	{
-		Tracer->WriteTraceToSchemaObject(static_cast<TraceKey>(Trace), RPCObject,
+		Tracer->WriteTraceToSchemaObject(static_cast<TraceKey>(Key), Obj,
 										 (Schema_FieldId)SpatialConstants::UNREAL_RPC_PAYLOAD_TRACE_ID);
 	}
 #endif
