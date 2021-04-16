da1260d85e08f8c4e5e4970dcae456e543a2867b
Tilman Schmidt <tilmanschmidt+github@improbable.io>
2021-04-16 17:23:44 +0100

    Re-order internal functions

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/Components/CustomPersistenceComponent.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/Components/CustomPersistenceComponent.cpp
@@ -15,6 +15,48 @@ UCustomPersistenceComponent::UCustomPersistenceComponent()
 	bHasProvidedPersistenceData = false;
 }
 
+void UCustomPersistenceComponent::PostReplication()
+{
+	// Work with spatial turned off
+	if (!USpatialStatics::IsSpatialNetworkingEnabled())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UCustomPersistenceComponent, not using spatial networking."));
+		return;
+	}
+
+	AActor* Owner = GetOwner();
+	if (Owner == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("UCustomPersistenceComponent, didn't have an owner actor during PostReplication."));
+		return;
+	}
+
+	if (!IsValid(Owner))
+	{
+		UE_LOG(LogTemp, Error, TEXT("UCustomPersistenceComponent, Owner is not yet valid."));
+		return;
+	}
+
+	const uint64 EntityID = USpatialStatics::GetActorEntityId(Owner);
+	if (EntityID == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UCustomPersistenceComponent, didn't have an entity ID in PostReplication."));
+		return;
+	}
+
+	const USpatialNetDriver* NetDriver = Cast<USpatialNetDriver>(GetWorld()->GetNetDriver());
+	if (NetDriver == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("UCustomPersistenceComponent, running with spatial but can't find a spatial net driver."));
+		return;
+	}
+
+	// Will have to see if the ComponentUpdate type makes sense to be user-facing.
+	SpatialGDK::ComponentUpdate Update(GetComponentId());
+	GetComponentUpdate(Update);
+	NetDriver->Connection->GetCoordinator().SendComponentUpdate(EntityID, MoveTemp(Update), {});
+}
+
 // Once we gain authority, we know we have all the data for an entity, and authority to modify it.
 // If we can find data for the actor's persistence spatial component, pass it to the user implementation via OnPersistenceDataAvailable
 // If we don't have any data for the component, add it to the entity. This usually happens when the actor is loaded for the first time in a
@@ -89,48 +131,6 @@ void UCustomPersistenceComponent::OnAuthorityGained()
 		GetAddComponentData(Data);
 		Coordinator.SendAddComponent(EntityID, MoveTemp(Data), {});
 	}
-}
-
-void UCustomPersistenceComponent::PostReplication()
-{
-	// Work with spatial turned off
-	if (!USpatialStatics::IsSpatialNetworkingEnabled())
-	{
-		UE_LOG(LogTemp, Warning, TEXT("UCustomPersistenceComponent, not using spatial networking."));
-		return;
-	}
-
-	AActor* Owner = GetOwner();
-	if (Owner == nullptr)
-	{
-		UE_LOG(LogTemp, Error, TEXT("UCustomPersistenceComponent, didn't have an owner actor during PostReplication."));
-		return;
-	}
-
-	if (!IsValid(Owner))
-	{
-		UE_LOG(LogTemp, Error, TEXT("UCustomPersistenceComponent, Owner is not yet valid."));
-		return;
-	}
-
-	const uint64 EntityID = USpatialStatics::GetActorEntityId(Owner);
-	if (EntityID == 0)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("UCustomPersistenceComponent, didn't have an entity ID in PostReplication."));
-		return;
-	}
-
-	const USpatialNetDriver* NetDriver = Cast<USpatialNetDriver>(GetWorld()->GetNetDriver());
-	if (NetDriver == nullptr)
-	{
-		UE_LOG(LogTemp, Error, TEXT("UCustomPersistenceComponent, running with spatial but can't find a spatial net driver."));
-		return;
-	}
-
-	// Will have to see if the ComponentUpdate type makes sense to be user-facing.
-	SpatialGDK::ComponentUpdate Update(GetComponentId());
-	GetComponentUpdate(Update);
-	NetDriver->Connection->GetCoordinator().SendComponentUpdate(EntityID, MoveTemp(Update), {});
 }
 
 void UCustomPersistenceComponent::GetAddComponentData(SpatialGDK::ComponentData& Data) {}
