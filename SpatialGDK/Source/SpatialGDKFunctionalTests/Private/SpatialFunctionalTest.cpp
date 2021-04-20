47cb64ab70d24ba76fa4f7b2b43ed9636ab5d8a9
Razvan Nicolescu <razvannicolescu@improbable.io>
2021-04-20 13:24:39 +0100

    Added default timeout to timesteps

--- SpatialGDK/Source/SpatialGDKFunctionalTests/Private/SpatialFunctionalTest.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/Private/SpatialFunctionalTest.cpp
@@ -493,7 +493,7 @@ FString ASpatialFunctionalTest::GetLocalWorkerString()
 
 void ASpatialFunctionalTest::AddStepBlueprint(const FString& StepName, const FWorkerDefinition& Worker,
 											  const FStepIsReadyDelegate& IsReadyEvent, const FStepStartDelegate& StartEvent,
-											  const FStepTickDelegate& TickEvent, float StepTimeLimit /*= 0.0f*/)
+											  const FStepTickDelegate& TickEvent, float StepTimeLimit /*= 20.0f*/)
 {
 	if (StepName.IsEmpty())
 	{
@@ -608,7 +608,7 @@ FSpatialFunctionalTestStepDefinition& ASpatialFunctionalTest::AddStep(const FStr
 																	  FIsReadyEventFunc IsReadyEvent /*= nullptr*/,
 																	  FStartEventFunc StartEvent /*= nullptr*/,
 																	  FTickEventFunc TickEvent /*= nullptr*/,
-																	  float StepTimeLimit /*= 0.0f*/)
+																	  float StepTimeLimit /*= 20.0f*/)
 {
 	if (StepName.IsEmpty())
 	{
