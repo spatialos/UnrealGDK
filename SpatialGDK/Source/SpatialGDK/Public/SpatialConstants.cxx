6a3accc3b8b9add27cc558b5190c47878e7634e1
Vix2021 <VictoriaBloom@improbable.io>
2021-04-23 15:03:19 +0300

    Fixed an issue with migration diagnostic logging failing. (#3050)
    * Fix issue with migration diagnostic logging failling.
    
    * Updated changelog.

--- SpatialGDK/Source/SpatialGDK/Public/SpatialConstants.cxx
+++ SpatialGDK/Source/SpatialGDK/Public/SpatialConstants.cxx
@@ -233,6 +233,7 @@ const TMap<Worker_ComponentId, FString> ServerAuthorityWellKnownComponents = {
     { SERVER_ENDPOINT_COMPONENT_ID, "unreal.generated.UnrealServerEndpoint" },
     { CROSSSERVER_SENDER_ENDPOINT_COMPONENT_ID, "unreal.generated.UnrealCrossServerSenderRPCs" },
     { CROSSSERVER_RECEIVER_ACK_ENDPOINT_COMPONENT_ID, "unreal.generated.UnrealCrossServerReceiverACKRPCs" },
+	{ MIGRATION_DIAGNOSTIC_COMPONENT_ID, "unreal.MigrationDiagnostic" },
 };
 
 const TArray<FString> ClientAuthorityWellKnownSchemaImports = { "unreal/gdk/player_controller.schema", "unreal/gdk/rpc_components.schema",
