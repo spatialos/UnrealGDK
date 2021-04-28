df560094539664e85ea2fe7c2213ce2934569cda
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-04-28 17:57:33 +0100

    UNR-4843: Added a new ownership completeness handler class

---
+++ SpatialGDK/Source/SpatialGDK/Public/Interop/OwnershipCompletenessHandler.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Misc/Optional.h"
+
+#include "SpatialCommonTypes.h"
+#include "SpatialView/SubView.h"
+
+class USpatialNetDriver;
+
+namespace SpatialGDK
+{
+class ViewCoordinator;
+
+class FOwnershipCompletenessHandler
+{
+public:
+	FOwnershipCompletenessHandler(bool bInIsServer)
+		: bIsServer(bInIsServer)
+	{
+	}
+	bool IsOwnershipComplete(Worker_EntityId EntityId, const EntityViewElement& Entity) const;
+	void AddPlayerEntity(Worker_EntityId EntityId);
+	void RemovePlayerEntity(Worker_EntityId EntityId);
+	void AddSubView(FSubView& InSubView);
+
+	static TArray<FDispatcherRefreshCallback> GetCallbacks(ViewCoordinator& Coordinator);
+
+private:
+	bool bIsServer;
+	TSet<Worker_EntityId_Key> PlayerOwnedEntities;
+	TArray<FSubView*> SubViewsToRefresh;
+};
+} // namespace SpatialGDK
