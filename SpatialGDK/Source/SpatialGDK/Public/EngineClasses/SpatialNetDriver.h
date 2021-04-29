c09b7f631c9161b71ecf3f9b8613dcc1eee32627
martin-improbable <martinkift@improbable.io>
2021-04-29 12:45:07 +0100

    [UNR-5438] Change UNetDriver GetActorEntityId to not lazy construct
    Changed GetActorEntityId() to not lazy construct an entityid, and the API to be const.

--- SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialNetDriver.h
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialNetDriver.h
@@ -266,7 +266,7 @@ public:
 
 	virtual int64 GetClientID() const override;
 
-	virtual int64 GetActorEntityId(AActor& Actor) override;
+	virtual int64 GetActorEntityId(const AActor& Actor) const override;
 
 	FShutdownEvent OnShutdown;
 
