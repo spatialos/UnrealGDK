7d9b154753b6a929263b93b373392e10d198d849
Razvan Nicolescu <razvannicolescu@improbable.io>
2021-05-05 12:28:38 +0100

    Delete ReplicatedTestPawnBase_RepGraphAlwaysReplicate.cpp

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/TestActors/ReplicatedTestPawnBase_RepGraphAlwaysReplicate.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
-
-#include "ReplicatedTestPawnBase_RepGraphAlwaysReplicate.h"
-
-AReplicatedTestPawnBase_RepGraphAlwaysReplicate::AReplicatedTestPawnBase_RepGraphAlwaysReplicate()
-{
-	bReplicates = true;
-}
