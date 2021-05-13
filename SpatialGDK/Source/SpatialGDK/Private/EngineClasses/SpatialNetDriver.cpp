e1571f43799092623f30ce23b499fdc8a0060451
Danny Birch <danielbirch@improbable.io>
2021-05-13 10:09:10 +0100

    Verbose log in all cases instead, seems that this path is part of standard map loading flow

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
@@ -1068,16 +1068,22 @@ void USpatialNetDriver::NotifyActorDestroyed(AActor* ThisActor, bool IsSeamlessT
 		// Check if this is a dormant entity, and if so retire the entity
 		if (PackageMap != nullptr && World != nullptr)
 		{
-			// PackageMap != nullptr implies the spatial connection is connected, however World::BeginPlay may not been called yet which
-			// means we are still in a UEngine::LoadMap call. During the load process, actors are created and destroyed in the following
-			// scenarios:
-			// - When running in PIE, Blueprint loaded sub-levels can be duplicated and immediately unloaded.
-			// - ChildActorComponent::OnRegister
-			// If an actor is destroyed as part of these processes, we process here and if the actor is replicated etc., attempt
-			// to create a tombstone for. This would be incorrect behavior, as the level and actors were never intentionally loaded,
-			// but are present just as an effect of the map load process.
-			// So we ignore this destroy call if the world hasn't begun play.
-			if (World->bBegunPlay)
+			if (!World->bBegunPlay)
+			{
+				// PackageMap != nullptr implies the spatial connection is connected, however World::BeginPlay may not have been called yet which
+				// means we are still in a UEngine::LoadMap call. During the load process, actors are created and destroyed in the following
+				// scenarios:
+				// - When running in PIE, Blueprint loaded sub-levels can be duplicated and immediately unloaded.
+				// - ChildActorComponent::OnRegister
+				// If an actor is destroyed as part of these processes, we process here and if the actor is replicated etc., attempt
+				// to create a tombstone for. This would be incorrect behavior, as the level and actors were never intentionally loaded,
+				// but are present just as an effect of the map load process.
+				// So we ignore this destroy call if the world hasn't begun play.
+				UE_LOG(LogSpatialOSNetDriver, Verbose,
+						TEXT("USpatialNetDriver::NotifyActorDestroyed ignored because world hasn't begun play. Actor: %s."),
+						*ThisActor->GetName());
+			}
+			else
 			{
 				const Worker_EntityId EntityId = PackageMap->GetEntityIdFromObject(ThisActor);
 
