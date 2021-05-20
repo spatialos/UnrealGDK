fd5a87acbd990608873140d33340debd87118d47
Vix2021 <victoriabloom@improbable.io>
2021-05-20 13:07:06 +0300

    Formatting

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/PlayerDisconnect/SpatialTestPlayerDisconnect.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/PlayerDisconnect/SpatialTestPlayerDisconnect.cpp
@@ -17,70 +17,68 @@ ASpatialTestPlayerDisconnect::ASpatialTestPlayerDisconnect()
 void ASpatialTestPlayerDisconnect::PrepareTest()
 {
 	Super::PrepareTest();
-	
-		if (HasAuthority())
-		{
-			AddExpectedLogError("OSS: No game present to leave for session",2);
-		}
 
+	if (HasAuthority())
+	{
+		AddExpectedLogError("OSS: No game present to leave for session", 2);
+	}
 
-		AddStep(
-			TEXT("AllServers_ChecksBefore"), FWorkerDefinition::AllServers, nullptr,
-			[this]() {
-				int32 ActualNumberOfClients = GetNumberOfClientWorkers();
-				RequireEqual_Int(ActualNumberOfClients, 2, TEXT("Expected two clients."));
+	AddStep(
+		TEXT("AllServers_ChecksBefore"), FWorkerDefinition::AllServers, nullptr,
+		[this]() {
+			int32 ActualNumberOfClients = GetNumberOfClientWorkers();
+			RequireEqual_Int(ActualNumberOfClients, 2, TEXT("Expected two clients."));
 
-				TArray<AActor*> PlayerControllers;
-				UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerDisconnectController::StaticClass(), PlayerControllers);
-				RequireEqual_Int(PlayerControllers.Num(), 2, TEXT("Expected two player controllers."));
+			TArray<AActor*> PlayerControllers;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerDisconnectController::StaticClass(), PlayerControllers);
+			RequireEqual_Int(PlayerControllers.Num(), 2, TEXT("Expected two player controllers."));
 
-				TArray<AActor*> PlayerCharacters;
-				UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACharacter::StaticClass(), PlayerCharacters);
-				RequireEqual_Int(PlayerCharacters.Num(), 2, TEXT("Expected two player characters."));
-
-				FinishStep();
-			},
-			nullptr, 5.0f);
-
-		AddStep(TEXT("Client1_ReturnToMainMenu"), FWorkerDefinition::Client(1), nullptr, [this]() {
-			APlayerDisconnectController* LocalPlayerController =
-				Cast<APlayerDisconnectController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
-
-			LocalPlayerController->ReturnToMainMenu();
+			TArray<AActor*> PlayerCharacters;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACharacter::StaticClass(), PlayerCharacters);
+			RequireEqual_Int(PlayerCharacters.Num(), 2, TEXT("Expected two player characters."));
 
 			FinishStep();
-		});
+		},
+		nullptr, 5.0f);
 
-		// Need this additional step after client returns to main menu to deregister their flowcontroller from the server
-		// If the client itself deregisters it's own flow controller it can never send the FinishStep command and will fail the step.
-		AddStep(
-			TEXT("AllServers_RemoveFlowControllerForClient1"), FWorkerDefinition::AllServers, nullptr,
-			[this]() {
-				if (ASpatialFunctionalTestFlowController* FlowController = GetFlowController(ESpatialFunctionalTestWorkerType::Client, 1))
-				{
-					FlowController->DeregisterFlowController();
-				}
+	AddStep(TEXT("Client1_ReturnToMainMenu"), FWorkerDefinition::Client(1), nullptr, [this]() {
+		APlayerDisconnectController* LocalPlayerController =
+			Cast<APlayerDisconnectController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
 
-				FinishStep();
-			},
-			nullptr, 5.0f);
+		LocalPlayerController->ReturnToMainMenu();
 
-		AddStep(
-			TEXT("AllServers_ChecksAfter"), FWorkerDefinition::AllServers, nullptr,
-			[this]() {
-				int32 ActualNumberOfClients = GetNumberOfClientWorkers();
-				RequireEqual_Int(ActualNumberOfClients, 1, TEXT("Expected one client."));
+		FinishStep();
+	});
 
-				TArray<AActor*> PlayerControllers;
-				UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerDisconnectController::StaticClass(), PlayerControllers);
-				RequireEqual_Int(PlayerControllers.Num(), 1, TEXT("Expected one player controller."));
+	// Need this additional step after client returns to main menu to deregister their flowcontroller from the server
+	// If the client itself deregisters it's own flow controller it can never send the FinishStep command and will fail the step.
+	AddStep(
+		TEXT("AllServers_RemoveFlowControllerForClient1"), FWorkerDefinition::AllServers, nullptr,
+		[this]() {
+			if (ASpatialFunctionalTestFlowController* FlowController = GetFlowController(ESpatialFunctionalTestWorkerType::Client, 1))
+			{
+				FlowController->DeregisterFlowController();
+			}
 
-				TArray<AActor*> PlayerCharacters;
-				UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACharacter::StaticClass(), PlayerCharacters);
-				RequireEqual_Int(PlayerCharacters.Num(), 1, TEXT("Expected one player character."));
+			FinishStep();
+		},
+		nullptr, 5.0f);
 
-				FinishStep();
-			},
-			nullptr, 5.0f);
-	
+	AddStep(
+		TEXT("AllServers_ChecksAfter"), FWorkerDefinition::AllServers, nullptr,
+		[this]() {
+			int32 ActualNumberOfClients = GetNumberOfClientWorkers();
+			RequireEqual_Int(ActualNumberOfClients, 1, TEXT("Expected one client."));
+
+			TArray<AActor*> PlayerControllers;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerDisconnectController::StaticClass(), PlayerControllers);
+			RequireEqual_Int(PlayerControllers.Num(), 1, TEXT("Expected one player controller."));
+
+			TArray<AActor*> PlayerCharacters;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACharacter::StaticClass(), PlayerCharacters);
+			RequireEqual_Int(PlayerCharacters.Num(), 1, TEXT("Expected one player character."));
+
+			FinishStep();
+		},
+		nullptr, 5.0f);
 }
