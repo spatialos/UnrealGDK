6026eedbbbd60d62994b4663d2c568622f16ab42
Vix2021 <VictoriaBloom@improbable.io>
2021-06-11 17:08:45 +0300

    Fix spatial flow controller registration (#3175)
    * Remove multiple paths for registering flow controller and eliminate potential race conditions on test framework startup
    * Formatting
    * Additional checks to make sure the flow controller and owning test are ready.
    * Update changelog

--- SpatialGDK/Source/SpatialGDKFunctionalTests/Public/SpatialFunctionalTestFlowController.h
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/Public/SpatialFunctionalTestFlowController.h
@@ -24,8 +24,6 @@ public:
 
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-	virtual void BeginPlay() override;
-
 	virtual void OnAuthorityGained() override;
 
 	virtual void Tick(float DeltaSeconds) override;
@@ -79,14 +77,15 @@ public:
 	bool HasAckFinishedTest() const { return bHasAckFinishedTest; }
 
 	UFUNCTION()
+	void RegisterFlowController();
+	UFUNCTION()
+	void TrySetReadyToRunTest();
+	UFUNCTION()
 	void DeregisterFlowController();
 
 private:
 	// Current Step being executed
 	SpatialFunctionalTestStep CurrentStep;
-
-	UPROPERTY(ReplicatedUsing = OnReadyToRegisterWithTest)
-	uint8 bReadyToRegisterWithTest : 1;
 
 	UPROPERTY(Replicated)
 	bool bIsReadyToRunTest;
@@ -95,12 +94,7 @@ private:
 	bool bHasAckFinishedTest;
 
 	UFUNCTION()
-	void OnReadyToRegisterWithTest();
-
-	UFUNCTION()
 	void OnRep_OwningTest();
-
-	void TryRegisterFlowControllerWithOwningTest();
 
 	UFUNCTION(Server, Reliable)
 	void ServerSetReadyToRunTest(bool bIsReady);
