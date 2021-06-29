c5c1c7d47323a2ef8f21ce093ce3c849e4657a74
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-06-29 11:16:27 +0100

    UNR-5752: Added a test

---
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialActorResolutionTest/SpatialActorResolutionTest.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "SpatialCommonTypes.h"
+#include "SpatialFunctionalTest.h"
+
+#include "SpatialActorResolutionTest.generated.h"
+
+UCLASS()
+class ASelfDestroyingActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASelfDestroyingActor();
+
+	virtual bool ReplicateSubobjects(UActorChannel* Channel, FOutBunch* Bunch, FReplicationFlags* RepFlags) override;
+};
+
+UCLASS()
+class ASpatialActorResolutionTest : public ASpatialFunctionalTest
+{
+	GENERATED_BODY()
+public:
+	ASpatialActorResolutionTest();
+
+	virtual void PrepareTest() override;
+
+	UPROPERTY(Transient)
+	TWeakObjectPtr<ASelfDestroyingActor> TestActor;
+
+	float TimeElapsed;
+};
