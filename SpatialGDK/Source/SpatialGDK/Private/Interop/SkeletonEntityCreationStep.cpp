c4734694d74e5ad0e30d57f9507f06decee1fae0
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-07-06 17:06:34 +0100

    UNR-5491: Added skeleton entity creation step

---
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/SkeletonEntityCreationStep.cpp
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "SkeletonEntityCreationStep.h"
+
+#include "EngineClasses/SpatialNetDriver.h"
+#include "Interop/GlobalStateManager.h"
+
+namespace SpatialGDK
+{
+DEFINE_LOG_CATEGORY_STATIC(LogSpatialSkeletonEntityCreationStep, Log, All);
+
+bool FSkeletonEntityCreationStep::TryFinish()
+{
+	if (!bWasInitialized)
+	{
+		bWasInitialized = true;
+		Initialize();
+	}
+
+	bool bDidFinish = true;
+
+	if (ServerEntityCreator)
+	{
+		ServerEntityCreator->Advance();
+		bDidFinish = bDidFinish && ServerEntityCreator->IsReady();
+	}
+
+	if (EntityPopulator)
+	{
+		EntityPopulator->Advance();
+		bDidFinish = bDidFinish && EntityPopulator->IsReady();
+	}
+
+	if (bDidFinish)
+	{
+		UE_LOG(LogSpatialSkeletonEntityCreationStep, Log, TEXT("Finished skeleton entity creation"));
+	}
+
+	return bDidFinish;
+}
+
+void FSkeletonEntityCreationStep::Initialize()
+{
+	if (NetDriver->GlobalStateManager->HasAuthority())
+	{
+		UE_LOG(LogSpatialSkeletonEntityCreationStep, Log, TEXT("Starting GSM-auth skeleton entity creation step"));
+
+		ServerEntityCreator.Emplace(*NetDriver);
+		ServerEntityCreator->CreateSkeletonEntitiesForWorld(*NetDriver->GetWorld());
+	}
+
+	{
+		UE_LOG(LogSpatialSkeletonEntityCreationStep, Log, TEXT("Starting skeleton entity population step"));
+
+		EntityPopulator.Emplace(*NetDriver);
+	}
+}
+
+} // namespace SpatialGDK
---
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/SkeletonEntityCreationStep.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "Interop/SkeletonEntities.h"
+
+class USpatialNetDriver;
+
+namespace SpatialGDK
+{
+class FSkeletonEntityCreationStep
+{
+public:
+	explicit FSkeletonEntityCreationStep(USpatialNetDriver& InNetDriver)
+		: NetDriver(&InNetDriver)
+	{
+	}
+
+	bool TryFinish();
+
+private:
+	void Initialize();
+
+	USpatialNetDriver* NetDriver;
+	bool bWasInitialized = false;
+	TOptional<FDistributedStartupActorSkeletonEntityCreator> ServerEntityCreator;
+	TOptional<FSkeletonEntityPopulator> EntityPopulator;
+};
+} // namespace SpatialGDK
