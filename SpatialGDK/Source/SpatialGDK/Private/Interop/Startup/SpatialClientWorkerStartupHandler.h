c12367d4b4c8854c406d55d1853364616db4e71c
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-07-15 17:05:40 +0100

    UNR-5710: Added client worker startup handler

---
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/Startup/SpatialClientWorkerStartupHandler.h
@@ -0,0 +1,79 @@
+﻿#pragma once
+
+#include "Containers/Array.h"
+#include "Misc/Optional.h"
+
+#include "EngineClasses/SpatialVirtualWorkerTranslator.h"
+#include "Interop/EntityQueryHandler.h"
+#include "SpatialCommonTypes.h"
+
+class USpatialNetDriver;
+
+namespace SpatialGDK
+{
+class ViewCoordinator;
+
+class FSpatialClientStartupHandler
+{
+public:
+	struct FInitialSetup
+	{
+		int32 ExpectedServerWorkersCount;
+	};
+	explicit FSpatialClientStartupHandler(USpatialNetDriver& InNetDriver, UGameInstance& InGameInstance, const FInitialSetup& InSetup);
+	virtual ~FSpatialClientStartupHandler();
+	bool TryFinishStartup();
+	FString GetStartupStateDescription() const;
+
+private:
+	void QueryGSM();
+	ViewCoordinator& GetCoordinator();
+	const ViewCoordinator& GetCoordinator() const;
+	const TArray<Worker_Op>& GetOps() const;
+
+	bool bQueriedGSM = false;
+	bool bStartedRetrying = false;
+	FTimerHandle GSMQueryRetryTimer;
+	FEntityQueryHandler QueryHandler;
+	struct FDeploymentMapData
+	{
+		FString DeploymentMapURL;
+
+		bool bAcceptingPlayers;
+
+		int32 DeploymentSessionId;
+
+		uint32 SchemaHash;
+	};
+	TOptional<FDeploymentMapData> GSMData;
+	static bool GetFromComponentData(const Worker_ComponentData& Component, FDeploymentMapData& OutData);
+
+	struct FSnapshotData
+	{
+		uint64 SnapshotVersion;
+	};
+	TOptional<FSnapshotData> SnapshotData;
+	static bool GetFromComponentData(const Worker_ComponentData& Component, FSnapshotData& OutData);
+
+	bool bFinishedMapLoad = false;
+	FDelegateHandle PostMapLoadedDelegateHandle;
+	void OnMapLoaded(UWorld* LoadedWorld);
+
+	enum class EStage : uint8
+	{
+		QueryGSM,
+		WaitForMapLoad,
+		SendPlayerSpawnRequest,
+		Finished,
+		Initial = QueryGSM,
+	};
+
+	FInitialSetup Setup;
+
+	EStage Stage = EStage::Initial;
+
+	USpatialNetDriver* NetDriver;
+	TWeakObjectPtr<UGameInstance> GameInstance;
+};
+
+} // namespace SpatialGDK
