bfbd9bffbf8815433985824fa3e438b7505958de
samiwh <sami@improbable.io>
2021-07-16 14:15:56 +0100

    temp

--- SpatialGDK/Source/SpatialGDK/Public/Utils/WorkerVersionCheck.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/WorkerVersionCheck.h
@@ -4,7 +4,7 @@
 
 #include "improbable/c_worker.h"
 
-#define WORKER_SDK_VERSION "15.2.0"
+#define WORKER_SDK_VERSION "15.2.0-objective106-preview-01"
 
 constexpr bool StringsEqual(char const* A, char const* B)
 {
