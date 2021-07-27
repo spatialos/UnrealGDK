d4feceba7c7d061cc5de3849534120200cd36e9c
Arthur Baker <arthurbaker+github@improbable.io>
2021-07-27 10:02:00 +0100

    [UNR-5785] Fix SpatialPackageMap having ObjectRef->NetGUID mapping it shouldn't have (#3262)
    Previously, we were failing to remove the packagemap mappings for replicated dynamic Subobjects that had been created on that same worker and then were being deleted/leaving interest.
    
    This was because we were using Channel->CreateSubobjects to find the dynamic subobjects, which only contains subobjects that were received on, and created by, that channel

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialPackageMapClient.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialPackageMapClient.cpp
@@ -183,11 +183,12 @@ void USpatialPackageMapClient::ResolveSubobject(UObject* Object, const FUnrealOb
 	}
 	else
 	{
-		// TODO UNR-5785 - Remove this once fixed, as really SpatialGuidCache and Native's GuidCache should be in sync
+		// Sanity check consistency between SpatialGuidCache and native's GuidCache, as it would be pretty bad if they were out of sync.
 		const FNetworkGUID ObjectNetGUID = GetNetGUIDFromObject(Object);
 		if (!ObjectNetGUID.IsValid())
 		{
-			UE_LOG(LogSpatialPackageMap, Log,
+			// TODO UNR-5844: Remove this workaround if we have not seen this warning triggered.
+			UE_LOG(LogSpatialPackageMap, Warning,
 				   TEXT("SpatialGuidCache has a NetGUID mapping which native's GuidCache does not have for object: %s with NetGUID: %u. "
 						"Removing existing mapping from SpatialGuidCache and recreating."),
 				   *Object->GetName(), NetGUID.Value);
@@ -604,33 +605,38 @@ void FSpatialNetGUIDCache::RemoveEntityNetGUID(Worker_EntityId EntityId)
 		}
 	}
 
-	// Remove dynamically attached subobjects
+	// Remove dynamic subobjects and objects replicated on the actorchannel.
 	if (USpatialActorChannel* Channel = SpatialNetDriver->GetActorChannelByEntityId(EntityId))
 	{
-		for (UObject* DynamicSubobject : Channel->CreateSubObjects)
+		for (const auto& ObjReplicatorPair : Channel->ReplicationMap)
 		{
-			if (FNetworkGUID* SubobjectNetGUID = NetGUIDLookup.Find(DynamicSubobject))
+			if (FNetworkGUID* ObjectNetGUID = NetGUIDLookup.Find(ObjReplicatorPair.Value->GetWeakObjectPtr()))
 			{
-				if (FUnrealObjectRef* SubobjectRef = NetGUIDToUnrealObjectRef.Find(*SubobjectNetGUID))
+				if (FUnrealObjectRef* SubobjectRef = NetGUIDToUnrealObjectRef.Find(*ObjectNetGUID))
 				{
 					UnrealObjectRefToNetGUID.Remove(*SubobjectRef);
-					NetGUIDToUnrealObjectRef.Remove(*SubobjectNetGUID);
+					NetGUIDToUnrealObjectRef.Remove(*ObjectNetGUID);
 				}
 			}
 		}
 	}
 
-	// Remove actor.
+	// Remove actor mappings.
 	FNetworkGUID EntityNetGUID = GetNetGUIDFromEntityId(EntityId);
-	// TODO: Figure out why NetGUIDToUnrealObjectRef might not have this GUID. UNR-989
-	if (FUnrealObjectRef* ActorRef = NetGUIDToUnrealObjectRef.Find(EntityNetGUID))
+	if (EntityNetGUID.IsValid())
 	{
-		UnrealObjectRefToNetGUID.Remove(*ActorRef);
-	}
-	NetGUIDToUnrealObjectRef.Remove(EntityNetGUID);
-	if (StablyNamedRefOption.IsSet())
-	{
-		UnrealObjectRefToNetGUID.Remove(StablyNamedRefOption.GetValue());
+		// This may have already been removed by the code removing items replicated on the ActorChannel.
+
+		// TODO: Figure out why NetGUIDToUnrealObjectRef might not have this GUID. UNR-989
+		if (FUnrealObjectRef* ActorRef = NetGUIDToUnrealObjectRef.Find(EntityNetGUID))
+		{
+			UnrealObjectRefToNetGUID.Remove(*ActorRef);
+		}
+		NetGUIDToUnrealObjectRef.Remove(EntityNetGUID);
+		if (StablyNamedRefOption.IsSet())
+		{
+			UnrealObjectRefToNetGUID.Remove(StablyNamedRefOption.GetValue());
+		}
 	}
 }
 
