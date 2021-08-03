spatialos/UnrealGDK

commit 2816027a86d957869d989fea1b448ef978869657

[UNR-5819] pre/post net receive match native (#3268)
Currently we call PreNetReceive immediately before applying data for a spatial component, and PostNetReceive after. This means we can call pre/postnetreceive multiple times on a given actor or object in a single tick.

Native only calls Pre/PostNetReceive once per object per tick

We can do that as well so probably should

SpatialGDK/Source/SpatialGDK/Private/Interop/ActorSystem.cpp
@@ -147,10 +147,11 @@ void ActorSystem::ProcessUpdates(const FEntitySubViewUpdate& SubViewUpdate)
 	{
 		if (Delta.Type == EntityDelta::UPDATE)
 		{
+			TArray<FWeakObjectPtr> ToResolveOps;
 			for (const ComponentChange& Change : Delta.ComponentsAdded)
 			{
 				ApplyComponentAdd(Delta.EntityId, Change.ComponentId, Change.Data);
-				ComponentAdded(Delta.EntityId, Change.ComponentId, Change.Data);
+				ComponentAdded(Delta.EntityId, Change.ComponentId, Change.Data, ToResolveOps);
 			}
 			for (const ComponentChange& Change : Delta.ComponentUpdates)
 			{
@@ -159,12 +160,15 @@ void ActorSystem::ProcessUpdates(const FEntitySubViewUpdate& SubViewUpdate)
 			for (const ComponentChange& Change : Delta.ComponentsRefreshed)
 			{
 				ApplyComponentAdd(Delta.EntityId, Change.ComponentId, Change.CompleteUpdate.Data);
-				ComponentAdded(Delta.EntityId, Change.ComponentId, Change.CompleteUpdate.Data);
+				ComponentAdded(Delta.EntityId, Change.ComponentId, Change.CompleteUpdate.Data, ToResolveOps);
 			}
 			for (const ComponentChange& Change : Delta.ComponentsRemoved)
 			{
 				ComponentRemoved(Delta.EntityId, Change.ComponentId);
 			}
+
+			InvokePostNetReceives();
+			ResolvePendingOpsFromEntityUpdate(ToResolveOps);
 		}
 	}
 }
@@ -185,16 +189,16 @@ void ActorSystem::ProcessAdds(const FEntitySubViewUpdate& SubViewUpdate)
 				continue;
 			}
 
-			if (!PresentEntities.Contains(Delta.EntityId))
+			if (!PresentEntities.Contains(EntityId))
 			{
 				// Create new actor for the entity.
-				EntityAdded(Delta.EntityId);
+				EntityAdded(EntityId);
 
-				PresentEntities.Emplace(Delta.EntityId);
+				PresentEntities.Emplace(EntityId);
 			}
 			else
 			{
-				RefreshEntity(Delta.EntityId);
+				RefreshEntity(EntityId);
 			}
 		}
 	}
@@ -545,7 +549,8 @@ void ActorSystem::HandleActorAuthority(const Worker_EntityId EntityId, const Wor
 	}
 }
 
-void ActorSystem::ComponentAdded(const Worker_EntityId EntityId, const Worker_ComponentId ComponentId, Schema_ComponentData* Data)
+void ActorSystem::ComponentAdded(const Worker_EntityId EntityId, const Worker_ComponentId ComponentId, Schema_ComponentData* Data,
+								 TArray<FWeakObjectPtr>& OutToResolveOps)
 {
 	if (ComponentId == SpatialConstants::DORMANT_COMPONENT_ID)
 	{
@@ -587,7 +592,7 @@ void ActorSystem::ComponentAdded(const Worker_EntityId EntityId, const Worker_Co
 		return;
 	}
 
-	HandleIndividualAddComponent(EntityId, ComponentId, Data);
+	HandleIndividualAddComponent(EntityId, ComponentId, Data, OutToResolveOps);
 }
 
 void ActorSystem::ComponentUpdated(const Worker_EntityId EntityId, const Worker_ComponentId ComponentId, Schema_ComponentUpdate* Update)
@@ -815,7 +820,7 @@ void ActorSystem::HandleDormantComponentAdded(const Worker_EntityId EntityId) co
 }
 
 void ActorSystem::HandleIndividualAddComponent(const Worker_EntityId EntityId, const Worker_ComponentId ComponentId,
-											   Schema_ComponentData* Data)
+											   Schema_ComponentData* Data, TArray<FWeakObjectPtr>& OutToResolveOps)
 {
 	uint32 Offset = 0;
 	bool bFoundOffset = NetDriver->ClassInfoManager->GetOffsetByComponentId(ComponentId, Offset);
@@ -895,11 +900,12 @@ void ActorSystem::HandleIndividualAddComponent(const Worker_EntityId EntityId, c
 
 	if (bComponentsComplete)
 	{
-		AttachDynamicSubobject(Actor, EntityId, Info);
+		AttachDynamicSubobject(Actor, EntityId, Info, OutToResolveOps);
 	}
 }
 
-void ActorSystem::AttachDynamicSubobject(AActor* Actor, Worker_EntityId EntityId, const FClassInfo& Info)
+void ActorSystem::AttachDynamicSubobject(AActor* Actor, Worker_EntityId EntityId, const FClassInfo& Info,
+										 TArray<FWeakObjectPtr>& OutToResolveOps)
 {
 	USpatialActorChannel* Channel = NetDriver->GetActorChannelByEntityId(EntityId);
 	if (Channel == nullptr)
@@ -939,8 +945,8 @@ void ActorSystem::AttachDynamicSubobject(AActor* Actor, Worker_EntityId EntityId
 		Components.Remove(ComponentId);
 	});
 
-	// Resolve things like RepNotify or RPCs after applying component data.
-	ResolvePendingOperations(Subobject, SubobjectRef);
+	// Resolve things like RPCs and user object references after we have applied all other component updates for the entity.
+	OutToResolveOps.Emplace(FWeakObjectPtr(Subobject));
 }
 
 void ActorSystem::ApplyComponentData(USpatialActorChannel& Channel, UObject& TargetObject, const Worker_ComponentId ComponentId,
@@ -968,7 +974,18 @@ void ActorSystem::ApplyComponentData(USpatialActorChannel& Channel, UObject& Tar
 		bool bOutReferencesChanged = false;
 
 		FObjectRepNotifies& ObjectRepNotifiesOut = GetObjectRepNotifies(TargetObject);
-		Reader.ApplyComponentData(ComponentId, Data, TargetObject, Channel, ObjectRepNotifiesOut, bOutReferencesChanged);
+		const bool bSuccessfullyPreNetReceived = InvokePreNetReceive(TargetObject);
+		if (bSuccessfullyPreNetReceived)
+		{
+			Reader.ApplyComponentData(ComponentId, Data, TargetObject, Channel, ObjectRepNotifiesOut, bOutReferencesChanged);
+		}
+		else
+		{
+			UE_LOG(LogActorSystem, Log,
+				   TEXT("ApplyComponentData: Did not invoke PreNetReceive for object %s, entity id %lld, component id %u. No data will be "
+						"applied."),
+				   *TargetObject.GetName(), Channel.GetEntityId(), ComponentId);
+		}
 
 		RepStateHelper.Update(*this, Channel, bOutReferencesChanged);
 	}
@@ -976,6 +993,34 @@ void ActorSystem::ApplyComponentData(USpatialActorChannel& Channel, UObject& Tar
 	{
 		UE_LOG(LogActorSystem, Verbose, TEXT("Entity: %d Component: %d - Skipping because RPC components don't have actual data."),
 			   Channel.GetEntityId(), ComponentId);
+	}
+}
+
+void ActorSystem::ResolvePendingOpsFromEntityUpdate(const TArray<FWeakObjectPtr>& ToResolveOps)
+{
+	// This should be called after all component updates and adds have been completed, and PostNetReceives have been called to avoid user
+	// code from seeing inconsistent state
+	for (const FWeakObjectPtr& WeakObjectPtr : ToResolveOps)
+	{
+		UObject* Object = WeakObjectPtr.Get();
+		if (!Object)
+		{
+			UE_LOG(LogActorSystem, Log,
+				   TEXT("ResolvePendingOpsFromEntityUpdate: Did not resolve pending ops for object %s as it was no longer valid."),
+				   *GetNameSafe(Object));
+			continue;
+		}
+
+		const FUnrealObjectRef ObjectRef = NetDriver->PackageMap->GetUnrealObjectRefFromObject(Object);
+
+		if (!ObjectRef.IsValid())
+		{
+			UE_LOG(LogActorSystem, Error,
+				   TEXT("ResolvePendingOpsFromEntityUpdate: Tried to resolve pending ops for %s but object ref was not valid."),
+				   *Object->GetName());
+		}
+
+		ResolvePendingOperations(Object, ObjectRef);
 	}
 }
 
@@ -1261,7 +1306,19 @@ void ActorSystem::ApplyComponentUpdate(const Worker_ComponentId ComponentId, Sch
 	ComponentReader Reader(NetDriver, RepStateHelper.GetRefMap(), NetDriver->Connection->GetEventTracer());
 	bool bOutReferencesChanged = false;
 	FObjectRepNotifies& ObjectRepNotifiesOut = GetObjectRepNotifies(TargetObject);
-	Reader.ApplyComponentUpdate(ComponentId, ComponentUpdate, TargetObject, Channel, ObjectRepNotifiesOut, bOutReferencesChanged);
+
+	const bool bSuccessfullyPreNetReceived = InvokePreNetReceive(TargetObject);
+	if (bSuccessfullyPreNetReceived)
+	{
+		Reader.ApplyComponentUpdate(ComponentId, ComponentUpdate, TargetObject, Channel, ObjectRepNotifiesOut, bOutReferencesChanged);
+	}
+	else
+	{
+		UE_LOG(LogActorSystem, Log,
+			   TEXT("ApplyComponentUpdate: Did not invoke PreNetReceive for object %s, entity id %lld, component id %u. No data will be "
+					"applied."),
+			   *TargetObject.GetName(), Channel.GetEntityId(), ComponentId);
+	}
 	RepStateHelper.Update(*this, Channel, bOutReferencesChanged);
 
 	// This is a temporary workaround, see UNR-841:
@@ -1422,7 +1479,9 @@ void ActorSystem::ApplyFullState(const Worker_EntityId EntityId, USpatialActorCh
 		ClientNetLoadActorHelper.RemoveRuntimeRemovedComponents(EntityId, EntityComponents, EntityActor);
 	}
 
-	// Resolve things like RepNotify or RPCs after applying component data.
+	InvokePostNetReceives();
+
+	// Resolve things like RPCs and unresolved references after applying component data.
 	for (const ObjectPtrRefPair& ObjectToResolve : ObjectsToResolvePendingOpsFor)
 	{
 		ResolvePendingOperations(ObjectToResolve.Key, ObjectToResolve.Value);
@@ -1712,6 +1771,55 @@ USpatialActorChannel* ActorSystem::TryRestoreActorChannelForStablyNamedActor(AAc
 	return Channel;
 }
 
+bool ActorSystem::InvokePreNetReceive(UObject& Object)
+{
+	if (Object.IsPendingKill())
+	{
+		UE_LOG(LogActorSystem, Log, TEXT("InvokePreNetReceive: Did not invoke PreNetReceive for object %s, as object is pending kill."),
+			   *Object.GetName());
+		return false;
+	}
+
+	// We can have multiple spatial components receiving updates per unreal object but we only want to call PreNetReceive a single time for
+	// an object for each tick.
+	if (!PostNetReceivesToSend.Contains(FWeakObjectPtr(&Object)))
+	{
+		UE_LOG(LogActorSystem, VeryVerbose, TEXT("InvokePreNetReceive: Invoking PreNetReceive for object %s."), *Object.GetName());
+
+		Object.PreNetReceive();
+		PostNetReceivesToSend.Emplace(FWeakObjectPtr(&Object));
+	}
+	else
+	{
+		UE_LOG(LogActorSystem, VeryVerbose,
+			   TEXT("InvokePreNetReceive: Not invoking PreNetReceive for object %s as it is already contained within "
+					"PostNetReceivesToSend."),
+			   *Object.GetName());
+	}
+
+	return true;
+}
+
+void ActorSystem::InvokePostNetReceives()
+{
+	for (const FWeakObjectPtr& WeakPtr : PostNetReceivesToSend)
+	{
+		UObject* Object = WeakPtr.Get();
+		if (!Object)
+		{
+			// An object could have been set to pending kill as a result of the user callback PreNetReceive.
+			UE_LOG(LogActorSystem, Log, TEXT("Not sending PostNetReceive for object: %s as it is not valid."), *GetNameSafe(Object));
+			continue;
+		}
+
+		UE_LOG(LogActorSystem, VeryVerbose, TEXT("Sending PostNetReceive for object %s."), *Object->GetName());
+
+		Object->PostNetReceive();
+	}
+
+	PostNetReceivesToSend.Empty();
+}
+
 FObjectRepNotifies& ActorSystem::GetObjectRepNotifies(UObject& Object)
 {
 	return Object.IsA<AActor>() ? ActorRepNotifiesToSend.FindOrAdd(FWeakObjectPtr(&Object))
@@ -1739,11 +1847,6 @@ void ActorSystem::InvokeRepNotifies()
 
 void ActorSystem::TryInvokeRepNotifiesForObject(FWeakObjectPtr& WeakObjectPtr, FObjectRepNotifies& ObjectRepNotifies) const
 {
-	if (ObjectRepNotifies.RepNotifies.Num() == 0)
-	{
-		return;
-	}
-
 	// Object could have been killed during a RepNotify
 	UObject* Object = WeakObjectPtr.Get();
 	if (!Object)
@@ -1772,7 +1875,7 @@ void ActorSystem::TryInvokeRepNotifiesForObject(FWeakObjectPtr& WeakObjectPtr, F
 	});
 
 	RemoveRepNotifiesWithUnresolvedObjs(*Object, *Channel, ObjectRepNotifies.RepNotifies);
-	Channel->PostReceiveSpatialUpdate(Object, ObjectRepNotifies.RepNotifies, ObjectRepNotifies.PropertySpanIds);
+	Channel->InvokeRepNotifies(Object, ObjectRepNotifies.RepNotifies, ObjectRepNotifies.PropertySpanIds);
 }
 
 void ActorSystem::RemoveRepNotifiesWithUnresolvedObjs(UObject& Object, const USpatialActorChannel& Channel,

SpatialGDK/Source/SpatialGDK/Public/Interop/ActorSystem.h
@@ -56,6 +56,7 @@ public:
 
 	void MoveMappedObjectToUnmapped(const FUnrealObjectRef& Ref);
 	void CleanupRepStateMap(FSpatialObjectRepState& RepState);
+	void ResolvePendingOpsFromEntityUpdate(const TArray<FWeakObjectPtr>& ToResolveOps);
 	void ResolveAsyncPendingLoad(UObject* LoadedObject, const FUnrealObjectRef& ObjectRef);
 	void ResolvePendingOperations(UObject* Object, const FUnrealObjectRef& ObjectRef);
 	void RetireWhenAuthoritative(Worker_EntityId EntityId, Worker_ComponentId ActorClassId, bool bIsNetStartup, bool bNeedsTearOff);
@@ -124,7 +125,8 @@ private:
 	void AuthorityGained(Worker_EntityId EntityId, Worker_ComponentSetId ComponentSetId);
 	void HandleActorAuthority(Worker_EntityId EntityId, Worker_ComponentSetId ComponentSetId, Worker_Authority Authority);
 
-	void ComponentAdded(Worker_EntityId EntityId, Worker_ComponentId ComponentId, Schema_ComponentData* Data);
+	void ComponentAdded(Worker_EntityId EntityId, Worker_ComponentId ComponentId, Schema_ComponentData* Data,
+						TArray<FWeakObjectPtr>& OutToResolveOps);
 	void ComponentUpdated(Worker_EntityId EntityId, Worker_ComponentId ComponentId, Schema_ComponentUpdate* Update);
 	void ComponentRemoved(Worker_EntityId EntityId, Worker_ComponentId ComponentId) const;
 
@@ -146,8 +148,9 @@ private:
 
 	// Component add
 	void HandleDormantComponentAdded(Worker_EntityId EntityId) const;
-	void HandleIndividualAddComponent(Worker_EntityId EntityId, Worker_ComponentId ComponentId, Schema_ComponentData* Data);
-	void AttachDynamicSubobject(AActor* Actor, Worker_EntityId EntityId, const FClassInfo& Info);
+	void HandleIndividualAddComponent(Worker_EntityId EntityId, Worker_ComponentId ComponentId, Schema_ComponentData* Data,
+									  TArray<FWeakObjectPtr>& OutToResolveOps);
+	void AttachDynamicSubobject(AActor* Actor, Worker_EntityId EntityId, const FClassInfo& Info, TArray<FWeakObjectPtr>& OutToResolveOps);
 	void ApplyComponentData(USpatialActorChannel& Channel, UObject& TargetObject, const Worker_ComponentId ComponentId,
 							Schema_ComponentData* Data);
 
@@ -171,6 +174,8 @@ private:
 										   USpatialActorChannel& Channel, TArray<ObjectPtrRefPair>& OutObjectsToResolve);
 
 	USpatialActorChannel* TryRestoreActorChannelForStablyNamedActor(AActor* StablyNamedActor, Worker_EntityId EntityId);
+	bool InvokePreNetReceive(UObject& Object);
+	void InvokePostNetReceives();
 	FObjectRepNotifies& GetObjectRepNotifies(UObject& Object);
 
 	// Entity remove
@@ -205,6 +210,9 @@ private:
 	TMap<Worker_EntityId_Key, TSet<Worker_ComponentId>> PendingDynamicSubobjectComponents;
 
 	FChannelsToUpdatePosition ChannelsToUpdatePosition;
+
+	// Objects for a given entity are stored here then sent after all updates for that entity have been applied.
+	TArray<FWeakObjectPtr> PostNetReceivesToSend;
 
 	// RepNotifies are stored here then sent after all updates we have are applied
 	FObjectToRepNotifies ActorRepNotifiesToSend;

SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialActorChannel.cpp
@@ -434,8 +434,8 @@ FRepChangeState USpatialActorChannel::CreateInitialRepChangeState(TWeakObjectPtr
 		const FRepLayoutCmd& Cmd = Replicator.RepLayout->Cmds[CmdIdx];
 		const FRepChangedParent& Parent = Replicator.RepState->GetSendingRepState()->RepChangedPropertyTracker->Parents[Cmd.ParentIndex];
 
-		// HACK: Need special case here because the gdk relies on the whole of the RepMovement struct being replicated to the client to decide whether to replicate physics.
-		// see: ComponentReader::ApplySchemaObject
+		// HACK: Need special case here because the gdk relies on the whole of the RepMovement struct being replicated to the client to
+		// decide whether to replicate physics. see: ComponentReader::ApplySchemaObject
 
 		// UNR-5843 TODO: fix this so we no longer need this special handling, for instance by replicating bRepPhysics as a separate always
 		// replicated field.
@@ -1030,7 +1030,7 @@ bool USpatialActorChannel::TryResolveActor()
 	return true;
 }
 
-FObjectReplicator* USpatialActorChannel::PreReceiveSpatialUpdate(UObject* TargetObject)
+FObjectReplicator* USpatialActorChannel::GetObjectReplicatorForSpatialUpdate(UObject* TargetObject)
 {
 	// If there is no NetGUID for this object, we will crash in FObjectReplicator::StartReplicating, so we verify this here.
 	FNetworkGUID ObjectNetGUID = Connection->Driver->GuidCache->GetOrAssignNetGUID(TargetObject);
@@ -1040,19 +1040,18 @@ FObjectReplicator* USpatialActorChannel::PreReceiveSpatialUpdate(UObject* Target
 	{
 		// SpatialReceiver tried to resolve this object in the PackageMap, but it didn't propagate to GuidCache.
 		// This could happen if the UnrealObjectRef was already mapped to a different object that's been destroyed.
-		UE_LOG(LogSpatialActorChannel, Error, TEXT("PreReceiveSpatialUpdate: NetGUID is invalid! Object: %s"),
+		UE_LOG(LogSpatialActorChannel, Error, TEXT("GetObjectReplicatorForSpatialUpdate: NetGUID is invalid! Object: %s"),
 			   *TargetObject->GetPathName());
 		return nullptr;
 	}
 
 	FObjectReplicator& Replicator = FindOrCreateReplicator(TargetObject).Get();
-	TargetObject->PreNetReceive();
 
 	return &Replicator;
 }
 
-void USpatialActorChannel::PostReceiveSpatialUpdate(UObject* TargetObject, const TArray<GDK_PROPERTY(Property) *>& RepNotifies,
-													const TMap<GDK_PROPERTY(Property) *, FSpatialGDKSpanId>& PropertySpanIds)
+void USpatialActorChannel::InvokeRepNotifies(UObject* TargetObject, const TArray<GDK_PROPERTY(Property) *>& RepNotifies,
+											 const TMap<GDK_PROPERTY(Property) *, FSpatialGDKSpanId>& PropertySpanIds)
 {
 	FObjectReplicator& Replicator = FindOrCreateReplicator(TargetObject).Get();
 

SpatialGDK/Source/SpatialGDK/Private/Utils/ComponentReader.cpp
@@ -171,10 +171,10 @@ void ComponentReader::ApplySchemaObject(Schema_Object* ComponentObject, UObject&
 										const TArray<Schema_FieldId>& UpdatedIds, Worker_ComponentId ComponentId,
 										FObjectRepNotifies& ObjectRepNotifiesOut, bool& bOutReferencesChanged)
 {
-	FObjectReplicator* Replicator = Channel.PreReceiveSpatialUpdate(&Object);
+	FObjectReplicator* Replicator = Channel.GetObjectReplicatorForSpatialUpdate(&Object);
 	if (Replicator == nullptr)
 	{
-		// Can't apply this schema object. Error printed from PreReceiveSpatialUpdate.
+		// Can't apply this schema object. Error printed from GetObjectReplicatorForSpatialUpdate.
 		return;
 	}
 
@@ -407,9 +407,6 @@ void ComponentReader::ApplySchemaObject(Schema_Object* ComponentObject, UObject&
 			}
 		}
 	}
-
-	// PostReceiveSpatialUpdate is called later when sending RepNotifies in ActorSystem
-	Object.PostNetReceive();
 }
 
 void ComponentReader::ApplyProperty(Schema_Object* Object, Schema_FieldId FieldId, FObjectReferencesMap& InObjectReferencesMap,

SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialActorChannel.h
@@ -249,9 +249,9 @@ public:
 
 	FRepChangeState CreateInitialRepChangeState(TWeakObjectPtr<UObject> Object);
 
-	FObjectReplicator* PreReceiveSpatialUpdate(UObject* TargetObject);
-	void PostReceiveSpatialUpdate(UObject* TargetObject, const TArray<GDK_PROPERTY(Property) *>& RepNotifies,
-								  const TMap<GDK_PROPERTY(Property) *, FSpatialGDKSpanId>& PropertySpanIds);
+	FObjectReplicator* GetObjectReplicatorForSpatialUpdate(UObject* TargetObject);
+	void InvokeRepNotifies(UObject* TargetObject, const TArray<GDK_PROPERTY(Property) *>& RepNotifies,
+						   const TMap<GDK_PROPERTY(Property) *, FSpatialGDKSpanId>& PropertySpanIds);
 
 	void RemoveRepNotifiesWithUnresolvedObjs(TArray<GDK_PROPERTY(Property) *>& RepNotifies, const FRepLayout& RepLayout,
 											 const FObjectReferencesMap& RefMap, const UObject* Object) const;

SpatialGDK/Source/SpatialGDKFunctionalTests/Private/TestMaps/SpatialNetworkingMap.cpp
@@ -2,6 +2,7 @@
 
 #include "TestMaps/SpatialNetworkingMap.h"
 
+#include "SpatialGDK/SpatialTestNetReceive/SpatialTestNetReceive.h"
 #include "SpatialGDK/StaticSubobjectsTest/StaticSubobjectTestActor.h"
 #include "SpatialGDK/StaticSubobjectsTest/StaticSubobjectsTest.h"
 #include "SpatialGDKFunctionalTests/SpatialGDK/DormancyTests/DormancyTestActor.h"
@@ -48,6 +49,7 @@ void USpatialNetworkingMap::CreateCustomContentForMap()
 	AddActorToLevel<ADynamicSubobjectsTest>(CurrentLevel, FTransform::Identity);
 	AddActorToLevel<AStaticSubobjectsTest>(CurrentLevel, FTransform::Identity);
 	AddActorToLevel<ASpatialDynamicComponentsFastReadditionTest>(CurrentLevel, FTransform::Identity);
+	AddActorToLevel<ASpatialTestNetReceive>(CurrentLevel, FTransform::Identity);
 
 	AddActorToLevel<AInitiallyDormantDynamicActorTest>(CurrentLevel, FTransform::Identity);
 	AddActorToLevel<ADynamicActorSetToAwakeTest>(CurrentLevel, FTransform::Identity);

commit 37b0a85378c4946020b5c7e15869841ac23de7c9

Move tear off channels after rep notifies (#3278)
In this PR I have moved the tearing off of channels that have been newly marked as tear off (and we noticed after an applycomponentupdate), to after rep notifies.

Why:

The TearOff test started failing after #3268 (files) was merged, due to the Failed to invoke rep notifies for an object as its entity id was invalid warning.

However, that PR did not actually break tearoff. The reason it surfaced there was because I removed the early out clause for repnotifies.

void ActorSystem::TryInvokeRepNotifiesForObject(FWeakObjectPtr& WeakObjectPtr, FObjectRepNotifies& ObjectRepNotifies) const
{
	if (ObjectRepNotifies.RepNotifies.Num() == 0)
	{
		return;
	}
The change that broke this was the change that moved repnotifies for all entities till after all updates had been applied.

This changed the ordering of closing channels for tear off from after rep notifies had been applied, to before rep notifies were applied. Since applying rep notifies (with current implementation) requires a channel, this broke sending rep notifies.

SpatialGDK/Source/SpatialGDK/Private/Interop/ActorSystem.cpp
@@ -338,6 +338,10 @@ void ActorSystem::Advance()
 	// authoritative you are supposed to be the only server that can update properties.
 	InvokeRepNotifies();
 
+	// And finally send clean up for channels that we got a tear off update for.
+	// We don't do this earlier as Rep Notifies require the actor channel.
+	CleanUpTornOffChannels();
+
 	// No need to ProcessAuthorityGains on SimulatedSubView as we won't have gained authority on Entities in that SubView.
 	ProcessAuthorityGains(EntityAuthSubView);
 	ProcessAuthorityGains(EntityOwnershipSubView);
@@ -973,10 +977,10 @@ void ActorSystem::ApplyComponentData(USpatialActorChannel& Channel, UObject& Tar
 		ComponentReader Reader(NetDriver, RepStateHelper.GetRefMap(), NetDriver->Connection->GetEventTracer());
 		bool bOutReferencesChanged = false;
 
-		FObjectRepNotifies& ObjectRepNotifiesOut = GetObjectRepNotifies(TargetObject);
 		const bool bSuccessfullyPreNetReceived = InvokePreNetReceive(TargetObject);
 		if (bSuccessfullyPreNetReceived)
 		{
+			FObjectRepNotifies& ObjectRepNotifiesOut = GetObjectRepNotifies(TargetObject);
 			Reader.ApplyComponentData(ComponentId, Data, TargetObject, Channel, ObjectRepNotifiesOut, bOutReferencesChanged);
 		}
 		else
@@ -1305,11 +1309,11 @@ void ActorSystem::ApplyComponentUpdate(const Worker_ComponentId ComponentId, Sch
 
 	ComponentReader Reader(NetDriver, RepStateHelper.GetRefMap(), NetDriver->Connection->GetEventTracer());
 	bool bOutReferencesChanged = false;
-	FObjectRepNotifies& ObjectRepNotifiesOut = GetObjectRepNotifies(TargetObject);
 
 	const bool bSuccessfullyPreNetReceived = InvokePreNetReceive(TargetObject);
 	if (bSuccessfullyPreNetReceived)
 	{
+		FObjectRepNotifies& ObjectRepNotifiesOut = GetObjectRepNotifies(TargetObject);
 		Reader.ApplyComponentUpdate(ComponentId, ComponentUpdate, TargetObject, Channel, ObjectRepNotifiesOut, bOutReferencesChanged);
 	}
 	else
@@ -1330,7 +1334,7 @@ void ActorSystem::ApplyComponentUpdate(const Worker_ComponentId ComponentId, Sch
 		// Check if bTearOff has been set to true
 		if (GetBoolFromSchema(ComponentObject, SpatialConstants::ACTOR_TEAROFF_ID))
 		{
-			Channel.ConditionalCleanUp(false, EChannelCloseReason::TearOff);
+			EntityChannelsToSetTornOff.Add(Channel.GetEntityId());
 		}
 	}
 }
@@ -1889,6 +1893,18 @@ void ActorSystem::RemoveRepNotifiesWithUnresolvedObjs(UObject& Object, const USp
 			Channel.RemoveRepNotifiesWithUnresolvedObjs(RepNotifies, *Replicator.RepLayout, ObjectRepState->ReferenceMap, &Object);
 		}
 	}
+}
+
+void ActorSystem::CleanUpTornOffChannels()
+{
+	for (const Worker_EntityId EntityId : EntityChannelsToSetTornOff)
+	{
+		if (USpatialActorChannel* Channel = NetDriver->GetActorChannelByEntityId(EntityId))
+		{
+			Channel->ConditionalCleanUp(false, EChannelCloseReason::TearOff);
+		}
+	}
+	EntityChannelsToSetTornOff.Empty();
 }
 
 void ActorSystem::RemoveActor(const Worker_EntityId EntityId)

SpatialGDK/Source/SpatialGDK/Public/Interop/ActorSystem.h
@@ -140,6 +140,7 @@ private:
 	void TryInvokeRepNotifiesForObject(FWeakObjectPtr& Object, FObjectRepNotifies& ObjectRepNotifies) const;
 	static void RemoveRepNotifiesWithUnresolvedObjs(UObject& Object, const USpatialActorChannel& Channel,
 													TArray<GDK_PROPERTY(Property) *>& RepNotifies);
+	void CleanUpTornOffChannels();
 
 	// Authority
 	bool HasEntityBeenRequestedForDelete(Worker_EntityId EntityId) const;
@@ -217,6 +218,9 @@ private:
 	// RepNotifies are stored here then sent after all updates we have are applied
 	FObjectToRepNotifies ActorRepNotifiesToSend;
 	FObjectToRepNotifies SubobjectRepNotifiesToSend;
+
+	// Channels are set as torn off after we have sent all our other user callbacks, such as rep notifies.
+	TArray<Worker_EntityId> EntityChannelsToSetTornOff;
 
 	// Deserialized state store for Actor relevant components.
 	TMap<Worker_EntityId_Key, ActorData> ActorDataStore;