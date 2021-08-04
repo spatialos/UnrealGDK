4ba31510ffcf0bf6b7ac34fc498fa68045a87fa9
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-08-04 17:42:13 +0100

    UNR-5710: Swapped inline client startup out for a startup executor

--- SpatialGDK/Source/SpatialGDK/Private/Interop/Startup/SpatialClientWorkerStartupHandler.h
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/Startup/SpatialClientWorkerStartupHandler.h
@@ -19,46 +19,12 @@ class FSpatialClientStartupHandler final
 {
 public:
 	explicit FSpatialClientStartupHandler(USpatialNetDriver& InNetDriver, UGameInstance& InGameInstance);
-	virtual ~FSpatialClientStartupHandler();
 	bool TryFinishStartup();
 	FString GetStartupStateDescription() const;
 
 private:
-	void QueryGSM();
 	ViewCoordinator& GetCoordinator();
 	const ViewCoordinator& GetCoordinator() const;
-	const TArray<Worker_Op>& GetOps() const;
-
-	bool bQueriedGSM = false;
-	bool bStartedRetrying = false;
-	TOptional<FString> ClientStartupExtraState;
-	FTimerHandle GSMQueryRetryTimer;
-	FEntityQueryHandler QueryHandler;
-
-	TOptional<FDeploymentMapData> GSMData;
-	static bool GetFromComponentData(const Worker_ComponentData& Component, FDeploymentMapData& OutData);
-
-	struct FSnapshotData
-	{
-		uint64 SnapshotVersion;
-	};
-	TOptional<FSnapshotData> SnapshotData;
-	static bool GetFromComponentData(const Worker_ComponentData& Component, FSnapshotData& OutData);
-
-	bool bFinishedMapLoad = false;
-	FDelegateHandle PostMapLoadedDelegateHandle;
-	void OnMapLoaded(UWorld* LoadedWorld);
-
-	enum class EStage : uint8
-	{
-		QueryGSM,
-		WaitForMapLoad,
-		SendPlayerSpawnRequest,
-		Finished,
-		Initial = QueryGSM,
-	};
-
-	EStage Stage = EStage::Initial;
 
 	USpatialNetDriver* NetDriver;
 	TWeakObjectPtr<UGameInstance> GameInstance;
