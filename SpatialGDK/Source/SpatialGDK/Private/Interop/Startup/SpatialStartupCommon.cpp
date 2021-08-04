534378451aef14a6225ec3b8d18c2fe1a96434f0
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-08-04 16:29:42 +0100

    UNR-5710: Added startup step and startup executor classes

---
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/Startup/SpatialStartupCommon.cpp
@@ -0,0 +1,12 @@
+#include "Interop/Startup/SpatialStartupCommon.h"
+
+#include "WorkerSDK/improbable/c_schema.h"
+
+#include "SpatialConstants.h"
+#include "Utils/SchemaUtils.h"
+
+DEFINE_LOG_CATEGORY(LogSpatialStartupHandler);
+
+namespace SpatialGDK
+{
+} // namespace SpatialGDK
---
+++ SpatialGDK/Source/SpatialGDK/Private/Interop/Startup/SpatialStartupCommon.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "Containers/Array.h"
+#include "Templates/Function.h"
+#include "Templates/UniqueObj.h"
+#include "Templates/UniquePtr.h"
+
+#include "WorkerSDK/improbable/c_schema.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogSpatialStartupHandler, Log, All);
+
+namespace SpatialGDK
+{
+struct FStartupStep
+{
+	virtual ~FStartupStep() = default;
+
+	virtual void Start(){};
+	virtual bool TryFinish() { return true; }
+	virtual FString Describe() const { return StepName; }
+
+	FString StepName;
+};
+
+struct FStartupExecutor
+{
+	explicit FStartupExecutor(TArray<TUniquePtr<FStartupStep>> InSteps)
+	{
+		class FBlankStartupStep : public FStartupStep
+		{
+		};
+
+		// This lets us only execute actual startup logic
+		// during TryFinish() calls and not when creating
+		// the executor.
+		Steps.Emplace(MakeUnique<FBlankStartupStep>());
+
+		Steps.Append(MoveTemp(InSteps));
+	}
+
+	bool TryFinish()
+	{
+		while (Steps.Num() > 0)
+		{
+			if (!Steps[0]->TryFinish())
+			{
+				return false;
+			}
+
+			Steps.RemoveAt(0);
+
+			if (Steps.Num() > 0)
+			{
+				Steps[0]->Start();
+			}
+		}
+		return true;
+	}
+
+	FString Describe() const
+	{
+		if (Steps.Num() > 0)
+		{
+			return Steps[0]->Describe();
+		}
+		return TEXT("Startup finished");
+	}
+
+	TArray<TUniquePtr<FStartupStep>> Steps;
+};
+} // namespace SpatialGDK
