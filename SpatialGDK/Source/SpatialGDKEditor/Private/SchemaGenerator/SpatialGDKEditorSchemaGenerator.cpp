ff71bf53870703ef1bc3a66d63f965c1c05e2d47
Joshua Huburn <31517089+joshuahuburn@users.noreply.github.com>
2021-09-23 18:02:21 +0100

    UNR-5896 - Test Timeouts (#3312)

--- SpatialGDK/Source/SpatialGDKEditor/Private/SchemaGenerator/SpatialGDKEditorSchemaGenerator.cpp
+++ SpatialGDK/Source/SpatialGDKEditor/Private/SchemaGenerator/SpatialGDKEditorSchemaGenerator.cpp
@@ -1295,23 +1295,28 @@ void ResetUsedNames()
 	}
 }
 
-bool RunSchemaCompiler(FString& SchemaBundleJsonOutput, FString SchemaInputDir, FString BuildDir)
+bool RunSchemaCompiler(FString& SchemaBundleJsonOutput, FString SchemaInputDir, FString BuildDir, FString CompiledSchemaDir)
 {
 	FString PluginDir = FSpatialGDKServicesModule::GetSpatialGDKPluginDirectory();
 
 	// Get the schema_compiler path and arguments
 	FString SchemaCompilerExe = FPaths::Combine(PluginDir, TEXT("SpatialGDK/Binaries/ThirdParty/Improbable/Programs/schema_compiler.exe"));
 
-	if (SchemaInputDir == "")
+	if (SchemaInputDir.IsEmpty())
 	{
 		SchemaInputDir = FPaths::Combine(SpatialGDKServicesConstants::SpatialOSDirectory, TEXT("schema"));
 	}
 
-	if (BuildDir == "")
+	if (BuildDir.IsEmpty())
 	{
 		BuildDir = FPaths::Combine(SpatialGDKServicesConstants::SpatialOSDirectory, TEXT("build"));
 	}
-	FString CompiledSchemaDir = FPaths::Combine(BuildDir, TEXT("assembly/schema"));
+
+	if (CompiledSchemaDir.IsEmpty())
+	{
+		CompiledSchemaDir = FPaths::Combine(BuildDir, TEXT("assembly/schema"));
+	}
+
 	FString CoreSDKSchemaDir = FPaths::Combine(BuildDir, TEXT("dependencies/schema/standard_library"));
 
 	FString CompiledSchemaASTDir = FPaths::Combine(CompiledSchemaDir, TEXT("ast"));
@@ -1456,8 +1461,13 @@ bool CreateCustomAuthoritySet(FString SchemaInputPath, FString SchemaOutputPath,
 			return true;
 		});
 
+		// Put the generated schema binary for our custom schema in the Intermediate folder.
+		// This is so it doesn't stomp the users (or tests) proper schema binary used by the runtime.
+		FString SchemaCompilerIntermediateDir = FPaths::Combine(FPaths::ProjectIntermediateDir(), TEXT("Improbable"), TEXT("schema"));
+		Filesystem.CreateDirectoryTree(*SchemaCompilerIntermediateDir);
+
 		FString SchemaJsonPath;
-		if (!RunSchemaCompiler(SchemaJsonPath, SchemaInputPath))
+		if (!RunSchemaCompiler(SchemaJsonPath, SchemaInputPath, "" /*BuildDir*/, SchemaCompilerIntermediateDir))
 		{
 			UE_LOG(LogSpatialGDKSchemaGenerator, Error, TEXT("Failed to parse %s meta data schema files"), *SetDesc.ComponentSetName);
 			return false;
