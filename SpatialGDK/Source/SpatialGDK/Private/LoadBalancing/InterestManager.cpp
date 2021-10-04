b447acb9c4016bef666d4efe29ae697b09fffa91
Nicolas Colombe <nicolascolombe@improbable.io>
2021-10-04 16:46:29 +0100

    Pull out Bullet for now.

--- SpatialGDK/Source/SpatialGDK/Private/LoadBalancing/InterestManager.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/LoadBalancing/InterestManager.cpp
@@ -2,19 +2,7 @@
 #include "Schema/ChangeInterest.h"
 #include "Utils/InterestFactory.h"
 
-#ifdef HAS_BULLET_PHYSICS
-// This is needed to suppress some warnings that UE4 escalates that Bullet doesn't
-THIRD_PARTY_INCLUDES_START
-// This is needed to fix memory alignment issues
-PRAGMA_PUSH_PLATFORM_DEFAULT_PACKING
-#include <BulletCollision/BroadphaseCollision/btDbvt.h>
-PRAGMA_POP_PLATFORM_DEFAULT_PACKING
-THIRD_PARTY_INCLUDES_END
-#else
-
 // Try to grab improbable's phTree thingy
-
-#endif
 
 #include <algorithm>
 #include <cmath>
@@ -63,207 +51,17 @@ struct FInterestManager::FBroadphaseImpl
 {
 	FBroadphaseImpl() {}
 
-	void Add(uint32 Slot, FVector Position)
-	{
-#ifdef HAS_BULLET_PHYSICS
-		btVector3 pos(Position.X / 100, Position.Y / 100, Position.Z / 100);
-		auto box = btDbvtVolume::FromCE(pos, btVector3(0.1, 0.1, 0.1));
+	void Add(uint32 Slot, FVector Position) {}
 
-		btDbvtNode* node = Tree.insert(box, (void*)Slot);
-		node->data = (void*)Slot;
+	void Add(uint32 Slot, FBox2D Box) {}
 
-		int32 TreeId = Nodes.Num();
+	void Update(uint32 Slot, FVector Position) {}
 
-		check(Nodes.Num() == Slot) Nodes.Push(node);
-		Positions.push_back(pos);
-		Volumes.expand(node->volume);
-#endif
-	}
+	void SwapEntries(int32 Slot1, int32 Slot2) {}
 
-	void Add(uint32 Slot, FBox2D Box)
-	{
-#ifdef HAS_BULLET_PHYSICS
-		btVector3 btMin(Box.Min.X / 100, Box.Min.Y / 100, -FLT_MAX);
-		btVector3 btMax(Box.Max.X / 100, Box.Max.Y / 100, FLT_MAX);
+	void Remove(uint32 Slot) {}
 
-		btDbvtNode* node = Tree.insert(btDbvtVolume::FromMM(btMin, btMax), (void*)Slot);
-		node->data = (void*)Slot;
-
-		int32 TreeId = Nodes.Num();
-
-		check(Nodes.Num() == Slot) Nodes.Push(node);
-		Positions.push_back((btMin + btMax) * 0.5);
-		Volumes.expand(node->volume);
-#endif
-	}
-
-	void Update(uint32 Slot, FVector Position)
-	{
-#ifdef HAS_BULLET_PHYSICS
-		btVector3 pos(Position.X / 100, Position.Y / 100, Position.Z / 100);
-		Positions[Slot] = pos;
-		Volumes[Slot] = btDbvtVolume::FromCE(pos, btVector3(0.1, 0.1, 0.1));
-		Tree.update(Nodes[Slot], Volumes[Slot], 1.0);
-#endif
-	}
-
-	void SwapEntries(int32 Slot1, int32 Slot2)
-	{
-#ifdef HAS_BULLET_PHYSICS
-		Swap(Positions[Slot1], Positions[Slot2]);
-		Swap(Volumes[Slot1], Volumes[Slot2]);
-		Swap(Nodes[Slot1], Nodes[Slot2]);
-#endif
-	}
-
-	void Remove(uint32 Slot)
-	{
-#ifdef HAS_BULLET_PHYSICS
-		if (Nodes.Num() > 0)
-		{
-			int32 LastSlot = Nodes.Num();
-			SwapEntries(Slot, LastSlot);
-		}
-
-		Tree.remove(Nodes[Slot]);
-		Positions.pop_back();
-		Volumes.pop_back();
-		Nodes.Pop();
-#endif
-	}
-
-#ifdef HAS_BULLET_PHYSICS
-	struct VisitStackElem
-	{
-		VisitStackElem(btDbvtNode* InNode, uint32 InListEnd)
-			: Node(InNode)
-			, ObjListEnd(InListEnd)
-		{
-		}
-
-		btDbvtNode* Node;
-		uint32 ObjListEnd;
-		int32 CurChild = -1;
-	};
-
-	template <typename CheckBBoxFn, typename CheckLeafFn>
-	void TraverseTree(uint32 NumVisitors, const CheckBBoxFn& CheckBBox, const CheckLeafFn& CheckLeaf)
-	{
-		Tree.optimizeIncremental(1);
-
-		if (Nodes.Num() == 0)
-		{
-			return;
-		}
-
-		TArray<uint32> Visitors;
-		Visitors.Reserve(NumVisitors);
-		for (uint32 i = 0; i < NumVisitors; ++i)
-		{
-			Visitors.Add(i);
-		}
-
-		TArray<VisitStackElem> NodeStack;
-		NodeStack.Reserve(64);
-
-		NodeStack.Add(VisitStackElem(Tree.m_root, NumVisitors));
-
-		while (NodeStack.Num() > 0)
-		{
-			VisitStackElem& CurNode = NodeStack.Last();
-			if (CurNode.CurChild == 1)
-			{
-				NodeStack.Pop();
-			}
-			else
-			{
-				CurNode.CurChild++;
-				btDbvtNode* Child = CurNode.Node->childs[CurNode.CurChild];
-
-				if (!Child->isleaf())
-				{
-					VisitStackElem& StackElem = NodeStack.Add_GetRef(VisitStackElem(Child, CurNode.ObjListEnd));
-
-					for (int32 i = 0; i < (int32)StackElem.ObjListEnd; ++i)
-					{
-						if (!CheckBBox(Visitors[i], Child->volume))
-						{
-							Swap(Visitors[i], Visitors[StackElem.ObjListEnd - 1]);
-							--StackElem.ObjListEnd;
-							--i;
-						}
-					}
-				}
-				else
-				{
-					union CastStruct
-					{
-						uint32 u32;
-						void* ptr;
-					} Slot;
-					Slot.ptr = Child->data;
-					for (uint32_t i = 0; i < CurNode.ObjListEnd; ++i)
-					{
-						CheckLeaf(Visitors[i], Slot.u32);
-					}
-				}
-			}
-		}
-	}
-#endif
-
-	void ComputeBoxVisibility(const TArray<FBox2D>& Regions, uint64* OutVisibility)
-	{
-#ifdef HAS_BULLET_PHYSICS
-		btAlignedObjectArray<btDbvtVolume> btRegions;
-		btRegions.reserve(Regions.Num());
-
-		for (const FBox2D& Region : Regions)
-		{
-			btVector3 btMin(Region.Min.X / 100, Region.Min.Y / 100, -FLT_MAX);
-			btVector3 btMax(Region.Max.X / 100, Region.Max.Y / 100, FLT_MAX);
-			btRegions.push_back(btDbvtVolume::FromMM(btMin, btMax));
-		}
-
-		const btDbvtVolume* regionsPtr = &btRegions[0];
-
-		TraverseTree(
-			Regions.Num(),
-			[&](uint32 RegionNum, const btDbvtVolume& Volume) {
-				return Intersect(regionsPtr[RegionNum], Volume);
-			},
-			[&](uint32 RegionNum, uint32 Slot) {
-				if (Intersect(btRegions[RegionNum], Positions[Slot]))
-				{
-					OutVisibility[Slot] |= 1ull << RegionNum;
-				}
-			});
-#endif
-	}
-
-#ifdef HAS_BULLET_PHYSICS
-	void ComputePointVisibility(const btAlignedObjectArray<btVector3>& InPositions, TArray<uint64>& OutVisibility)
-	{
-		TraverseTree(
-			InPositions.size(),
-			[&](uint32 PosNum, const btDbvtVolume& Volume) {
-				return Intersect(Volume, InPositions[PosNum]);
-			},
-			[&](uint32 PosNum, uint32 Slot) {
-				if (Intersect(Volumes[Slot], InPositions[PosNum]))
-				{
-					OutVisibility[PosNum] |= 1ull << Slot;
-				}
-			});
-	}
-#endif
-
-#ifdef HAS_BULLET_PHYSICS
-	btDbvt Tree;
-	TArray<btDbvtNode*> Nodes;
-	btAlignedObjectArray<btVector3> Positions;
-	btAlignedObjectArray<btDbvtVolume> Volumes;
-#endif
+	void ComputeBoxVisibility(const TArray<FBox2D>& Regions, uint64* OutVisibility) {}
 };
 
 FInterestManager::~FInterestManager() = default;
@@ -281,9 +79,6 @@ FInterestManager::FInterestManager(InterestFactory& InInterestF, FSpatialPositio
 	EntityFlags.Reserve(1 << 16);
 	Visibility.Reserve(1 << 16);
 	ActiveTimestamp.Reserve(1 << 16);
-#ifdef HAS_BULLET_PHYSICS
-	// Broadphase = MakeUnique<FBroadphaseImpl>();
-#endif
 }
 
 void FInterestManager::Advance(const TSet<Worker_EntityId_Key>& DeletedEntities)
