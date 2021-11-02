48211937c510882814fbc63a1cc0ed9850225f68
Wiryawan Mehanda <wiryawanmehanda@improbable.io>
2021-11-02 16:34:57 +0000

    fixed flaking tests

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestRPCTimeout/SpatialTestRPCTimeout.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestRPCTimeout/SpatialTestRPCTimeout.cpp
@@ -8,12 +8,24 @@
 #include "RPCTimeoutPlayerController.h"
 #include "RPCTimeoutGameMode.h"
 
+/**
+ * This test ensures that RPC calls with unresolved parameters are queued until their parameters are correctly resolved.
+ * This test contains 1 Server and 2 Client workers running in multiple processes.
+ *
+ * The flow is as follows:
+ * - Setup:
+ *  - Launch at least one client in a separate process
+ * - Test:
+ *  - All clients launched outside of the editor process must ensure that the referenced material asset is not in memory at the start of the test.
+ *  - All clients launched outside of the editor process must successfully resolve the material asset before passing it into a client RPC function.
+ */
 
 ASpatialTestRPCTimeout::ASpatialTestRPCTimeout()
 	: Super()
 {
 	Author = "Iwan";
-	Description = TEXT("");
+	Description = TEXT(
+		"This test calls an RPC with an asset that was softly referenced and check that it will be asynchronously loaded with a timeout of 0.");
 }
 
 void ASpatialTestRPCTimeout::CreateCustomContentForMap()
@@ -25,10 +37,9 @@ void ASpatialTestRPCTimeout::PrepareTest()
 {
 	Super::PrepareTest();
 
-
 	AddStep(
-		TEXT("Check that the material was not initially loaded on non-editor clients"), FWorkerDefinition::AllClients, nullptr,
-		[this]() {
+		TEXT("Check that the material was not initially loaded on non-editor clients"), FWorkerDefinition::AllClients, nullptr, nullptr,
+		[this](float DeltaTime) {
 			if (!GIsEditor)
 			{
 				ACharacter* TestCharacter = Cast<ACharacter>(GetLocalFlowPawn());
@@ -39,6 +50,7 @@ void ASpatialTestRPCTimeout::PrepareTest()
 					UMaterial* Material = TestController->SoftMaterialPtr.Get();
 
 					RequireTrue(Material == nullptr, TEXT("The soft-pointed material must not be in memory at the start of the test."));
+					UE_LOG(LogTemp, Display, TEXT("A"));
 
 					FinishStep();
 				}
@@ -47,19 +59,18 @@ void ASpatialTestRPCTimeout::PrepareTest()
 			{
 				FinishStep();
 			}
-		},
-		nullptr, 5.0f);
+		}, 2.0f);
 
 
 	AddStep(
-		TEXT("Check that the material is correctly loaded after about 3 seconds delay. This should be done synchronously bu the controller."), FWorkerDefinition::AllClients, nullptr, nullptr,
+		TEXT("Check that the material is correctly loaded after about 2 seconds delay. We should wait "), FWorkerDefinition::AllClients, nullptr, nullptr,
 		[this](float DeltaTime) {
 			if (!GIsEditor)
 			{
 				ACharacter* TestCharacter = Cast<ACharacter>(GetLocalFlowPawn());
 				ARPCTimeoutPlayerController* TestController = Cast<ARPCTimeoutPlayerController>(TestCharacter->GetController());
 
-				RequireTrue(TestController->SoftMaterialPtr.Get() != nullptr, TEXT("The soft-pointed material is synchronously loaded into the non-editor process."));
+				RequireTrue(TestController->IsSuccessfullyResolved(), TEXT("The soft-pointed material is synchronously loaded into the non-editor process."));
 				FinishStep();
 			}
 			else
