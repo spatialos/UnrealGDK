4b0309d37d98393e85bb784fad4d43e095507f19
Wiryawan Mehanda <wiryawanmehanda@improbable.io>
2021-11-02 17:18:42 +0000

    changed classnames for RPC Timeout test

--- SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestRPCTimeout/SpatialTestRPCTimeoutCharacter.cpp
+++ SpatialGDK/Source/SpatialGDKFunctionalTests/SpatialGDK/SpatialTestRPCTimeout/SpatialTestRPCTimeoutCharacter.cpp
@@ -1,18 +1,17 @@
 // Copyright (c) Improbable Worlds Ltd, All Rights Reserved
 
-
 #include "SpatialTestRPCTimeoutCharacter.h"
-#include "UObject/ConstructorHelpers.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "UObject/ConstructorHelpers.h"
 
 // Sets default values
-ARPCTimeoutCharacter::ARPCTimeoutCharacter()
+ASpatialTestRPCTimeoutCharacter::ASpatialTestRPCTimeoutCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	
+
 	const TCHAR* DefaultSkeletalMeshPathString = TEXT("SkeletalMesh'/Engine/EngineMeshes/SkeletalCube.SkeletalCube'");
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh>DefaultSkeletalMeshFinder(DefaultSkeletalMeshPathString);
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> DefaultSkeletalMeshFinder(DefaultSkeletalMeshPathString);
 	USkeletalMesh* DefaultSkeletalMesh = DefaultSkeletalMeshFinder.Object;
 	checkf(IsValid(DefaultSkeletalMesh), TEXT("Could not find failed material asset %ls"), DefaultSkeletalMeshPathString);
 
@@ -21,11 +20,12 @@ ARPCTimeoutCharacter::ARPCTimeoutCharacter()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = 300.0f;		// The camera follows at this distance behind the character
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
-												// Create a follow camera
+	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom
+																				// adjust to match the controller orientation
+	FollowCamera->bUsePawnControlRotation = false;								// Camera does not rotate relative to arm
 }
