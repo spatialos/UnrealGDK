0f4ac31c422f7e0ac1e3884b6c1b63b4c9295e88
Wiryawan Mehanda <wiryawanmehanda@improbable.io>
2021-11-03 16:05:20 +0000

    moved module force loading from engine to the GDK

--- SpatialGDK/Source/SpatialGDK/Private/SpatialGDKModule.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/SpatialGDKModule.cpp
@@ -13,7 +13,14 @@ DEFINE_LOG_CATEGORY(LogSpatialGDKModule);
 
 IMPLEMENT_MODULE(FSpatialGDKModule, SpatialGDK)
 
-void FSpatialGDKModule::StartupModule() {}
+void FSpatialGDKModule::StartupModule()
+{
+	if (FString(FCommandLine::Get()).Contains(TEXT("-SpatialTest")))
+	{
+		FModuleManager::Get().LoadModule("SpatialGDKServices");
+		FModuleManager::Get().LoadModule("SpatialGDKFunctionalTests");
+	}
+}
 
 void FSpatialGDKModule::ShutdownModule() {}
 
