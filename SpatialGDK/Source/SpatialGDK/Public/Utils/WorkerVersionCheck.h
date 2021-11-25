770e0366778fe1b7447ed608b8c7135a2fbe3360
Michael Samiec <michaelsamiec@improbable.io>
2021-11-25 11:35:55 +0000

    Switch SDK check to WORKER_API_VERSION_FULL_STR

--- SpatialGDK/Source/SpatialGDK/Public/Utils/WorkerVersionCheck.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/WorkerVersionCheck.h
@@ -4,15 +4,14 @@
 
 #include "improbable/c_worker.h"
 
-#define WORKER_SDK_VERSION "15.4.0"
+#define WORKER_SDK_VERSION_FULL_STR "15.4.0-b942-aa3ea09-WORKER-SNAPSHOT"
 
 constexpr bool StringsEqual(char const* A, char const* B)
 {
 	return *A == *B && (*A == '\0' || StringsEqual(A + 1, B + 1));
 }
 
-// Check if the current version of the Worker SDK is compatible with the current version of UnrealGDK
-// WORKER_SDK_VERSION is incremented here when breaking changes are made that make previous versions of the SDK
-// incompatible
-static_assert(StringsEqual(WORKER_API_VERSION_STR, WORKER_SDK_VERSION),
+// Check if the current version of the Worker SDK is compatible with the current version of UnrealGDK WORKER_SDK_VERSION_FULL_STR
+// is incremented here when breaking changes are made that make previous versions of the SDK incompatible
+static_assert(StringsEqual(WORKER_API_VERSION_FULL_STR, WORKER_SDK_VERSION_FULL_STR),
 			  "Worker SDK version is incompatible with the UnrealGDK version. Check both the Worker SDK and GDK are up to date");
