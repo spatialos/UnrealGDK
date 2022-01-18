3bb00361ba8a271231449954c7cf0e4763c276ce
Akshay Narayan <akshaynarayan@improbable.io>
2022-01-18 13:05:26 +0000

    Account for Buildkite env var weirdness

--- SpatialGDK/Source/SpatialGDK/SpatialGDK.Build.cs
+++ SpatialGDK/Source/SpatialGDK/SpatialGDK.Build.cs
@@ -16,9 +16,19 @@ public class SpatialGDK : ModuleRules
         bLegacyPublicIncludePaths = false;
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
         
-		string unity_mode_env = System.Environment.GetEnvironmentVariable("GDK_UNITY_MODE");
-		bool unity_mode = bool.Parse(unity_mode_env);
-		bUseUnity = unity_mode;
+        // Buildkite can store environment variables like "True" and "False" as "1" and "0", which can't be parsed by bool.TryParse() or bool.Parse()
+		string unity_mode_env = Environment.GetEnvironmentVariable("GDK_UNITY_MODE");
+		bool unity_mode;
+        if (bool.TryParse(unity_mode_env, unity_mode)) {
+            // unity_mode_env is "True", "False" or case-insensitive versions of these
+    		bUseUnity = unity_mode;
+        } else if (unity_mode_env.Equals("0")) {
+            bUseUnity = false;
+        } else if (unity_mode_env.Equals("1")) {
+            bUseUnity = true;
+        } else {
+            throw new FormatException("Value of GDK_UNITY_MODE environment variable was not recognized as a valid Boolean.");
+        }
 
         PrivateIncludePaths.Add("SpatialGDK/Private");
 
